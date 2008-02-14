1312cf4e42f4225adbb465e1a53f2bfc16c07b49
dcaoyuan@netbeans.org <none@none>
2008-02-14 23:07:44 +0800

    Fixed: highlighting of occurences is not properly refreshed. It's caused by fireHighlightsChange in timer task maybe canceled. To avoid parser NPE, we can check if parser is ready.

--- languages/src/org/netbeans/modules/languages/features/TokenHighlightsLayer.java
+++ languages/src/org/netbeans/modules/languages/features/TokenHighlightsLayer.java
@@ -29,17 +29,22 @@ package org.netbeans.modules.languages.features;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.Document;
 import javax.swing.text.SimpleAttributeSet;
 
 import org.netbeans.api.languages.Highlighting;
+import org.netbeans.api.languages.LanguageDefinitionNotFoundException;
 import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenSequence;
-import org.netbeans.spi.editor.highlighting.HighlightsSequence;
+import org.netbeans.modules.languages.Language;
+import org.netbeans.modules.languages.LanguagesManager;
 import org.netbeans.spi.editor.highlighting.HighlightsSequence;
 import org.netbeans.spi.editor.highlighting.support.AbstractHighlightsContainer;
+import org.openide.util.Exceptions;
 import org.openide.util.WeakListeners;
 
 
@@ -53,9 +58,23 @@ class TokenHighlightsLayer extends AbstractHighlightsContainer {
     private TokenHierarchy          hierarchy;
     private Document                document;
     private final PropertyChangeListener listener = new PropertyChangeListener () {
+        private boolean parserInited;
+
         public void propertyChange (final PropertyChangeEvent evt) {
-            fireHighlightsChange ((Integer) evt.getOldValue (), (Integer) evt.getNewValue ());
-        }
+            if (!parserInited) {
+                String mimeType = (String) document.getProperty ("mimeType");
+                try {
+                    Language language = LanguagesManager.getDefault().getLanguage(mimeType);
+                    if (language.getParser() != null) {
+                        parserInited = true;
+                    }
+                } catch (LanguageDefinitionNotFoundException ex) {
+                }
+            }
+            if (parserInited) {
+                fireHighlightsChange ((Integer) evt.getOldValue (), (Integer) evt.getNewValue ());
+            }
+        }            
     };
     
     TokenHighlightsLayer (final Document document) {
