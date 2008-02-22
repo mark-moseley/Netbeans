b022744e018ac2a2ca1d342bb097a2243597c3e9
S. Aubrecht <saubrecht@netbeans.org>
2008-02-22 16:22:56 +0100

    windows system visual effects, JNA library

--- o.n.swing.tabcontrol/src/org/netbeans/swing/tabcontrol/plaf/AbstractViewTabDisplayerUI.java
+++ o.n.swing.tabcontrol/src/org/netbeans/swing/tabcontrol/plaf/AbstractViewTabDisplayerUI.java
@@ -103,7 +103,7 @@ public abstract class AbstractViewTabDisplayerUI extends TabDisplayerUI {
 
     private TabDataModel dataModel;
 
-    private ViewTabLayoutModel layoutModel;
+    private TabLayoutModel layoutModel;
 
     private FontMetrics fm;
 
@@ -120,6 +120,8 @@ public abstract class AbstractViewTabDisplayerUI extends TabDisplayerUI {
     /** Pin action */
     private final Action pinAction = new PinAction();
     private static final String PIN_ACTION = "pinAction";
+    //toggle transparency action
+    private static final String TRANSPARENCY_ACTION = "transparencyAction";
     
     public AbstractViewTabDisplayerUI (TabDisplayer displayer) {
         super (displayer);
@@ -131,7 +133,15 @@ public abstract class AbstractViewTabDisplayerUI extends TabDisplayerUI {
         ToolTipManager.sharedInstance().registerComponent(displayer);
         controller = createController();
         dataModel = displayer.getModel();
-        layoutModel = new ViewTabLayoutModel(dataModel, displayer);
+        if (Boolean.getBoolean("winsys.non_stretching_view_tabs")) {
+            ViewTabLayoutModel2.PaddingInfo padding = new ViewTabLayoutModel2.PaddingInfo();
+            padding.iconsXPad = 5;
+            padding.txtIconsXPad = 10;
+            padding.txtPad = new Dimension(5, 2);
+            layoutModel = new ViewTabLayoutModel2(displayer, padding);
+        } else {
+            layoutModel = new ViewTabLayoutModel(dataModel, c);
+        }
         dataModel.addChangeListener (controller);
         dataModel.addComplexListDataListener(controller);
         displayer.addPropertyChangeListener (controller);
@@ -375,6 +385,7 @@ public abstract class AbstractViewTabDisplayerUI extends TabDisplayerUI {
         comp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).
             remove(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD0,
                                 InputEvent.CTRL_DOWN_MASK));
+        comp.getActionMap().remove(TRANSPARENCY_ACTION);
     }
 
     /** Registers shortcut for enable/ disable auto-hide functionality */
@@ -383,6 +394,16 @@ public abstract class AbstractViewTabDisplayerUI extends TabDisplayerUI {
             put(KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE,
                                 InputEvent.CTRL_DOWN_MASK), PIN_ACTION);
         comp.getActionMap().put(PIN_ACTION, pinAction);
+
+        //TODO make shortcut configurable
+        comp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).
+            put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD0,
+                                InputEvent.CTRL_DOWN_MASK), TRANSPARENCY_ACTION);
+        comp.getActionMap().put(TRANSPARENCY_ACTION, new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                shouldPerformAction(TabbedContainer.COMMAND_TOGGLE_TRANSPARENCY, getSelectionModel().getSelectedIndex(), null);
+            }
+        });
     }
     
     public Polygon getExactTabIndication(int index) {
