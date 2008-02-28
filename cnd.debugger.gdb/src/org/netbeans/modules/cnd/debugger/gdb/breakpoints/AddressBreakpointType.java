3b2b396b9e866a2657fee839a7c00c87d888ef0a
Egor Ushakov <gorrus@netbeans.org>
2008-02-28 19:37:51 +0300

    implementing disassembly view: Address breakpoint customization implemented

--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/breakpoints/AddressBreakpointType.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/breakpoints/AddressBreakpointType.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,73 +31,47 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.debugger.gdb.breakpoints;
 
-import java.util.List;
 import javax.swing.JComponent;
-import org.netbeans.api.debugger.DebuggerManager;
-import org.netbeans.api.project.Project;
-import org.netbeans.api.project.ui.OpenProjects;
-import org.netbeans.modules.cnd.api.project.NativeProject;
-import org.netbeans.modules.cnd.debugger.gdb.EditorContextBridge;
 import org.netbeans.spi.debugger.ui.BreakpointType;
-
 import org.openide.util.NbBundle;
 
-
 /**
- * Implementation of breakpoint on function.
  *
- * @author   Nik Molchanov (copied from Jan Jancura's JPDA implementation). 
+ * @author eu155513
  */
-public class FunctionBreakpointType extends BreakpointType {
-
+public class AddressBreakpointType extends BreakpointType {
     public String getCategoryDisplayName() {
-        return NbBundle.getMessage(FunctionBreakpointType.class,
+        return NbBundle.getMessage(LineBreakpointType.class,
                     "CTL_Gdb_breakpoint_events_category_name"); // NOI18N
     }
     
     public JComponent getCustomizer() {
-        return new FunctionBreakpointPanel ();
+        return new AddressBreakpointPanel();
     }
     
     public String getTypeDisplayName() {
-        return NbBundle.getMessage(FunctionBreakpointType.class, "CTL_Gdb_Function_Breakpoint"); // NOI18N
+        return NbBundle.getMessage(LineBreakpointType.class, "CTL_Gdb_Address_Breakpoint"); // NOI18N
     }
     
-    public boolean isDefault() {
-        // First, check for an open file. Is it one of ours?
-	String mime = EditorContextBridge.getContext().getMostRecentMIMEType();
-        if (mime.length() > 0) {
-            if (mime.equals("text/x-c++") || mime.equals("text/x-c") || // NOI18N
-                        mime.equals("text/x-fortran")) { // NOI18N
-                return true;
-            } else {
-                return false;
-            }
-        }
-        
-        // Next, check the main project. Is it one of ours?
-        Project project = OpenProjects.getDefault().getMainProject();
-        if (project != null) {
-            NativeProject np = (NativeProject) project.getLookup().lookup(NativeProject.class);
-            if (np != null) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        
-        // Last, count breakpoint types. We define 3. If thats all that are returned, then
-        // we're the only active debugger and should be the default.
-        List breakpointTypes = DebuggerManager.getDebuggerManager().lookup(null, BreakpointType.class);
-        if (breakpointTypes.size() == 3) {
-            return true;
-        }
-        	
-	return false;
+    /**
+     *  Tell debuggercore if this should be the default breakpoint.
+     *
+     *  Currently we always return false because we want to defer to FunctionBreakpointType.
+     *  Eventually, this class and FunctionBreakpointType should both become smart enough
+     *  that FBT is the default if the cursor is inside a function and LBT if its outside
+     *  of a function (in both cases, its false if the current file in the editor isn't a
+     *  C, C++, or Fortran file)
+     */
+    public boolean isDefault() {	
+	return false;	// do false for now because FunctionBreakpointType currently
+			// overrides this anyway.
     }
 }
-
