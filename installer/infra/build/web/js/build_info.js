61deacc45db25426e2ca91ed5e0626508b818213
Dmitry Lipin <dlipin@netbeans.org>
2008-03-04 20:44:10 +0300

    Move all build-dependent properties to build_info.js. All other files are not changed during the build anymore

--- installer/infra/build/web/js/build_info.js
+++ installer/infra/build/web/js/build_info.js
@@ -34,44 +34,20 @@
  * copyright holder.
  */
 
-PLATFORM_LONG_NAMES  [0] = "Windows 2000/XP/Vista";
-PLATFORM_SHORT_NAMES [0] = "Windows";
-PLATFORM_LONG_NAMES  [1] = "Linux (x86/x64)";
-PLATFORM_SHORT_NAMES [1] = "Linux";
-PLATFORM_LONG_NAMES  [2] = "Solaris (x86/x64)";
-PLATFORM_SHORT_NAMES [2] = "Solaris (x86/x64)";
-PLATFORM_LONG_NAMES  [3] = "Solaris (sparc)";
-PLATFORM_SHORT_NAMES [3] = "Solaris (sparc)";
-PLATFORM_LONG_NAMES  [4] = "Mac OS X";
-PLATFORM_SHORT_NAMES [4] = "Mac OS X";
-PLATFORM_LONG_NAMES  [5] = "OS Independent Zip";
-PLATFORM_SHORT_NAMES [5] = "OS Independent Zip";
-/*
-// Commented since NBI installers are not used for now
-// Translate these strings as well (if they can be translated) and leave them commented
-PLATFORM_LONG_NAMES  [4] = "Mac OS X (intel)";
-PLATFORM_SHORT_NAMES [4] = "Mac OS X";
 
-PLATFORM_LONG_NAMES  [5] = "Mac OS X (powerpc)";
-PLATFORM_SHORT_NAMES [5] = "Mac OS X";
-*/
+var BUILD_DISPLAY_VERSION       = "{build.display.version}";
+var BUILD_DISPLAY_VERSION_SHORT = "{build.display.version.short}";
 
-BUNDLE_SHORT_NAMES[0] = "Web&nbsp;&amp;&nbsp;Java&nbsp;EE";
-BUNDLE_SHORT_NAMES[1] = "Mobility";
-BUNDLE_SHORT_NAMES[2] = "Java&nbsp;SE";
-BUNDLE_SHORT_NAMES[3] = "Ruby";
-BUNDLE_SHORT_NAMES[4] = "C/C++";
-BUNDLE_SHORT_NAMES[5] = "";
+var ZIP_FILES_PREFIX            = "{nb.zip.files.prefix}";
+var BUNDLE_FILES_PREFIX         = "{nb.bundle.files.prefix}";
 
-BUNDLE_LONG_NAMES[0] = "Web&nbsp;&amp;&nbsp;Java&nbsp;EE";
-BUNDLE_LONG_NAMES[1] = "Mobility";
-BUNDLE_LONG_NAMES[2] = "Java&nbsp;SE";
-BUNDLE_LONG_NAMES[3] = "Ruby";
-BUNDLE_LONG_NAMES[4] = "C/C++";
-BUNDLE_LONG_NAMES[5] = "All";
+var JDK_LATEST_UPDATE = "4";
 
-var NETBEANS_DOWNLOAD_PAGE_TITLE       = "NetBeans IDE {0} Download";
-var NETBEANS_DOWNLOAD_PAGE_DESCRIPTION = "NetBeans IDE {0} Download";
+var LOAD_OMNITURE_CODE = 0;
 
-var RELEASE_NOTES_LINK  = "http://www.netbeans.org/community/releases/61/relnotes.html";
-var INSTALL_NOTES_LINK  = "http://www.netbeans.org/community/releases/61/install.html";
+function add_download_tabs() {
+	add_download_tab("6.0.1", "http://download.netbeans.org/netbeans/6.0/final/");
+	add_download_tab("6.1M1","http://bits.netbeans.org/download/6.1/m1/latest/");
+	add_download_tab(DEVELOPMENT_TITLE /*,DEVELOPMENT_BUILDS_LINK*/);
+	add_download_tab(ARCHIVE_TITLE,ARCHIVE_BUILDS_LINK);
+}
