1541618b08d2aea9e0433432d0fba60124aacf91
Narayana Rallabandi <narayanaa@netbeans.org>
2008-03-07 01:20:16 +0530

    Fix for SQL SE / JDBC BC query generation in the Designtime or WSDL Generation

--- sql.wizard/src/org/netbeans/modules/jdbcwizard/builder/wsdl/WSDLGenerator.java
+++ sql.wizard/src/org/netbeans/modules/jdbcwizard/builder/wsdl/WSDLGenerator.java
@@ -59,6 +59,7 @@ import org.netbeans.modules.jdbcwizard.builder.model.OracleQueryGenerator;
 import org.netbeans.modules.jdbcwizard.builder.model.DB2QueryGenerator;
 import org.netbeans.modules.jdbcwizard.builder.model.SQLServerQueryGenerator;
 import org.netbeans.modules.jdbcwizard.builder.model.JdbcQueryGenerator;
+import org.netbeans.modules.jdbcwizard.builder.model.MySQLQueryGenerator;
 import org.netbeans.modules.jdbcwizard.builder.util.XMLCharUtil;
 
 
@@ -363,8 +364,8 @@ public class WSDLGenerator {
             this.dbDataAccessObject = this.getQueryGenerator();
             this.dbDataAccessObject.init(this.mTable);
 
-            this.mTableName = this.mTable.getSchema()+"."+this.mTable.getName();
-
+            //this.mTableName = this.mTable.getSchema()+"."+this.mTable.getName();
+            this.mTableName = this.mTable.getName();
             // Generate Queries
             insertQuery = this.dbDataAccessObject.createInsertQuery();
             updateQuery = this.dbDataAccessObject.createUpdateQuery();
@@ -773,6 +774,8 @@ public class WSDLGenerator {
             objDataAccess = DB2QueryGenerator.getInstance();
         } else if (this.mDBType.equalsIgnoreCase("SQLServer")) {
             objDataAccess = SQLServerQueryGenerator.getInstance();
+        } else if (this.mDBType.equalsIgnoreCase("MYSQL")) {
+            objDataAccess = MySQLQueryGenerator.getInstance();
         }else {
             objDataAccess = JdbcQueryGenerator.getInstance();
         }
