55193449319aef93a8d3da87c0dcfa7c924a816f
Gordon Prieur <gordonp@netbeans.org>
2008-03-07 11:05:35 -0800

    Fixed 128516  Step Over on dlopen works like Continue Also added some untested support for 120647 on MacOS (I'll pull to a Mac and test before pushing...)

--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/breakpoints/AddressBreakpointImpl.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/breakpoints/AddressBreakpointImpl.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,6 +31,10 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.debugger.gdb.breakpoints;
@@ -45,71 +43,45 @@ import org.netbeans.api.debugger.Session;
 import org.netbeans.modules.cnd.debugger.gdb.GdbDebugger;
 
 /**
-* Implementation of breakpoint on method.
-*
-* @author   Gordon Prieur (copied from Jan Jancura's JPDA implementation)
-*/
-public class LineBreakpointImpl extends BreakpointImpl {
-
-    private LineBreakpoint      breakpoint;
-    private int                 lineNumber;
+ *
+ * @author eu155513
+ */
+public class AddressBreakpointImpl extends BreakpointImpl {
+    private AddressBreakpoint  breakpoint;
     private BreakpointsReader   reader;
-
     
-    public LineBreakpointImpl(LineBreakpoint breakpoint, BreakpointsReader reader,
-                GdbDebugger debugger, Session session) {
+    public AddressBreakpointImpl(AddressBreakpoint breakpoint, BreakpointsReader reader,
+            GdbDebugger debugger, Session session) {
         super(breakpoint, reader, debugger, session);
         this.reader = reader;
         this.breakpoint = breakpoint;
-        lineNumber = breakpoint.getLineNumber();
         set();
     }
-    
+
     @Override
-    void fixed() {
-        lineNumber = breakpoint.getLineNumber();
-        super.fixed();
-    }
-    
     protected void setRequests() {
-        String st = getState();
-        if (getDebugger().getState().equals(GdbDebugger.STATE_RUNNING) && !st.equals(BPSTATE_REVALIDATE)) {
+        if (getDebugger().getState().equals(GdbDebugger.STATE_RUNNING)) {
             getDebugger().setSilentStop();
             setRunWhenValidated(true);
         }
-        if (st.equals(BPSTATE_UNVALIDATED) || st.equals(BPSTATE_REVALIDATE)) {
-            if (st.equals(BPSTATE_REVALIDATE) && getBreakpointNumber() > 0) {
-                getDebugger().getGdbProxy().break_delete(getBreakpointNumber());
-            }
+        if (getState().equals(BPSTATE_UNVALIDATED)) {
             setState(BPSTATE_VALIDATION_PENDING);
-            lineNumber = breakpoint.getLineNumber();
-            String path = getDebugger().getBestPath(breakpoint.getPath());
-            int token;
-            if (getBreakpoint().getSuspend() == GdbBreakpoint.SUSPEND_THREAD) {
-                token = getDebugger().getGdbProxy().break_insert(GdbBreakpoint.SUSPEND_THREAD,
-                        path + ':' + lineNumber, getBreakpoint().getThreadID());
-            } else {
-                token = getDebugger().getGdbProxy().break_insert(path + ':' + lineNumber);
-            }
+            int token = getDebugger().getGdbProxy().break_insert("*" + breakpoint.getAddress()); // NOI18N
             getDebugger().addPendingBreakpoint(token, this);
-	} else {
-            int bnum = getBreakpointNumber();
-            if (bnum > 0) { // bnum < 0 for breakpoints from other projects...
-                if (st.equals(BPSTATE_DELETION_PENDING)) {
-                    getDebugger().getGdbProxy().break_delete(bnum);
-                } else if (st.equals(BPSTATE_VALIDATED)) {
-                    if (breakpoint.isEnabled()) {
-                        getDebugger().getGdbProxy().break_enable(bnum);
-                    } else {
-                        getDebugger().getGdbProxy().break_disable(bnum);
-                    }
-                }
-                if (isRunWhenValidated()) {
-                    getDebugger().setRunning();
-                    setRunWhenValidated(false);
+        } else {
+            if (getState().equals(BPSTATE_DELETION_PENDING)) {
+                getDebugger().getGdbProxy().break_delete(getBreakpointNumber());
+	    } else if (getState().equals(BPSTATE_VALIDATED)) {
+                if (breakpoint.isEnabled()) {
+                    getDebugger().getGdbProxy().break_enable(getBreakpointNumber());
+                } else {
+                    getDebugger().getGdbProxy().break_disable(getBreakpointNumber());
                 }
             }
-	}
+            if (isRunWhenValidated()) {
+                getDebugger().setRunning();
+                setRunWhenValidated(false);
+            }
+        }
     }
 }
-
