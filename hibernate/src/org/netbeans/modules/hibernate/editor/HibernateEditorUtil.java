e85999f00122fa217a8264f211f311a0fb960028
Dongmei Cao <dongmeic@netbeans.org>
2008-03-24 16:36:20 -0700

    Fix for issue http://www.netbeans.org/issues/show_bug.cgi?id=130959 - CC on property not working when inside subclass, joined-subclass, union-subclass.
    --HG--
    extra : convert_revision : 9afc297e482dd41a56067b1624fc9d40a261f365

--- hibernate/src/org/netbeans/modules/hibernate/editor/HibernateEditorUtil.java
+++ hibernate/src/org/netbeans/modules/hibernate/editor/HibernateEditorUtil.java
@@ -39,16 +39,24 @@
 package org.netbeans.modules.hibernate.editor;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.SimpleElementVisitor6;
 import javax.swing.text.Document;
 import org.netbeans.api.java.project.JavaProjectConstants;
 import org.netbeans.api.java.source.ClasspathInfo;
 import org.netbeans.api.java.source.CompilationController;
+import org.netbeans.api.java.source.ElementUtilities;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.Task;
 import org.netbeans.api.java.source.ui.ElementOpen;
@@ -92,14 +100,20 @@ public class HibernateEditorUtil {
             return null;
         }
 
-        if (tag.getNodeName().equalsIgnoreCase("class")) { // NOI18N
+        if (tag.getNodeName().equalsIgnoreCase("class") || // NOI18N
+                tag.getNodeName().equalsIgnoreCase("subclass") || // NOI18N
+                tag.getNodeName().equalsIgnoreCase("joined-subclass") || // NOI18N
+                tag.getNodeName().equalsIgnoreCase("union-subclass")) { // NOI18N
             return tag;
         }
 
         Node current = tag;
         while (true) {
             Node parent = current.getParentNode();
-            if (parent.getNodeName().equalsIgnoreCase("class")) {
+            if (parent.getNodeName().equalsIgnoreCase("class") || // NOI18N
+                    parent.getNodeName().equalsIgnoreCase("subclass") || // NOI18N
+                    parent.getNodeName().equalsIgnoreCase("joined-subclass") || // NOI18N
+                    parent.getNodeName().equalsIgnoreCase("union-subclass")) {
                 // Found it
                 return parent;
             } else if (parent.getNodeName().equalsIgnoreCase("hibernate-mapping")) {
@@ -210,5 +224,40 @@ public class HibernateEditorUtil {
             }
         }
     }
+    
+    public static VariableElement findFieldElementOnType(ElementUtilities eu, TypeMirror type, String fieldName) {
+        FieldAcceptor fieldAcceptor = new FieldAcceptor(fieldName);
+        Iterable<? extends Element> matchingProp = eu.getMembers(type, fieldAcceptor);
+        Iterator<? extends Element> it = matchingProp.iterator();
+        
+        // no matching element found
+        if (!it.hasNext()) {
+            return null;
+        } else
+            return (VariableElement)it.next();
+        
     }
+
+    private static class FieldAcceptor implements ElementUtilities.ElementAcceptor {
+
+        private String fieldName;
+
+        public FieldAcceptor(String fieldName) {
+            this.fieldName = fieldName;
+        }
+
+        public boolean accept(Element e, TypeMirror type) {
+            
+            if (e.getKind() != ElementKind.FIELD) {
+                return false;
+            } else {
+                if( e.getSimpleName().toString().equals(fieldName) ) {
+                     return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
 
