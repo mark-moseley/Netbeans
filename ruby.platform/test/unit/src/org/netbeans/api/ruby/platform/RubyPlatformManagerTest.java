7d6210b5b673d3bdd9bda5e667d04e7b44bf845f
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-03-27 13:56:51 +0100

    Prevent to add the same platform twice (other part of #131277)

--- ruby.platform/test/unit/src/org/netbeans/api/ruby/platform/RubyPlatformManagerTest.java
+++ ruby.platform/test/unit/src/org/netbeans/api/ruby/platform/RubyPlatformManagerTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -22,7 +22,7 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -57,6 +57,14 @@ public final class RubyPlatformManagerTest extends RubyTestBase {
     }
 
     public void testAddPlatform() throws Exception {
+        assertEquals("bundle JRuby", 1, RubyPlatformManager.getPlatforms().size());
+        RubyPlatform defPlatform = RubyPlatformManager.getDefaultPlatform();
+        assertNotNull("has default platform", defPlatform);
+        assertEquals("platform already present", defPlatform, RubyPlatformManager.addPlatform(defPlatform.getInterpreterFile()));
+        assertEquals("was not added twice", 1, RubyPlatformManager.getPlatforms().size());
+    }
+    
+    public void testAddingTheSamePlatformTwice() throws Exception {
         assertEquals("bundle JRuby", 1, RubyPlatformManager.getPlatforms().size());
         RubyPlatform ruby = RubyPlatformManager.addPlatform(setUpRuby());
         File defaultRubyHome = getTestRubyHome();
@@ -102,11 +110,11 @@ public final class RubyPlatformManagerTest extends RubyTestBase {
     public void testRepositoriesAreStored() throws Exception {
         RubyPlatform platform = RubyPlatformManager.getDefaultPlatform();
         GemManager gemManager = platform.getGemManager();
-        String dummyRepo = getWorkDirPath() + "/a";
+        File dummyRepo = new File(getWorkDirPath(), "/a");
         assertEquals("one repositories", 1, gemManager.getRepositories().size());
         
         // add and check
-        gemManager.addRepository(dummyRepo);
+        gemManager.addGemPath(dummyRepo);
         assertEquals("two repositories", 2, gemManager.getRepositories().size());
         RubyPlatformManager.resetPlatforms();
         platform = RubyPlatformManager.getDefaultPlatform();
@@ -114,12 +122,14 @@ public final class RubyPlatformManagerTest extends RubyTestBase {
         assertEquals("two repositories", 2, gemManager.getRepositories().size());
 
         // remove and check
-        gemManager.removeRepository(dummyRepo);
+        gemManager.removeGemPath(dummyRepo);
+        // XXX this is neeeded on Windows. But we do use FileObject everywhere (?!)
+        platform.getLibFO().refresh();
         RubyPlatformManager.resetPlatforms();
         platform = RubyPlatformManager.getDefaultPlatform();
         gemManager = platform.getGemManager();
-        assertEquals("two repositories", 1, RubyPlatformManager.getDefaultPlatform().getGemManager().getRepositories().size());
-        gemManager.removeRepository(dummyRepo);
+        assertEquals("two repositories", 1, gemManager.getRepositories().size());
+        gemManager.removeGemPath(dummyRepo);
     }
 
 }
