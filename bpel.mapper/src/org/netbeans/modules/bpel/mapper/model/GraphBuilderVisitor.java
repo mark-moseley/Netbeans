c7ca658783a9c3b7d6a20a1e7407006061badaa2
Alex Petrov <alexpetrov@netbeans.org>
2008-04-08 21:23:27 +0400

    Refactoring of fix code for [Issue 131950]

--- bpel.mapper/src/org/netbeans/modules/bpel/mapper/model/GraphBuilderVisitor.java
+++ bpel.mapper/src/org/netbeans/modules/bpel/mapper/model/GraphBuilderVisitor.java
@@ -19,17 +19,21 @@
 
 package org.netbeans.modules.bpel.mapper.model;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 import javax.swing.tree.TreePath;
+import org.netbeans.modules.bpel.mapper.multiview.BpelDesignContext;
+import org.netbeans.modules.bpel.mapper.multiview.DesignContextControllerImpl2;
 import org.netbeans.modules.bpel.mapper.tree.MapperSwingTreeModel;
 import org.netbeans.modules.bpel.mapper.tree.search.FinderListBuilder;
 import org.netbeans.modules.bpel.mapper.tree.search.PartFinder;
+import org.netbeans.modules.bpel.mapper.tree.search.TreeFinderProcessor;
 import org.netbeans.modules.bpel.mapper.tree.search.VariableFinder;
 import org.netbeans.modules.bpel.mapper.tree.spi.TreeItemFinder;
-import org.netbeans.modules.bpel.model.api.VariableDeclaration;
+import org.netbeans.modules.bpel.model.api.AbstractVariableDeclaration;
 import org.netbeans.modules.bpel.model.api.support.XPathBpelVariable;
 import org.netbeans.modules.soa.mappercore.model.Graph;
 import org.netbeans.modules.soa.mappercore.model.Link;
@@ -56,6 +60,7 @@ import org.netbeans.modules.xml.xpath.ext.metadata.StubExtFunction;
 import org.netbeans.modules.xml.xpath.ext.visitor.XPathVisitorAdapter;
 import org.netbeans.modules.xml.wsdl.model.Part;
 import org.netbeans.modules.xml.xpath.ext.spi.XPathVariable;
+import org.openide.util.NbBundle;
 
 /**
  * Populates the Graph object with a complex content by an XPath expression.
@@ -72,11 +77,14 @@ public class GraphBuilderVisitor extends XPathVisitorAdapter {
 
     protected Stack<VertexBuilderData> mVertexStack = new Stack<VertexBuilderData>();
     
+    protected BpelDesignContext currentBpelDesignContext;   
+    
     public GraphBuilderVisitor(Graph graph, MapperSwingTreeModel leftTreeModel, 
-            boolean connectToTargetTree) {
+            boolean connectToTargetTree, BpelDesignContext context) {
         mGraph = graph;
         mLeftTreeModel = leftTreeModel;
         mConnectToTargetTree = connectToTargetTree;
+        currentBpelDesignContext = context;
     }
 
     @Override
@@ -130,6 +138,11 @@ public class GraphBuilderVisitor extends XPathVisitorAdapter {
         // It seems they are not supported in the BPEL
         // It can be used by predicates, but they will be shown 
         // in a separate view and will have separate loading code.
+        //
+        // TODO: 
+        // In can be necessary if the Variable-Part-Query for of 
+        // an assign->copy is used. Now it isn't supported by the runtime
+        // But later it can be necessary to support. 
     }
 
     @Override
@@ -226,7 +239,7 @@ public class GraphBuilderVisitor extends XPathVisitorAdapter {
         ArrayList<TreeItemFinder> finderList = new ArrayList<TreeItemFinder>();
         //
         if (xPathVar != null) {
-            VariableDeclaration varDecl = xPathVar.getVarDecl();
+            AbstractVariableDeclaration varDecl = xPathVar.getVarDecl();
             if (varDecl != null) {
                 finderList.add(new VariableFinder(varDecl));
                 //
@@ -241,16 +254,33 @@ public class GraphBuilderVisitor extends XPathVisitorAdapter {
     }
 
     protected void connectToLeftTree(XPathExpressionPath path) {
-        connectToLeftTree(FinderListBuilder.build(path));
+        if ((! connectToLeftTree(FinderListBuilder.build(path))) && 
+            (currentBpelDesignContext != null)) {
+            StringBuffer errMsgBuffer = currentBpelDesignContext.getValidationErrMsgBuffer();
+            if (errMsgBuffer == null) return;
+            
+            String 
+                errMsgPattern = NbBundle.getMessage(DesignContextControllerImpl2.class, 
+                "LBL_Bpel_Mapper_Err_Msg_Wrong_XPathExpr_Data"),
+                errMsg = MessageFormat.format(errMsgPattern, new Object[] {"from",
+                    path.getExpressionString()});                
+ 
+                errMsgBuffer.append((errMsgBuffer.length() > 0) ? 
+                    " " + errMsg : errMsg);                
+                errMsgBuffer.append(",\n");
+        }
     }
 
-    protected void connectToLeftTree(List<TreeItemFinder> finderList) {
-        TreePath sourceTreePath = mLeftTreeModel.findFirstNode(finderList);
+    protected boolean connectToLeftTree(List<TreeItemFinder> finderList) {
+        TreeFinderProcessor fProcessor = new TreeFinderProcessor(mLeftTreeModel);
+        TreePath sourceTreePath = fProcessor.findFirstNode(finderList);
+        // TreePath sourceTreePath = mLeftTreeModel.findFirstNode(finderList);
         if (sourceTreePath != null) {
             TreeSourcePin sourcePin = new TreeSourcePin(sourceTreePath);
-            //
             linkToParent(sourcePin);
+            return true;
         }
+        return false;
     }
 
     /**
