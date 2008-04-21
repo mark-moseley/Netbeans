09a9c417787c0e9c95d13e8d0cb356aac2e7ad73
Radek Matous <rmatous@netbeans.org>
2008-04-21 10:13:46 +0200

    makes possible runing, debugging simultaneously + adding possibilty debug fisrt file only or all files (on switch - not UI yet)+ refactoring

--- php.dbgp/src/org/netbeans/modules/php/dbgp/DebuggerOptions.java
+++ php.dbgp/src/org/netbeans/modules/php/dbgp/DebuggerOptions.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,32 +31,45 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 package org.netbeans.modules.php.dbgp;
 
-import org.netbeans.api.debugger.DebuggerEngine;
-import org.netbeans.api.debugger.DebuggerInfo;
-import org.netbeans.api.debugger.DebuggerManager;
-import org.netbeans.modules.php.dbgp.api.Debugger;
-import org.netbeans.modules.php.dbgp.api.SessionId;
-
+import java.util.prefs.Preferences;
+import org.openide.util.NbPreferences;
 
 /**
- * @author ads
  *
+ * @author Radek Matous
  */
-public class DebuggerImpl implements Debugger {
-
-    /* (non-Javadoc)
-     * @see org.netbeans.modules.php.dbgp.api.Debugger#debug()
-     */
-    public void debug( SessionId id ) {
-        DebugSession session = new DebugSession();
-        DebuggerInfo dInfo = DebuggerInfo.create( ID ,new Object[] {id, session });
-        
-        DebuggerEngine[] engines = 
-            DebuggerManager.getDebuggerManager().startDebugging(dInfo);
-        StartActionProviderImpl.getInstance().start(id);
+public final class DebuggerOptions  {
+    private static final String PHP_DEBUGGER_PORT = "phpDebuggerPort"; // NOI18N
+    private static final String PHP_DEBUGGER_STOP_AT_FIRST_LINE = "phpDebuggerStopAtFirstLine"; // NOI18N
+    private static final String PHP_INTERPRETER = "phpInterpreter"; // NOI18N    
+    public static int getPort() {
+        return getPreferences().getInt(PHP_DEBUGGER_PORT, 9000);     
+    } 
+    
+    public static boolean isDebugForFirstPageOnly() {
+        return false;
     }
 
+    public static boolean isDebugForAllPages() {
+        return !isDebugForFirstPageOnly();
+    }
+    
+    public static boolean isDebuggerStoppedAtTheFirstLine() {
+        return getPreferences().getBoolean(PHP_DEBUGGER_STOP_AT_FIRST_LINE, false);
+    }
+    
+    public static String getPhpInterpreter() {
+        return getPreferences().get(PHP_INTERPRETER, null);
+    }
+    
+    private static Preferences getPreferences() {
+        return NbPreferences.root().node("org/netbeans/modules/php/project");// NOI18N
+    }
 }
