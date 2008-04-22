40794bf13f8b90e17ec4006ae439a0b698adae0a
Nav <nav064@netbeans.org>
2008-04-22 14:06:46 +0530

    Issue 132273 - I18N - some words not from localized bundle files in wsdl from db  wizard

--- sql.wizard/src/org/netbeans/modules/jdbcwizard/wizards/JDBCWizardTablePanel.java
+++ sql.wizard/src/org/netbeans/modules/jdbcwizard/wizards/JDBCWizardTablePanel.java
@@ -45,7 +45,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemListener;
 
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Rectangle;
@@ -130,7 +129,7 @@ public class JDBCWizardTablePanel extends JPanel {
             final JTableHeader header = this.getTableHeader();
 
             header.setReorderingAllowed(false);
-            header.setResizingAllowed(false);
+            header.setResizingAllowed(true);
         }
     }
 
@@ -243,9 +242,8 @@ public class JDBCWizardTablePanel extends JPanel {
         private static final long serialVersionUID = 1L;
         private JPanel myButPanel;
 
-        MyButtonRenderer(final String disp) {
+        MyButtonRenderer() {
             super();
-            this.setText(disp);
             this.setOpaque(true);
             this.setHorizontalAlignment(SwingConstants.CENTER);
             this.myButPanel = new JPanel();
@@ -302,7 +300,8 @@ public class JDBCWizardTablePanel extends JPanel {
 
                 this.setEnabled(true);
                 this.setFocusable(true);
-                this.setText("Advanced..");
+                this.setText(NbBundle.getMessage(JDBCWizardTablePanel.class,"LABEL_BTN_ADV"));
+                this.setMnemonic(NbBundle.getMessage(JDBCWizardTablePanel.class,"MNE_BTN_ADV").charAt(0));
             }
             return this.myButPanel;
         }
@@ -349,7 +348,8 @@ public class JDBCWizardTablePanel extends JPanel {
 					this.myButPanel.setBackground(table.getBackground());
 				}
 				this.setFocusable(true);
-				this.setText("Advanced..");
+				this.setText(NbBundle.getMessage(JDBCWizardTablePanel.class,"LABEL_BTN_ADV"));
+                                this.setMnemonic(NbBundle.getMessage(JDBCWizardTablePanel.class,"MNE_BTN_ADV").charAt(0));
 			}
 			return this.myButPanel;
 		}
@@ -404,13 +404,13 @@ public class JDBCWizardTablePanel extends JPanel {
     }
 
     class ColumnAction implements ActionListener {
-        protected JButton okbutton = new JButton("OK");
+        protected JButton okbutton = new JButton(NbBundle.getMessage(JDBCWizardTablePanel.class,"LABEL_BTN_OK"));
 
-        protected JButton cancelbutton = new JButton("Cancel");
+        protected JButton cancelbutton = new JButton(NbBundle.getMessage(JDBCWizardTablePanel.class,"LABEL_BTN_CANCEL"));
         
-        protected JButton selectallbutton = new JButton("Select All");
+        protected JButton selectallbutton = new JButton(NbBundle.getMessage(JDBCWizardTablePanel.class,"LABEL_BTN_SELECT_ALL"));
         
-        protected JButton clearallbutton = new JButton("Clear All");
+        protected JButton clearallbutton = new JButton(NbBundle.getMessage(JDBCWizardTablePanel.class,"LABEL_BTN_CLEAR_ALL"));
 
         protected JPanel buttonpanel = new JPanel();
 
@@ -438,16 +438,21 @@ public class JDBCWizardTablePanel extends JPanel {
 
         ColumnDialog columnDisplayDialog = null;
 
-        protected String title = "Columns";
+        protected String title = NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_COLS");
         
         RowDataWrapper takes;
         public ColumnAction(final RowDataWrapper takes) {
-        	this.takes = takes;
-        	columnDisplayDialog = new ColumnDialog(new JDialog(), true);
-        	this.columnDisplayDialog.setAlwaysOnTop(true);
-            this.columnDisplayDialog.setResizable(false);
+            this.takes = takes;
+            columnDisplayDialog = new ColumnDialog(new JDialog(), true);
+            this.columnDisplayDialog.setAlwaysOnTop(true);
+            this.columnDisplayDialog.setResizable(true);
             this.columnDisplayDialog.setEnabled(true);
             this.columnDisplayDialog.setFocusable(true);
+            //set mnemonics for buttons
+            okbutton.setMnemonic(NbBundle.getMessage(JDBCWizardTablePanel.class,"MNE_BTN_OK").charAt(0));
+            cancelbutton.setMnemonic(NbBundle.getMessage(JDBCWizardTablePanel.class,"MNE_BTN_CANCEL").charAt(0));
+            selectallbutton.setMnemonic(NbBundle.getMessage(JDBCWizardTablePanel.class,"MNE_BTN_SELECT_ALL").charAt(0));
+            clearallbutton.setMnemonic(NbBundle.getMessage(JDBCWizardTablePanel.class,"MNE_BTN_CLEAR_ALL").charAt(0));
         }
 
         // this is the default action when the button corresponding to the
@@ -485,10 +490,10 @@ public class JDBCWizardTablePanel extends JPanel {
          */
         public void initializeColumn() {
             this.columnDisplayDialog.setTitle(this.title);
-            this.correspInsertCol.setName("Insert");
-            this.correspUpdateCol.setName("Update");
-            this.correspChosenCol.setName("Find");//
-            this.correspPolledCol.setName("PollRecords");
+            this.correspInsertCol.setName(NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_INS"));
+            this.correspUpdateCol.setName(NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_UPDT"));
+            this.correspChosenCol.setName(NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_FIND"));//
+            this.correspPolledCol.setName(NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_POLL"));
             
             this.myTabpane = new JTabbedPane();
             this.myTabpane.add(this.correspInsertCol);
@@ -538,12 +543,12 @@ public class JDBCWizardTablePanel extends JPanel {
                 cnt++;
             }
             this.columnDisplayDialog.dispose();
-            this.title = "Columns";
+            this.title = NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_COLS");
         }
 
         public void okAction() {
             this.columnDisplayDialog.dispose();
-            this.title = "Columns";
+            this.title = NbBundle.getMessage(JDBCWizardTablePanel.class,"TITLE_COLS");
         }
         
         public void selectAllAction(int index){
@@ -644,7 +649,9 @@ public class JDBCWizardTablePanel extends JPanel {
          */
         private static final long serialVersionUID = 1L;
 
-        private final String[] columnNames = { "Select", "Table Name", "Properties" };
+        private final String[] columnNames = { NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_SEL"), 
+            NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_TAB_NAME"), 
+            NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_PROP") };
 
         private List rowList;
 
@@ -870,12 +877,6 @@ public class JDBCWizardTablePanel extends JPanel {
         }
     }
 
-    /* font selection for column data in table body */
-    private static final Font FONT_TABLE_COLUMNS = new Font("Dialog", Font.PLAIN, 10);
-
-    /* font selection for column headers in table body */
-    private static final Font FONT_TABLE_HEADER = new Font("Dialog", Font.BOLD, 10);
-
     private JPanel headerPnl;
 
     /* table to display meta data */
@@ -944,20 +945,18 @@ public class JDBCWizardTablePanel extends JPanel {
     public void resetTable(final List tableNameList) {
         final MyTableModel myMod = new MyTableModel(tableNameList);
         this.metaDataTable.setModel(myMod);
-        this.metaDataTable.getColumn("Properties").setCellRenderer(new MyButtonRenderer("Advanced.."));
-        this.metaDataTable.getColumn("Properties").setCellEditor(new MyButtonRenderer("Advanced.."));
+        this.metaDataTable.getColumn(NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_PROP"))
+                .setCellRenderer(new MyButtonRenderer());
+        this.metaDataTable.getColumn(NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_PROP"))
+                .setCellEditor(new MyButtonRenderer());
         // set checkbox column size
         final TableColumn column = this.metaDataTable.getColumnModel().getColumn(0);
-        column.setResizable(false);
-        column.setMinWidth(40);
+        column.setResizable(true);
         column.setPreferredWidth(80);
-        column.setMaxWidth(120);
         
         final TableColumn columnAdv = this.metaDataTable.getColumnModel().getColumn(2);
-        columnAdv.setResizable(false);
-        columnAdv.setMinWidth(60);
+        columnAdv.setResizable(true);
         columnAdv.setPreferredWidth(80);
-        columnAdv.setMaxWidth(100);
     }
 
     /**
@@ -965,22 +964,20 @@ public class JDBCWizardTablePanel extends JPanel {
      */
     public void addTable(final List testList) {
         this.metaDataTable = new MetaTableComponent();
-        this.metaDataTable.setFont(JDBCWizardTablePanel.FONT_TABLE_COLUMNS);
-        this.metaDataTable.getTableHeader().setFont(JDBCWizardTablePanel.FONT_TABLE_HEADER);
         final MyTableModel myModel = new MyTableModel(testList);
         this.metaDataTable.setModel(myModel);
-        this.metaDataTable.getColumn("Properties").setCellRenderer(new MyButtonRenderer("Advacned.."));
-        this.metaDataTable.getColumn("Properties").setCellEditor(new MyButtonRenderer("Advanced.."));
+        this.metaDataTable.getColumn(NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_PROP"))
+                .setCellRenderer(new MyButtonRenderer());
+        this.metaDataTable.getColumn(NbBundle.getMessage(JDBCWizardTablePanel.class,"LBL_PROP"))
+                .setCellEditor(new MyButtonRenderer());
         this.setLayout(new BorderLayout());
         // add(headerPnl, BorderLayout.NORTH);
         this.setPreferredSize(new Dimension(100, 100));
         this.setMaximumSize(new Dimension(150, 150));
         // set checkbox column size
         final TableColumn column = this.metaDataTable.getColumnModel().getColumn(0);
-        column.setResizable(false);
-        column.setMinWidth(40);
+        column.setResizable(true);
         column.setPreferredWidth(40);
-        column.setMaxWidth(80);
         this.tableScroll = new JScrollPane(this.metaDataTable);
         final javax.swing.border.Border inside = BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(3, 3, 3,
                 3), BorderFactory.createLineBorder(Color.GRAY));
