221b58a14411929aaec688ed9c8380ec03921f6c
rnithya@netbeans.org <none@none>
2008-04-28 14:09:54 +0530

    Fix for CommandLineImport-Sivashankari

--- etl.project/src/org/netbeans/modules/etl/project/EtlproProjectGenerator.java
+++ etl.project/src/org/netbeans/modules/etl/project/EtlproProjectGenerator.java
@@ -21,6 +21,8 @@ package org.netbeans.modules.etl.project;
 import java.io.File;
 import java.io.IOException;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
 import org.openide.filesystems.FileObject;
@@ -34,8 +36,6 @@ import org.netbeans.spi.project.support.ant.ProjectGenerator;
 
 import org.netbeans.modules.compapp.projects.base.ui.customizer.IcanproProjectProperties;
 import net.java.hulp.i18n.Logger;
-import org.netbeans.modules.etl.logger.Localizer;
-import org.netbeans.modules.etl.logger.LogUtil;
 import org.netbeans.modules.mashup.tables.wizard.MashupTableWizardIterator;
 import org.netbeans.modules.sql.framework.common.utils.DBExplorerUtil;
 import org.openide.DialogDisplayer;
@@ -52,31 +52,44 @@ import org.w3c.dom.Element;
  */
 public class EtlproProjectGenerator {
 
-    private static transient final Logger mLogger = LogUtil.getLogger(EtlproProjectGenerator.class.getName());
-    private static transient final Localizer mLoc = Localizer.get();
-    private static final String nbBundle1 = mLoc.t("PRSR001: collaborations");
-    private static final String nbBundle2 = mLoc.t("PRSR001: conf");
-    private static final String nbBundle3 = mLoc.t("PRSR001: setup");
-    private static final String nbBundle4 = mLoc.t("PRSR001: bpelasa");
-    private static final String nbBundle5 = mLoc.t("PRSR001: build");
-    private static final String nbBundle6 = mLoc.t("PRSR001: data");
-    private static final String nbBundle7 = mLoc.t("PRSR001: databases");
-    private static final String nbBundle8 = mLoc.t("PRSR001: nbproject");
-    private static final String nbBundle9 = mLoc.t("PRSR001: jdbc:axiondb:");
+    private static transient final Logger mLogger = Logger.getLogger(EtlproProjectGenerator.class.getName());
+    /*private static transient final Localizer mLoc = Localizer.get();
+    private static final String nbBundle1 = mLoc.t("BUND713: Collaborations");
+    private static final String nbBundle2 = mLoc.t("BUND714: conf");
+    private static final String nbBundle3 = mLoc.t("BUND715: setup");
+    private static final String nbBundle4 = mLoc.t("BUND716: bpelasa");
+    private static final String nbBundle5 = mLoc.t("BUND717: build");
+    private static final String nbBundle6 = mLoc.t("BUND718: data");
+    private static final String nbBundle7 = mLoc.t("BUND719: databases");
+    private static final String nbBundle8 = mLoc.t("BUND720: nbproject");
+    private static final String nbBundle9 = mLoc.t("BUND721: jdbc:axiondb:");
+    private static final String nbBundle11 = mLoc.t("BUND722: Default");*/
     //Trimming the initial spaces
-    private static final String DEFAULT_DOC_BASE_FOLDER = Localizer.parse(nbBundle2).trim(); //NOI18N
-    private static final String DEFAULT_SRC_FOLDER = Localizer.parse(nbBundle1).trim(); //NOI18N
-    private static final String DEFAULT_RESOURCE_FOLDER = Localizer.parse(nbBundle3).trim(); //NOI18N
-    private static final String DEFAULT_BPELASA_FOLDER = Localizer.parse(nbBundle4).trim(); //NOI18N
-    private static final String DEFAULT_BUILD_DIR = Localizer.parse(nbBundle5).trim(); //NOI18N
-    private static final String DEFAULT_DATA_DIR = Localizer.parse(nbBundle6).trim(); //NOI18N
-    private static final String DEFAULT_DATABASES_DIR = Localizer.parse(nbBundle7).trim(); //NOI18N
-    private static final String DEFAULT_NBPROJECT_DIR = Localizer.parse(nbBundle8).trim(); //NOI18N
-    private static final String DEFAULT_FLATFILE_JDBC_URL_PREFIX = Localizer.parse(nbBundle9).trim();
+    private static final String DEFAULT_DOC_BASE_FOLDER = "conf";//nbBundle2.substring(15).trim(); //NOI18N
+
+    public static final String DEFAULT_SRC_FOLDER = "Collaborations";//nbBundle1.substring(15).trim(); //NOI18N
+
+    private static final String DEFAULT_RESOURCE_FOLDER = "setup";//nbBundle3.substring(15).trim(); //NOI18N
+
+    private static final String DEFAULT_BPELASA_FOLDER = "bpelasa";//nbBundle4.substring(15).trim(); //NOI18N
+
+    private static final String DEFAULT_BUILD_DIR = "build";//nbBundle5.substring(15).trim(); //NOI18N
+
+    public static final String DEFAULT_DATA_DIR = "data";//nbBundle6.substring(15).trim(); //NOI18N
+
+    private static final String DEFAULT_DB_DIR = "Default";//nbBundle11.substring(15).trim(); //NOI18N
+
+    public static final String DEFAULT_DATABASES_DIR = "databases";//nbBundle7.substring(15).trim(); //NOI18N
+
+    private static final String DEFAULT_NBPROJECT_DIR = "nbproject";//nbBundle8.substring(15).trim(); //NOI18N
+
+    private static final String DEFAULT_FLATFILE_JDBC_URL_PREFIX = "jdbc:axiondb:";//nbBundle9.substring(15).trim();
     private static FileObject dbObj = null;
     private static File databases = null;
     private static FileObject data = null;
     public static String PRJ_LOCATION_DIR = "";
+    private static String prjName = null;
+    private static String fs = File.separator;
 
     private EtlproProjectGenerator() {
     }
@@ -88,8 +101,9 @@ public class EtlproProjectGenerator {
      * @return the helper object permitting it to be further customized
      * @throws IOException in case something went wrong
      */
-    public static AntProjectHelper createProject(File dir, String name, String j2eeLevel) throws IOException {
+    public static AntProjectHelper createProject(File dir, String name, String j2eeLevel) throws IOException {        
         dir.mkdirs();
+        prjName = name;
         // XXX clumsy way to refresh, but otherwise it doesn't work for new folders
         File rootF = dir;
         PRJ_LOCATION_DIR = rootF.getPath();
@@ -110,21 +124,26 @@ public class EtlproProjectGenerator {
         assert fo.getChildren().length == 0 : "Dir must have been empty: " + dir;
         AntProjectHelper h = setupProject(fo, name, j2eeLevel);
         fo.createFolder(DEFAULT_SRC_FOLDER); // NOI18N
-        data = fo.createFolder(DEFAULT_DATA_DIR); // NOI18N   
 
-        databases = new File(PRJ_LOCATION_DIR + "\\" + DEFAULT_NBPROJECT_DIR + "\\" + "private" + "\\" + DEFAULT_DATABASES_DIR);
+        data = fo.createFolder(DEFAULT_DATA_DIR); // NOI18N         
+
+        databases = new File(PRJ_LOCATION_DIR + fs + DEFAULT_NBPROJECT_DIR + fs + "private" + fs + DEFAULT_DATABASES_DIR);
         dbObj = FileUtil.createFolder(databases);
+        FileObject defaultFileObj = dbObj.createFolder(DEFAULT_DB_DIR);
         //dbObj.lock();  
         MashupTableWizardIterator.setProjectInfo(name, PRJ_LOCATION_DIR, true);
 
 
-        String dbName = databases.getPath();
+        String dbName = FileUtil.toFile(defaultFileObj).getAbsolutePath();
         createDefaultDatabase(dbName);
 
         EditableProperties ep = h.getProperties(AntProjectHelper.PROJECT_PROPERTIES_PATH);
         ep.put(IcanproProjectProperties.SOURCE_ROOT, DEFAULT_SRC_FOLDER); //NOI18N
+
         ep.setProperty(IcanproProjectProperties.META_INF, "${" + IcanproProjectProperties.SOURCE_ROOT + "}/" + DEFAULT_DOC_BASE_FOLDER); //NOI18N
+
         ep.setProperty(IcanproProjectProperties.SRC_DIR, "${" + IcanproProjectProperties.SOURCE_ROOT + "}"); //NOI18N
+
         ep.setProperty(IcanproProjectProperties.RESOURCE_DIR, DEFAULT_RESOURCE_FOLDER);
         h.putProperties(AntProjectHelper.PROJECT_PROPERTIES_PATH, ep);
 
@@ -139,10 +158,13 @@ public class EtlproProjectGenerator {
         Element data = h.getPrimaryConfigurationData(true);
         Document doc = data.getOwnerDocument();
         Element nameEl = doc.createElementNS(EtlproProjectType.PROJECT_CONFIGURATION_NAMESPACE, "name"); // NOI18N
+
         nameEl.appendChild(doc.createTextNode(name));
         data.appendChild(nameEl);
         Element minant = doc.createElementNS(EtlproProjectType.PROJECT_CONFIGURATION_NAMESPACE, "minimum-ant-version"); // NOI18N
+
         minant.appendChild(doc.createTextNode("1.6")); // NOI18N
+
         data.appendChild(minant);
         h.putPrimaryConfigurationData(data, true);
 
@@ -173,11 +195,17 @@ public class EtlproProjectGenerator {
 
         //============= Start of IcanPro========================================//
         ep.setProperty(IcanproProjectProperties.JBI_SETYPE_PREFIX, "sun-etl-engine"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.ASSEMBLY_UNIT_ALIAS, "This Assembly Unit"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.ASSEMBLY_UNIT_DESCRIPTION, "Represents this Assembly Unit"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.APPLICATION_SUB_ASSEMBLY_ALIAS, "This Application Sub-Assembly"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.APPLICATION_SUB_ASSEMBLY_DESCRIPTION, "This represents the Application Sub-Assembly"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.JBI_COMPONENT_CONF_ROOT, "nbproject/private"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.JBI_DEPLOYMENT_CONF_ROOT, "nbproject/deployment"); // NOI18N            
 
         ep.setProperty(IcanproProjectProperties.BC_DEPLOYMENT_JAR, "${" + IcanproProjectProperties.BUILD_DIR + "}/" + "BCDeployment.jar");
@@ -190,6 +218,7 @@ public class EtlproProjectGenerator {
         //ep.setProperty(IcanproProjectProperties.J2EE_SERVER_INSTANCE, serverInstanceID);
         //============= Start of IcanPro========================================//
         ep.setProperty(IcanproProjectProperties.JBI_COMPONENT_CONF_FILE, "ComponentInformation.xml"); // NOI18N
+
         ep.setProperty(IcanproProjectProperties.JBI_DEPLOYMENT_CONF_FILE, "default.xml"); // NOI18N
         //============= End of IcanPro========================================//
 
@@ -199,28 +228,34 @@ public class EtlproProjectGenerator {
         return h;
     }
 
-    private static void createDefaultDatabase(String name) {
-        String url = DEFAULT_FLATFILE_JDBC_URL_PREFIX + "Default" + ":" + name + "\\" + "Default";
-        File f = new File(name + "\\" + "Default");
+    public static void createDefaultDatabase(String name) {
+        // Modified for Other OS - Solaris
+        /*File f = new File(name + fs + DEFAULT_DB_DIR);
+        try {
+        FileUtil.createFolder(f);
+        } catch (IOException ex) {
+        Exceptions.printStackTrace(ex);
+        }*/
+        String url = DEFAULT_FLATFILE_JDBC_URL_PREFIX + DEFAULT_DB_DIR + ":" + name;
         char[] ch = name.toCharArray();
         if (ch == null) {
-            String nbBundle10 = mLoc.t("PRSR001: No Database name specified.");
+            String nbBundle10 = "No Database name specified.";//mLoc.t("BUND723: No Database name specified.");
             NotifyDescriptor d =
-                    new NotifyDescriptor.Message(Localizer.parse(nbBundle10), NotifyDescriptor.INFORMATION_MESSAGE);
+                    new NotifyDescriptor.Message(nbBundle10/*.substring(15)*/, NotifyDescriptor.INFORMATION_MESSAGE);
             DialogDisplayer.getDefault().notify(d);
-        } else if (f.exists()) {
-            String nbBundle11 = mLoc.t("PRSR001: Database {0} already exists.", name);
-            NotifyDescriptor d =
-                    new NotifyDescriptor.Message(Localizer.parse(nbBundle11), NotifyDescriptor.INFORMATION_MESSAGE);
-            DialogDisplayer.getDefault().notify(d);
-        } else {
+        } /*else if (f.exists()) {
+        String nbBundle11 = mLoc.t("PRJS001: Database {0} already exists.", name);
+        NotifyDescriptor d =
+        new NotifyDescriptor.Message(Localizer.parse(nbBundle11), NotifyDescriptor.INFORMATION_MESSAGE);
+        DialogDisplayer.getDefault().notify(d);
+        }*/ else {
             Connection conn = null;
             try {
                 conn = DBExplorerUtil.createConnection("org.axiondb.jdbc.AxionDriver", url, "sa", "sa");
             } catch (Exception ex) {
-                String nbBundle12 = mLoc.t("PRSR001: Axion driver could not be loaded.");
+                String nbBundle12 = "Axion driver could not be loaded.";//mLoc.t("BUND724: Axion driver could not be loaded.");
                 NotifyDescriptor d =
-                        new NotifyDescriptor.Message(Localizer.parse(nbBundle12), NotifyDescriptor.INFORMATION_MESSAGE);
+                        new NotifyDescriptor.Message(nbBundle12/*.substring(15)*/, NotifyDescriptor.INFORMATION_MESSAGE);
                 DialogDisplayer.getDefault().notify(d);
             } finally {
                 try {
@@ -234,7 +269,10 @@ public class EtlproProjectGenerator {
             }
         }
     }
+    
+    
     //Need for Migration - Start
+
     public static File getDatabasesFolder() {
         return databases;
     }
@@ -243,7 +281,7 @@ public class EtlproProjectGenerator {
         //return databases.getPath();
         String path = FileUtil.toFile(dbObj).getAbsolutePath();
         /*if (Utilities.isWindows()) {
-            path = path.replace("\\", "/"); // NOI18N
+        path = path.replace("\\", "/"); // NOI18N
         }*/
         return path;
     }
@@ -253,8 +291,32 @@ public class EtlproProjectGenerator {
         String path = FileUtil.toFile(data).getAbsolutePath();
         if (Utilities.isWindows()) {
             path = path.replace("\\", "/"); // NOI18N
+
         }
         return path;
     }
-    //Need for Migration - End
+
+    public static boolean isCommandLineImport(File dir) {
+        java.util.logging.Logger.getLogger(EtlproProjectGenerator.class.getName()).info("***********EtlproProjectGenerator dir " + dir);
+        while (dir.getParentFile() != null) {
+            dir = dir.getParentFile();
+        }
+        FileObject fo = FileUtil.toFileObject(dir);
+        java.util.logging.Logger.getLogger(EtlproProjectGenerator.class.getName()).info("***********EtlproProjectGenerator FileObject " + fo);
+        if (null != fo) {
+            isCommandLine = false;            
+            java.util.logging.Logger.getLogger(EtlproProjectGenerator.class.getName()).info("***********EtlproProjectGenerator isCommandLine " + isCommandLine);
+        } else {
+            isCommandLine = true;            
+            java.util.logging.Logger.getLogger(EtlproProjectGenerator.class.getName()).info("***********EtlproProjectGenerator else part isCommandLine " + isCommandLine);
+        }
+        DBExplorerUtil.isCmdLineImport(isCommandLine);
+        return isCommandLine;
+    }
+   
+    public static URL getResourceURL(String resource){        
+        return EtlproProject.class.getResource("resources/build.xsl");
+    }
+    private static boolean isCommandLine = false;
+    //Need for Migration - End  
 }
