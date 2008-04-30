0b1ec1e48a810af814446026e76c96f52eb54919
rohanranade@netbeans.org <none@none>
2008-04-30 13:30:11 +0530

    Adding completion query type to CompletionContext (for filtering)

---
+++ spring.beans/test/unit/src/org/netbeans/modules/spring/beans/completion/CompletionContextTest.java
@@ -0,0 +1,130 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+
+package org.netbeans.modules.spring.beans.completion;
+
+import junit.framework.TestCase;
+import org.netbeans.editor.BaseDocument;
+import org.netbeans.modules.spring.beans.TestUtils;
+import org.netbeans.modules.spring.beans.completion.CompletionContext.CompletionType;
+import org.netbeans.spi.editor.completion.CompletionProvider;
+
+/**
+ *
+ * @author Rohan Ranade (Rohan.Ranade@Sun.COM)
+ */
+public class CompletionContextTest extends TestCase {
+
+    private static final int QUERY_TYPE = CompletionProvider.COMPLETION_QUERY_TYPE;
+    
+    public void testAttributeValueCompletion() throws Exception {
+        String config = TestUtils.createXMLConfigText("<bean id='petStore' " +
+                "class='org.springframework.PetStoreImpl'/>");
+        BaseDocument doc = TestUtils.createSpringXMLConfigDocument(config);
+        CompletionContext ctx = new CompletionContext(doc, config.indexOf("'petStore'"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf("petStore"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE_VALUE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf("Store"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE_VALUE, "pet", "bean");
+    }
+
+    public void testAttributeCompletion() throws Exception {
+        String config = TestUtils.createXMLConfigText("<bean id='petStore' " +
+                "class='org.springframework.PetStoreImpl' p:name ='Sample Petstore' p:ag   ='29'/>");
+        BaseDocument doc = TestUtils.createSpringXMLConfigDocument(config);
+        CompletionContext ctx = new CompletionContext(doc, config.indexOf("id='petStore"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf("id='petStore"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf("lass='org."), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE, "c", "bean");
+        ctx = new CompletionContext(doc, config.indexOf(" ='29'"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+    }
+    
+    // test for IZ#129020
+    public void testAttributeCompletionAtTagEnd() throws Exception {
+        // empty tag
+        String config = TestUtils.createXMLConfigText("<bean id='petStore' " +
+                "class='org.springframework.PetStoreImpl' p:name ='Sample Petstore' />");
+        BaseDocument doc = TestUtils.createSpringXMLConfigDocument(config);
+        CompletionContext ctx = new CompletionContext(doc, config.indexOf("/>"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf("/>") + 1, QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf(" />"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf("name"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE, "p:", "bean");
+        
+        config = TestUtils.createXMLConfigText("<bean id='petStore' " +
+                "class='org.springframework.PetStoreImpl' p:name ='Sample Petstore'/>");
+        doc = TestUtils.createSpringXMLConfigDocument(config);
+        ctx = new CompletionContext(doc, config.indexOf("/>"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+        
+        // start tag
+        config = TestUtils.createXMLConfigText("<bean id='petStore' " +
+                "class='org.springframework.PetStoreImpl' p:name ='Sample Petstore' ></bean>");
+        doc = TestUtils.createSpringXMLConfigDocument(config);
+        ctx = new CompletionContext(doc, config.indexOf("></bean>"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.ATTRIBUTE, "", "bean");
+        ctx = new CompletionContext(doc, config.indexOf(" ></bean>"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+        
+        config = TestUtils.createXMLConfigText("<bean id='petStore' " +
+                "class='org.springframework.PetStoreImpl' p:name ='Sample Petstore'></bean>");
+        ctx = new CompletionContext(doc, config.indexOf("></bean>"), QUERY_TYPE);
+        assertContext(ctx, CompletionType.NONE, "", "bean");
+    }
+
+    private void assertContext(CompletionContext context, CompletionType expectedType,
+            String expectedPrefix, String expectedTag) {
+        assertEquals(expectedType, context.getCompletionType());
+        assertEquals(expectedPrefix, context.getTypedPrefix());
+        if(expectedTag == null) {
+            assertNull(context.getTag());
+        } else {
+            assertEquals(expectedTag, context.getTag().getNodeName());
+        }
+    }
+}
