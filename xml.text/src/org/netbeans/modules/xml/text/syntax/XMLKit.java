c6a4ff4cc6fa06ef3566de4ac42ba47b11fb4a5d
Vita Stejskal <vstejskal@netbeans.org>
2008-05-12 10:57:07 +0200

    fixing post merge problems

--- xml.text/src/org/netbeans/modules/xml/text/syntax/XMLKit.java
+++ xml.text/src/org/netbeans/modules/xml/text/syntax/XMLKit.java
@@ -101,15 +101,12 @@ public class XMLKit extends NbEditorKit implements org.openide.util.HelpCtx.Prov
     }
     
     /** Create new instance of syntax coloring parser */
+    @Override
     public Syntax createSyntax(Document doc) {
         return new XMLDefaultSyntax();
-//        return new JJEditorSyntax(
-//            new XMLSyntaxTokenManager(null).new Bridge(),
-//            new XMLSyntaxTokenMapper(),
-//            XMLTokenContext.contextPath
-//        );
     }
 
+    @Override
     public Document createDefaultDocument() {
         if(J2EE_LEXER_COLORING) {
             Document doc = new XMLEditorDocument(this.getClass());
@@ -126,20 +123,16 @@ public class XMLKit extends NbEditorKit implements org.openide.util.HelpCtx.Prov
 
 
     /** Create syntax support */
+    @Override
     public SyntaxSupport createSyntaxSupport(BaseDocument doc) {
         return new XMLSyntaxSupport(doc);
-    }
-    
-
-    public Completion createCompletion(ExtEditorUI extEditorUI) {
-        //return new org.netbeans.modules.xml.text.completion.XMLCompletion(extEditorUI);
-        return null;
     }
     
     public Completion createCompletionForProvider(ExtEditorUI extEditorUI) {
         return new XMLCompletion(extEditorUI);
     }
     
+    @Override
     public void install(JEditorPane c) {
         super.install(c);
         if (Boolean.getBoolean("netbeans.experimental.xml.nodeselectors")) {  // NOI18N
