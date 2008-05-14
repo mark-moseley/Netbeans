ef1fc45ee87d271bd0806efb27dd2936e18a7274
Radek Matous <rmatous@netbeans.org>
2008-05-14 18:01:11 +0200

    slightly modified fix: "#134782 Unable to step into a function belonging to a different projected as working directory when launching(debugging) php script" - not to open the copy of the project  file

--- php.dbgp/src/org/netbeans/modules/php/dbgp/api/SessionId.java
+++ php.dbgp/src/org/netbeans/modules/php/dbgp/api/SessionId.java
@@ -42,12 +42,22 @@ package org.netbeans.modules.php.dbgp.api;
 
 import java.io.File;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.netbeans.api.project.FileOwnerQuery;
 import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.api.project.Sources;
+import org.netbeans.modules.php.dbgp.SessionProgress;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.URLMapper;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.util.Exceptions;
 
 
 /**
@@ -60,16 +70,17 @@ import org.openide.loaders.DataObjectNotFoundException;
  * @author ads
  *
  */
-public class SessionId {
-    
-    private static final String PREFIX      = "netbeans-dbg-";         // NOI18N
-    
-    private static final String SLASH       = "/";                     // NOI18N
-    
+public class SessionId {    
+    private static final String PREFIX      = "netbeans-xdebug";         // NOI18N    
+    private static final String SLASH       = "/";                     // NOI18N    
     private static final String BACK_SLASH  = "\\";                    // NOI18N           
+        
+    //keep synchronized with org.netbeans.modules.php.rt.utils.PhpProjectSharedConstants
+    private static final String SOURCES_TYPE_PHP = "PHPSOURCE"; // NOI18N
+    
     
     public SessionId( FileObject fileObject ) {
-        myId = PREFIX + mySessionsCount;
+        myId = PREFIX;//+ mySessionsCount;
         mySessionsCount++;
         myFileObject = fileObject;
         myDebugFile = fileObject;
@@ -97,18 +108,22 @@ public class SessionId {
             computeBases();
         }
         notifyAll();
+        SessionProgress s = SessionProgress.forSessionId(this);
+        if (s != null) {
+            s.notifyConnectionFinished();
+        }        
     }
 
-    public synchronized String waitServerFile( int time ) {
-        if ( getFileUri() != null ) {
-            return getFileUri();
-        }
-        else {
-            if ( time == 0 ) {
-                return getFileUri();
+    public synchronized String waitServerFile( boolean  wait ) {
+        String retval = getFileUri();
+        if (  retval != null ) {
+            return retval;
+        } else {
+            if ( !wait ) {
+                return null;
             }
             try {
-                wait( time );
+                wait( );
             }
             catch (InterruptedException e) {
                 return getFileUri();
@@ -165,23 +180,34 @@ public class SessionId {
     }
 
     public FileObject getFileObjectByRemote( String uri ){
-        if ( uri == null ) {
-            return null;
+        FileObject retval = null;
+        if (uri != null) {
+            if (uri.equals(getFileUri())) {
+                retval = getDebugFile();
+            } else if (myRemoteBase != null && myLocalBase != null && uri.startsWith(myRemoteBase)) {
+                String relativePath = uri.substring(myRemoteBase.length());
+                relativePath = relativePath.replace(myRemoteSeparator, File.separator);
+                retval = myLocalBase.getFileObject(relativePath);
+            }
+        }   
+        if (retval == null) {
+            try {
+                URL url = new URI(uri).toURL();
+                if ("file".equals(url.getProtocol())) { //NOI18N
+                    FileObject fo = URLMapper.findFileObject(url);
+                    if (fo != null) {
+                        return fo;
+                    }
+                }
+            } catch (MalformedURLException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (URISyntaxException ex) {
+                Exceptions.printStackTrace(ex);
+            }            
         }
-        if ( uri.equals( getFileUri() )) {
-            return getDebugFile();
-        }
-        if ( myRemoteBase == null || myLocalBase == null) {
-            return null;
-        }
-        if ( !uri.startsWith( myRemoteBase) ) {
-            return null;
-        }
-        String relativePath = uri.substring( myRemoteBase.length() );
-        relativePath = relativePath.replace( myRemoteSeparator, File.separator );
-        return myLocalBase.getFileObject(relativePath);
+        return retval;
     }
-    
+        
     private String getFileUri() {
         return myFileUri;
     }
@@ -228,12 +254,30 @@ public class SessionId {
             }
             localFolder = getSessionFileObject().getParent();
         }
-        setupBases( localFolder , remoteFolderName );
+        setupBases( localFolder , remoteFolderName, fileName );
     }
 
-    private void setupBases( FileObject localFolder, String remoteFolder)
+    private void setupBases( FileObject localFolder, String remoteFolder, String fileName)
     {
-        if ( localFolder.equals( getProject() ) ) {
+        Project project = getProject();
+        FileObject projectDirectory = (project != null) ? project.getProjectDirectory() : null;
+        if (projectDirectory == null /*fos test purposes*/) {
+            assert "On".equals(System.getProperty("TestRun", "Off"));
+            setRemoteBase(  remoteFolder );
+            myLocalBase =  localFolder;
+            myDebugFile = myLocalBase.getFileObject( fileName );
+            return;
+        }
+        if (localFolder.equals(projectDirectory)) {
+            //TODO: review this code  -just hot fix - for debugging project (debug not debug.single)
+            setRemoteBase(  remoteFolder );
+            myLocalBase = getSourceRoot();
+            if (myDebugFile == null) { 
+                myDebugFile = getSourceRoot().getFileObject( fileName );
+            }
+            return;
+        }
+        if ( localFolder.equals( getSourceRoot() ) ) {
             setRemoteBase(  remoteFolder );
             myLocalBase = localFolder;
             return;
@@ -247,13 +291,7 @@ public class SessionId {
         assert indx+1 < remoteFolder.length();
         String folderName = remoteFolder.substring( indx +1 );
         String localName = localFolder.getNameExt();
-        if ( folderName.equals(localName)) {
-            setupBases( localFolder.getParent() , remoteFolder.substring( 0, indx));
-        }
-        else {
-            setRemoteBase( remoteFolder );
-            myLocalBase = localFolder;
-        }
+        setupBases( localFolder.getParent() , remoteFolder.substring( 0, indx), fileName);
     }
     
     private void setRemoteBase( String base ) {
@@ -269,6 +307,26 @@ public class SessionId {
         return myDebugFile;
     }
 
+    private FileObject getSourceRoot() {
+	final FileObject[] sourceObjects = getSourceObjects(getProject());
+        return (sourceObjects != null && sourceObjects.length > 0) ? sourceObjects[0] : null;
+    }
+    
+    public static SourceGroup[] getSourceGroups(Project phpProject) {
+        Sources sources = ProjectUtils.getSources(phpProject);
+        SourceGroup[] groups = sources.getSourceGroups(SOURCES_TYPE_PHP);//NOI18N
+        return groups;
+    }
+
+    public static FileObject[] getSourceObjects(Project phpProject) {
+        SourceGroup[] groups = getSourceGroups(phpProject);        
+        FileObject[] fileObjects = new FileObject[groups.length];
+        for (int i = 0; i < groups.length; i++) {
+            fileObjects[i] = groups[i].getRootFolder();
+        }
+        return fileObjects;
+    }
+    
     private String myId;
     
     private static int mySessionsCount = 1;
