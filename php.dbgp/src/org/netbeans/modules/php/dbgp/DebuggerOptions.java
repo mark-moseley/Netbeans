958d94d78392455ff227c60b8795b1de1360a75c
Radek Matous <rmatous@netbeans.org>
2008-05-16 15:47:24 +0200

    fixing randomly failing test + sync.starting dbg.session
    --HG--
    rename : php.dbgp/test/unit/src/org/netbeans/modules/php/dbgp/api/DebuggerTest.java => php.dbgp/test/unit/src/org/netbeans/modules/php/dbgp/DebuggerTest.java
    rename : php.dbgp/test/unit/src/org/netbeans/modules/php/dbgp/api/resources/index.php => php.dbgp/test/unit/src/org/netbeans/modules/php/dbgp/resources/index.php

--- php.dbgp/src/org/netbeans/modules/php/dbgp/DebuggerOptions.java
+++ php.dbgp/src/org/netbeans/modules/php/dbgp/DebuggerOptions.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,16 +31,72 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 package org.netbeans.modules.php.dbgp;
 
-import org.netbeans.modules.php.dbgp.DebuggerOptions;
+import org.netbeans.modules.php.project.api.PhpOptions;
 
 /**
  * @author Radek Matous
- *
  */
-//keeps php.rt compilable (after deleteing php.rt should be deleted)
-public interface Debugger {
-    public void debug( SessionId id, DebuggerOptions options );
+public class DebuggerOptions {
+    private static final DebuggerOptions GLOBAL_INSTANCE = new DefaultGlobal();
+    int port = -1;
+    Boolean debugForFirstPageOnly;
+    Boolean debuggerStoppedAtTheFirstLine;
+    String phpInterpreter;
+    
+    public static DebuggerOptions getGlobalInstance() {
+        return GLOBAL_INSTANCE;
+    }
+    
+    public int getPort() {                
+        return (port != -1) ? port :  getGlobalInstance().getPort();
+    }
+    
+    
+    public boolean isDebugForFirstPageOnly() {
+        return (debugForFirstPageOnly != null) ? debugForFirstPageOnly : 
+            getGlobalInstance().isDebugForFirstPageOnly();
+    }
+
+    public boolean isDebuggerStoppedAtTheFirstLine() {
+        return (debuggerStoppedAtTheFirstLine != null) ? debuggerStoppedAtTheFirstLine :
+            getGlobalInstance().isDebuggerStoppedAtTheFirstLine();
+    }
+
+    public String getPhpInterpreter() {
+        return (phpInterpreter != null) ? phpInterpreter :
+            getGlobalInstance().getPhpInterpreter();
+    }
+
+    private static class DefaultGlobal extends DebuggerOptions {
+        public DefaultGlobal() {
+        }
+
+        @Override
+        public int getPort() {
+            return PhpOptions.getInstance().getDebuggerPort();
+        }
+
+        @Override
+        public boolean isDebugForFirstPageOnly() {
+            return false;
+        }
+
+
+        @Override
+        public boolean isDebuggerStoppedAtTheFirstLine() {
+            return PhpOptions.getInstance().isDebuggerStoppedAtTheFirstLine();
+        }
+
+        @Override
+        public String getPhpInterpreter() {
+            return PhpOptions.getInstance().getPhpInterpreter();
+        }
+    }
 }
