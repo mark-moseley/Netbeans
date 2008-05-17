eb1598b6893910e66c4276c5f753014696031c21
thuydn@netbeans.org <none@none>
2008-05-17 02:10:08 -0700

    Fixed to make sure the model is updated properly when adding/moving/deleting a subpartition to/from a partition.

--- uml.drawingarea/src/org/netbeans/modules/uml/drawingarea/widgets/ContainerWidget.java
+++ uml.drawingarea/src/org/netbeans/modules/uml/drawingarea/widgets/ContainerWidget.java
@@ -48,15 +48,14 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import org.netbeans.api.visual.action.AcceptProvider;
 import org.netbeans.api.visual.action.ActionFactory;
-import org.netbeans.api.visual.action.ConnectorState;
 import org.netbeans.api.visual.action.WidgetAction;
 import org.netbeans.api.visual.graph.GraphScene;
 import org.netbeans.api.visual.layout.LayoutFactory;
 import org.netbeans.api.visual.model.ObjectScene;
 import org.netbeans.api.visual.widget.Scene;
 import org.netbeans.api.visual.widget.Widget;
+import org.netbeans.modules.uml.core.metamodel.common.commonactivities.IActivityGroup;
 import org.netbeans.modules.uml.core.metamodel.common.commonactivities.IActivityNode;
 import org.netbeans.modules.uml.core.metamodel.core.foundation.IElement;
 import org.netbeans.modules.uml.core.metamodel.core.foundation.INamedElement;
@@ -65,6 +64,7 @@ import org.netbeans.modules.uml.core.metamodel.core.foundation.IPresentationElem
 import org.netbeans.modules.uml.core.metamodel.diagrams.IDiagram;
 import org.netbeans.modules.uml.core.metamodel.dynamics.ICombinedFragment;
 import org.netbeans.modules.uml.core.metamodel.dynamics.ILifeline;
+import org.netbeans.modules.uml.core.support.umlutils.ETList;
 import org.netbeans.modules.uml.drawingarea.actions.SceneAcceptProvider;
 import org.netbeans.modules.uml.drawingarea.actions.WidgetAcceptAction;
 import org.netbeans.modules.uml.drawingarea.util.Util;
@@ -347,28 +347,39 @@ public class ContainerWidget extends Widget
         return changed;
     }
     
+    protected boolean allowed(IElement... elements)
+    {
+        return true;
+    }
+    
     public class ContainerAcceptProvider extends SceneAcceptProvider
     {
         public ContainerAcceptProvider()
         {
             super(null);
         }
-
+        
         @Override
-        public ConnectorState isAcceptable(Widget widget, Point point, Transferable transferable)
+        protected boolean elementsAllowed(IElement... elements)
         {
-            ConnectorState retVal = super.isAcceptable(widget, point, transferable);
-            
-            if(retVal != ConnectorState.ACCEPT)
-            {
-                if(isWidgetMove(transferable) == true)
-                {
-                    retVal = ConnectorState.ACCEPT;
-                }
-            }
-            
-            return retVal;
+            return allowed(elements);
         }
+
+//        @Override
+//        public ConnectorState isAcceptable(Widget widget, Point point, Transferable transferable)
+//        {
+//            ConnectorState retVal = super.isAcceptable(widget, point, transferable);
+//            
+//            if(retVal != ConnectorState.ACCEPT)
+//            {
+//                if(isWidgetMove(transferable) == true)
+//                {
+//                    retVal = ConnectorState.ACCEPT;
+//                }
+//            }
+//            
+//            return retVal;
+//        }
 
         
         @Override
@@ -433,13 +444,28 @@ public class ContainerWidget extends Widget
                 
                 if(ns != null) 
                 {
-                    ns.addOwnedElement(element);
+//                    ns.addOwnedElement(element);
                    
-                    // An activiy node that is contained in an activity group still
-                    // has  Activity as its name space; hence adding 
-                    // the node to its activity namespace
-                    if (element instanceof  IActivityNode) 
+                     if (element instanceof  IActivityNode && ns instanceof IActivityGroup) 
                     {   
+                        IActivityGroup group = (IActivityGroup) ns;
+                        IActivityNode activityElem = (IActivityNode)element;
+                        
+                        // Remove an activity node from its previous containers, i.e., activity groups.
+                        // This is the case when the activity node is moved from one container to the other.
+                        ETList<IActivityGroup> previousGroups = activityElem.getGroups();
+                        for (IActivityGroup aGroup : previousGroups)
+                        {
+                            aGroup.removeNodeContent(activityElem);
+                            activityElem.removeGroup(aGroup);
+                        }
+                        // add the activity node to the new container
+                        group.addOwnedElement(element);
+                        activityElem.addGroup(group);
+                       
+                        // An activity node that is contained in an activity group is still under 
+                        // an Activity namespace; hence adding the activity node
+                        // to its Activity namespace.
                         INamespace activityNamespace = ((DesignerScene) scene).getDiagram().getNamespace();
                         activityNamespace.addOwnedElement(element);
                     }
