134ba67d27cb59b9c1c876219dcd9b39662d71b0
Sergey Grinev <sergius@netbeans.org>
2008-05-19 17:11:03 +0400

    cleanup: removing occurrences of "occurence" typo
    --HG--
    rename : cnd.highlight/src/org/netbeans/modules/cnd/highlight/resources/occurence_annotation.xml => cnd.highlight/src/org/netbeans/modules/cnd/highlight/resources/occurrence_annotation.xml
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/keyword.cc.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/keyword.cc.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/macros.cc.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/macros.cc.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.classname.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.classname.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.ctor.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.ctor.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.ctor2.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.ctor2.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.err => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.err
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.field.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.field.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.localvar.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.localvar.dat
    rename : cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc.macro.dat => cnd.highlight/test/unit/data/goldenfiles/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc.macro.dat
    rename : cnd.highlight/test/unit/data/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest/markocc.cc => cnd.highlight/test/unit/data/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest/markocc.cc
    rename : cnd.highlight/test/unit/src/org/netbeans/modules/cnd/highlight/semantic/MarkOccurencesTest.java => cnd.highlight/test/unit/src/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest.java

--- cnd.highlight/test/unit/src/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest.java
+++ cnd.highlight/test/unit/src/org/netbeans/modules/cnd/highlight/semantic/MarkOccurrencesTest.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
- *
+ * 
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -39,32 +39,60 @@
  * made subject to such option by the copyright holder.
  */
 
-package org.netbeans.modules.cnd.highlight;
+package org.netbeans.modules.cnd.highlight.semantic;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import org.netbeans.modules.cnd.test.BaseTestSuite;
-import org.netbeans.modules.cnd.highlight.semantic.InactiveCodeTest;
-import org.netbeans.modules.cnd.highlight.semantic.MacroUsagesTest;
-import org.netbeans.modules.cnd.highlight.semantic.MarkOccurrencesTest;
+import java.util.Collection;
+import org.netbeans.modules.cnd.api.model.CsmOffsetable;
+import org.netbeans.modules.cnd.highlight.semantic.MarkOccurrencesHighlighter;
+import org.netbeans.modules.cnd.modelimpl.csm.core.FileImpl;
 
 /**
  *
-* @author Sergey Grinev
+ * @author Sergey Grinev
  */
-public class HighlightingTests extends BaseTestSuite {
-    
-    public HighlightingTests() {
-        super("Code Highlighting"); // NOI18N
-        
-        addTestSuite(InactiveCodeTest.class);
-        addTestSuite(MacroUsagesTest.class);
-        addTestSuite(MarkOccurrencesTest.class);
+public class MarkOccurrencesTest extends SemanticHighlightingTestBase {
+
+    public MarkOccurrencesTest(String testName) {
+        super(testName);
     }
 
-    public static Test suite() {
-        TestSuite suite = new HighlightingTests();
-        return suite;
+    private static String source = "markocc.cc"; // NOI18N
+
+    private void doTest(String name, int pos) throws Exception {
+        performTest(source, source + "." + name + ".dat", source + ".err", new Integer(pos)); // NOI18N
     }
-    
+
+    public void testMacro() throws Exception {
+        // FOO 
+        doTest("macro", 214); // NOI18N
+    }
+
+    public void testLocalVariable() throws Exception {
+        // int moo 
+        doTest("localvar", 236); // NOI18N
+    }
+
+    public void testField() throws Exception {
+        //boo 
+        doTest("field", 122); // NOI18N
+    }
+
+    public void testCtor() throws Exception {
+        // Foo() 
+        doTest("ctor", 115); // NOI18N
+    }
+
+    public void testCtor2() throws Exception {
+        // Foo(int) 
+        doTest("ctor2", 138); // NOI18N
+    }
+
+    public void testClassName() throws Exception {
+        // class Foo 
+        doTest("classname", 110); // NOI18N
+    }
+
+    protected Collection<? extends CsmOffsetable> getBlocks(FileImpl testFile,int offset) {
+        return MarkOccurrencesHighlighter.getOccurrences(testFile, offset);
+    }
 }
