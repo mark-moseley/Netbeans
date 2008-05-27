b8a4338a86b8b3e7a10ec1cd7eebff858971d93b
Jaroslav Tulach <jtulach@netbeans.org>
2008-05-27 11:30:17 +0200

    Using netbeans.systemclassloader.patches per Jesse's request

---
+++ nbjunit/test/unit/src/org/netbeans/junit/NbModuleSuiteTest.java
@@ -0,0 +1,205 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.junit;
+
+import test.pkg.not.in.junit.NbModuleSuiteIns;
+import test.pkg.not.in.junit.NbModuleSuiteT;
+import java.io.File;
+import org.netbeans.testjunit.AskForOrgOpenideUtilEnumClass;
+import java.util.Properties;
+import java.util.Set;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Jaroslav Tulach <jaroslav.tulach@netbeans.org>
+ */
+public class NbModuleSuiteTest extends TestCase {
+    
+    public NbModuleSuiteTest(String testName) {
+        super(testName);
+    }            
+    
+    public static Test suite() {
+        //return new NbModuleSuiteTest("testAccessExtraDefinedAutoload");
+        return new NbTestSuite(NbModuleSuiteTest.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testRun() {
+        Test instance = NbModuleSuite.create(NbModuleSuite.createConfiguration(NbModuleSuiteT.class).gui(false));
+        junit.textui.TestRunner.run(instance);
+        
+        assertEquals("OK", System.getProperty("t.one"));
+        assertProperty("netbeans.full.hack", "true");
+    }
+    
+    public void testPreparePathes() {
+        Properties p = new Properties();
+        
+        String prop = File.separator + "x" + File.separator + "c:org-openide-util.jar" + File.pathSeparator + 
+            File.separator + "x" + File.separator + "org-openide-nodes.jar" + File.pathSeparator +
+            File.separator + "x" + File.separator + "org-openide-util" + File.separator  + "tests.jar" + File.pathSeparator +
+            File.separator + "x" + File.separator + "org-openide-filesystems.jar";
+        
+        NbModuleSuite.S.preparePatches(prop, p);
+        
+        
+        assertNull(
+            p.getProperty("netbeans.patches.org.openide.util")
+        );
+        assertEquals(
+            File.separator + "x" + File.separator + "org-openide-util" + File.separator  + "tests.jar",
+            p.getProperty("netbeans.systemclassloader.patches")
+        );
+    }
+
+    public void testAccessToInsaneAndFS() {
+        Test instance = NbModuleSuite.create(NbModuleSuite.createConfiguration(NbModuleSuiteIns.class).gui(false).enableModules(".*"));
+        junit.textui.TestRunner.run(instance);
+        
+        assertProperty("ins.one", "OK");
+        assertProperty("ins.fs", "OK");
+    }
+
+    public void testOneCanEnumerateMethodsFromTheSuite() {
+        System.setProperty("ins.one", "No");
+        System.setProperty("ins.two", "No");
+        System.setProperty("ins.three", "No");
+
+        
+        
+        
+        Test instance = NbModuleSuite.create(
+            NbModuleSuite.createConfiguration(NbModuleSuiteIns.class).addTest("testOne").
+            addTest("testThree").gui(false)
+        
+        );
+        junit.textui.TestRunner.run(instance);
+        
+        assertProperty("ins.one", "OK");
+        assertProperty("ins.two", "No");
+        assertProperty("ins.three", "OK");
+    }
+    
+    private static void assertProperty(String name, String value) {
+        String v = System.getProperty(name);
+        assertEquals("Property " + name, value, v);
+    }
+
+    public void testAccessExtraDefinedAutoload() {
+        NbModuleSuite.Configuration config = NbModuleSuite.Configuration.create(AskForOrgOpenideUtilEnumClass.class);
+        NbModuleSuite.Configuration addEnum = config.enableModules("org.openide.util.enumerations");
+        Test instance = NbModuleSuite.create(addEnum.gui(false));
+        junit.textui.TestRunner.run(instance);
+        
+        assertEquals("OK", System.getProperty("en.one"));
+    }
+    /*
+    public void testAccessClassPathDefinedAutoload() {
+        
+        NbModuleSuite.Configuration config = NbModuleSuite.Configuration.create(En.class);
+        String manifest =
+"Manifest-Version: 1.0\n" +
+"OpenIDE-Module-Module-Dependencies: org.openide.util.enumerations>1.5\n" +
+"OpenIDE-Module: org.netbeans.modules.test.nbjunit\n" +
+"OpenIDE-Module-Specification-Version: 1.0\n";
+                
+        ClassLoader loader = new ManifestClassLoader(config.parentClassLoader, manifest);
+        NbModuleSuite.Configuration load = config.classLoader(loader);
+        Test instance = NbModuleSuite.create(load);
+        junit.textui.TestRunner.run(instance);
+        
+        assertEquals("OK", System.getProperty("en.one"));
+    }
+     */
+    
+    public void testModulesForCL() throws Exception {
+        Set<String> s = NbModuleSuite.S.findEnabledModules(ClassLoader.getSystemClassLoader());
+        s.remove("org.netbeans.modules.nbjunit");
+        assertEquals("Two modules left: " + s, 3, s.size());
+        
+        assertTrue("Util: " + s, s.contains("org.openide.util"));
+        assertTrue("junit: " + s, s.contains("org.netbeans.libs.junit4"));
+        assertTrue("insane: " + s, s.contains("org.netbeans.insane"));
+    }
+    
+    public void testIfOneCanLoadFromToolsJarOneShallDoThatInTheFrameworkAsWell() throws Exception {
+        
+        Class<?> vmm;
+        try {
+            vmm = ClassLoader.getSystemClassLoader().loadClass("com.sun.jdi.VirtualMachineManager");
+        } catch (ClassNotFoundException ex) {
+            vmm = null;
+            //throw ex;
+        }
+        Class<?> own;
+        try {
+            own = Thread.currentThread().getContextClassLoader().loadClass("com.sun.jdi.VirtualMachineManager");
+        } catch (ClassNotFoundException ex) {
+            //own = null;
+            throw ex;
+        }
+        
+        //assertEquals(vmm, own);
+        
+    }
+
+    public void testModulesForMe() throws Exception {
+        Set<String> s = NbModuleSuite.S.findEnabledModules(getClass().getClassLoader());
+        s.remove("org.netbeans.modules.nbjunit");
+        assertEquals("Two modules left: " + s, 3, s.size());
+        
+        assertTrue("Util: " + s, s.contains("org.openide.util"));
+        assertTrue("JUnit: " + s, s.contains("org.netbeans.libs.junit4"));
+        assertTrue("insane: " + s, s.contains("org.netbeans.insane"));
+    }
+    
+}
