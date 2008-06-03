4f21d7b5934704005e7302050a5e6616fb0bddda
Alexander Simon <alexvsimon@netbeans.org>
2008-06-03 17:49:23 +0400

    fixed: IZ#136170:Add template parameters in the completion list (refactoring)

--- cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/NbCsmCompletionQuery.java
+++ cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/NbCsmCompletionQuery.java
@@ -42,11 +42,13 @@
 package org.netbeans.modules.cnd.completion.cplusplus;
 import org.netbeans.modules.cnd.api.model.CsmEnumerator;
 import org.netbeans.modules.cnd.api.model.CsmMacro;
+import org.netbeans.modules.cnd.api.model.CsmTemplateParameter;
 import org.netbeans.modules.cnd.api.model.CsmTypedef;
 import org.netbeans.modules.cnd.completion.cplusplus.ext.CsmFinder;
 import org.netbeans.modules.cnd.completion.cplusplus.ext.CsmResultItem;
 import org.netbeans.modules.cnd.completion.cplusplus.ext.CsmCompletionQuery;
 import org.netbeans.modules.cnd.completion.cplusplus.ext.CsmCompletionExpression;
+import org.netbeans.modules.cnd.completion.cplusplus.ext.CsmResultItem.TemplateParameterResultItem;
 import org.netbeans.modules.cnd.modelutil.CsmUtilities;
 import org.netbeans.modules.cnd.completion.csm.CompletionResolver;
 import org.netbeans.modules.cnd.completion.csm.CompletionResolverImpl;
@@ -71,11 +73,11 @@ import org.openide.loaders.DataObject;
  */
 public class NbCsmCompletionQuery extends CsmCompletionQuery {
     private CsmFile csmFile;
-    private final boolean localContext;
+    private final QueryScope queryScope;
     
-    protected NbCsmCompletionQuery(CsmFile csmFile, boolean localContext) {
+    protected NbCsmCompletionQuery(CsmFile csmFile, QueryScope localContext) {
         this.csmFile = csmFile;
-        this.localContext = localContext;
+        this.queryScope = localContext;
     }
     
     protected CsmFinder getFinder() {
@@ -96,18 +98,23 @@ public class NbCsmCompletionQuery extends CsmCompletionQuery {
         }
         return this.csmFile;
     }
+
+    @Override
+    protected QueryScope getCompletionQueryScope() {
+        return this.queryScope;
+    }
     
     protected CompletionResolver getCompletionResolver(boolean openingSource, boolean sort) {
-	return getCompletionResolver(getBaseDocument(), getCsmFile(), openingSource, sort, localContext);
+	return getCompletionResolver(getBaseDocument(), getCsmFile(), openingSource, sort, queryScope);
     }
 
     private static CompletionResolver getCompletionResolver(BaseDocument bDoc, CsmFile csmFile, 
-            boolean openingSource, boolean sort, boolean localContext) {
+            boolean openingSource, boolean sort, QueryScope queryScope) {
 	CompletionResolver resolver = null; 
         if (csmFile != null) {
             Class kit = bDoc.getKitClass();
             resolver = new CompletionResolverImpl(csmFile, openingSource || isCaseSensitive(kit), sort, isNaturalSort(kit));
-            ((CompletionResolverImpl)resolver).setResolveLocalContextOnly(localContext);
+            ((CompletionResolverImpl)resolver).setResolveScope(queryScope);
         }
         return resolver;
     }    
@@ -142,7 +149,8 @@ public class NbCsmCompletionQuery extends CsmCompletionQuery {
     
     private static final int FILE_LOCAL_VAR_PRIORITY = TYPEDEF_PRIORITY + PRIORITY_SHIFT;
     private static final int FILE_LOCAL_ENUMERATOR_PRIORITY = FILE_LOCAL_VAR_PRIORITY + PRIORITY_SHIFT;
-    private static final int FILE_LOCAL_MACRO_PRIORITY = FILE_LOCAL_ENUMERATOR_PRIORITY + PRIORITY_SHIFT;
+    private static final int FILE_LOCAL_FUNCTION_PRIORITY = FILE_LOCAL_ENUMERATOR_PRIORITY + PRIORITY_SHIFT;
+    private static final int FILE_LOCAL_MACRO_PRIORITY = FILE_LOCAL_FUNCTION_PRIORITY + PRIORITY_SHIFT;
     private static final int FILE_INCLUDED_PRJ_MACRO_PRIORITY = FILE_LOCAL_MACRO_PRIORITY + PRIORITY_SHIFT;
     
     private static final int GLOBAL_VAR_PRIORITY = FILE_INCLUDED_PRJ_MACRO_PRIORITY + PRIORITY_SHIFT;
@@ -212,6 +220,10 @@ public class NbCsmCompletionQuery extends CsmCompletionQuery {
             return new NbCsmResultItem.NbMacroResultItem(mac, FILE_LOCAL_MACRO_PRIORITY);  
         }
         
+        public CsmResultItem.FileLocalFunctionResultItem createFileLocalFunctionResultItem(CsmFunction fun, CsmCompletionExpression substituteExp) {
+            return new NbCsmResultItem.NbFileLocalFunctionResultItem(fun, substituteExp, FILE_LOCAL_FUNCTION_PRIORITY); 
+        }
+        
         public CsmResultItem.MacroResultItem createFileIncludedProjectMacroResultItem(CsmMacro mac) {
             return new NbCsmResultItem.NbMacroResultItem(mac, FILE_INCLUDED_PRJ_MACRO_PRIORITY);  
         }
@@ -276,6 +288,10 @@ public class NbCsmCompletionQuery extends CsmCompletionQuery {
 
         public CsmResultItem.NamespaceAliasResultItem createLibNamespaceAliasResultItem(CsmNamespaceAlias alias, boolean displayFullNamespacePath) {
             return new NbCsmResultItem.NbNamespaceAliasResultItem(alias, displayFullNamespacePath, LIB_NAMESPACE_ALIAS_PRIORITY);
-        }        
+        }
+
+        public TemplateParameterResultItem createTemplateParameterResultItem(CsmTemplateParameter par) {
+            return new NbCsmResultItem.TemplateParameterResultItem(par, CLASS_ENUMERATOR_PRIORITY);
+        }
     }
 }
