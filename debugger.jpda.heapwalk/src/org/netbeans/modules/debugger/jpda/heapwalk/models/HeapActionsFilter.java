54f31a0efb910e486f272a92a58236537e2d3f67
Rudolf Balada <rbalada@netbeans.org>
2008-06-05 12:56:54 +0200

    #132160: ClassCastException on root node inside Evaluate Expression Result tree

--- debugger.jpda.heapwalk/src/org/netbeans/modules/debugger/jpda/heapwalk/models/HeapActionsFilter.java
+++ debugger.jpda.heapwalk/src/org/netbeans/modules/debugger/jpda/heapwalk/models/HeapActionsFilter.java
@@ -79,8 +79,7 @@ public class HeapActionsFilter implements NodeActionsProviderFilter {
     
     /** Creates a new instance of HeapActionsFilter */
     public HeapActionsFilter(ContextProvider contextProvider) {
-        debugger = (JPDADebugger) contextProvider.
-            lookupFirst (null, JPDADebugger.class);
+        debugger = contextProvider.lookupFirst(null, JPDADebugger.class);
 
     }
     
@@ -111,6 +110,9 @@ public class HeapActionsFilter implements NodeActionsProviderFilter {
         NbBundle.getBundle(HeapActionsFilter.class).getString("CTL_References_Label"),
         new Models.ActionPerformer () {
             public boolean isEnabled (Object node) {
+                if ((node == null) || (!(node instanceof ObjectVariable))) {
+                    return false;
+                }
                 ObjectVariable var = (ObjectVariable) node;
                 return var.getUniqueID() != 0L;
             }
