1bbf08134984f5d606d33b03c21579fb7f9a66b4
Jesse Glick <jglick@netbeans.org>
2008-06-11 19:30:47 -0400

    Misuse of CreateFromTemplateAttributesProvider meant that CDDL+GPL was not being used for special templates added to nb.org modules.

--- apisupport.project/src/org/netbeans/modules/apisupport/project/CreatedModifiedFilesFactory.java
+++ apisupport.project/src/org/netbeans/modules/apisupport/project/CreatedModifiedFilesFactory.java
@@ -82,6 +82,7 @@ import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileSystem;
 import org.openide.filesystems.FileUtil;
 import org.openide.loaders.CreateFromTemplateAttributesProvider;
+import org.openide.loaders.DataFolder;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.modules.SpecificationVersion;
@@ -286,6 +287,15 @@ public final class CreatedModifiedFilesFactory {
             } else {
                 copyAndSubstituteTokens(content, target, tokens);
             }
+            // #129446: form editor doesn't work sanely unless you do this:
+            if (target.hasExt("form")) { // NOI18N
+                FileObject java = FileUtil.findBrother(target, "java"); // NOI18N
+                if (java != null) {
+                    java.setAttribute("justCreatedByNewWizard", true); // NOI18N
+                }
+            } else if (target.hasExt("java") && FileUtil.findBrother(target, "form") != null) { // NOI18N
+                target.setAttribute("justCreatedByNewWizard", true); // NOI18N
+            }
         }
         
     }
@@ -305,12 +315,13 @@ public final class CreatedModifiedFilesFactory {
     }
     
     private static void copyAndSubstituteTokens(FileObject content, FileObject target, Map<String,String> tokens) throws IOException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("freemarker");
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine engine = scriptEngineManager.getEngineByName("freemarker");
+        assert engine != null : scriptEngineManager.getEngineFactories();
         Map<String,Object> bindings = engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE);
         String basename = target.getName();
         for (CreateFromTemplateAttributesProvider provider : Lookup.getDefault().lookupAll(CreateFromTemplateAttributesProvider.class)) {
-            DataObject d = DataObject.find(content);
-            Map<String,?> map = provider.attributesFor(d, d.getFolder(), basename);
+            Map<String,?> map = provider.attributesFor(DataObject.find(content), DataFolder.findFolder(target.getParent()), basename);
             if (map != null) {
                 bindings.putAll(map);
             }
@@ -550,7 +561,7 @@ public final class CreatedModifiedFilesFactory {
             if (layer != null && layer.findResource(layerPath) != null) {
                 layerOp = new CreatedModifiedFiles.Operation() {
                     public void run() throws IOException {
-                        throw new IOException("cannot run"); // NOI18N
+                        throw new IOException("cannot overwrite " + layerPath); // NOI18N
                     }
                     public String[] getModifiedPaths() {
                         return new String[0];
