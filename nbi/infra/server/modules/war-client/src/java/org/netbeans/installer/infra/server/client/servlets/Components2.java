714a0b21bf4dc2f5902e2ee88b8321131141866d
Dmitry Lipin <dlipin@netbeans.org>
2008-06-17 11:44:15 +0400

    Add Groovy to NBI installers (Web and JavaEE, ALL)

--- nbi/infra/server/modules/war-client/src/java/org/netbeans/installer/infra/server/client/servlets/Components2.java
+++ nbi/infra/server/modules/war-client/src/java/org/netbeans/installer/infra/server/client/servlets/Components2.java
@@ -70,6 +70,7 @@ public class Components2 extends HttpServlet {
                 "nb-base",
                 "nb-javase",
                 "nb-javaee",                
+                "nb-groovy",
                 "glassfish",
                 "tomcat",
                 "sjsas");
@@ -86,6 +87,10 @@ public class Components2 extends HttpServlet {
                 "nb-platform",
                 "nb-base",
                 "nb-cnd");
+        final List<String> php = Arrays.asList(
+                "nb-platform",
+                "nb-base",
+                "nb-php");
         
         final List<String> full = Arrays.asList(
                 "nb-platform",
@@ -97,6 +102,7 @@ public class Components2 extends HttpServlet {
                 "nb-soa",
                 "nb-uml",
                 "nb-ruby",
+                "nb-groovy",
                 "nb-php",
                 "glassfish",
                 "openesb",
@@ -106,6 +112,7 @@ public class Components2 extends HttpServlet {
         final List<String> hidden = Arrays.asList(
                 "nb-platform",
                 //"nb-base",
+                //"nb-php",
                 "openesb",
                 "sjsam");
         final Map<String, String> notes = new HashMap<String, String>();
@@ -197,6 +204,9 @@ public class Components2 extends HttpServlet {
                 }
 		if (cnd.contains(product.getUid())) {
                     properties += " | PROPERTY_CND";
+                }
+		if (php.contains(product.getUid())) {
+                    properties += " | PROPERTY_PHP";
                 }
                 if (full.contains(product.getUid())) {
                     properties += " | PROPERTY_FULL";
