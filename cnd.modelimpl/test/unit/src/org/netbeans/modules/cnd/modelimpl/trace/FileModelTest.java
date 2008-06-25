56141d044abdf51c8d0653216d9209cf0f295be7
Alexey Vladykin <alexey_vladykin@netbeans.org>
2008-06-25 19:45:18 +0400

    Fixed IZ#136729: Code model is broken by _Complex keyword

--- cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest.java
+++ cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest.java
@@ -54,6 +54,7 @@ public class FileModelTest extends TraceModelTestBase {
     @Override
     protected void setUp() throws Exception {
 	System.setProperty("parser.report.errors", "true");
+        System.setProperty("antlr.exceptions.hideExpectedTokens", "true");
         super.setUp();
     }
 
@@ -62,6 +63,16 @@ public class FileModelTest extends TraceModelTestBase {
         // init flags needed for file model tests
         getTraceModel().setDumpModel(true);
         getTraceModel().setDumpPPState(true);
+    }
+    
+    public void testDeclSpec() throws Exception {
+        // IZ#132136: code completion for C++ and Qt does not work under Windows
+        performTest("declspec.cc");
+    }
+    
+    public void testTemplateFunctionInTemplateClass() throws Exception {
+        // IZ#
+        performTest("template_fun_in_template_class.cc");
     }
     
     public void testIncludeMacroExpansion() throws Exception {
@@ -254,6 +265,60 @@ public class FileModelTest extends TraceModelTestBase {
     
     public void testFunctionsAndVariables() throws Exception {
         performTest("functions_and_variables.cc"); // NOI18N
+    }
+
+    public void testStaticFunction() throws Exception {
+        performTest("static_function.cc"); // NOI18N
+    }
+
+    public void testTypename() throws Exception {
+        // IZ 131012 : missed declaration with "typename" keyword
+        performTest("typename.cc"); // NOI18N
+    }
+    
+    public void testArray() throws Exception {
+        // IZ 130678 : incorrect offsets for type of array delcaration
+        performTest("array.cc"); // NOI18N
+    }
+    
+    public void testTemplateDestrucror() throws Exception {
+        // IZ 131407 : parser doesn't handle specialized destructor
+        performTest("template_destructor.cc"); // NOI18N
+    }
+    
+    public void testConversionOperator() throws Exception {
+        // IZ 137468 : grammar does not support conversion operator invocation
+        performTest("conversion_operator.cc"); // NOI18N
+    }
+
+    public void testClassQualifiers() throws Exception {
+        // IZ 136821 : Keyword volatile breakes classifier content
+        performTest("class_qualifiers.cc"); // NOI18N
+    }
+
+    public void testExtensions() throws Exception {
+        // IZ 137118 : IDE highlights GTK_WIDGET_SET_FLAGS and GTK_CAN_DEFAULT macros
+        performTest("extensions.cc"); // NOI18N
+    }
+
+    public void testClassTemplateMethodCall() throws Exception {
+        // IZ 137531 : IDE highlights db.template cursor<T> line as error
+        performTest("class_template_method_call.cc"); // NOI18N
+    }
+
+    public void testGccAttribute() throws Exception {
+        // IZ 136947 : IDE highlights code with 'typedef' as wrong
+        performTest("gcc_attribute.c"); // NOI18N
+    }
+
+    public void testComplex() throws Exception {
+        // IZ 136729 : Code model is broken by _Complex keyword
+        performTest("complex.c"); // NOI18N
+    }
+
+    public void testStringizeMacro() throws Exception {
+        // IZ 137465 : wrong macro expansion for #x
+        performPreprocessorTest("stringize_macro.cc"); // NOI18N
     }
     
     /////////////////////////////////////////////////////////////////////
