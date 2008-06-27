f811361acb30bcfacf8201531d1131452a6c0ae3
sherylsu@netbeans.org <none@none>
2008-06-27 13:28:25 -0700

    138483 - types of join/fork elements are different in 6.1 and 6.5 impl for report on diagram

--- uml.diagrams/src/org/netbeans/modules/uml/diagrams/nodes/initializers/ForkStateInitializer.java
+++ uml.diagrams/src/org/netbeans/modules/uml/diagrams/nodes/initializers/ForkStateInitializer.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,23 +31,31 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
+package org.netbeans.modules.uml.diagrams.nodes.initializers;
 
-package org.netbeans.modules.uml.core.metamodel.diagrams;
+import org.netbeans.modules.uml.core.metamodel.common.commonstatemachines.IPseudoState;
+import org.netbeans.modules.uml.core.metamodel.core.foundation.IElement;
+import org.netbeans.modules.uml.core.metamodel.core.primitivetypes.IPseudostateKind;
+import org.netbeans.modules.uml.drawingarea.palette.NodeInitializer;
 
-import org.netbeans.modules.uml.core.support.umlsupport.IETRect;
-
-
-public interface INodeMapLocation extends IGraphicMapLocation
+/**
+ *
+ * @author Sheryl Su
+ */
+public class ForkStateInitializer implements NodeInitializer
 {
-    /**
-     * The location of this node within the graphic.
-     */
-    public IETRect getLocation();
 
-    /**
-     * The location of this node within the graphic.
-     */
-    public void setLocation( IETRect value );
+    public void initialize(IElement element)
+    {
+        if (element instanceof IPseudoState)
+        {
+            ((IPseudoState) element).setKind(IPseudostateKind.PK_JOIN);
+        }
+    }
 }
