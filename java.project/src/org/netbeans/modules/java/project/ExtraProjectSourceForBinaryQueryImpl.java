577cadf2a5a4fe704f6aecb1072e04457b1f337b
David Konecny <dkonecny@netbeans.org>
2008-07-03 10:01:51 +1200

    #138349 - check validity of source/javadoc root before returning it

--- java.project/src/org/netbeans/modules/java/project/ExtraProjectSourceForBinaryQueryImpl.java
+++ java.project/src/org/netbeans/modules/java/project/ExtraProjectSourceForBinaryQueryImpl.java
@@ -44,14 +44,19 @@ import java.beans.PropertyChangeListener;
 import java.net.MalformedURLException;
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.java.queries.SourceForBinaryQuery;
-import org.netbeans.spi.java.queries.SourceForBinaryQueryImplementation;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.spi.java.queries.SourceForBinaryQueryImplementation2;
 import org.netbeans.spi.project.support.ant.AntProjectHelper;
 import org.netbeans.spi.project.support.ant.PropertyEvaluator;
 import org.netbeans.spi.project.support.ant.PropertyUtils;
@@ -66,7 +71,7 @@ import org.openide.util.Exceptions;
  *
  * @author mkleint
  */
-public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHook implements SourceForBinaryQueryImplementation {
+public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHook implements SourceForBinaryQueryImplementation2 {
 
     private static final String REF_START = "file.reference."; //NOI18N
     private static final String SOURCE_START = "source.reference."; //NOI18N
@@ -76,14 +81,17 @@ public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHoo
     private Map<URL,ExtraResult>  cache = new HashMap<URL,ExtraResult>();
     private PropertyChangeListener listener;
     private Map<URL, URI> mappings = new HashMap<URL, URI>();
+    private final Object MAPPINGS_LOCK = new Object();
+    private Project project;
 
-    public ExtraProjectSourceForBinaryQueryImpl(AntProjectHelper helper, PropertyEvaluator evaluator) {
+    public ExtraProjectSourceForBinaryQueryImpl(Project prj, AntProjectHelper helper, PropertyEvaluator evaluator) {
         this.helper = helper;
         this.evaluator = evaluator;
+        project = prj;
         listener = new PropertyChangeListener() {
             public void propertyChange(PropertyChangeEvent evt) {
                 if (evt.getPropertyName() == null || evt.getPropertyName().startsWith(SOURCE_START)) {
-                    mappings = getExtraSources();
+                    checkAndRegisterExtraSources(getExtraSources());
                     Collection<ExtraResult> results = null;
                     synchronized (cache) {
                         results = new ArrayList<ExtraResult>(cache.values());
@@ -104,7 +112,7 @@ public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHoo
      * @param binaryRoot
      * @return
      */
-    public SourceForBinaryQuery.Result findSourceRoots(URL binaryRoot) {
+    public SourceForBinaryQueryImplementation2.Result findSourceRoots2 (URL binaryRoot) {
         synchronized (cache) {
             ExtraResult res = cache.get(binaryRoot);
             if (res != null) {
@@ -119,20 +127,24 @@ public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHoo
         return null;
     }
     
+    public SourceForBinaryQuery.Result findSourceRoots(URL binaryRoot) {
+        return this.findSourceRoots2(binaryRoot);
+    }
+    
     @Override
     protected void projectOpened() {
-        mappings = getExtraSources();
+        checkAndRegisterExtraSources(getExtraSources());
         evaluator.addPropertyChangeListener(listener);
     }
 
     @Override
     protected void projectClosed()   {
-        mappings = new HashMap<URL, URI>();
+        checkAndRegisterExtraSources(new HashMap<URL, URI>());
         evaluator.removePropertyChangeListener(listener);
     }
     
 
-    Map<URL, URI> getExtraSources() {
+    private Map<URL, URI> getExtraSources() {
         Map<URL, URI> result = new HashMap<URL, URI>();
         Map<String, String> props = evaluator.getProperties();
         for (Map.Entry<String, String> entry : props.entrySet()) {
@@ -141,26 +153,66 @@ public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHoo
                 String sourceKey = SOURCE_START + val;
                 String source = props.get(sourceKey);
                 File bin = PropertyUtils.resolveFile(FileUtil.toFile(helper.getProjectDirectory()), entry.getValue());
-                try {
-                    URL binURL = bin.toURI().toURL();
-                    if (FileUtil.isArchiveFile(binURL)) {
-                        binURL = FileUtil.getArchiveRoot(binURL);
-                    }
-                    if (source != null) {
-                        File src = PropertyUtils.resolveFile(FileUtil.toFile(helper.getProjectDirectory()), source);
+                URL binURL = FileUtil.urlForArchiveOrDir(bin);
+                if (source != null && binURL != null) {
+                    File src = PropertyUtils.resolveFile(FileUtil.toFile(helper.getProjectDirectory()), source);
+                    // #138349 - ignore non existing paths or entries with undefined IDE variables
+                    if (src.exists()) {
                         result.put(binURL, src.toURI());
-                    } else {
-                        result.put(binURL, null);
                     }
-                } catch (MalformedURLException ex) {
-                    Exceptions.printStackTrace(ex);
                 }
             }
         }
         return result;
     }
     
-    private class ExtraResult implements SourceForBinaryQuery.Result {
+    private void checkAndRegisterExtraSources(Map<URL, URI> newvalues) {
+        Set<URL> removed;
+        Set<URL> added;
+        synchronized (MAPPINGS_LOCK) {
+            removed = new HashSet<URL>(mappings.keySet());
+            removed.removeAll(newvalues.keySet());
+            added = new HashSet<URL>(newvalues.keySet());
+            added.removeAll(mappings.keySet());
+            mappings = newvalues;
+        }
+
+                //TODO removing/adding the mapping can cause lost javadoc/source for other open projects..
+                //the mappings should be probably static, or there should be a way to trigger recalculations 
+                //in other ant projects from here
+        
+        for (URL rem : removed) {
+            synchronized (cache) {
+                ExtraResult res = cache.remove(rem);
+                if (res != null) {
+                    res.fire();
+                }
+            }
+            try {
+                URL jaradd = FileUtil.getArchiveFile(rem);
+                if (jaradd != null) {
+                    rem = jaradd;
+                }
+                FileOwnerQuery.markExternalOwner(rem.toURI(), null, FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT);
+            } catch (URISyntaxException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        for (URL add : added) {
+            try {
+                URL jaradd = FileUtil.getArchiveFile(add);
+                if (jaradd != null) {
+                    add = jaradd;
+                }
+                FileOwnerQuery.markExternalOwner(add.toURI(), project, FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT);
+            } catch (URISyntaxException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        
+    }
+    
+    private class ExtraResult implements SourceForBinaryQueryImplementation2.Result {
         private URL binaryroot;
         private ChangeSupport chs = new ChangeSupport(this);
         
@@ -201,7 +253,10 @@ public final class ExtraProjectSourceForBinaryQueryImpl extends ProjectOpenedHoo
         public void removeChangeListener(ChangeListener l) {
             chs.removeChangeListener(l);
         }
-        
+
+        public boolean preferSources() {
+            return false;
+        }        
     }
 
 
