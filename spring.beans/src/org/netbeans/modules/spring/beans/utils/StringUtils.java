9160a0b5cdc8d9a0f995fcbdf2648e485898ed79
Rohan Ranade <rohanranade@netbeans.org>
2008-07-03 18:29:01 +0530

    Fix for 137928: Incorrect hyperlinking for property paths

--- spring.beans/src/org/netbeans/modules/spring/beans/utils/StringUtils.java
+++ spring.beans/src/org/netbeans/modules/spring/beans/utils/StringUtils.java
@@ -38,11 +38,10 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
 package org.netbeans.modules.spring.beans.utils;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -113,11 +112,9 @@ public final class StringUtils {
      * @see java.lang.String#trim()
      * @see #delimitedListToStringArray
      */
-    public static List<String> tokenize(
-            String str, String delimiters, boolean trimTokens, boolean ignoreEmptyTokens) {
-
+    private static List<String> tokenize(String str, String delimiters, boolean trimTokens, boolean ignoreEmptyTokens) {
         if (str == null) {
-            return null;
+            return Collections.emptyList();
         }
         StringTokenizer st = new StringTokenizer(str, delimiters);
         List<String> tokens = new ArrayList<String>();
@@ -132,4 +129,152 @@ public final class StringUtils {
         }
         return tokens;
     }
+
+    /**
+     * Joins a list of strings.
+     *
+     * @param strings the list of strings; can be empty, but not null.
+     * @param separator a separator; can be null.
+     * @return a string containing the joined strings; never null.
+     */
+    public static String join(List<String> strings, String separator) {
+        int size = strings.size();
+        if (size == 0) {
+            return ""; // NOI18N
+        }
+        StringBuilder sb = new StringBuilder(strings.size() * strings.get(0).length());
+        int index = 0;
+        int lastIndex = size - 1;
+        for (String string : strings) {
+            sb.append(string);
+            if (separator != null && index < lastIndex) {
+                sb.append(separator);
+            }
+            index++;
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Converts the specified string (typically upper camel case) to 
+     * lower camel case
+     * 
+     * @param str input string (typically upper camel case)
+     * @return lower camel case representation of the input string
+     */
+    public static String toLowerCamelCase(String str) {
+        char[] cs = str.toCharArray();
+        cs[0] = Character.toLowerCase(cs[0]);
+        return String.valueOf(cs);
+    }
+
+    /**
+     * Overloaded Method. Method to search for fist occurrence of a delimiter in input string.
+     * @param str - input string
+     * @param fromIndex - start index in string to search for delimiter
+     * @param delimiters - string of delimiters
+     * @return
+     */
+    public static int indexOfAnyDelimiter(String str, int fromIndex, String delimiters) {
+        if(!hasLength(str)) {
+            return -1;
+        }
+        
+        return indexOfAnyDelimiter(str, fromIndex, str.length(), delimiters);
+    }
+    
+    /**
+     * Method to search for first occurrence of a delimiter in input string between two indexes.
+     * @param str - input string
+     * @param fromIndex - start index in string to search for delimiter
+     * @param toIndex - end index in string to search for delimiter
+     * @param delimiters - string of delimiters
+     * @return
+     */
+    public static int indexOfAnyDelimiter(String str, int fromIndex, int toIndex, String delimiters) {
+        if(!hasLength(str) || !hasLength(delimiters)) {
+            return -1;
+        }
+        
+        if(toIndex <= 0 || fromIndex >= str.length()) {
+            return -1;
+        }
+        
+        fromIndex = Math.max(fromIndex, 0);
+        toIndex = Math.min(toIndex, str.length());
+        
+        char[] charArray = str.toCharArray();
+        char[] delims = delimiters.toCharArray();
+        
+        for (int i = fromIndex; i < toIndex; i++) {
+            for (char d : delims) {
+                if (d == charArray[i]) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    /**
+     * Overloaded method. Method to search for last occurrence of a delimiter in input string
+     * @param str - input string
+     * @param fromIndex - start index in string to search for delimiter
+     * @param delimiters - string of delimiters
+     * @return
+     */
+    public static int lastIndexOfAnyDelimiter(String str, int fromIndex, String delimiters) {
+        if(!hasLength(str)) {
+            return -1;
+        }
+        return lastIndexOfAnyDelimiter(str, fromIndex, str.length(), delimiters);
+    }
+    
+    /**
+     * Method to search for last occurrence of a delimiter in input string between two indexes.
+     * @param str - input string
+     * @param fromIndex - start index in string to search for delimiter
+     * @param toIndex - end index in string to search for delimiter
+     * @param delimiters - string of delimiters
+     * @return
+     */
+    public static int lastIndexOfAnyDelimiter(String str, int fromIndex, int toIndex, String delimiters) {
+        if(!hasLength(str) || !hasLength(delimiters)) {
+            return -1;
+        }
+        
+        if(toIndex < 0 || fromIndex >= str.length()) {
+            return -1;
+        }
+        
+        fromIndex = Math.max(fromIndex, 0);
+        toIndex = Math.min(toIndex, str.length());
+
+        char[] charArray = str.toCharArray();
+        char[] delims = delimiters.toCharArray();
+        
+        for (int i = toIndex - 1; i >= fromIndex; --i) {
+            for (char d : delims) {
+                if (d == charArray[i]) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Returns whether there is an occurrence of a specified substring in
+     * the specified string after a specified index
+     *
+     * @param str input string
+     * @param subString sub string to search for
+     * @param fromIndex starting index (inclusive)
+     *
+     * @return true if there is an occurrence; false otherwise
+     */
+    public static boolean occurs(String str, String subString, int fromIndex) {
+        return (str.indexOf(subString, fromIndex) == -1) ? false : true;
+    }
 }
+
