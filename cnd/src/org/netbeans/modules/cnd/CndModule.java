dc31dcba7b62543772d5ab9389d4f6ca5376369f
Vladimir Kvashin <vkvashin@netbeans.org>
2008-07-07 21:53:12 +0400

    Fixed #137359 NullPointerException at startup

--- cnd/src/org/netbeans/modules/cnd/CndModule.java
+++ cnd/src/org/netbeans/modules/cnd/CndModule.java
@@ -44,18 +44,9 @@ package org.netbeans.modules.cnd;
 import java.io.File;
 import java.io.IOException;
 import org.netbeans.editor.Settings;
-import org.netbeans.modules.cnd.builds.OutputWindowOutputStream;
-import org.netbeans.modules.cnd.editor.cplusplus.CCKit;
-import org.netbeans.modules.cnd.editor.cplusplus.CCSettingsInitializer;
-import org.netbeans.modules.cnd.editor.cplusplus.CKit;
-import org.netbeans.modules.cnd.editor.fortran.FKit;
-import org.netbeans.modules.cnd.editor.fortran.FSettingsInitializer;
-import org.netbeans.modules.cnd.editor.makefile.MakefileKit;
-import org.netbeans.modules.cnd.editor.makefile.MakefileSettingsInitializer;
 import org.netbeans.modules.cnd.editor.shell.ShellKit;
 import org.netbeans.modules.cnd.editor.shell.ShellSettingsInitializer;
 import org.openide.ErrorManager;
-import org.openide.modules.InstalledFileLocator;
 import org.openide.modules.ModuleInstall;
 import org.openide.modules.InstalledFileLocator;
 import org.openide.util.Utilities;
@@ -65,25 +56,10 @@ public class CndModule extends ModuleInstall {
     // Used in other CND sources...
     public static final ErrorManager err = ErrorManager.getDefault().getInstance("org.netbeans.modules.cnd"); // NOI18N
 
-    @Override public void uninstalled() {
-        OutputWindowOutputStream.detachAllAnnotations();
-
-        // Print Options
-//        PrintSettings ps = (PrintSettings) PrintSettings.findObject(PrintSettings.class, true);
-//	ps.removeOption((SystemOption)SystemOption.findObject(FPrintOptions.class, true));
-//      ps.removeOption((SystemOption)SystemOption.findObject(CCPrintOptions.class, true));
-//	ps.removeOption((SystemOption)SystemOption.findObject(MakefilePrintOptions.class, true));
-//	ps.removeOption((SystemOption)SystemOption.findObject(ShellPrintOptions.class, true));
-    }
-
     /** Module is being opened (NetBeans startup, or enable-toggled) */
     @Override public void restored() {
 
 	// Settings for editor kits
-        Settings.addInitializer(new CCSettingsInitializer(CCKit.class));
-	Settings.addInitializer(new CCSettingsInitializer(CKit.class));
-	Settings.addInitializer(new FSettingsInitializer(FKit.class));
-	Settings.addInitializer(new MakefileSettingsInitializer(MakefileKit.class));
 	Settings.addInitializer(new ShellSettingsInitializer(ShellKit.class));
 	
 //	PrintSettings ps = (PrintSettings) PrintSettings.findObject(PrintSettings.class, true);
@@ -95,12 +71,23 @@ public class CndModule extends ModuleInstall {
         if (Utilities.isUnix()) {
             setExecutionPermission("bin/dorun.sh"); // NOI18N
             setExecutionPermission("bin/stdouterr.sh"); // NOI18N
+            if (Utilities.getOperatingSystem() == Utilities.OS_SOLARIS) {
+                if (System.getProperty("os.arch").equals("sparc")) {
+                    setExecutionPermission("bin/GdbHelper-SunOS-sparc.so"); // NOI18N
+                } else {
+                    setExecutionPermission("bin/GdbHelper-SunOS-x86.so"); // NOI18N
+                }
+            } else if (Utilities.getOperatingSystem() == Utilities.OS_LINUX) {
+                setExecutionPermission("bin/GdbHelper-Linux-x86.so"); // NOI18N
+            } else if (Utilities.isMac()) {
+                setExecutionPermission("bin/GdbHelper-Mac_OS_X-x86.dylib"); // NOI18N
+            }
         }
     }
     
     private void setExecutionPermission(String relpath) {
         File file = InstalledFileLocator.getDefault().locate(relpath, null, false);
-        if (file.exists()) {
+        if (file != null && file.exists()) {
             ProcessBuilder pb = new ProcessBuilder("/bin/chmod", "755", file.getAbsolutePath()); // NOI18N
             try {
                 pb.start();
