ab677f76f23a8e368d919734de5be7d46762424f
Jan Lahoda <jlahoda@netbeans.org>
2008-07-07 18:51:12 +0200

    #134428: preventing deadlock (acquire project read access before locking the private lock).

--- java.debug/src/org/netbeans/modules/java/debug/SourceForBinaryQueryImpl.java
+++ java.debug/src/org/netbeans/modules/java/debug/SourceForBinaryQueryImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -40,50 +40,106 @@
  */
 package org.netbeans.modules.java.debug;
 
+import java.io.IOException;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.java.queries.SourceForBinaryQuery.Result;
+import org.netbeans.api.project.ProjectManager;
 import org.netbeans.spi.java.queries.SourceForBinaryQueryImplementation;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.URLMapper;
+import org.openide.util.Mutex.Action;
 
 /**
  *
  * @author Jan Lahoda
  */
 public class SourceForBinaryQueryImpl implements SourceForBinaryQueryImplementation {
+
+    private Map<URL,Reference<Result>> url2Result = new WeakHashMap<URL, Reference<Result>>();
+    private Map<Result, URL> result2URL = new WeakHashMap<Result, URL>();
     
     public SourceForBinaryQueryImpl() {
     }
     
-    public synchronized Result findSourceRoots(URL binaryRoot) {
-        try {
-            String binaryRootS = binaryRoot.toExternalForm();
-            URL result = null;
-            if (binaryRootS.startsWith("jar:file:")) { // NOI18N
-                if (binaryRootS.endsWith("/java/source/javacapi/external/javac-api-nb-7.0-b07.jar!/")) { // NOI18N
-                    result = new URL(binaryRootS.substring("jar:".length(), binaryRootS.length() - "/java/source/javacapi/external/javac-api-nb-7.0-b07.jar!/".length()) + "/retouche/Jsr199/src"); // NOI18N
-                } else if (binaryRootS.endsWith("/java/source/javacimpl/external/javac-impl-nb-7.0-b07.jar!/")) { // NOI18N
-                    result = new URL(binaryRootS.substring("jar:".length(), binaryRootS.length() - "/java/source/javacimpl/external/javac-impl-nb-7.0-b07.jar!/".length()) + "/retouche/Jsr199/src"); // NOI18N
-                }
-                final FileObject resultFO = result != null ? URLMapper.findFileObject(result) : null;
-                if (resultFO != null) {
-                    return new Result() {
-                        public FileObject[] getRoots() {
-                            return new FileObject[] {resultFO};
-                        }
-                        public void addChangeListener(ChangeListener l) {}
-                        public void removeChangeListener(ChangeListener l) {}
-                    };
-                }
+    public Result findSourceRoots(final URL binaryRoot) {
+        return ProjectManager.mutex().readAccess(new Action<Result>() {
+            public Result run() {
+                return findSourceRootsImpl(binaryRoot);
             }
-        } catch (MalformedURLException e) {
-            Logger.getLogger("global").log(Level.INFO, null, e); //NOI18N
+        });
+    }
+
+    private synchronized Result findSourceRootsImpl(URL binaryRoot) {
+        Reference<Result> ref = url2Result.get(binaryRoot);
+        Result r = ref != null ? ref.get() : null;
+
+        if (r != null) {
+            return r;
+        }
+        
+        String binaryRootS = binaryRoot.toExternalForm();
+        URL[] urls = null;
+        if (binaryRootS.startsWith("jar:file:")) { // NOI18N
+            if ((urls = checkForBinaryRoot(binaryRootS, "/libs.javacapi/external/javac-api")) == null) { // NOI18N
+                urls = checkForBinaryRoot(binaryRootS, "/libs.javacimpl/external/javac-impl"); // NOI18N
+            }
+            final FileObject resultFO = urls != null ? URLMapper.findFileObject(urls[0]) : null;
+            if (resultFO != null) {
+                FileObject projectFO = urls != null ? URLMapper.findFileObject(urls[1]) : null;
+                if (projectFO != null) {
+                    try {
+                        ProjectManager.getDefault().findProject(projectFO);
+                    } catch (IOException ex) {
+                        Logger.getLogger(SourceForBinaryQueryImpl.class.getName()).log(Level.FINE, null, ex);
+                    } catch (IllegalArgumentException ex) {
+                        Logger.getLogger(SourceForBinaryQueryImpl.class.getName()).log(Level.FINE, null, ex);
+                    }
+                }
+                
+                Result result = new Result() {
+                    public FileObject[] getRoots() {
+                        return new FileObject[]{resultFO};
+                    }
+
+                    public void addChangeListener(ChangeListener l) {
+                    }
+
+                    public void removeChangeListener(ChangeListener l) {
+                    }
+                };
+
+                url2Result.put(binaryRoot, new WeakReference<Result>(result));
+                result2URL.put(result, binaryRoot);
+
+                return result;
+            }
         }
         return null;
     }
 
+    private URL[] checkForBinaryRoot(String ext, String prefix) {
+        if (ext.endsWith(prefix + "-nb-7.0-b07.jar!/")) { // NOI18N
+            try {
+                String part = ext.substring("jar:".length(), ext.length() - prefix.length() - "-nb-7.0-b07.jar!/".length()); // NOI18N
+                
+                return new URL[] {
+                    new URL(part + "/nb-javac/src/share/classes"), // NOI18N
+                    new URL(part + "/nb-javac/make/netbeans/nb-javac")  // NOI18N
+                };
+            } catch (MalformedURLException ex) {
+                Logger.getLogger("global").log(Level.INFO, null, ex); //NOI18N
+            }
+        }
+        
+        return null;
+    }
+    
 }
