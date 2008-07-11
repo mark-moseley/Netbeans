757b65a940a12aa641c634543eeeb5ee15056203
Maros Sandor <msandor@netbeans.org>
2008-07-11 15:01:16 +0200

    #138361: Added merge colors to diff colors panel

--- diff/src/org/netbeans/modules/diff/DiffModuleConfig.java
+++ diff/src/org/netbeans/modules/diff/DiffModuleConfig.java
@@ -64,17 +64,25 @@ public class DiffModuleConfig {
                                                                                              
     public static final String PREF_EXTERNAL_DIFF_COMMAND = "externalDiffCommand"; // NOI18N
 
-    private static final String PREF_IGNORE_WHITESPACE = "ignoreWhitespace"; // NOI18N
+    private static final String PREF_IGNORE_LEADINGTRAILING_WHITESPACE = "ignoreWhitespace"; // NOI18N
+    private static final String PREF_IGNORE_INNER_WHITESPACE = "ignoreInnerWhitespace"; // NOI18N
+    private static final String PREF_IGNORE_CASE = "ignoreCase"; // NOI18N
     private static final String PREF_USE_INTERNAL_DIFF = "useInternalDiff"; // NOI18N
     private static final String PREF_ADDED_COLOR = "addedColor"; // NOI18N
     private static final String PREF_CHANGED_COLOR = "changedColor"; // NOI18N
     private static final String PREF_DELETED_COLOR = "deletedColor"; // NOI18N
+    private static final String PREF_MERGE_UNRESOLVED_COLOR = "merge.unresolvedColor"; // NOI18N
+    private static final String PREF_MERGE_APPLIED_COLOR = "merge.appliedColor"; // NOI18N
+    private static final String PREF_MERGE_NOTAPPLIED_COLOR = "merge.notappliedColor"; // NOI18N
     
     private static final DiffModuleConfig INSTANCE = new DiffModuleConfig();
     
     private final Color defaultAddedColor = new Color(180, 255, 180);
     private final Color defaultChangedColor = new Color(160, 200, 255);
     private final Color defaultDeletedColor = new Color(255, 160, 180);
+    private final Color defaultAppliedColor = new Color(180, 255, 180);
+    private final Color defaultNotAppliedColor = new Color(160, 200, 255);
+    private final Color defaultUnresolvedColor = new Color(255, 160, 180);
 
     public static DiffModuleConfig getDefault() {
         return INSTANCE;
@@ -94,6 +102,18 @@ public class DiffModuleConfig {
     public Color getDeletedColor() {
         return getColor(PREF_DELETED_COLOR, defaultDeletedColor);
     }
+
+    public Color getAppliedColor() {
+        return getColor(PREF_MERGE_APPLIED_COLOR, defaultAppliedColor);
+    }
+
+    public Color getNotAppliedColor() {
+        return getColor(PREF_MERGE_NOTAPPLIED_COLOR, defaultNotAppliedColor);
+    }
+
+    public Color getUnresolvedColor() {
+        return getColor(PREF_MERGE_UNRESOLVED_COLOR, defaultUnresolvedColor);
+    }
     
     public void setChangedColor(Color changedColor) {
         putColor(PREF_CHANGED_COLOR, changedColor);
@@ -107,6 +127,18 @@ public class DiffModuleConfig {
         putColor(PREF_DELETED_COLOR, deletedColor);
     }
 
+    public void setNotAppliedColor(Color notAppliedColor) {
+        putColor(PREF_MERGE_NOTAPPLIED_COLOR, notAppliedColor);
+    }
+    
+    public void setAppliedColor(Color appliedColor) {
+        putColor(PREF_MERGE_APPLIED_COLOR, appliedColor);
+    }
+    
+    public void setUnresolvedColor(Color unresolvedColor) {
+        putColor(PREF_MERGE_UNRESOLVED_COLOR, unresolvedColor);
+    }
+    
     private void putColor(String key, Color color) {
         getPreferences().putInt(key, color.getRGB());
     }
@@ -119,7 +151,7 @@ public class DiffModuleConfig {
     public DiffProvider getDefaultDiffProvider() {
         DiffProvider provider = Lookup.getDefault().lookup(DiffProvider.class);
         if (provider instanceof BuiltInDiffProvider) {
-            ((BuiltInDiffProvider) provider).setTrimLines(isIgnoreWhitespace());
+            ((BuiltInDiffProvider) provider).setOptions(getOptions());
         } else if (provider instanceof CmdlineDiffProvider) {
             ((CmdlineDiffProvider) provider).setDiffCommand(getDiffCommand());
         }
@@ -130,11 +162,21 @@ public class DiffModuleConfig {
         return getPreferences().get(PREF_EXTERNAL_DIFF_COMMAND, "diff {0} {1}");
     }
 
-    public void setIgnoreWhitespace(boolean ignoreWhitespace) {
-        getPreferences().putBoolean(PREF_IGNORE_WHITESPACE, ignoreWhitespace);
-        getBuiltinProvider().setTrimLines(ignoreWhitespace);
+    public void setOptions(BuiltInDiffProvider.Options options) {
+        getPreferences().putBoolean(PREF_IGNORE_LEADINGTRAILING_WHITESPACE, options.ignoreLeadingAndtrailingWhitespace);
+        getPreferences().putBoolean(PREF_IGNORE_INNER_WHITESPACE, options.ignoreInnerWhitespace);
+        getPreferences().putBoolean(PREF_IGNORE_CASE, options.ignoreCase);
+        getBuiltinProvider().setOptions(options);
     }
 
+    public BuiltInDiffProvider.Options getOptions() {
+        BuiltInDiffProvider.Options options = new BuiltInDiffProvider.Options();
+        options.ignoreLeadingAndtrailingWhitespace = getPreferences().getBoolean(PREF_IGNORE_LEADINGTRAILING_WHITESPACE, true);
+        options.ignoreInnerWhitespace = getPreferences().getBoolean(PREF_IGNORE_INNER_WHITESPACE, false);
+        options.ignoreCase = getPreferences().getBoolean(PREF_IGNORE_CASE, false);
+        return options;
+    }
+        
     private BuiltInDiffProvider getBuiltinProvider() {
         Collection<? extends DiffProvider> diffs = Lookup.getDefault().lookupAll(DiffProvider.class);
         for (DiffProvider diff : diffs) {
@@ -143,10 +185,6 @@ public class DiffModuleConfig {
             }
         }
         throw new IllegalStateException("No builtin diff provider");
-    }
-    
-    public boolean isIgnoreWhitespace() {
-        return getPreferences().getBoolean(PREF_IGNORE_WHITESPACE, true);
     }
     
     public void setUseInteralDiff(boolean useInternal) {
