d895f72f2cd17e6e4ccf4cd50da26d6ead321af6
mkuchtiak@netbeans.org <none@none>
2008-07-22 14:44:13 +0200

    140965: enable to setup xendorsed, verbose, extension

--- websvc.jaxwsapi/src/org/netbeans/modules/websvc/jaxws/spi/ProjectJAXWSSupport.java
+++ websvc.jaxwsapi/src/org/netbeans/modules/websvc/jaxws/spi/ProjectJAXWSSupport.java
@@ -56,14 +56,16 @@ import org.apache.tools.ant.module.api.support.ActionUtils;
 import org.netbeans.api.project.Project;
 import org.netbeans.modules.j2ee.dd.api.webservices.WebservicesMetadata;
 import org.netbeans.modules.j2ee.metadata.model.api.MetadataModel;
-import org.netbeans.modules.websvc.api.jaxws.project.GeneratedFilesHelper;
 import org.netbeans.modules.websvc.api.jaxws.project.WSUtils;
 import org.netbeans.modules.websvc.api.jaxws.project.config.JaxWsModel;
 import org.netbeans.modules.websvc.api.jaxws.project.config.Service;
 import org.netbeans.modules.websvc.api.jaxws.project.config.ServiceAlreadyExistsExeption;
+import org.netbeans.modules.websvc.api.jaxws.project.config.WsimportOption;
+import org.netbeans.modules.websvc.api.jaxws.project.config.WsimportOptions;
 import org.netbeans.modules.websvc.jaxws.api.WsdlWrapperGenerator;
 import org.netbeans.modules.websvc.jaxws.api.WsdlWrapperHandler;
 import org.netbeans.spi.project.support.ant.AntProjectHelper;
+import org.netbeans.spi.project.support.ant.GeneratedFilesHelper;
 import org.openide.ErrorManager;
 import org.openide.execution.ExecutorTask;
 import org.openide.filesystems.FileLock;
@@ -80,6 +82,9 @@ import org.xml.sax.SAXException;
  * Created on February 7, 2006, 11:09 AM
  */
 public abstract class ProjectJAXWSSupport implements JAXWSSupportImpl {
+    
+    private static String[] defaultWsimportOptions = {"extension", "verbose"};  //NOI18N
+    
     private Project project;
     private AntProjectHelper antProjectHelper;
     
@@ -90,8 +95,9 @@ public abstract class ProjectJAXWSSupport implements JAXWSSupportImpl {
     }
     
     public void removeService(String serviceName) {
+        assert serviceName != null;
         JaxWsModel jaxWsModel = project.getLookup().lookup(JaxWsModel.class);
-        if (jaxWsModel!=null) {
+        if (jaxWsModel != null && serviceName != null) {
             Service service = jaxWsModel.findServiceByName(serviceName);
             if (service!=null) {
                 // remove the service element as well as the implementation class
@@ -174,7 +180,7 @@ public abstract class ProjectJAXWSSupport implements JAXWSSupportImpl {
      */
     
     public String addService(String name, String serviceImpl, String wsdlUrl, String serviceName,
-            String portName, String packageName, boolean isJsr109) {
+            String portName, String packageName, boolean isJsr109, boolean useProvider) {
         JaxWsModel jaxWsModel = project.getLookup().lookup(JaxWsModel.class);
         if (jaxWsModel!=null) {
             String finalServiceName = WSUtils.findProperServiceName(name, jaxWsModel);
@@ -242,12 +248,24 @@ public abstract class ProjectJAXWSSupport implements JAXWSSupportImpl {
                     try {
                         service = jaxWsModel.addService(finalServiceName, serviceImpl, wsdlUrl, serviceName, portName, packageName);
                     } catch (ServiceAlreadyExistsExeption ex) {
-                        //this shouldn't happen
+                        ErrorManager.getDefault().notify(ex);
                     }
+                    service.setUseProvider(useProvider);
                     String localWsdlUrl = FileUtil.getRelativePath(xmlResorcesFo, localWsdl);
                     service.setLocalWsdlFile(localWsdlUrl);
                     FileObject catalog = getCatalogFileObject();
                     if (catalog!=null) service.setCatalogFile(CATALOG_FILE);
+                    
+                    WsimportOptions wsimportOptions = service.getWsImportOptions();
+                    if (wsimportOptions != null) {
+                        for (String option:defaultWsimportOptions) {
+                            WsimportOption wsimportOption = wsimportOptions.newWsimportOption();
+                            wsimportOption.setWsimportOptionName(option);
+                            wsimportOption.setWsimportOptionValue("true"); //NOI18N
+                            wsimportOptions.addWsimportOption(wsimportOption);
+                        }
+                    }
+                    
                     writeJaxWsModel(jaxWsModel);
                     serviceAdded=true;
                 }
@@ -274,7 +292,7 @@ public abstract class ProjectJAXWSSupport implements JAXWSSupportImpl {
                         ErrorManager.getDefault().notify(ex); //TODO handle this
                     }
                 }
-                FileObject buildImplFo = project.getProjectDirectory().getFileObject(GeneratedFilesHelper.BUILD_IMPL_XML_PATH);
+                FileObject buildImplFo = project.getProjectDirectory().getFileObject(GeneratedFilesHelper.BUILD_XML_PATH);
                 try {
                     ExecutorTask wsimportTask =
                             ActionUtils.runTarget(buildImplFo,new String[]{"wsimport-service-"+finalServiceName},null); //NOI18N
@@ -311,27 +329,29 @@ public abstract class ProjectJAXWSSupport implements JAXWSSupportImpl {
     private void writeJaxWsModel(final JaxWsModel jaxWsModel) {
         try {
             final FileObject jaxWsFo = project.getProjectDirectory().getFileObject("nbproject/jax-ws.xml"); //NOI18N
-            jaxWsFo.getFileSystem().runAtomicAction(new AtomicAction() {
-                public void run() {
-                    FileLock lock=null;
-                    OutputStream os=null;
-                    try {
-                        lock = jaxWsFo.lock();
-                        os = jaxWsFo.getOutputStream(lock);
-                        jaxWsModel.write(os);
-                        os.close();
-                    } catch (java.io.IOException ex) {
-                        ErrorManager.getDefault().notify(ex);
-                    } finally {
-                        if (os!=null) {
-                            try {
-                                os.close();
-                            } catch (IOException ex) {}
+            if (jaxWsFo != null) {
+                jaxWsFo.getFileSystem().runAtomicAction(new AtomicAction() {
+                    public void run() {
+                        FileLock lock=null;
+                        OutputStream os=null;
+                        try {
+                            lock = jaxWsFo.lock();
+                            os = jaxWsFo.getOutputStream(lock);
+                            jaxWsModel.write(os);
+                            os.close();
+                        } catch (java.io.IOException ex) {
+                            ErrorManager.getDefault().notify(ex);
+                        } finally {
+                            if (os!=null) {
+                                try {
+                                    os.close();
+                                } catch (IOException ex) {}
+                            }
+                            if (lock!=null) lock.releaseLock();
                         }
-                        if (lock!=null) lock.releaseLock();
                     }
-                }
-            });
+                });
+            }
         } catch (IOException ex) {
             ErrorManager.getDefault().notify(ex);
         }
