aa216da8bb3c912f92d1da8b9fc733fc4f40f478
Milan Kubec <mkubec@netbeans.org>
2008-07-30 15:33:08 +0200

    #77266: better setting of preferredSize

--- projectui/src/org/netbeans/modules/project/ui/SimpleTargetChooserPanelGUI.java
+++ projectui/src/org/netbeans/modules/project/ui/SimpleTargetChooserPanelGUI.java
@@ -45,13 +45,10 @@ import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
-import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentListener;
 import org.netbeans.api.project.Project;
@@ -98,18 +95,16 @@ public class SimpleTargetChooserPanelGUI extends javax.swing.JPanel implements A
         if ( bottomPanel != null ) {
             bottomPanelContainer.add( bottomPanel, java.awt.BorderLayout.CENTER );
         }
-        initValues( null, null );
-        
+        initValues( null, null, null );
+
+        setPreferredSize(PREF_DIM);
+
         browseButton.addActionListener( this );
         locationComboBox.addActionListener( this );
         documentNameTextField.getDocument().addDocumentListener( this );
         folderTextField.getDocument().addDocumentListener( this );
         
         setName (NbBundle.getMessage(SimpleTargetChooserPanelGUI.class, "LBL_SimpleTargetChooserPanel_Name")); // NOI18N
-    }
-    
-    public void initValues( FileObject template, FileObject preselectedFolder ) {
-        initValues(template, preselectedFolder, null);
     }
     
     public void initValues( FileObject template, FileObject preselectedFolder, String documentName ) {
@@ -153,23 +148,22 @@ public class SimpleTargetChooserPanelGUI extends javax.swing.JPanel implements A
             displayName = template.getName ();
         }
         putClientProperty ("NewFileWizard_Title", displayName);// NOI18N        
-        
         if (template != null) {
+            final String baseName = NEW_FILE_PREFIX + template.getName ();
             if (documentName == null) {
-                final String baseName = NEW_FILE_PREFIX + template.getName ();
                 documentName = baseName;
-                if (preselectedFolder != null) {
-                    int index = 0;
-                    while (true) {
-                        FileObject _tmp = preselectedFolder.getFileObject(documentName, template.getExt());
-                        if (_tmp == null) {
-                            break;
-                        }
-                        documentName = baseName + ++index;
-                    }
-                }
-                
             }
+            if (preselectedFolder != null) {
+                int index = 0;
+                while (true) {
+                    FileObject _tmp = preselectedFolder.getFileObject(documentName, template.getExt());
+                    if (_tmp == null) {
+                        break;
+                    }
+                    documentName = baseName + ++index;
+                }
+            }
+                
             documentNameTextField.setText (documentName);
             documentNameTextField.selectAll ();
         }
@@ -211,11 +205,6 @@ public class SimpleTargetChooserPanelGUI extends javax.swing.JPanel implements A
         else {
             return text;
         }
-    }
-    
-        
-    public java.awt.Dimension getPreferredSize() {
-        return PREF_DIM;
     }
     
     public void addChangeListener(ChangeListener l) {
