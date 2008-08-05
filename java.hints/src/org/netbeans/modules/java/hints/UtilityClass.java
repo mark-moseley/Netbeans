448b45816aa425406fdcf2a2c91e8c19e24adf4d
Max Sauer <msauer@netbeans.org>
2008-08-05 11:29:14 +0200

    #118625: further improvement

--- java.hints/src/org/netbeans/modules/java/hints/UtilityClass.java
+++ java.hints/src/org/netbeans/modules/java/hints/UtilityClass.java
@@ -53,24 +53,21 @@ import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.TypeParameterElement;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.swing.JComponent;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
 import org.netbeans.api.java.source.Task;
 import org.netbeans.api.java.source.CompilationInfo;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.ModificationResult;
 import org.netbeans.api.java.source.TreePathHandle;
 import org.netbeans.api.java.source.WorkingCopy;
-import org.netbeans.modules.java.editor.semantic.Utilities;
 import org.netbeans.modules.java.hints.spi.AbstractHint;
 import org.netbeans.spi.editor.hints.ChangeInfo;
 import org.netbeans.spi.editor.hints.ErrorDescription;
 import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
 import org.netbeans.spi.editor.hints.Fix;
 import org.openide.filesystems.FileObject;
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 
 /**
@@ -80,17 +77,17 @@ import org.openide.util.NbBundle;
 public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean,CompilationInfo> {
     private boolean clazz;
     private transient volatile boolean stop;
-    
+
     /** Creates a new instance of AddOverrideAnnotation */
     private UtilityClass(boolean b) {
         super( false, true, b ? AbstractHint.HintSeverity.WARNING : AbstractHint.HintSeverity.CURRENT_LINE_WARNING);
         clazz = b;
     }
-    
+
     public Set<Kind> getTreeKinds() {
-        return EnumSet.of(clazz ? Kind.CLASS : Kind.METHOD); 
+        return EnumSet.of(clazz ? Kind.CLASS : Kind.METHOD);
     }
-    
+
     public static UtilityClass withoutConstructor() {
         return new UtilityClass(true);
     }
@@ -101,104 +98,103 @@ public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean
     public List<ErrorDescription> run(CompilationInfo compilationInfo,
                                       TreePath treePath) {
         stop = false;
-        try {
-            Document doc = compilationInfo.getDocument();
-            
-            if (doc == null) {
-                return null;
-            }
-            
-            Element e = compilationInfo.getTrees().getElement(treePath);
-            if (e == null) {
-                return null;
-            }
-            
-            if (clazz) {
-                if (e.getKind() == ElementKind.ENUM) {
-                    return null;
-                }
-                if (e.getKind() == ElementKind.INTERFACE) {
-                    return null;
-                }
-                if (e.getKind() == ElementKind.ANNOTATION_TYPE) {
-                    return null;
-                }
-                if (e.getKind() == ElementKind.CLASS) {
-                    TypeMirror supr = ((TypeElement)e).getSuperclass();
-                    if (supr == null) {
-                        return null;
-                    }
-                    Element superElem = compilationInfo.getTypes().asElement(supr);
-                    if (superElem instanceof TypeElement) {
-                        Name superName = compilationInfo.getElements().getBinaryName((TypeElement)superElem);
-                        if (superName != null && !superName.contentEquals("java.lang.Object")) {
-                            return null;
-                        }
-                    }
-                }
-
-                int cnt = 0;
-                for (Element m : e.getEnclosedElements()) {
-                    if (stop) {
-                        return null;
-                    }
-                    if (m.accept(this, compilationInfo)) {
-                        return null;
-                    }
-                    if (m.getKind() == ElementKind.METHOD || m.getKind() == ElementKind.FIELD) {
-                        cnt++;
-                    }
-                }
-                
-                if (cnt == 0) {
-                    return null;
-                }
-                
-            } else {
-                if (e.getKind() != ElementKind.CONSTRUCTOR) {
-                    return null;
-                }
-                ExecutableElement x = (ExecutableElement)e;
-                for (Element m : x.getEnclosingElement().getEnclosedElements()) {
-                    if (stop) {
-                        return null;
-                    }
-                    if (m.accept(this, compilationInfo)) {
-                        return null;
-                    }
-                }
-                if (x.getModifiers().contains(Modifier.PROTECTED) || x.getModifiers().contains(Modifier.PUBLIC)) {
-                    // ok
-                } else {
-                    return null;
-                }
-            }
-            List<Fix> fixes = Collections.<Fix>singletonList(new FixImpl(
-                clazz,
-                TreePathHandle.create(e, compilationInfo),
-                compilationInfo.getFileObject()
-                ));
-
-            int[] span = Utilities.findIdentifierSpan(treePath, compilationInfo, doc);
-
-            if (span[0] != (-1) && span[1] != (-1)) {
-                ErrorDescription ed = ErrorDescriptionFactory.createErrorDescription(
-                        getSeverity().toEditorSeverity(),
-                        NbBundle.getMessage(UtilityClass.class, clazz ? "MSG_UtilityClass" : "MSG_PublicConstructor"), // NOI18N
-                        fixes,
-                        doc,
-                        doc.createPosition(span[0]),
-                        doc.createPosition(span[1])
-                        );
-                
-                return Collections.singletonList(ed);
-            }
-        } catch (BadLocationException e) {
-            Exceptions.printStackTrace(e);
-        } catch (IOException e) {
-            Exceptions.printStackTrace(e);
+        Element e = compilationInfo.getTrees().getElement(treePath);
+        if (e == null) {
+            return null;
         }
-        
+
+        if (clazz) {
+            if (e.getKind() == ElementKind.ENUM) {
+                return null;
+            }
+            if (e.getKind() == ElementKind.INTERFACE) {
+                return null;
+            }
+            if (e.getKind() == ElementKind.ANNOTATION_TYPE) {
+                return null;
+            }
+            if (e.getKind() == ElementKind.CLASS) {
+                TypeMirror supr = ((TypeElement)e).getSuperclass();
+                if (supr == null) {
+                    return null;
+                }
+                Element superElem = compilationInfo.getTypes().asElement(supr);
+                if (superElem instanceof TypeElement) {
+                    Name superName = compilationInfo.getElements().getBinaryName((TypeElement)superElem);
+                    if (superName != null && !superName.contentEquals("java.lang.Object")) {
+                        return null;
+                    }
+                }
+            }
+
+            int cnt = 0;
+            for (Element m : e.getEnclosedElements()) {
+                if (stop) {
+                    return null;
+                }
+                if (m.accept(this, compilationInfo)) {
+                    return null;
+                }
+                if (m.getKind() == ElementKind.METHOD || m.getKind() == ElementKind.FIELD) {
+                    cnt++;
+                }
+            }
+
+            if (cnt == 0) {
+                return null;
+            }
+
+        } else {
+            if (e.getKind() != ElementKind.CONSTRUCTOR) {
+                return null;
+            }
+            ExecutableElement x = (ExecutableElement)e;
+
+            List<? extends Element> enclosedElements = x.getEnclosingElement().getEnclosedElements();
+            if(enclosedElements.size() == 1 && enclosedElements.get(0).equals(x))
+                return null; //class contains the ctor only
+
+            for (Element m : enclosedElements) {
+                if (stop) {
+                    return null;
+                }
+                if (m.accept(this, compilationInfo)) {
+                    return null;
+                }
+            }
+            if (x.getModifiers().contains(Modifier.PROTECTED) || x.getModifiers().contains(Modifier.PUBLIC)) {
+                // ok
+            } else {
+                return null;
+            }
+        }
+        List<Fix> fixes = Collections.<Fix>singletonList(new FixImpl(
+            clazz,
+            TreePathHandle.create(e, compilationInfo),
+            compilationInfo.getFileObject()
+            ));
+
+        int[] span = null;
+
+        switch (treePath.getLeaf().getKind()) {
+            case METHOD: span = compilationInfo.getTreeUtilities().findNameSpan((MethodTree) treePath.getLeaf()); break;
+            case CLASS: span = compilationInfo.getTreeUtilities().findNameSpan((ClassTree) treePath.getLeaf()); break;
+            case VARIABLE: span = compilationInfo.getTreeUtilities().findNameSpan((VariableTree) treePath.getLeaf()); break;
+        }
+
+        if (span != null) {
+            ErrorDescription ed = ErrorDescriptionFactory.createErrorDescription(
+                    getSeverity().toEditorSeverity(),
+                    NbBundle.getMessage(UtilityClass.class, clazz ? "MSG_UtilityClass" : "MSG_PublicConstructor"), // NOI18N
+                    fixes,
+                    compilationInfo.getFileObject(),
+                    span[0],
+                    span[1]
+                    );
+
+            return Collections.singletonList(ed);
+        }
+
         return null;
     }
 
@@ -215,18 +211,18 @@ public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean
     }
 
     public void cancel() {
-        // XXX implement me 
+        // XXX implement me
     }
-    
+
     public Preferences getPreferences() {
         return null;
     }
-    
+
     @Override
     public JComponent getCustomizer(Preferences node) {
         return null;
-    }    
-          
+    }
+
     public Boolean visit(Element arg0, CompilationInfo arg1) {
         return false;
     }
@@ -251,7 +247,13 @@ public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean
         if (clazz) {
             return !m.getModifiers().contains(Modifier.STATIC) && !arg1.getElementUtilities().isSynthetic(m);
         } else {
-            return !m.getModifiers().contains(Modifier.STATIC) && !m.getSimpleName().contentEquals("<init>"); // NOI18N
+            boolean staticCtor = !m.getModifiers().contains(Modifier.STATIC) && !m.getSimpleName().contentEquals("<init>"); // NOI18N
+            boolean main = m.getModifiers().contains(Modifier.STATIC) &&
+                    m.getSimpleName().contentEquals("main") &&
+                    (m.getReturnType().getKind() == TypeKind.VOID) &&
+                    (m.getParameters().size() == 1) &&
+                    (m.getParameters().get(0).asType().toString().equals("java.lang.String[]"));
+            return staticCtor || main;
         }
     }
 
@@ -267,30 +269,30 @@ public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean
         private TreePathHandle handle;
         private FileObject file;
         private boolean clazz;
-        
+
         public FixImpl(boolean clazz, TreePathHandle handle, FileObject file) {
             this.handle = handle;
             this.file = file;
             this.clazz = clazz;
         }
-        
+
         public String getText() {
             return NbBundle.getMessage(UtilityClass.class, clazz ? "MSG_PrivateConstructor" : "MSG_MakePrivate"); // NOI18N
         }
-        
+
         public ChangeInfo implement() throws IOException {
             ModificationResult result = JavaSource.forFileObject(file).runModificationTask(this);
             result.commit();
             return null;
         }
-        
+
         @Override public String toString() {
             return "FixUtilityClass"; // NOI18N
         }
 
         public void run(WorkingCopy wc) throws Exception {
             wc.toPhase(JavaSource.Phase.RESOLVED);
-            
+
             Element e = handle.resolveElement(wc);
             if (e == null) {
                 return;
@@ -301,13 +303,13 @@ public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean
                     return;
                 }
                 ClassTree cls = (ClassTree)outer;
-                
+
                 ModifiersTree modifiers = wc.getTreeMaker().Modifiers(Collections.singleton(Modifier.PRIVATE));
                 MethodTree m = wc.getTreeMaker().Constructor(
-                    modifiers, 
-                    Collections.<TypeParameterTree>emptyList(), 
-                    Collections.<VariableTree>emptyList(), 
-                    Collections.<ExpressionTree>emptyList(), 
+                    modifiers,
+                    Collections.<TypeParameterTree>emptyList(),
+                    Collections.<VariableTree>emptyList(),
+                    Collections.<ExpressionTree>emptyList(),
                     wc.getTreeMaker().Block(Collections.<StatementTree>emptyList(), false)
                 );
                 wc.rewrite(cls, wc.getTreeMaker().addClassMember(cls, m));
@@ -316,11 +318,11 @@ public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean
                     return;
                 }
                 MethodTree met = (MethodTree)outer;
-                
+
                 ModifiersTree modifiers = wc.getTreeMaker().Modifiers(Collections.singleton(Modifier.PRIVATE), met.getModifiers().getAnnotations());
                 wc.rewrite(met.getModifiers(), modifiers);
             }
         }
     }
-    
+
 }
