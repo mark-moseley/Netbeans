601a75a921fc8849de6cf0a159eab87042e39bea
Martin Fousek <marfous@netbeans.org>
2008-08-06 15:54:04 +0200

    repairing tests - fixed imports, fixed part of CallEJBTest.java

--- j2ee.kit/test/qa-functional/src/org/netbeans/test/j2ee/addmethod/AddSelectMethodTest.java
+++ j2ee.kit/test/qa-functional/src/org/netbeans/test/j2ee/addmethod/AddSelectMethodTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -38,51 +38,37 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
 package org.netbeans.test.j2ee.addmethod;
 
-import java.io.File;
 import java.io.IOException;
 import javax.swing.JTextField;
 import org.netbeans.jellytools.Bundle;
 import org.netbeans.jellytools.EditorOperator;
 import org.netbeans.jellytools.EditorWindowOperator;
-import org.netbeans.jellytools.JellyTestCase;
 import org.netbeans.jellytools.NbDialogOperator;
-import org.netbeans.jellytools.ProjectsTabOperator;
-import org.netbeans.jellytools.actions.ActionNoBlock;
-import org.netbeans.jellytools.actions.OpenAction;
-import org.netbeans.jellytools.nodes.Node;
-import org.netbeans.jemmy.JemmyException;
-import org.netbeans.jemmy.Waitable;
-import org.netbeans.jemmy.Waiter;
-import org.netbeans.jemmy.operators.JButtonOperator;
-import org.netbeans.jemmy.operators.JCheckBoxOperator;
-import org.netbeans.jemmy.operators.JComboBoxOperator;
 import org.netbeans.jemmy.operators.JLabelOperator;
-import org.netbeans.jemmy.operators.JRadioButtonOperator;
-import org.netbeans.jemmy.operators.JTabbedPaneOperator;
 import org.netbeans.jemmy.operators.JTextAreaOperator;
 import org.netbeans.jemmy.operators.JTextFieldOperator;
-import org.netbeans.jemmy.util.PNGEncoder;
-import org.netbeans.test.j2ee.*;
+import org.netbeans.jellytools.modules.java.editor.GenerateCodeOperator;
 
 /**
  *
  * @author lm97939
  */
 public class AddSelectMethodTest extends AddMethodTest {
-    
+
     protected String ejbql = null;
     private String toSearchFile;
-    
+
     /** Creates a new instance of AddMethodTest */
     public AddSelectMethodTest(String name) {
         super(name);
     }
-    
-    public void setUp() {
-        System.out.println("########  "+getName()+"  #######");
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        System.out.println("########  " + getName() + "  #######");
     }
 
     /** Use for execution inside IDE */
@@ -90,12 +76,9 @@ public class AddSelectMethodTest extends AddMethodTest {
         // run only selected test case
         junit.textui.TestRunner.run(new AddSelectMethodTest("testAddSelectMethod1InEB"));
     }
-    
-    
-    public void testAddSelectMethod1InEB()  throws IOException{
+
+    public void testAddSelectMethod1InEB() throws IOException {
         beanName = "TestingEntity";
-        editorPopup = Bundle.getStringTrimmed("org.netbeans.modules.j2ee.ejbcore.ui.logicalview.ejb.action.Bundle", "LBL_EJBActionGroup")
-                               +"|"+Bundle.getStringTrimmed("org.netbeans.modules.j2ee.ejbcore.ui.logicalview.ejb.action.Bundle", "LBL_AddSelectMethodAction");
         dialogTitle = Bundle.getStringTrimmed("org.netbeans.modules.j2ee.ejbcore.ui.logicalview.ejb.action.Bundle", "LBL_AddSelectMethodAction");
         methodName = "ejbSelectByTest1";
         returnType = "int";
@@ -107,48 +90,43 @@ public class AddSelectMethodTest extends AddMethodTest {
         addMethod();
     }
 
-    public void testAddSelectMethod2InEB()  throws IOException{
+    public void testAddSelectMethod2InEB() throws IOException {
         beanName = "TestingEntity";
-        editorPopup = Bundle.getStringTrimmed("org.netbeans.modules.j2ee.ejbcore.ui.logicalview.ejb.action.Bundle", "LBL_EJBActionGroup")
-                               +"|"+Bundle.getStringTrimmed("org.netbeans.modules.j2ee.ejbcore.ui.logicalview.ejb.action.Bundle", "LBL_AddSelectMethodAction");
         dialogTitle = Bundle.getStringTrimmed("org.netbeans.modules.j2ee.ejbcore.ui.logicalview.ejb.action.Bundle", "LBL_AddSelectMethodAction");
         methodName = "ejbSelectByTest2";
         returnType = "int";
-        parameters = new String[][] {{"java.lang.String", "a"}};
+        parameters = new String[][]{{"java.lang.String", "a"}};
         ejbql = null;
         //toSearchFile = beanName+"LocalHome.java";
         isDDModified = true;
         saveFile = true;
         addMethod();
     }
-    
+
+    @Override
     protected void addMethod() throws IOException {
-        EditorOperator editor = new EditorWindowOperator().getEditor(beanName+"Bean.java");
+        EditorOperator editor = EditorWindowOperator.getEditor(beanName + "Bean.java");
         editor.select(11);
 
         // invoke Add Business Method dialog
-        new ActionNoBlock(null,editorPopup).perform(editor);
+        GenerateCodeOperator.openDialog(dialogTitle, editor);
         NbDialogOperator dialog = new NbDialogOperator(dialogTitle);
         JLabelOperator lblOper = new JLabelOperator(dialog, "Name");
-        new JTextFieldOperator((JTextField)lblOper.getLabelFor()).setText(methodName);
+        new JTextFieldOperator((JTextField) lblOper.getLabelFor()).setText(methodName);
         if (returnType != null) {
-            new JTextFieldOperator(dialog,1).setText(returnType);
+            new JTextFieldOperator(dialog, 1).setText(returnType);
         }
-        
         fillParameters(dialog);
-        
         if (ejbql != null) {
             new JTextAreaOperator(dialog).setText(ejbql);
         }
         dialog.ok();
-        
-        if (saveFile) 
+        if (saveFile) {
             editor.save();
+        }
         if (toSearchFile != null) {
             waitForEditorText(editor, methodName);
         }
-        
         compareFiles();
     }
-    
 }
