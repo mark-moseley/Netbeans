19b1ab96db37040b35e356a711976d968b501717
Vladimir Voskresensky <vv159170@netbeans.org>
2008-08-07 19:51:02 +0400

    better fix for #139600 A parameter of function pointer type is highlighted as an error

--- cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/BasicHyperlinkTestCase/main.c
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/BasicHyperlinkTestCase/main.c
@@ -11,3 +11,28 @@ int boo(int aa, double bb) {
 void method_name_with_underscore() {
     method_name_with_underscore();
 }
+
+const int VALUE = 10;
+const int VALUE_2 = 10 + VALUE;
+
+void fun(char* aaa, char**bbb) {
+    int iiii = fun(null, null);
+}
+
+void sameNameDiffScope(int name) {
+    if (name++) {
+        string name;
+        name = "name";
+    } else if (name++) {
+        char* name;
+        strlen(name);     
+    }
+    name--;
+    
+    char* globalvar = 0;
+    fun(::globalvar, &globalvar);
+    ::globalvar = ++::globalvar + globalvar;
+    int (*funPtr)();
+}
+
+char* globalvar;
