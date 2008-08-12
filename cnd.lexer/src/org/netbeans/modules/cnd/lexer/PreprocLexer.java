f329db89f667416b2fbcab12863c5bcd745fbe7c
Vladimir Voskresensky <vv159170@netbeans.org>
2008-08-12 17:41:55 +0400

    support state info for CndLexer

--- cnd.lexer/src/org/netbeans/modules/cnd/lexer/PreprocLexer.java
+++ cnd.lexer/src/org/netbeans/modules/cnd/lexer/PreprocLexer.java
@@ -70,10 +70,11 @@ public final class PreprocLexer extends CndLexer {
     private static final int EXPRESSION         = DIRECTIVE_NAME + 1;
     private static final int INCLUDE_DIRECTIVE  = EXPRESSION + 1;
     private static final int OTHER              = INCLUDE_DIRECTIVE + 1;
-    
-    private static final String WHITESPACE_CATEGORY = CppTokenId.WHITESPACE.primaryCategory();
-    private static final String COMMENT_CATEGORY = CppTokenId.LINE_COMMENT.primaryCategory();
-    
+
+    // shift is the number of bits enough to mask all states
+    private static final int SHIFT = 3;
+    private static final int MASK  = 0x7; // ~((~0) << SHIFT)
+     
     private int state = INIT;
     private final Filter<CppTokenId> preprocFilter;
     private final Filter<CppTokenId> keywordsFilter;
@@ -82,18 +83,27 @@ public final class PreprocLexer extends CndLexer {
         super(info);
         this.preprocFilter = CndLexerUtilities.getPreprocFilter();
         @SuppressWarnings("unchecked")
-        Filter<CppTokenId> filter = (Filter<CppTokenId>) info.getAttributeValue("lexer-filter"); // NOI18N
+        Filter<CppTokenId> filter = (Filter<CppTokenId>) info.getAttributeValue(CndLexerUtilities.LEXER_FILTER);
         this.keywordsFilter = filter != null ? filter : defaultFilter;
-        fromState(info.state()); // last line in contstructor
+        fromState((Integer) info.state()); // last line in contstructor
     }
 
     @Override
     public Object state() {
-        return Integer.valueOf(state);
+        Integer baseState = super.getState();
+        int baseValue = baseState == null ? 0 : baseState.intValue();
+        int value = (baseValue << SHIFT) | state;
+        return Integer.valueOf(value);
     }
-    
-    private void fromState(Object state) {
-        this.state = state == null ? INIT : ((Integer)state).intValue();
+   
+    private void fromState(Integer state) {
+        if (state == null) {
+            this.state = INIT;
+            super.setState(null);
+        } else {
+            this.state = state.intValue() & MASK;
+            super.setState(state.intValue() >> SHIFT);
+        }
     }
 
     @Override
@@ -174,8 +184,8 @@ public final class PreprocLexer extends CndLexer {
                 state = DIRECTIVE_NAME;
                 break;
             case DIRECTIVE_NAME:
-                if (!WHITESPACE_CATEGORY.equals(id.primaryCategory()) &&
-                           !COMMENT_CATEGORY.equals(id.primaryCategory())) {
+                if (!CppTokenId.WHITESPACE_CATEGORY.equals(id.primaryCategory()) &&
+                           !CppTokenId.COMMENT_CATEGORY.equals(id.primaryCategory())) {
                     switch (id) {
                         case PREPROCESSOR_IF:
                         case PREPROCESSOR_ELIF:
