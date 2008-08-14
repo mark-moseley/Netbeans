3eee6ae051850a914631192c85cee3923c6296b4
Martin Adamek <martin_adamek@netbeans.org>
2008-08-14 17:03:55 +0200

    Code reformatting

--- groovy.grailsproject/src/org/netbeans/modules/groovy/grailsproject/ui/wizards/NewGrailsProjectWizardIterator.java
+++ groovy.grailsproject/src/org/netbeans/modules/groovy/grailsproject/ui/wizards/NewGrailsProjectWizardIterator.java
@@ -25,75 +25,64 @@
  *
  * Portions Copyrighted 2007 Sun Microsystems, Inc.
  */
-
 package org.netbeans.modules.groovy.grailsproject.ui.wizards;
 
 import java.awt.Component;
 import java.io.IOException;
-import java.io.Writer;
 import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Set;
 import javax.swing.event.ChangeListener;
+import org.netbeans.modules.extexecution.api.input.InputProcessor;
 import org.openide.WizardDescriptor;
 import org.openide.WizardDescriptor.Panel;
 import java.util.NoSuchElementException;
 import javax.swing.JComponent;
 import org.openide.util.NbBundle;
 import java.io.File;
-import java.nio.charset.Charset;
 import org.openide.filesystems.FileUtil;
 import org.openide.filesystems.FileObject;
 import java.util.logging.Logger;
 import org.netbeans.api.progress.ProgressHandle;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import org.netbeans.modules.extexecution.api.ExecutionDescriptor.InputProcessorFactory;
 import org.netbeans.modules.extexecution.api.ExecutionDescriptor;
-import org.netbeans.modules.extexecution.api.ExecutionDescriptorBuilder;
 import org.netbeans.modules.extexecution.api.ExecutionService;
-import org.netbeans.modules.extexecution.api.input.InputProcessor;
 import org.netbeans.modules.extexecution.api.input.InputProcessors;
 import org.netbeans.modules.groovy.grails.api.ExecutionSupport;
 import org.netbeans.modules.groovy.grails.api.GrailsRuntime;
 import org.netbeans.modules.groovy.grailsproject.GrailsProjectSettings;
 import org.netbeans.spi.project.ui.support.ProjectChooser;
-import org.openide.util.Task;
-import org.openide.windows.InputOutput;
-import org.openide.windows.OutputWriter;
-
-
+import org.openide.util.Exceptions;
 
 /**
  *
  * @author schmidtm
  */
-public class NewGrailsProjectWizardIterator implements  WizardDescriptor.InstantiatingIterator,
-                                                        WizardDescriptor.ProgressInstantiatingIterator{
+public class NewGrailsProjectWizardIterator implements WizardDescriptor.InstantiatingIterator,
+        WizardDescriptor.ProgressInstantiatingIterator {
 
     private static final Logger LOGGER = Logger.getLogger(NewGrailsProjectWizardIterator.class.getName());
-
     private transient int index;
     private transient WizardDescriptor.Panel[] panels;
     private transient WizardDescriptor wiz;
-
-    GetProjectLocationStep pls = null;
-    boolean        serverRunning = false;
-    boolean        serverConfigured = true;
+    private GetProjectLocationStep pls = null;
+    boolean serverRunning = false;
+    boolean serverConfigured = true;
     int baseCount;
 
-    private WizardDescriptor.Panel[] createPanels () {
-
+    private WizardDescriptor.Panel[] createPanels() {
         pls = new GetProjectLocationStep(serverRunning, serverConfigured);
-
-        return new WizardDescriptor.Panel[] { pls };
+        return new WizardDescriptor.Panel[]{pls};
     }
 
-     private String[] createSteps() {
-            return new String[] {
-                NbBundle.getMessage(NewGrailsProjectWizardIterator.class,"LAB_ConfigureProject")
-            };
+    private String[] createSteps() {
+        return new String[] {NbBundle.getMessage(NewGrailsProjectWizardIterator.class, "LAB_ConfigureProject")};
     }
 
-   public Set instantiate(ProgressHandle handle) throws IOException {
+    public Set instantiate(final ProgressHandle handle) throws IOException {
         Set<FileObject> resultSet = new HashSet<FileObject>();
 
         serverRunning = true;
@@ -103,15 +92,26 @@ public class NewGrailsProjectWizardIterator implements  WizardDescriptor.Instant
             String displayName = "<new project> (create-app)"; // NOI18N
             Callable<Process> callable = ExecutionSupport.getInstance().createCreateApp(
                     (File) wiz.getProperty("projectFolder")); // NOI18N
-            
-            ExecutionDescriptorBuilder builder = new ExecutionDescriptorBuilder();
-            builder.frontWindow(true).inputVisible(true);
-            builder.outProcessor(InputProcessors.bridge(new ProgressSnooper(handle, 100, 2)));
+
+            ExecutionDescriptor descriptor = new ExecutionDescriptor().frontWindow(true).inputVisible(true);
+            descriptor = descriptor.outProcessorFactory(new InputProcessorFactory() {
+
+                public InputProcessor newInputProcessor() {
+                    return InputProcessors.bridge(new ProgressSnooper(handle, 100, 2));
+                }
+            });
             // TODO refresh
-            
-            ExecutionService service = ExecutionService.newService(callable, builder.create(), displayName);
-            Task task = service.run();
-            task.waitFinished();
+
+            ExecutionService service = ExecutionService.newService(callable, descriptor, displayName);
+            Future<Integer> future = service.run();
+            try {
+                // TODO handle return value
+                future.get();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException ex) {
+                Exceptions.printStackTrace(ex.getCause());
+            }
         } finally {
             handle.progress(100);
         }
@@ -119,52 +119,48 @@ public class NewGrailsProjectWizardIterator implements  WizardDescriptor.Instant
         serverRunning = false;
         File dirF = (File) wiz.getProperty("projectFolder");
 
-       if (dirF != null) {
-           dirF = FileUtil.normalizeFile(dirF);
-           FileObject dir = FileUtil.toFileObject(dirF);
+        if (dirF != null) {
+            dirF = FileUtil.normalizeFile(dirF);
+            FileObject dir = FileUtil.toFileObject(dirF);
 
-           if (dir == null) {
-               LOGGER.warning("Folder was expected, but not found: " + dirF.getCanonicalPath());
-           } else {
-               resultSet.add(dir);
-               GrailsProjectSettings.getDefault().setNewProjectCount(baseCount);
+            if (dir == null) {
+                LOGGER.warning("Folder was expected, but not found: " + dirF.getCanonicalPath());
+            } else {
+                resultSet.add(dir);
+                GrailsProjectSettings.getDefault().setNewProjectCount(baseCount);
 
-               File parentDir = dirF.getParentFile();
+                File parentDir = dirF.getParentFile();
 
-               if (parentDir != null && parentDir.exists() && parentDir.isDirectory()) {
-                   GrailsProjectSettings.getDefault().setLastUsedArtifactFolder(dirF.getParentFile());
-               }
-           }
-           dirF = (dirF != null) ? dirF.getParentFile() : null;
-           if (dirF != null && dirF.exists()) {
-               ProjectChooser.setProjectsFolder(dirF);
-           }
-       }
+                if (parentDir != null && parentDir.exists() && parentDir.isDirectory()) {
+                    GrailsProjectSettings.getDefault().setLastUsedArtifactFolder(dirF.getParentFile());
+                }
+            }
+            dirF = (dirF != null) ? dirF.getParentFile() : null;
+            if (dirF != null && dirF.exists()) {
+                ProjectChooser.setProjectsFolder(dirF);
+            }
+        }
 
-       return resultSet;
+        return resultSet;
     }
 
     public Set instantiate() throws IOException {
-
-            Set<FileObject> resultSet = new HashSet<FileObject>();
-
-            return resultSet;
-
+        Set<FileObject> resultSet = new HashSet<FileObject>();
+        return resultSet;
     }
 
     public void initialize(WizardDescriptor wizard) {
         this.wiz = wizard;
         index = 0;
 
-        if(!GrailsRuntime.getInstance().isConfigured()) {
-            wizard.putProperty("WizardPanel_errorMessage",
+        if (!GrailsRuntime.getInstance().isConfigured()) {
+            wizard.putProperty(WizardDescriptor.PROP_ERROR_MESSAGE,
                     NbBundle.getMessage(NewGrailsProjectWizardIterator.class,
                     "NewGrailsProjectWizardIterator.NoGrailsServerConfigured"));
             serverConfigured = false;
-            }
+        }
 
         // get project counter from GrailsConfiguration
-
         baseCount = GrailsProjectSettings.getDefault().getNewProjectCount() + 1;
         wizard.putProperty("WizardPanel_GrailsProjectCounter", Integer.valueOf(baseCount));
 
@@ -180,48 +176,52 @@ public class NewGrailsProjectWizardIterator implements  WizardDescriptor.Instant
                 steps[i] = c.getName();
             }
             if (c instanceof JComponent) { // assume Swing components
-                JComponent jc = (JComponent)c;
+                JComponent jc = (JComponent) c;
                 // Step #.
-                jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i)); // NOI18N
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, new Integer(i)); // NOI18N
                 // Step name (actually the whole list for reference).
-                jc.putClientProperty("WizardPanel_contentData", steps); // NOI18N
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA, steps); // NOI18N
             }
         }
     }
 
     public void uninitialize(WizardDescriptor wizard) {
-
     }
 
     public Panel current() {
-            return panels[index];
+        return panels[index];
     }
 
     public String name() {
-        return MessageFormat.format (NbBundle.getMessage(NewGrailsProjectWizardIterator.class,"LAB_IteratorName"),
-            new Object[] {Integer.valueOf(index + 1), Integer.valueOf(panels.length)});
+        return MessageFormat.format(NbBundle.getMessage(NewGrailsProjectWizardIterator.class, "LAB_IteratorName"),
+                new Object[]{Integer.valueOf(index + 1), Integer.valueOf(panels.length)});
     }
 
     public boolean hasNext() {
-            return index < panels.length - 1;
+        return index < panels.length - 1;
     }
 
     public boolean hasPrevious() {
-            return index > 0;
+        return index > 0;
     }
 
     public void nextPanel() {
-        if (!hasNext()) throw new NoSuchElementException();
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
         index++;
     }
 
     public void previousPanel() {
-        if (!hasPrevious()) throw new NoSuchElementException();
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
         index--;
     }
 
-    public void addChangeListener(ChangeListener l) {}
+    public void addChangeListener(ChangeListener l) {
+    }
 
-    public void removeChangeListener(ChangeListener l) {}
-
+    public void removeChangeListener(ChangeListener l) {
+    }
 }
