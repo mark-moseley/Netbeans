65627bfbf45f18b8d6ca048b3482914864f3a084
Tomas Zezula <tzezula@netbeans.org>
2008-08-15 08:56:52 +0200

    #137134:Project Run multiple configurations do not get copied properly when the project is copied...

--- java.j2seproject/src/org/netbeans/modules/java/j2seproject/J2SEProjectOperations.java
+++ java.j2seproject/src/org/netbeans/modules/java/j2seproject/J2SEProjectOperations.java
@@ -48,6 +48,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import org.apache.tools.ant.module.api.support.ActionUtils;
+import org.netbeans.api.project.FileOwnerQuery;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectInformation;
 import org.netbeans.api.project.ProjectManager;
@@ -61,6 +62,9 @@ import org.netbeans.spi.project.support.ant.EditableProperties;
 import org.netbeans.spi.project.support.ant.GeneratedFilesHelper;
 import org.netbeans.spi.project.support.ant.PropertyUtils;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 
@@ -70,15 +74,30 @@ import org.openide.util.NbBundle;
  */
 public class J2SEProjectOperations implements DeleteOperationImplementation, CopyOperationImplementation, MoveOperationImplementation {
     
-    private J2SEProject project;
+    private final J2SEProject project;
+    private final J2SEActionProvider actionProvider;
     
     //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
     private String appArgs;
     //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
     private String workDir;
     
-    public J2SEProjectOperations(J2SEProject project) {
+    //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
+    private String libraryPath;
+    //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
+    private File libraryFile;
+    //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
+    private boolean libraryWithinProject;
+    //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
+    private String absolutesRelPath;
+    //RELY: Valid only on original project after the notifyMoving or notifyCopying was called
+    private FileSystem configs;
+    
+    public J2SEProjectOperations(final J2SEProject project, final J2SEActionProvider actionProvider) {
+        assert project != null;
+        assert actionProvider != null;
         this.project = project;
+        this.actionProvider = actionProvider;
     }
     
     private static void addFile(FileObject projectDirectory, String fileName, List<FileObject> result) {
@@ -94,7 +113,7 @@ public class J2SEProjectOperations implements DeleteOperationImplementation, Cop
         List<FileObject> files = new ArrayList<FileObject>();
         
         addFile(projectDirectory, "nbproject", files); // NOI18N
-        addFile(projectDirectory, "build.xml", files); // NOI18N
+        addFile(projectDirectory, J2SEProjectUtil.getBuildXmlName(project), files); // NOI18N
         addFile(projectDirectory, "xml-resources", files); //NOI18N
         addFile(projectDirectory, "catalog.xml", files); //NOI18N
         
@@ -107,17 +126,24 @@ public class J2SEProjectOperations implements DeleteOperationImplementation, Cop
         files.addAll(Arrays.asList(project.getTestSourceRoots().getRoots()));
         addFile(project.getProjectDirectory(), "manifest.mf", files); // NOI18N
         addFile(project.getProjectDirectory(), "master.jnlp", files); // NOI18N
+        // add libraries folder if it is within project:
+        AntProjectHelper helper = project.getAntProjectHelper();
+        if (helper.getLibrariesLocation() != null) {
+            File f = helper.resolveFile(helper.getLibrariesLocation());
+            if (f != null && f.exists()) {
+                FileObject libFolder = FileUtil.toFileObject(f).getParent();
+                if (FileUtil.isParentOf(project.getProjectDirectory(), libFolder)) {
+                    files.add(libFolder);
+                }
+            }
+        }
         return files;
     }
     
-    public void notifyDeleting() throws IOException {
-        J2SEActionProvider ap = project.getLookup().lookup(J2SEActionProvider.class);
-        
-        assert ap != null;
-        
+    public void notifyDeleting() throws IOException {                
         Properties p = new Properties();
-        String[] targetNames = ap.getTargetNames(ActionProvider.COMMAND_CLEAN, Lookup.EMPTY, p);
-        FileObject buildXML = project.getProjectDirectory().getFileObject(GeneratedFilesHelper.BUILD_XML_PATH);
+        String[] targetNames = this.actionProvider.getTargetNames(ActionProvider.COMMAND_CLEAN, Lookup.EMPTY, p);
+        FileObject buildXML = J2SEProjectUtil.getBuildXml(project);
         
         assert targetNames != null;
         assert targetNames.length > 0;
@@ -130,7 +156,9 @@ public class J2SEProjectOperations implements DeleteOperationImplementation, Cop
     }
     
     public void notifyCopying() {
+        rememberLibraryLocation();
         readPrivateProperties();
+        rememberConfigurations();
     }
     
     public void notifyCopied(Project original, File originalPath, String nueName) {
@@ -138,19 +166,23 @@ public class J2SEProjectOperations implements DeleteOperationImplementation, Cop
             //do nothing for the original project.
             return ;
         }
-        fixPrivateProperties(original.getLookup().lookup(J2SEProjectOperations.class));
+        J2SEProjectOperations origOperations = original.getLookup().lookup(J2SEProjectOperations.class);
+        fixLibraryLocation(origOperations);
+        fixPrivateProperties(origOperations);
         fixDistJarProperty (nueName);
-        project.getReferenceHelper().fixReferences(originalPath);
-        
+        project.getReferenceHelper().fixReferences(originalPath);        
         project.setName(nueName);
+        restoreConfigurations(origOperations);
     }
     
     public void notifyMoving() throws IOException {
-        if (!this.project.getUpdateHelper().requestSave()) {
+        if (!this.project.getUpdateHelper().requestUpdate()) {
             throw new IOException (NbBundle.getMessage(J2SEProjectOperations.class,
                 "MSG_OldProjectMetadata"));
         }
-        readPrivateProperties ();        
+        rememberLibraryLocation();
+        readPrivateProperties ();
+        rememberConfigurations();
         notifyDeleting();
     }
             
@@ -159,10 +191,48 @@ public class J2SEProjectOperations implements DeleteOperationImplementation, Cop
             project.getAntProjectHelper().notifyDeleted();
             return ;
         }                
-        fixPrivateProperties (original.getLookup().lookup(J2SEProjectOperations.class));
+        J2SEProjectOperations origOperations = original.getLookup().lookup(J2SEProjectOperations.class);
+        fixLibraryLocation(origOperations);
+        fixPrivateProperties (origOperations);
         fixDistJarProperty (nueName);
         project.setName(nueName);        
 	project.getReferenceHelper().fixReferences(originalPath);
+        restoreConfigurations(origOperations);
+    }
+
+    private void fixLibraryLocation(J2SEProjectOperations original) throws IllegalArgumentException {
+        String libPath = original.libraryPath;
+        if (libPath != null) {
+            if (!new File(libPath).isAbsolute()) {
+                //relative path to libraries
+                if (!original.libraryWithinProject) {
+                    File file = original.libraryFile;
+                    if (file == null) {
+                        // could happen in some rare cases, but in that case the original project was already broken, don't fix.
+                        return;
+                    }
+                    String relativized = PropertyUtils.relativizeFile(FileUtil.toFile(project.getProjectDirectory()), file);
+                    if (relativized != null) {
+                        project.getAntProjectHelper().setLibrariesLocation(relativized);
+                    } else {
+                        //cannot relativize, use absolute path
+                        project.getAntProjectHelper().setLibrariesLocation(file.getAbsolutePath());
+                    }
+                } else {
+                    //got copied over to new location.. the relative path is the same..
+                }
+            } else {
+
+                //absolute path to libraries..
+                if (original.libraryWithinProject) {
+                    if (original.absolutesRelPath != null) {
+                        project.getAntProjectHelper().setLibrariesLocation(PropertyUtils.resolveFile(FileUtil.toFile(project.getProjectDirectory()), original.absolutesRelPath).getAbsolutePath());
+                    }
+                } else {
+                    // absolute path to an external folder stays the same.
+                }
+            }
+        }
     }
     
     private void readPrivateProperties () {
@@ -204,6 +274,74 @@ public class J2SEProjectOperations implements DeleteOperationImplementation, Cop
                 }
             }
         });
+    }
+
+    private void rememberLibraryLocation() {
+        libraryWithinProject = false;
+        absolutesRelPath = null;
+        libraryPath = project.getAntProjectHelper().getLibrariesLocation();
+        if (libraryPath != null) {
+            File prjRoot = FileUtil.toFile(project.getProjectDirectory());
+            libraryFile = PropertyUtils.resolveFile(prjRoot, libraryPath);
+            if (FileOwnerQuery.getOwner(libraryFile.toURI()) == project && 
+                    libraryFile.getAbsolutePath().startsWith(prjRoot.getAbsolutePath())) {
+                //do not update the relative path if within the project..
+                libraryWithinProject = true;
+                FileObject fo = FileUtil.toFileObject(libraryFile);
+                if (new File(libraryPath).isAbsolute() && fo != null) {
+                    // if absolte path within project, it will get moved/copied..
+                    absolutesRelPath = FileUtil.getRelativePath(project.getProjectDirectory(), fo);
+                }
+            }
+        }
+    }
+    
+    private void rememberConfigurations () {
+        FileObject fo = project.getProjectDirectory().getFileObject(J2SEConfigurationProvider.CONFIG_PROPS_PATH);
+        if (fo != null) {
+            //Has configurations
+            try {
+                FileSystem fs = FileUtil.createMemoryFileSystem();
+                FileUtil.copyFile(fo, fs.getRoot(),fo.getName());
+                fo = project.getProjectDirectory().getFileObject("nbproject/private/configs");      //NOI18N
+                if (fo != null && fo.isFolder()) {
+                    FileObject cfgs = fs.getRoot().createFolder("configs");                         //NOI18N
+                    for (FileObject child : fo.getChildren()) {
+                        FileUtil.copyFile(child, cfgs, child.getName());
+                    }
+                }
+                configs = fs;
+            } catch (IOException ioe) {
+                Exceptions.printStackTrace(ioe);
+            }
+        }
+    }
+    
+    private void restoreConfigurations (final J2SEProjectOperations original) {
+        final FileSystem fs = original.configs;
+        original.configs = null;
+        if (fs != null) {
+            try {
+                FileObject fo = fs.getRoot().getFileObject("config.properties");        //NOI18N
+                if (fo != null) {
+                    FileObject privateFolder = FileUtil.createFolder(project.getProjectDirectory(), "nbproject/private");  //NOI18N
+                    if (privateFolder != null) {
+                        FileUtil.copyFile(fo, privateFolder, fo.getName());
+                    }                
+                }
+                fo = fs.getRoot().getFileObject("configs");                             //NOI18N
+                if (fo != null) {
+                    FileObject configsFolder = FileUtil.createFolder(project.getProjectDirectory(), "nbproject/private/configs");  //NOI18N
+                    if (configsFolder != null) {
+                        for (FileObject child : fo.getChildren()) {
+                            FileUtil.copyFile(child, configsFolder, child.getName());
+                        }
+                    }
+                }
+            } catch (IOException ioe) {
+                Exceptions.printStackTrace(ioe);
+            }
+        }
     }
     
 }
