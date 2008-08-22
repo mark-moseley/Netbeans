307afa0dd0528452d116bd8e609a24cc7a116312
S. Aubrecht <saubrecht@netbeans.org>
2008-08-22 10:04:46 +0200

    #143627 - AIOOB

--- openide.loaders/src/org/openide/awt/Toolbar.java
+++ openide.loaders/src/org/openide/awt/Toolbar.java
@@ -408,7 +408,8 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
         }
         
         public void dragGestureRecognized(DragGestureEvent e) {
-            if( !ToolbarPool.getDefault().isInEditMode() )  
+            if( !ToolbarPool.getDefault().isInEditMode()
+                    || "QuickSearch".equals(getName()) )  //HACK (137286)- there's not better way...
                 return;
             try {
                  Component c = e.getComponent();
@@ -447,8 +448,13 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
         }
         
         public void drop(DropTargetDropEvent dtde) {
-            if( validateDropPosition() ) {
-                dtde.dropComplete( handleDrop( dtde.getTransferable() ) );
+            boolean res = false;
+            try {
+                if( validateDropPosition() ) {
+                    res = handleDrop( dtde.getTransferable() );
+                }
+            } finally {
+                dtde.dropComplete(res);
             }
             resetDropGesture();
         }
@@ -581,11 +587,10 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
     private DataObject getDataObjectUnderDropCursor( int dropIndex, boolean dropBefore ) {
         DataObject[] buttons = backingFolder.getChildren();
         DataObject objUnderCursor = null;
-        boolean appendToEnd = false;
         if( buttons.length > 0 ) {
             if( !dropBefore )
                 dropIndex++;
-            if( dropIndex < buttons.length ) {
+            if( dropIndex < buttons.length && dropIndex >= 0 ) {
                 objUnderCursor = buttons[dropIndex];
             }
         }
@@ -787,7 +792,8 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
      * When Toolbar is floatable, ToolbarBump is added as Grip as first toolbar component
      * modified by Michael Wever, to use l&f's grip/bump. */
     void addGrip () {
-        if (floatable) {
+        //HACK (137286)- there's not better way...
+        if (floatable && !"QuickSearch".equals(getName()) ) { //NOI18N
             /** Uses L&F's grip **/
             String lfID = UIManager.getLookAndFeel().getID();
             JPanel dragarea = null;
@@ -840,11 +846,15 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
      * on the contrary to the programmatic name */
     public String getDisplayName () {
         if (displayName == null) {
-            if (!backingFolder.isValid()) {
-                // #17020
-                return backingFolder.getName();
+            if (backingFolder.isValid()) {
+                try {
+                    return backingFolder.getNodeDelegate ().getDisplayName ();
+                } catch (IllegalStateException ex) {
+                    // OK: #141387
+                }
             }
-            return backingFolder.getNodeDelegate ().getDisplayName ();
+            // #17020
+            return backingFolder.getName();
         }
         return displayName;
     }
@@ -853,6 +863,10 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
      * on the contrary to the programmatic name */
     public void setDisplayName (String displayName) {
         this.displayName = displayName;
+    }
+    
+    private static final void setToolTipText (JComponent comp, String text) {
+        comp.setToolTipText(Actions.cutAmpersand(text));
     }
 
     /** Fire drag of Toolbar
@@ -1149,7 +1163,7 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
             int width = WIDTH;
             dim = new Dimension (width, width);
             max = new Dimension (width, Integer.MAX_VALUE);
-            this.setToolTipText (Toolbar.this.getDisplayName());
+            Toolbar.setToolTipText (this, Toolbar.this.getDisplayName());
         }
 
         /** Paint bumps to specific Graphics. */
@@ -1222,7 +1236,7 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
             }
             dim = new Dimension (width, width);
             max = new Dimension (width, Integer.MAX_VALUE);
-            this.setToolTipText (Toolbar.this.getDisplayName());
+            Toolbar.setToolTipText (this, Toolbar.this.getDisplayName());
         }
         
         /** Paint bumps to specific Graphics. */
@@ -1332,7 +1346,7 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
         public ToolbarAqua() {
             dim = new Dimension (WIDTH, WIDTH);
             max = new Dimension (WIDTH, Integer.MAX_VALUE);
-            this.setToolTipText (Toolbar.this.getDisplayName());
+            Toolbar.setToolTipText (this, Toolbar.this.getDisplayName());
         }
         
         @Override
@@ -1414,7 +1428,7 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
         public ToolbarXP() {
             dim = new Dimension (WIDTH, WIDTH);
             max = new Dimension (WIDTH, Integer.MAX_VALUE);
-            this.setToolTipText (Toolbar.this.getDisplayName());
+            Toolbar.setToolTipText (this, Toolbar.this.getDisplayName());
         }
         
         @Override
@@ -1541,7 +1555,7 @@ public class Toolbar extends JToolBar /*implemented by patchsuperclass MouseInpu
             dim = new Dimension (width, width);
             max = new Dimension (width, Integer.MAX_VALUE);
             this.setBorder (new EmptyBorder (VGAP, HGAP, VGAP, HGAP));
-            this.setToolTipText (Toolbar.this.getDisplayName());
+            Toolbar.setToolTipText (this, Toolbar.this.getDisplayName());
         }
 
         /** Paint grip to specific Graphics. */
