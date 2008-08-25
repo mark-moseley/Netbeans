ca61f887148e9dd1540b2ca2b6195246eb94e0df
Tomas Zezula <tzezula@netbeans.org>
2008-08-25 11:58:21 +0200

    #128361:Compilation against jdk 6 broken....

--- jumpto/src/org/netbeans/spi/jumpto/symbol/SymbolProvider.java
+++ jumpto/src/org/netbeans/spi/jumpto/symbol/SymbolProvider.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,65 +31,69 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.spi.jumpto.type;
+package org.netbeans.spi.jumpto.symbol;
 
 import java.util.List;
 import org.netbeans.api.project.Project;
-import org.netbeans.modules.jumpto.type.TypeProviderAccessor;
+import org.netbeans.modules.jumpto.symbol.SymbolProviderAccessor;
+import org.netbeans.spi.jumpto.type.SearchType;
 
 /**
- * A Type Provider participates in the Goto Type dialog by providing TypeDescriptors,
- * one for each matched type, when asked to do so.
  * 
- * The Type Providers are registered in Lookup.
+ * A Symbol Provider participates in the Goto Symbol dialog by providing SymbolDescriptors,
+ * one for each matched symbol, when asked to do so.
  * 
- * @todo Should we return a Collection rather than a List?
+ * The Symbol Providers are registered in Lookup.
+ * @since 1.7
  * 
- * @author Tor Norbye
+ * @author Tomas Zezula
  */
-public interface TypeProvider {
+public interface SymbolProvider {
+    
     /** 
      * Describe this provider with an internal name, in case we want to provide
-     * some kind of programmatic filtering (e.g. a Java EE dialog wanting to include
-     * or omit specific type providers, without relying on class names or 
-     * localized display names)
+     * some kind of programmatic filtering
      * 
-     * @return An internal String uniquely identifying this type provider, such as
+     * @return An internal String uniquely identifying this symbol provider, such as
      *   "java"
      */
     String name();
 
     /** 
      * Describe this provider for the user, in case we want to offer filtering
-     * capabilities in the Go To Type dialog
+     * capabilities in the Go To Symbol dialog
      * 
-     * @return A display name describing the types being provided by this TypeProvider,
-     *  such as "Java Types", "Ruby Types", etc.
+     * @return A display name describing the symbols being provided by this SymbolProvider,
+     *  such as "Java Symbols", "Ruby Symbols", etc.
      */
     String getDisplayName();
     
     /** 
-     * Compute a list of TypeDescriptors that match the given search text for the given
+     * Compute a list of SymbolDescriptors that match the given search text for the given
      * search type. This might be a slow operation, and the infrastructure may end
-     * up calling {@link #cancel} on the same type provider during the operation, in which
+     * up calling {@link #cancel} on the same symbol provider during the operation, in which
      * case the method can return incomplete results. If there is a "current project",
-     * the Go To Type infrastructure will perform the search in two passes; first it
-     * will call {@link #getTypeNames} with the current project, which should be a reasonably
-     * fast search, and display those types first. It will then call the method again
-     * with a null project, which should return all types.
+     * the Go To Symbol infrastructure will perform the search in two passes; first it
+     * will call {@link #getSymbolNames} with the current project, which should be a reasonably
+     * fast search, and display those symbols first. It will then call the method again
+     * with a null project, which should return all symbols.
      * <p>
-     * Note that a useful performance optimization is for the TypeProvider to cache
+     * Note that a useful performance optimization is for the SymbolProvider to cache
      * a few of its most recent search results, and if the next search (e.g. more user
      * keystrokes) is a simple narrowing of the search, just filter the previous search
-     * result. There is an explicit {@link #cleanup} call that the Go To Type dialog
+     * result. There is an explicit {@link #cleanup} call that the Go To Symbol dialog
      * will make at the end of the dialog interaction, which can be used to clean up the cache.
      * 
      * @param context search context containg search text and type, optionally project
-     * @param result  filled with type descriptors and optional message
+     * @param result  filled with symbol descriptors and optional message
      */
-    void computeTypeNames(Context context, Result result);
+    void computeSymbolNames(Context context, Result result);
     
     /**
      * Cancel the current operation, if possible. This might be called if the user
@@ -106,10 +104,10 @@ public interface TypeProvider {
 
 
     /**
-     * The Go To Type dialog is dismissed for now - free up resources if applicable.
-     * (A new "session" will be indicated by a new call to getTypeNames.)
+     * The Go To Symbol dialog is dismissed for now - free up resources if applicable.
+     * (A new "session" will be indicated by a new call to getSymbolNames.)
      * 
-     * This allows the TypeProvider to cache its most recent search result, and if the next
+     * This allows the SymbolProvider to cache its most recent search result, and if the next
      * search is simply a narrower search, it can just filter the previous result.
      */
     void cleanup();
@@ -120,7 +118,6 @@ public interface TypeProvider {
      * Contains search type (such as prefix, regexp), search text and
      * optionally project where to search.
      *
-     * @since 1.5
      */
     public static final class Context extends Object {
         private final Project project;
@@ -128,16 +125,17 @@ public interface TypeProvider {
         private final SearchType type;
         
         static {
-            TypeProviderAccessor.DEFAULT = new TypeProviderAccessor() {
+            SymbolProviderAccessor.DEFAULT = new SymbolProviderAccessor() {
+
                 @Override
                 public Context createContext(Project p, String text, SearchType t) {
                     return new Context(p, text, t);
                 }
 
                 @Override
-                public Result createResult(List<? super TypeDescriptor> result, String[] message) {
+                public Result createResult(List<? super SymbolDescriptor> result, String[] message) {
                     return new Result(result, message);
-                }
+                }            
             };
         }
         
@@ -159,7 +157,7 @@ public interface TypeProvider {
           * Return the text used for search.
           *
           * @return The text used for the search; e.g. when getSearchType() == SearchType.PREFIX,
-          *   text is the prefix that all returned types should start with.
+          *   text is the prefix that all returned symbols should start with.
           */
         public String getText() { return text; }
 
@@ -172,18 +170,17 @@ public interface TypeProvider {
     }
     
     /**
-     * Represents a collection of <tt>TypeDescriptor</tt>s that match 
+     * Represents a collection of <tt>SymbolDescriptor</tt>s that match 
      * the given search criteria. Moreover, it can contain message 
      * for the user, such as an incomplete search result.
      *
-     * @since 1.5
      */
     public static final class Result extends Object {
         
-        private List<? super TypeDescriptor> result;
+        private List<? super SymbolDescriptor> result;
         private String[] message;
 
-        Result(List<? super TypeDescriptor> result, String[] message) {
+        Result(List<? super SymbolDescriptor> result, String[] message) {
             this.result = result;
             this.message = message;
         }
@@ -201,20 +198,20 @@ public interface TypeProvider {
         /**
           * Adds result descriptor.
           *
-          * @param  typeDescriptor  type descriptor to be added to result
+          * @param  symbolDescriptor  symbol descriptor to be added to result
           */
-        public void addResult(TypeDescriptor typeDescriptor) {
-            result.add(typeDescriptor);
+        public void addResult(SymbolDescriptor symbolDescriptor) {
+            result.add(symbolDescriptor);
         }
 
         /**
           * Adds list of result descriptors.
           *
-          * @param  typeDescriptor  type descriptor to be added to result
+          * @param  symbolDescriptor  symbol descriptor to be added to result
           */
         @SuppressWarnings("unchecked")
-        public void addResult(List<? extends TypeDescriptor> typeDescriptor) {
-            ((List)result).addAll(typeDescriptor);  //workaround javac issue http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6507334
+        public void addResult(List<? extends SymbolDescriptor> symbolDescriptor) {
+            ((List)result).addAll(symbolDescriptor);    //workaround javac issue http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6507334 
         }
     }
 
