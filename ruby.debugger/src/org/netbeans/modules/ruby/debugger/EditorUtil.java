f5b9eab22c7534690c0245fc5c81f92a186d796e
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-08-25 13:41:41 +0200

    Prevent editor pop-ups when running unit tests.

--- ruby.debugger/src/org/netbeans/modules/ruby/debugger/EditorUtil.java
+++ ruby.debugger/src/org/netbeans/modules/ruby/debugger/EditorUtil.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -43,57 +43,37 @@ package org.netbeans.modules.ruby.debugger;
 
 import java.awt.EventQueue;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
-import javax.swing.JEditorPane;
-import javax.swing.SwingUtilities;
-import javax.swing.text.Caret;
-import javax.swing.text.StyledDocument;
-import org.openide.cookies.EditorCookie;
+import org.netbeans.spi.debugger.ui.EditorContextDispatcher;
 import org.openide.cookies.LineCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
-import org.openide.nodes.Node;
-import org.openide.text.Annotatable;
 import org.openide.text.Line;
-import org.openide.text.NbDocument;
-import org.openide.windows.TopComponent;
 
-/**
- * @author Martin Krauskopf
- */
 public final class EditorUtil {
-    
-    private static DebuggerAnnotation[] currentLineDA;
-    
-    public static boolean contains(final Object currentLine, final Line line) {
-        if (currentLine == null) return false;
-        final Annotatable[] annotables = (Annotatable[]) currentLine;
-        for (Annotatable ann : annotables) {
-            if (ann.equals(line)) {
-                return true;
-            }
-            if (ann instanceof Line.Part && ((Line.Part) ann).getLine().equals(line)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    private static DebuggerAnnotation createCurrentLineAnnotation(final Annotatable annotable) {
-        String annType = (annotable instanceof Line.Part)
-                ? DebuggerAnnotation.CURRENT_LINE_PART_ANNOTATION_TYPE2
-                : DebuggerAnnotation.CURRENT_LINE_ANNOTATION_TYPE2;
-        return new DebuggerAnnotation(annType, annotable);
-    }
+    /** For unit tests to be able to prevent editor pop-ups. */
+    public static boolean showLines = true;
     
-    public static void removeAnnotation(DebuggerAnnotation[] annotations) {
-        for (DebuggerAnnotation annotation : annotations) {
-            annotation.detach();
-        }
-    }
+    private EditorUtil() {}
+    
+    private static DebuggerAnnotation currentLineDA;
+    
+//    public static boolean contains(final Object currentLine, final Line line) {
+//        if (currentLine == null) return false;
+//        final Annotatable[] annotables = (Annotatable[]) currentLine;
+//        for (Annotatable ann : annotables) {
+//            if (ann.equals(line)) {
+//                return true;
+//            }
+//            if (ann instanceof Line.Part && ((Line.Part) ann).getLine().equals(line)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     /**
      * Make line in the editor current - shows the line and colorizes it
@@ -103,39 +83,16 @@ public final class EditorUtil {
      * @see unmarkCurrent()
      */
     static void markCurrent(final String filePath, final int lineNumber) {
-        markCurrent(getLine(filePath, lineNumber));
+        markCurrent(getLineAnnotable(filePath, lineNumber));
     }
     
-    private static void markCurrent(final Object line) {
+    private static void markCurrent(final Line line) {
         unmarkCurrent();
         if (line == null) {
             return;
         }
-        currentLineDA = createDebuggerAnnotation(line,
-                DebuggerAnnotation.CURRENT_LINE_ANNOTATION_TYPE,
-                DebuggerAnnotation.CURRENT_LINE_PART_ANNOTATION_TYPE);
+        currentLineDA = new DebuggerAnnotation(DebuggerAnnotation.CURRENT_LINE_ANNOTATION_TYPE, line);
         showLine(line, true);
-    }
-    
-    public static DebuggerAnnotation[] createDebuggerAnnotation(final Object line,
-            final String lineAnnotation, final String partAnnotation) {
-        Annotatable[] annotables = (Annotatable[]) line;
-        DebuggerAnnotation[] annotations = new DebuggerAnnotation[annotables.length];
-        
-        // first line with icon in gutter
-        String annType = (partAnnotation != null && annotables[0] instanceof Line.Part) ? partAnnotation : lineAnnotation;
-        annotations[0] = new DebuggerAnnotation(annType, annotables[0]);
-        
-        // other lines
-        for (int i = 1; i < annotables.length; i++) {
-            annotations[i] = createCurrentLineAnnotation(annotables[i]);
-        }
-        return annotations;
-    }
-    
-    public static DebuggerAnnotation[] createDebuggerAnnotation(final Object line,
-            final String lineAnnotation) {
-        return createDebuggerAnnotation(line, lineAnnotation, null);
     }
     
     /**
@@ -144,32 +101,37 @@ public final class EditorUtil {
      */
     static void unmarkCurrent() {
         if (currentLineDA != null) {
-            removeAnnotation(currentLineDA);
+            currentLineDA.detach();
             currentLineDA = null;
         }
     }
     
-    public static Object getLine(final String filePath, final int lineNumber) {
-        Annotatable[] annotables = null;
-        
+    public static Line getLineAnnotable(final String filePath, final int lineNumber) {
+        return getLine(filePath, lineNumber);
+    }
+
+    public static Line getLine(final String filePath, final int lineNumber) {
         if (filePath == null || lineNumber < 0) {
             return null;
         }
-        
+
         File file = new File(filePath);
         FileObject fileObject = FileUtil.toFileObject(FileUtil.normalizeFile(file));
         if (fileObject == null) {
             Util.info("Cannot resolve \"" + filePath + '"');
             return null;
         }
-        
+
         LineCookie lineCookie = getLineCookie(fileObject);
         assert lineCookie != null;
-        Line line = lineCookie.getLineSet().getCurrent(lineNumber);
-        annotables = new Annotatable[] { line };
-        return annotables;
+        try {
+            return lineCookie.getLineSet().getCurrent(lineNumber);
+        } catch (IndexOutOfBoundsException ioobe) {
+            // invalid line number for the document
+            return null;
+        }
     }
-    
+
     public static LineCookie getLineCookie(final FileObject fo) {
         LineCookie result = null;
         try {
@@ -183,27 +145,21 @@ public final class EditorUtil {
         return result;
     }
     
-    public static void showLine(final Object line, final boolean toFront) {
-        if (line == null) {
+    public static void showLine(final Line lineToShow, final boolean toFront) {
+        if (!showLines) {
             return;
         }
-        Annotatable[] a = (Annotatable[]) line;
-        final Line lineToShow;
-        if (a[0] instanceof Line) {
-            lineToShow = ((Line) a[0]);
-        } else if (a[0] instanceof Line.Part) {
-            lineToShow = ((Line.Part) a[0]).getLine();
-        } else {
-            throw new RuntimeException("Cannot cast to neither Line nor Line.Part: " + a[0]); // NOI18N
+        if (lineToShow == null) {
+            return;
         }
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                lineToShow.show(toFront ? Line.SHOW_TOFRONT : Line.SHOW_GOTO);
+                lineToShow.show(Line.ShowOpenType.REUSE, Line.ShowVisibilityType.FRONT);
             }
         });
     }
     
-    public static void showLine(final Object line) {
+    public static void showLine(final Line line) {
         showLine(line, false);
     }
     
@@ -214,67 +170,14 @@ public final class EditorUtil {
      * supported mime-types}. For unsupported ones returns <code>null</code>.
      */
     public static Line getCurrentLine() {
-        Node[] nodes = TopComponent.getRegistry().getCurrentNodes();
-        if (nodes == null) return null;
-        if (nodes.length != 1) return null;
-        Node n = nodes [0];
-        FileObject fo = n.getLookup().lookup(FileObject.class);
-        if (fo == null) {
-            DataObject dobj = n.getLookup().lookup(DataObject.class);
-            if (dobj != null) {
-                fo = dobj.getPrimaryFile();
-            }
-        }
+        FileObject fo = EditorContextDispatcher.getDefault().getCurrentFile();
         if (fo == null) { return null; }
         if (!Util.isRubySource(fo)) {
             return null;
         }
-        LineCookie lineCookie = n.getCookie(LineCookie.class);
-        if (lineCookie == null) return null;
-        EditorCookie editorCookie = n.getCookie(EditorCookie.class);
-        if (editorCookie == null) return null;
-        JEditorPane jEditorPane = getEditorPane(editorCookie);
-        if (jEditorPane == null) return null;
-        StyledDocument document = editorCookie.getDocument();
-        if (document == null) return null;
-        Caret caret = jEditorPane.getCaret();
-        if (caret == null) return null;
-        int lineNumber = NbDocument.findLineNumber(document, caret.getDot());
-        try {
-            Line.Set lineSet = lineCookie.getLineSet();
-            assert lineSet != null : lineCookie;
-            return lineSet.getCurrent(lineNumber);
-        } catch (IndexOutOfBoundsException ex) {
-            return null;
-        }
+        return EditorContextDispatcher.getDefault().getCurrentLine();
     }
     
-    private static JEditorPane getEditorPane_(EditorCookie editorCookie) {
-        JEditorPane[] op = editorCookie.getOpenedPanes();
-        if ((op == null) || (op.length < 1)) return null;
-        return op [0];
-    }
-    
-    private static JEditorPane getEditorPane(final EditorCookie editorCookie) {
-        if (SwingUtilities.isEventDispatchThread()) {
-            return getEditorPane_(editorCookie);
-        } else {
-            final JEditorPane[] ce = new JEditorPane[1];
-            try {
-                EventQueue.invokeAndWait(new Runnable() {
-                    public void run() {
-                        ce[0] = getEditorPane_(editorCookie);
-                    }
-                });
-            } catch (InvocationTargetException ex) {
-                Util.severe(ex);
-            } catch (InterruptedException ex) {
-                Util.severe(ex);
-                Thread.currentThread().interrupt();
-            }
-            return ce[0];
-        }
-    }
     // </editor-fold>
     
 }
--- ruby.debugger/test/unit/src/org/netbeans/modules/ruby/debugger/TestBase.java
+++ ruby.debugger/test/unit/src/org/netbeans/modules/ruby/debugger/TestBase.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -47,6 +47,7 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.Stack;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -59,31 +60,30 @@ import org.netbeans.api.ruby.platform.TestUtil;
 import org.netbeans.junit.MockServices;
 import org.netbeans.modules.ruby.RubyTestBase;
 import org.netbeans.modules.ruby.debugger.breakpoints.RubyBreakpoint;
+import org.netbeans.modules.ruby.debugger.breakpoints.RubyLineBreakpoint;
 import org.netbeans.modules.ruby.debugger.breakpoints.RubyBreakpointManager;
-import org.netbeans.modules.ruby.platform.DebuggerPreferences;
 import org.netbeans.modules.ruby.platform.execution.DirectoryFileLocator;
 import org.netbeans.modules.ruby.platform.execution.ExecutionDescriptor;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.modules.InstalledFileLocator;
 import org.openide.text.Line;
+import org.openide.util.Exceptions;
 import org.openide.util.lookup.Lookups;
 import org.rubyforge.debugcommons.RubyDebugEvent;
 import org.rubyforge.debugcommons.RubyDebugEventListener;
 import org.rubyforge.debugcommons.RubyDebuggerException;
 import org.rubyforge.debugcommons.RubyDebuggerProxy;
 
-/**
- * @author Martin Krauskopf
- */
 public abstract class TestBase extends RubyTestBase {
 
     static {
         RubySession.TEST = true;
+        EditorUtil.showLines = false;
     }
-    
+
     private enum Engine { CLASSIC, RDEBUG_IDE }
-    
+
     protected static boolean watchStepping = false;
     private Stack<Engine> engines;
     private RubyPlatform platform;
@@ -97,15 +97,18 @@ public abstract class TestBase extends RubyTestBase {
             org.rubyforge.debugcommons.Util.LOGGER.addHandler(new TestHandler(getName()));
         }
     }
-    
+
     @Override
     protected void setUp() throws Exception {
+        MockServices.setServices(DialogDisplayerImpl.class, IFL.class);
+        touch(getWorkDir(), "config/Services/org-netbeans-modules-debugger-Settings.properties");
         super.setUp();
         platform = RubyPlatformManager.addPlatform(TestBase.getFile("ruby.executable", true));
+        assertFalse("is native Ruby", platform.isJRuby());
         assertTrue(platform.getInterpreter() + " has RubyGems installed", platform.hasRubyGemsInstalled());
-        String problems = platform.getFastDebuggerProblems();
+        String problems = platform.getFastDebuggerProblemsInHTML();
         assertNull("fast debugger installed: " + problems, problems);
-        
+
         engines = new Stack<Engine>();
         engines.push(Engine.CLASSIC);
         if (isRDebugExecutableCorrectlySet()) {
@@ -114,34 +117,49 @@ public abstract class TestBase extends RubyTestBase {
         doCleanUp();
     }
 
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        doCleanUp();
+    }
+
     private void doCleanUp() {
         for (RubyBreakpoint bp : RubyBreakpointManager.getBreakpoints()) {
-            RubyBreakpointManager.removeBreakpoint(bp);
+            DebuggerManager.getDebuggerManager().addBreakpoint(bp);
         }
         DebuggerManager.getDebuggerManager().finishAllSessions();
     }
-    
+
     protected TestBase(final String name) {
         this(name, false);
     }
-    
-    
+
+
     protected Process startDebugging(final File f) throws RubyDebuggerException, IOException, InterruptedException {
         return startDebugging(f, true);
     }
-    
+
+    protected Process startDebugging(final File toTest, final RubyPlatform platform) throws RubyDebuggerException, IOException, InterruptedException {
+        return startDebugging(toTest, true, platform);
+    }
+
     protected Process startDebugging(final File toTest, final boolean waitForSuspension) throws RubyDebuggerException, IOException, InterruptedException {
-        MockServices.setServices(DialogDisplayerImpl.class, IFL.class);
+        return startDebugging(toTest, waitForSuspension, platform);
+    }
+
+    private Process startDebugging(final File toTest, final boolean waitForSuspension, final RubyPlatform platform) throws RubyDebuggerException, IOException, InterruptedException {
         ExecutionDescriptor desc = new ExecutionDescriptor(platform,
                 toTest.getName(), toTest.getParentFile(), toTest.getAbsolutePath());
         desc.fileLocator(new DirectoryFileLocator(FileUtil.toFileObject(toTest.getParentFile())));
-        Process process = RubyDebugger.startDebugging(desc);
+        RubySession session = RubyDebugger.startDebugging(desc);
+        session.getProxy().startDebugging(RubyBreakpointManager.getBreakpoints());
+        Process process = session.getProxy().getDebugTarged().getProcess();
         if (waitForSuspension) {
             waitForSuspension();
         }
         return process;
     }
-    
+
     private void waitForSuspension() throws InterruptedException {
         RubySession session = Util.getCurrentSession();
         //        while (session.getFrames() == null || session.getFrames().length == 0) {
@@ -149,7 +167,7 @@ public abstract class TestBase extends RubyTestBase {
             Thread.sleep(300);
         }
     }
-    
+
     protected boolean switchToNextEngine() {
         if (engines.isEmpty()) {
             return false;
@@ -157,29 +175,24 @@ public abstract class TestBase extends RubyTestBase {
         Engine engine = engines.pop();
         switch (engine) {
             case CLASSIC:
-                DebuggerPreferences.getInstance().setUseClassicDebugger(platform, true);
+                forceClassicDebugger(true);
                 break;
             case RDEBUG_IDE:
-                switchToRDebugIDE();
+                forceClassicDebugger(false);
                 break;
             default:
                 fail("Unknown engine type: " + engine);
         }
         return true;
     }
-    
+
     protected boolean tryToSwitchToRDebugIDE() {
         assertFalse("JRuby Fast debugger not supported yet", platform.isJRuby());
         boolean available = isRDebugExecutableCorrectlySet();
         if (available) {
-            switchToRDebugIDE();
+            forceClassicDebugger(false);
         }
         return available;
-    }
-    
-    protected void switchToRDebugIDE() {
-        assertFalse("JRuby Fast debugger not supported yet", platform.isJRuby());
-        DebuggerPreferences.getInstance().setUseClassicDebugger(platform, false);
     }
 
     protected void switchToJRuby() {
@@ -202,7 +215,7 @@ public abstract class TestBase extends RubyTestBase {
     protected File createScript(final String[] scriptContent) throws IOException {
         return createScript(scriptContent, "test.rb");
     }
-    
+
     /**
      * Creates script with the given name in the {@link #getWorkDir} with the
      * given content.
@@ -219,12 +232,12 @@ public abstract class TestBase extends RubyTestBase {
         }
         return FileUtil.toFile(script);
     }
-    
-    protected static RubyBreakpoint addBreakpoint(final FileObject fo, final int line) throws RubyDebuggerException {
-        return RubyBreakpointManager.addBreakpoint(createDummyLine(fo, line - 1));
+
+    protected static RubyLineBreakpoint addBreakpoint(final FileObject fo, final int line) throws RubyDebuggerException {
+        return RubyBreakpointManager.addLineBreakpoint(createDummyLine(fo, line - 1));
     }
-    
-    static void doAction(final Object action) throws InterruptedException {
+
+    public static void doAction(final Object action) throws InterruptedException {
         if (watchStepping) {
             Thread.sleep(3000);
         }
@@ -233,7 +246,7 @@ public abstract class TestBase extends RubyTestBase {
         ActionsManager actionManager = engine.getActionsManager();
         actionManager.doAction(action);
     }
-    
+
     protected void doContinue() throws InterruptedException {
         waitForEvents(Util.getCurrentSession().getProxy(), 1, new Runnable() {
             public void run() {
@@ -245,12 +258,31 @@ public abstract class TestBase extends RubyTestBase {
             }
         });
     }
-    
+
+    protected void waitFor(final Process p) throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    p.waitFor();
+                    latch.countDown();
+                } catch (InterruptedException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }).start();
+        latch.await(10, TimeUnit.SECONDS);
+        if (latch.getCount() > 0) {
+            fail("Process " + p + " was not finished.");
+            p.destroy();
+        }
+    }
+
     protected void waitForEvents(RubyDebuggerProxy proxy, int n, Runnable block) throws InterruptedException {
         final CountDownLatch events = new CountDownLatch(n);
         RubyDebugEventListener listener = new RubyDebugEventListener() {
             public void onDebugEvent(RubyDebugEvent e) {
-                Util.finest("Received event: " + e);
+                Util.finer("Received event: " + e);
                 events.countDown();
             }
         };
@@ -259,7 +291,7 @@ public abstract class TestBase extends RubyTestBase {
         events.await();
         proxy.removeRubyDebugEventListener(listener);
     }
-    
+
     @SuppressWarnings("deprecation")
     static Line createDummyLine(final FileObject fo, final int editorLineNum) {
         return new Line(Lookups.singleton(fo)) {
@@ -273,8 +305,13 @@ public abstract class TestBase extends RubyTestBase {
             public void unmarkCurrentLine() { throw new UnsupportedOperationException("Not supported."); }
         };
     }
-    
+
+    protected void forceClassicDebugger(boolean force) {
+        RubyDebugger.FORCE_CLASSIC = force;
+    }
+
     public static final class IFL extends InstalledFileLocator {
+
         public IFL() {}
         @Override
         public File locate(String relativePath, String codeNameBase, boolean localized) {
@@ -283,41 +320,41 @@ public abstract class TestBase extends RubyTestBase {
                 File cd = new File(rubydebugDir, "classic-debug.rb");
                 assertTrue("classic-debug found in " + rubydebugDir, cd.isFile());
                 return cd;
-            } else if (relativePath.equals("jruby-1.1RC1")) {
+            } else if (relativePath.equals("jruby-1.1.3")) {
                 return TestUtil.getXTestJRubyHome();
             } else {
                 return null;
             }
         }
     }
-    
+
     private static File resolveFile(final String property, final boolean mandatory) {
         String path = System.getProperty(property);
         assertTrue("must set " + property, !mandatory || (path != null));
         return path == null ? null : new File(path);
     }
-    
+
     static File getFile(final String property, boolean mandatory) {
         File file = resolveFile(property, mandatory);
         assertTrue(file + " is file", !mandatory || file.isFile());
         return file;
-        
+
     }
-    
+
     static File getDirectory(final String property, final boolean mandatory) {
         File directory = resolveFile(property, mandatory);
         assertTrue(directory + " is directory", !mandatory || directory.isDirectory());
         return directory;
     }
-    
+
     private static class TestHandler extends Handler {
-        
+
         private final String name;
-        
+
         TestHandler(final String name) {
             this.name = name;
         }
-        
+
         public void publish(LogRecord rec) {
             PrintStream os = rec.getLevel().intValue() >= Level.WARNING.intValue() ? System.err : System.out;
             os.println("[" + System.currentTimeMillis() + "::" + name + "::" + rec.getLevel().getName() + "]: " + rec.getMessage());
@@ -326,14 +363,14 @@ public abstract class TestBase extends RubyTestBase {
                 th.printStackTrace(os);
             }
         }
-        
+
         public void flush() {
             throw new UnsupportedOperationException("Not supported yet.");
         }
-        
+
         public void close() throws SecurityException {
             throw new UnsupportedOperationException("Not supported yet.");
         }
     }
-    
+
 }
