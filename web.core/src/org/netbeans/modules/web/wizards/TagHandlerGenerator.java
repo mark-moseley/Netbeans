7c6b218de2b4bd48c04072cafef56729e886d88b
pslechta@netbeans.org <none@none>
2008-08-25 11:18:33 +0200

    144091 Can't change Body Content setting if no TLD file

--- web.core/src/org/netbeans/modules/web/wizards/TagHandlerGenerator.java
+++ web.core/src/org/netbeans/modules/web/wizards/TagHandlerGenerator.java
@@ -43,24 +43,20 @@ package org.netbeans.modules.web.wizards;
 
 import java.io.IOException;
 import java.util.Collections;
-
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
-
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.TypeParameterTree;
 import com.sun.source.tree.VariableTree;
-
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.JavaSource.Phase;
 import org.netbeans.api.java.source.ModificationResult;
 import org.netbeans.api.java.source.Task;
 import org.netbeans.api.java.source.TreeMaker;
 import org.netbeans.api.java.source.WorkingCopy;
-
 import org.netbeans.modules.j2ee.core.api.support.java.GenerationUtils;
 import org.netbeans.modules.j2ee.core.api.support.java.SourceUtils;
 
@@ -85,7 +81,7 @@ public class TagHandlerGenerator {
     }
     
     public void generate() throws IOException {
-        Task task = new Task<WorkingCopy>() {
+        Task<WorkingCopy> task = new Task<WorkingCopy>() {
             public void run(WorkingCopy workingCopy) throws Exception {
                 workingCopy.toPhase(Phase.RESOLVED);
                 TreeMaker make = workingCopy.getTreeMaker();
@@ -128,11 +124,10 @@ public class TagHandlerGenerator {
 
     private MethodTree addBodyEvaluatorCheck(boolean evaluateBody, TreeMaker make) throws IOException {        
         StringBuffer methodBody = new StringBuffer();
-        methodBody.append("{\n//"); //NOI18N
-        methodBody.append("\n// TODO: code that determines whether the body should be"); //NOI18N
-        methodBody.append("\n//       evaluated should be placed here."); //NOI18N
-        methodBody.append("\n//       Called from the doStartTag() method."); //NOI18N
-        methodBody.append("\n//"); //NOI18N
+        methodBody.append("{"); //NOI18N
+        methodBody.append("\n        // TODO: code that determines whether the body should be"); //NOI18N
+        methodBody.append("\n        //       evaluated should be placed here."); //NOI18N
+        methodBody.append("\n        //       Called from the doStartTag() method."); //NOI18N
         methodBody.append("\nreturn " + (evaluateBody ? "true;" : "false;")); //NOI18N
         methodBody.append("\n}"); //NOI18N
         
