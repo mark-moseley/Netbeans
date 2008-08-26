6baa94b28bfe9f556e603fb0dd96800793a45f77
Vladimir Voskresensky <vv159170@netbeans.org>
2008-08-26 18:53:02 +0400

    fixing IZ#144869: pair members are not resolved when accessed via iterator

---
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/TemplateInstantiationTestCase/instantiation.cc
@@ -0,0 +1,16 @@
+#include "instantiation.h"
+
+typedef MYmap<A, B> mymap;
+mymap freq;
+mymap::iterator iter;
+
+void main() {
+   freq.key_BAD().foo();
+   freq.tp_BAD().boo();
+   freq.td_key_BAD().foo();
+   freq.td_pair_BAD().getKey().foo();
+   freq.td_pair_BAD().getValue().boo();
+   iter->key_OK();
+   MyVector<A>::iterator i;
+   i->foo();
+}
