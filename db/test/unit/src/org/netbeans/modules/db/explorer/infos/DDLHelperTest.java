759d71b1603d8289e89b0951df7b62b5cb7f743d
David Van Couvering <davidvc@netbeans.org>
2008-08-28 07:56:11 -0700

    Failing tests when running with Derby.  Still a few failures to be tracked down.

--- db/test/unit/src/org/netbeans/modules/db/explorer/infos/DDLHelperTest.java
+++ db/test/unit/src/org/netbeans/modules/db/explorer/infos/DDLHelperTest.java
@@ -28,13 +28,13 @@
 package org.netbeans.modules.db.explorer.infos;
 
 import java.sql.Types;
-import org.netbeans.modules.db.util.DBTestBase;
+import org.netbeans.modules.db.test.DDLTestBase;
 import org.netbeans.modules.db.util.InfoHelper;
 
 /**
  * @author <href="mailto:david@vancouvering.com">David Van Couvering</href>
  */
-public class DDLHelperTest extends DBTestBase {
+public class DDLHelperTest extends DDLTestBase {
     InfoHelper helper;
 
     public DDLHelperTest(String name) {
@@ -52,7 +52,7 @@ public class DDLHelperTest extends DBTestBase {
         // Create an index
         createSimpleIndex(tablename, indexname, colname);
         
-        DDLHelper.deleteIndex(spec, SCHEMA, 
+        DDLHelper.deleteIndex(getSpecification(), getSchema(),
                 fixIdentifier(tablename), 
                 fixIdentifier(indexname));
         
@@ -65,7 +65,7 @@ public class DDLHelperTest extends DBTestBase {
         createBasicTable(tablename, "id");
         assertTrue(tableExists(tablename));
         
-        DDLHelper.deleteTable(spec, SCHEMA, fixIdentifier(tablename));
+        DDLHelper.deleteTable(getSpecification(), getSchema(), fixIdentifier(tablename));
         
         assertFalse(tableExists(tablename));
     }
@@ -79,7 +79,7 @@ public class DDLHelperTest extends DBTestBase {
         createView(viewname, "SELECT * FROM " + tablename);
         assertTrue(viewExists(viewname));
         
-        DDLHelper.deleteView(spec, SCHEMA, fixIdentifier(viewname));
+        DDLHelper.deleteView(getSpecification(), getSchema(), fixIdentifier(viewname));
         
         assertFalse(viewExists(viewname));
     }
