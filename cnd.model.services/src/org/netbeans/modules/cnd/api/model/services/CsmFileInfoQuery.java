fccbb4a91576f701c056799f47b2816d2996494b
Alexander Simon <alexvsimon@netbeans.org>
2008-08-29 15:29:57 +0400

    noIZ: implement new method in file info query: list include directives from source to header

--- cnd.model.services/src/org/netbeans/modules/cnd/api/model/services/CsmFileInfoQuery.java
+++ cnd.model.services/src/org/netbeans/modules/cnd/api/model/services/CsmFileInfoQuery.java
@@ -44,8 +44,10 @@ package org.netbeans.modules.cnd.api.model.services;
 import java.util.Collections;
 import java.util.List;
 import org.netbeans.modules.cnd.api.model.CsmFile;
+import org.netbeans.modules.cnd.api.model.CsmInclude;
 import org.netbeans.modules.cnd.api.model.CsmOffsetable;
 import org.netbeans.modules.cnd.api.model.xref.CsmReference;
+import org.netbeans.modules.cnd.api.project.NativeFileItem;
 import org.openide.util.Lookup;
 
 /**
@@ -96,6 +98,23 @@ public abstract class CsmFileInfoQuery {
      */
     public abstract List<CsmReference> getMacroUsages(CsmFile file);
 
+    /**
+     * @return dwarf block offset or null if there are no dwarf blocks in file
+     */
+    public abstract CsmOffsetable getGuardOffset(CsmFile file);
+
+    /**
+     * @return native file item associated with model file
+     */
+    public abstract NativeFileItem getNativeFileItem(CsmFile file);
+    
+    /**
+     * 
+     * @param file header file (for sourse file result is empty list)
+     * @return list of include directives from source file to header file
+     */
+    public abstract List<CsmInclude> getIncludeStack(CsmFile file);
+
     //
     // Implementation of the default query
     //
@@ -117,6 +136,20 @@ public abstract class CsmFileInfoQuery {
 
         public List<CsmReference> getMacroUsages(CsmFile file) {
             return Collections.<CsmReference>emptyList();
+        }
+
+        public CsmOffsetable getGuardOffset(CsmFile file) {
+            return null;
+        }
+
+        @Override
+        public NativeFileItem getNativeFileItem(CsmFile file) {
+            return null;
+        }
+
+        @Override
+        public List<CsmInclude> getIncludeStack(CsmFile file) {
+            return Collections.<CsmInclude>emptyList();
         }
     } 
 }
