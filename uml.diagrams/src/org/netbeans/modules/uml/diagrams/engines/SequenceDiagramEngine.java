ff8cfe7ac4edb1a8f5d53360f79c601c06e04a90
tspiva@netbeans.org <none@none>
2008-08-29 16:26:36 -0600

    Issue 145627 - clicking on empty diagram area does not deselect element selection locked using Ctrl+Shift+Plus

--- uml.diagrams/src/org/netbeans/modules/uml/diagrams/engines/SequenceDiagramEngine.java
+++ uml.diagrams/src/org/netbeans/modules/uml/diagrams/engines/SequenceDiagramEngine.java
@@ -435,6 +435,7 @@ public class SequenceDiagramEngine extends DiagramEngine implements SQDDiagramEn
         //
         WidgetAction.Chain selectTool = widget.createActions(DesignerTools.SELECT);
         
+        selectTool.addAction(DiagramEngine.lockSelectionAction);
         selectTool.addAction(mouseHoverAction);
         selectTool.addAction(sceneSelectAction);
         selectTool.addAction(ActionFactory.createPopupMenuAction(menuProvider));
@@ -486,6 +487,7 @@ public class SequenceDiagramEngine extends DiagramEngine implements SQDDiagramEn
             edgeKind=getMessageKindAsString(msg.getKind());
         }
                 
+        selectTool.addAction(DiagramEngine.lockSelectionAction);
         selectTool.addAction(sceneSelectAction);
         selectTool.addAction(ActionFactory.createPopupMenuAction(menuProvider));
         if("Synchronous".equals(edgeKind) || "Asynchronous".equals(edgeKind))selectTool.addAction(ActionFactory.createReconnectAction(new MessageReconnectDecorator(widget),new MessagesReconnectProvider()));//only these messages was possible to reconnect in 6.0
