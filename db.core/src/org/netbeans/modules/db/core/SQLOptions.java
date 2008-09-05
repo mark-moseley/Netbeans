13237283a242beff19b60f49be6d82fb1288af8e
David Van Couvering <davidvc@netbeans.org>
2008-09-04 17:22:28 -0700

    #144829 - Add toggle button to control whether old tabs are removed on SQL execution

--- db.core/src/org/netbeans/modules/db/core/SQLOptions.java
+++ db.core/src/org/netbeans/modules/db/core/SQLOptions.java
@@ -41,6 +41,8 @@
 
 package org.netbeans.modules.db.core;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.prefs.Preferences;
 import org.openide.util.NbBundle;
 import org.openide.util.NbPreferences;
@@ -53,6 +55,10 @@ public class SQLOptions  {
     private static SQLOptions INSTANCE = new SQLOptions();
     private static final String PROP_FETCH_STEP = "fetchStep"; // NOI18N
     private static final int DEFAULT_FETCH_STEP = 200;
+    private static final String PROP_MAX_ROWS = "maxRows";
+    private static final int DEFAULT_MAX_ROWS = 200000;
+
+    public static final String PROP_KEEP_OLD_TABS = "keepOldResultTabs"; // NOI18N
 
     public static SQLOptions getDefault() {
         return INSTANCE;
@@ -72,5 +78,21 @@ public class SQLOptions  {
     
     public void setFetchStep(int value) {
         getPreferences().putInt(PROP_FETCH_STEP, value);
-    }    
+    }   
+    
+    public int getMaxRows() {
+        return getPreferences().getInt(PROP_MAX_ROWS, DEFAULT_MAX_ROWS);
+    }
+    
+    public void setMaxRows(int rows) {
+        getPreferences().putInt(PROP_MAX_ROWS, rows);
+    }
+    public boolean isKeepOldResultTabs() {
+        return getPreferences().getBoolean(PROP_KEEP_OLD_TABS, false);
+    }
+
+    public void setKeepOldResultTabs(boolean keepOldTabs) {
+        getPreferences().putBoolean(PROP_KEEP_OLD_TABS, keepOldTabs);
+    }
+
 }
