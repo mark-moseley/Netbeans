3268b35b9eebd6bc059e3f8b22a74b0d3dab9ecd
Alexey Vladykin <alexey_vladykin@netbeans.org>
2008-09-08 19:47:32 +0400

    Fixed IZ#146464: IDE can't find 'wchar_t' identifier in C projects

--- cnd.apt/src/org/netbeans/modules/cnd/apt/impl/support/lang/APTStdCFilter.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/impl/support/lang/APTStdCFilter.java
@@ -75,11 +75,12 @@ public class APTStdCFilter extends APTBaseLanguageFilter {
         filter("const_cast", APTTokenTypes.LITERAL_const_cast); // NOI18N
         filter("volatile", APTTokenTypes.LITERAL_volatile); // NOI18N
         filter("char", APTTokenTypes.LITERAL_char); // NOI18N
-        filter("wchar_t", APTTokenTypes.LITERAL_wchar_t); // NOI18N
         filter("bool", APTTokenTypes.LITERAL_bool); // NOI18N
         filter("short", APTTokenTypes.LITERAL_short); // NOI18N
         filter("int", APTTokenTypes.LITERAL_int); // NOI18N
         filter("long", APTTokenTypes.LITERAL_long); // NOI18N
+        filter("signed", APTTokenTypes.LITERAL_signed); // NOI18N
+        filter("__signed__", APTTokenTypes.LITERAL___signed__); // NOI18N
         filter("unsigned", APTTokenTypes.LITERAL_unsigned); // NOI18N
         filter("__unsigned__", APTTokenTypes.LITERAL___unsigned__); // NOI18N
         filter("float", APTTokenTypes.LITERAL_float); // NOI18N
@@ -89,8 +90,6 @@ public class APTStdCFilter extends APTBaseLanguageFilter {
         filter("__declspec", APTTokenTypes.LITERAL___declspec); // NOI18N   
         filter("struct", APTTokenTypes.LITERAL_struct); // NOI18N
         filter("union", APTTokenTypes.LITERAL_union); // NOI18N   
-        filter("true", APTTokenTypes.LITERAL_true); // NOI18N
-        filter("false", APTTokenTypes.LITERAL_false); // NOI18N        
         filter("case", APTTokenTypes.LITERAL_case); // NOI18N
         filter("default", APTTokenTypes.LITERAL_default); // NOI18N
         filter("if", APTTokenTypes.LITERAL_if); // NOI18N
@@ -121,6 +120,6 @@ public class APTStdCFilter extends APTBaseLanguageFilter {
         filter("__w64", APTTokenTypes.LITERAL___w64); // NOI18N
         filter("__extension__", APTTokenTypes.LITERAL___extension__); // NOI18N
         filter("__restrict", APTTokenTypes.LITERAL___restrict); // NOI18N    
-        
+        filter("_Complex", APTTokenTypes.LITERAL__Complex); // NOI18N
     }    
 }
