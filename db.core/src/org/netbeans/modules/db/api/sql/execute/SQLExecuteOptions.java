4e18c6abb91ceeac42f83ed4240532c39eeea9c4
Andrei Badea <abadea@netbeans.org>
2008-09-08 14:07:51 +0200

    #144829: Don't open new dataview tab for every query
    --HG--
    rename : db.sql.editor/src/org/netbeans/modules/db/sql/editor/resources/toggleremovetabs.png => db.sql.editor/src/org/netbeans/modules/db/sql/editor/resources/keepoldresulttabs.png

--- db.core/src/org/netbeans/modules/db/api/sql/execute/SQLExecuteOptions.java
+++ db.core/src/org/netbeans/modules/db/api/sql/execute/SQLExecuteOptions.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,60 +31,52 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.db.core;
+package org.netbeans.modules.db.api.sql.execute;
 
-import java.util.prefs.Preferences;
-import org.openide.util.NbBundle;
-import org.openide.util.NbPreferences;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import org.netbeans.modules.db.core.SQLOptions;
 
 /**
+ * Configurable options for SQL execution
  *
- * @author Andrei Badea
+ * @author David Van Couvering
  */
-public class SQLOptions  {
-    private static SQLOptions INSTANCE = new SQLOptions();
-    private static final String PROP_FETCH_STEP = "fetchStep"; // NOI18N
-    private static final int DEFAULT_FETCH_STEP = 200;
-    private static final String PROP_MAX_ROWS = "maxRows";
-    private static final int DEFAULT_MAX_ROWS = 200000;
+public class SQLExecuteOptions {
 
-    public static final String PROP_KEEP_OLD_RESULT_TABS = "keepOldResultTabs"; // NOI18N
+    private static final SQLExecuteOptions DEFAULT = new SQLExecuteOptions();
 
-    public static SQLOptions getDefault() {
-        return INSTANCE;
+    public static final String PROP_KEEP_OLD_TABS = SQLOptions.PROP_KEEP_OLD_RESULT_TABS;
+
+    private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+    public static SQLExecuteOptions getDefault() {
+        return DEFAULT;
     }
-    
-    public String displayName() {
-        return NbBundle.getMessage(SQLOptions.class, "LBL_SQLOptions");
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        pcs.addPropertyChangeListener(listener);
     }
-    
-    private static Preferences getPreferences() {
-        return NbPreferences.forModule(SQLOptions.class);
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        pcs.removePropertyChangeListener(listener);
     }
-        
-    public int getFetchStep() {
-        return getPreferences().getInt(PROP_FETCH_STEP, DEFAULT_FETCH_STEP);
-    }
-    
-    public void setFetchStep(int value) {
-        getPreferences().putInt(PROP_FETCH_STEP, value);
-    }   
-    
-    public int getMaxRows() {
-        return getPreferences().getInt(PROP_MAX_ROWS, DEFAULT_MAX_ROWS);
-    }
-    
-    public void setMaxRows(int rows) {
-        getPreferences().putInt(PROP_MAX_ROWS, rows);
-    }
+
     public boolean isKeepOldResultTabs() {
-        return getPreferences().getBoolean(PROP_KEEP_OLD_RESULT_TABS, false);
+        return SQLOptions.getDefault().isKeepOldResultTabs();
     }
 
-    public void setKeepOldResultTabs(boolean keepOldTabs) {
-        getPreferences().putBoolean(PROP_KEEP_OLD_RESULT_TABS, keepOldTabs);
+    public void setKeepOldResultTabs(boolean keepOldResultTabs) {
+        boolean oldKeepOldResultTabs = SQLOptions.getDefault().isKeepOldResultTabs();
+        if (oldKeepOldResultTabs != keepOldResultTabs) {
+            SQLOptions.getDefault().setKeepOldResultTabs(keepOldResultTabs);
+            pcs.firePropertyChange(PROP_KEEP_OLD_TABS, oldKeepOldResultTabs, keepOldResultTabs);
+        }
     }
-
 }
