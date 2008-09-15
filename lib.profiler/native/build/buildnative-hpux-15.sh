8e6551c246c0cceaaceca661b692868bc5817787
Tomas Hurka <thurka@netbeans.org>
2008-09-15 10:13:44 +0200

    execute flag added

--- lib.profiler/native/build/buildnative-hpux-15.sh
+++ lib.profiler/native/build/buildnative-hpux-15.sh
@@ -4,9 +4,9 @@ BuildForJDK()
 {
         JAVA_HOME=$1
         JDK_ID=$2
-        echo $JAVA_HOME $JDK_ID
-	gcc32 -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -DLINUX -pthread -fPIC -shared -O3 -Wall -m64  \
-	-o ../../release/lib/deployed/$JDK_ID/linux-amd64/libprofilerinterface.so \
+	echo $JAVA_HOME $JDK_ID
+	cc -I$JAVA_HOME/include -I$JAVA_HOME/include/hp-ux -Ae +u4 +z -D_HPUX -mt -O3 -Wall \
+	-b -o ../../release/lib/deployed/$JDK_ID/hpux-pa_risc2.0/libprofilerinterface.sl \
 	../src-jdk15/class_file_cache.c \
 	../src-jdk15/attach.c \
 	../src-jdk15/Classes.c \
@@ -17,7 +17,7 @@ BuildForJDK()
 	../src-jdk15/Stacks.c \
 	../src-jdk15/common_functions.c
 
-        rm -f *.o
+	rm -f *.o
 }
 
 BuildForJDK "$JAVA_HOME_15" "jdk15"
