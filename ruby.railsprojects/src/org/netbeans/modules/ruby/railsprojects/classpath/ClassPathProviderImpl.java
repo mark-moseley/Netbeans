1ead26c57bdc58831203915360f475187ca77cfd
Pete Lebet <plebet@netbeans.org>
2008-09-19 09:12:34 -0400

    Issue 139651 - Undesirable behavior of JS libraries in Rails project

--- ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/classpath/ClassPathProviderImpl.java
+++ ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/classpath/ClassPathProviderImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -45,15 +45,15 @@ import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.util.Map;
 import java.util.HashMap;
-import org.netbeans.api.gsfpath.classpath.ClassPath;
+import org.netbeans.modules.gsfpath.api.classpath.ClassPath;
 import org.netbeans.api.project.SourceGroup;
 import org.netbeans.modules.ruby.railsprojects.SourceRoots;
 import org.netbeans.modules.ruby.railsprojects.classpath.SourcePathImplementation;
 import org.netbeans.modules.ruby.spi.project.support.rake.PropertyEvaluator;
-import org.netbeans.spi.gsfpath.classpath.ClassPathFactory;
-import org.netbeans.spi.gsfpath.classpath.ClassPathProvider;
+import org.netbeans.modules.gsfpath.spi.classpath.ClassPathFactory;
+import org.netbeans.modules.gsfpath.spi.classpath.ClassPathProvider;
 import org.netbeans.modules.ruby.spi.project.support.rake.RakeProjectHelper;
-import org.netbeans.spi.gsfpath.classpath.support.ClassPathSupport;
+import org.netbeans.modules.gsfpath.spi.classpath.support.ClassPathSupport;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.WeakListeners;
@@ -78,7 +78,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     private final PropertyEvaluator evaluator;
     private final SourceRoots sourceRoots;
     private final SourceRoots testSourceRoots;
-    private final ClassPath[] cache = new ClassPath[8];
+    private final ClassPath[] cache = new ClassPath[9];
 
     private final Map<String,FileObject> dirCache = new HashMap<String,FileObject>();
 
@@ -94,7 +94,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     }
 
     private synchronized FileObject getDir(String propname) {
-        FileObject fo = (FileObject) this.dirCache.get (propname);
+        FileObject fo = this.dirCache.get(propname);
         if (fo == null ||  !fo.isValid()) {
             String prop = evaluator.getProperty(propname);
             if (prop != null) {
@@ -108,6 +108,30 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     
     private FileObject[] getPrimarySrcPath() {
         return this.sourceRoots.getRoots();
+    }
+
+    private ClassPath getPublicWebClassPath() {
+        ClassPath cp = cache[8];
+        if (cp == null) {
+            cp = ClassPathFactory.createClassPath(new PublicClassPathImplementation(projectDirectory));
+            cache[8] = cp;
+            
+            return cp;
+        } else {
+            return cp;
+        }
+    }
+ 
+    private ClassPath getJavascriptsWebClassPath() {
+        ClassPath cp = cache[8];
+        if (cp == null) {
+            cp = ClassPathFactory.createClassPath(new JavascriptsClassPathImplementation(projectDirectory));
+            cache[8] = cp;
+            
+            return cp;
+        } else {
+            return cp;
+        }
     }
     
     private FileObject[] getTestSrcDir() {
@@ -143,98 +167,8 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         // separate test roots etc.
         // TODO - check if files are even within the project?
         return 0;
-//        FileObject[] srcPath = getPrimarySrcPath();
-//        for (int i=0; i < srcPath.length; i++) {
-//            FileObject root = srcPath[i];
-//            if (root.equals(file) || FileUtil.isParentOf(root, file)) {
-//                return 0;
-//            }
-//        }        
-//        srcPath = getTestSrcDir();
-//        for (int i=0; i< srcPath.length; i++) {
-//            FileObject root = srcPath[i];
-//            if (root.equals(file) || FileUtil.isParentOf(root, file)) {
-//                return 1;
-//            }
-//        }
-//        FileObject dir = getBuildClassesDir();
-//        if (dir != null && (dir.equals(file) || FileUtil.isParentOf(dir, file))) {
-//            return 2;
-//        }
-//        dir = getDistJar(); // not really a dir at all, of course
-//        if (dir != null && dir.equals(FileUtil.getArchiveFile(file))) {
-//            // XXX check whether this is really the root
-//            return 4;
-//        }
-//        dir = getBuildTestClassesDir();
-//        if (dir != null && (dir.equals(file) || FileUtil.isParentOf(dir,file))) {
-//            return 3;
-//        }
-//        return -1;
     }
     
-//    private synchronized ClassPath getCompileTimeClasspath(FileObject file) {
-//        int type = getType(file);
-//        return this.getCompileTimeClasspath(type);
-//    }
-//    
-//    private ClassPath getCompileTimeClasspath(int type) {        
-//        if (type < 0 || type > 1) {
-//            // Not a source file.
-//            return null;
-//        }
-//        ClassPath cp = cache[2+type];
-//        if ( cp == null) {            
-//            if (type == 0) {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {JAVAC_CLASSPATH})); // NOI18N
-//            }
-//            else {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {JAVAC_TEST_CLASSPATH})); // NOI18N
-//            }
-//            cache[2+type] = cp;
-//        }
-//        return cp;
-//    }
-//    
-//    private synchronized ClassPath getRunTimeClasspath(FileObject file) {
-//        int type = getType(file);
-//        if (type < 0 || type > 4) {
-//            // Unregistered file, or in a JAR.
-//            // For jar:file:$projdir/dist/*.jar!/**/*.class, it is misleading to use
-//            // run.classpath since that does not actually contain the file!
-//            // (It contains file:$projdir/build/classes/ instead.)
-//            return null;
-//        } else if (type > 1) {
-//            type-=2;            //Compiled source transform into source
-//        }
-//        ClassPath cp = cache[4+type];
-//        if ( cp == null) {
-//            if (type == 0) {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {RUN_CLASSPATH})); // NOI18N
-//            }
-//            else if (type == 1) {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {RUN_TEST_CLASSPATH})); // NOI18N
-//            }
-//            else if (type == 2) {
-//                //Only to make the CompiledDataNode hapy
-//                //Todo: Strictly it should return ${run.classpath} - ${build.classes.dir} + ${dist.jar}
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {DIST_JAR})); // NOI18N
-//            }
-//            cache[4+type] = cp;
-//        }
-//        return cp;
-//    }
-//    
     private synchronized ClassPath getSourcepath(FileObject file) {
         int type = getType(file);
         return this.getSourcepath(type);
@@ -251,7 +185,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
                     cp = ClassPathFactory.createClassPath(new SourcePathImplementation (this.sourceRoots, helper, evaluator));
                     break;
                 case 1:
-                    cp = ClassPathFactory.createClassPath(new SourcePathImplementation (this.testSourceRoots));
+                    cp = ClassPathFactory.createClassPath(new SourcePathImplementation(this.testSourceRoots));
                     break;
             }
         }
@@ -261,7 +195,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     
     private synchronized ClassPath getBootClassPath() {
         ClassPath cp = cache[7];
-        if ( cp== null ) {
+        if (cp == null) {
             cp = ClassPathFactory.createClassPath(new BootClassPathImplementation(projectDirectory, evaluator));
             cache[7] = cp;
         }
@@ -278,6 +212,8 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         } else if (type.equals(ClassPath.COMPILE)) {
             // Bogus
             return getBootClassPath();
+        } else if (type.equals("js/library")) { // NOI18N
+            return getJavascriptsWebClassPath();
         } else {
             return null;
         }
@@ -291,19 +227,12 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         if (ClassPath.BOOT.equals(type)) {
             return new ClassPath[]{getBootClassPath()};
         }
-//        if (ClassPath.COMPILE.equals(type)) {
-//            ClassPath[] l = new ClassPath[2];
-//            l[0] = getCompileTimeClasspath(0);
-//            l[1] = getCompileTimeClasspath(1);
-//            return l;
-//        }
         if (ClassPath.SOURCE.equals(type)) {
             ClassPath[] l = new ClassPath[2];
             l[0] = getSourcepath(0);
             l[1] = getSourcepath(1);
             return l;
         }
-//        assert false;
         return null;
     }
 
@@ -318,10 +247,6 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         if (ClassPath.SOURCE.equals(type)) {
             return getSourcepath(0);
         }
-//        if (ClassPath.COMPILE.equals(type)) {
-//            return getCompileTimeClasspath(0);
-//        }
-//        assert false;
         return null;
     }
 
