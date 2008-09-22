48f751339490045d078585751e1f64d9f9219735
Jiri Rechtacek <jrechtacek@netbeans.org>
2008-09-22 17:34:05 +0200

    dismiss slowly ballon on mouse exit

--- autoupdate.ui/src/org/netbeans/modules/autoupdate/ui/actions/BalloonManager.java
+++ autoupdate.ui/src/org/netbeans/modules/autoupdate/ui/actions/BalloonManager.java
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -46,6 +46,7 @@ import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
@@ -53,16 +54,22 @@ import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.Shape;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.Action;
 import javax.swing.BorderFactory;
@@ -70,13 +77,13 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
-import org.openide.util.RequestProcessor;
-import org.openide.util.Utilities;
+import org.openide.util.ImageUtilities;
 
 /**
  * Shows, hides balloon-like tooltip windows.
@@ -88,7 +95,8 @@ public class BalloonManager {
     private static Balloon currentBalloon;
     private static JLayeredPane currentPane;
     private static ComponentListener listener;
-    private static RequestProcessor.Task hideToolTipTask = null;
+    private static WindowStateListener windowListener;
+    private static Window ownerWindow;
     
     /**
      * Show balloon-like tooltip pointing to the given component. The balloon stays
@@ -96,28 +104,16 @@ public class BalloonManager {
      * @param owner The component which the balloon will point to
      * @param content Content to be displayed in the balloon.
      * @param defaultAction Action to invoked when the balloon is clicked, can be null.
+     * @param timeoutMillies Number of milliseconds before the balloon disappears, 0 to keep it visible forever
      */
-    public static synchronized void show( final JComponent owner, JComponent content, Action defaultAction, int timeout ) {
+    public static synchronized void show( final JComponent owner, JComponent content, Action defaultAction, int timeoutMillis ) {
         assert null != owner;
         assert null != content;
         
         //hide current balloon (if any)
         dismiss();
-        if (timeout > 0) {
-            if (hideToolTipTask == null || hideToolTipTask.isFinished ()) {
-                hideToolTipTask = RequestProcessor.getDefault ().post (new Runnable () {
-                    public void run () {
-                        SwingUtilities.invokeLater (new Runnable () {
-                            public void run () {
-                                dismiss ();
-                            }
-                        });
-                    }
-                }, timeout);
-            }
-        }
-        
-        currentBalloon = new Balloon( content, defaultAction );
+            
+        currentBalloon = new Balloon( content, defaultAction, timeoutMillis );
         currentPane = JLayeredPane.getLayeredPaneAbove( owner );
         
         listener = new ComponentListener() {
@@ -136,6 +132,15 @@ public class BalloonManager {
                 dismiss();
             }
         };
+        windowListener = new WindowStateListener() {
+            public void windowStateChanged(WindowEvent e) {
+                dismiss();
+            }
+        };
+        ownerWindow = SwingUtilities.getWindowAncestor(owner);
+        if( null != ownerWindow ) {
+            ownerWindow.addWindowStateListener(windowListener);
+        }
         currentPane.addComponentListener( listener );
         configureBalloon( currentBalloon, currentPane, owner );
         currentPane.add( currentBalloon, new Integer(JLayeredPane.POPUP_LAYER-1) );
@@ -147,12 +152,45 @@ public class BalloonManager {
     public static synchronized void dismiss() {
         if( null != currentBalloon ) {
             currentBalloon.setVisible( false );
+            currentBalloon.stopDismissTimer();
             currentPane.remove( currentBalloon );
             currentPane.repaint();
             currentPane.removeComponentListener( listener );
+            if( null != ownerWindow ) {
+                ownerWindow.removeWindowStateListener(windowListener);
+            }
             currentBalloon = null;
             currentPane = null;
             listener = null;
+            ownerWindow = null;
+            windowListener = null;
+        }
+    }
+    
+    public static synchronized void dismissSlowly (final int timeout) {
+        if( null != currentBalloon ) {
+            if( currentBalloon.timeoutMillis > 0 ) {
+                SwingUtilities.invokeLater( new Runnable() {
+                    public void run() {
+                        currentBalloon.startDismissTimer (timeout);
+                    }
+                });
+            } else {
+                dismiss ();
+            }
+        }
+    }
+    
+    public static synchronized void stopDismissSlowly () {
+        if( null != currentBalloon ) {
+            if( currentBalloon.timeoutMillis > 0 ) {
+                currentBalloon.timeoutMillis = ToolTipManager.sharedInstance ().getDismissDelay (); // on MouseEnter cut timeout on 100ms
+                SwingUtilities.invokeLater( new Runnable() {
+                    public void run() {
+                        currentBalloon.stopDismissTimer ();
+                    }
+                });
+            }
         }
     }
     
@@ -195,27 +233,32 @@ public class BalloonManager {
         //upper left corent
         } else {
             balloon.setArrowLocation( GridBagConstraints.NORTHWEST );
-            balloon.setBounds( ownerCompBounds.x-balloonSize.width+Balloon.ARC/2, 
+            balloon.setBounds( ownerCompBounds.x-balloonSize.width/*+Balloon.ARC/2*/, 
                     ownerCompBounds.y-balloonSize.height, balloonSize.width+Balloon.ARC, balloonSize.height );
         }
     }
 
     private static class Balloon extends JPanel {
 
-        private static final int Y_OFFSET = 16;
+        private static final int Y_OFFSET = 8;
         private static final int ARC = 15;
-        private static final int SHADOW_SIZE = 10;
+        private static final int SHADOW_SIZE = 3;
 
 
         private JComponent content;
         private Action defaultAction;
         private JButton btnDismiss;
         private int arrowLocation = GridBagConstraints.SOUTHEAST;
+        private float currentAlpha = 1.0f;
+        private Timer dismissTimer;
+        private int timeoutMillis;
+        private boolean isMouseOverEffect = false;
 
-        public Balloon( final JComponent content, final Action defaultAction ) {
+        public Balloon( final JComponent content, final Action defaultAction, final int timeoutMillis ) {
             super( new GridBagLayout() );
             this.content = content;
             this.defaultAction = defaultAction;
+            this.timeoutMillis = timeoutMillis;
             content.setOpaque( false );
 
             btnDismiss = new DismissButton();
@@ -225,9 +268,8 @@ public class BalloonManager {
                 }
             });
 
-            add( content, new GridBagConstraints(0,0,1,1,1.0,0.0,GridBagConstraints.NORTH,GridBagConstraints.BOTH,new Insets(4,4,4,4),0,0)); 
-            add( new JLabel(), new GridBagConstraints(0,1,1,1,0.0,1.0,GridBagConstraints.NORTH,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0)); 
-            add( btnDismiss, new GridBagConstraints(1,0,1,1,0.0,0.0,GridBagConstraints.NORTHEAST,GridBagConstraints.NONE,new Insets(4,0,4,4),0,0)); 
+            add( content, new GridBagConstraints(0,0,1,1,1.0,1.0,GridBagConstraints.NORTH,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0)); 
+            add( btnDismiss, new GridBagConstraints(1,0,1,1,0.0,0.0,GridBagConstraints.NORTHEAST,GridBagConstraints.NONE,new Insets(7,0,0,7),0,0)); 
 
             setOpaque( false );
 
@@ -247,23 +289,79 @@ public class BalloonManager {
 
                     public void mouseEntered(MouseEvent e) {
                         content.setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+                        stopDismissTimer();
+                        repaint();
                     }
 
                     public void mouseExited(MouseEvent e) {
                         content.setCursor( Cursor.getDefaultCursor() );
+                        if( Balloon.this.timeoutMillis > 0 )
+                            startDismissTimer (ToolTipManager.sharedInstance ().getDismissDelay ());
                     }
-
                 });
             }
-            setBorder( BorderFactory.createEmptyBorder(SHADOW_SIZE, 0, 0, SHADOW_SIZE));
+            
+            if( timeoutMillis > 0 ) {
+                SwingUtilities.invokeLater( new Runnable() {
+                    public void run() {
+                        startDismissTimer (timeoutMillis);
+                    }
+                });
+            }
+            
+            MouseListener mouseOverAdapter = new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    isMouseOverEffect = true;
+                    repaint();
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    isMouseOverEffect = false;
+                    repaint();
+                }
+            };
+            
+            addMouseListener(mouseOverAdapter);
+            content.addMouseListener(mouseOverAdapter);
+            btnDismiss.addMouseListener(mouseOverAdapter);
+        }
+        
+        private static final float ALPHA_DECREMENT = 0.03f;
+        private static final int DISMISS_REPAINT_REPEAT = 100;
+        
+        synchronized void startDismissTimer (int timeout) {
+            stopDismissTimer();
+            currentAlpha = 1.0f;
+            dismissTimer = new Timer(DISMISS_REPAINT_REPEAT, new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    currentAlpha -= ALPHA_DECREMENT;
+                    if( currentAlpha <= ALPHA_DECREMENT ) {
+                        stopDismissTimer();
+                        dismiss();
+                    }
+                    repaint();
+                }
+            });
+            dismissTimer.setInitialDelay (timeout);
+            dismissTimer.start();
+        }
+        
+        synchronized void stopDismissTimer() {
+            if( null != dismissTimer ) {
+                dismissTimer.stop();
+                dismissTimer = null;
+                currentAlpha = 1.0f;
+            }
         }
         
         void setArrowLocation( int arrowLocation) {
             this.arrowLocation = arrowLocation;
             if( arrowLocation == GridBagConstraints.NORTHEAST || arrowLocation == GridBagConstraints.NORTHWEST ) {
-                setBorder( BorderFactory.createEmptyBorder(SHADOW_SIZE, 0, 0, SHADOW_SIZE));
+                setBorder( BorderFactory.createEmptyBorder(0, 0, Y_OFFSET, btnDismiss.getWidth()));
             } else {
-                setBorder( BorderFactory.createEmptyBorder(Y_OFFSET, 0, 0, SHADOW_SIZE));
+                setBorder( BorderFactory.createEmptyBorder(Y_OFFSET, 0, 0, btnDismiss.getWidth()));
             }
         }
         
@@ -276,26 +374,26 @@ public class BalloonManager {
             case GridBagConstraints.SOUTHEAST: 
                 area = new Area(new RoundRectangle2D.Float(0, Y_OFFSET, w, h-Y_OFFSET-SHADOW_SIZE, ARC, ARC));
                 path.moveTo(ARC/2, 0);
-                path.lineTo(ARC/2+Y_OFFSET/2, Y_OFFSET);
-                path.lineTo(ARC/2+Y_OFFSET/2+Y_OFFSET, Y_OFFSET);
+                path.lineTo(ARC/2, Y_OFFSET);
+                path.lineTo(ARC/2+Y_OFFSET, Y_OFFSET);
                 break;
             case GridBagConstraints.NORTHEAST: 
                 area = new Area(new RoundRectangle2D.Float(0, SHADOW_SIZE, w, h-Y_OFFSET-SHADOW_SIZE, ARC, ARC));
                 path.moveTo(ARC/2, h-1);
-                path.lineTo(ARC/2+Y_OFFSET/2, h-1-Y_OFFSET);
-                path.lineTo(ARC/2+Y_OFFSET/2+Y_OFFSET, h-1-Y_OFFSET);
+                path.lineTo(ARC/2, h-1-Y_OFFSET);
+                path.lineTo(ARC/2+Y_OFFSET, h-1-Y_OFFSET);
                 break;
             case GridBagConstraints.SOUTHWEST: 
                 area = new Area(new RoundRectangle2D.Float(0, Y_OFFSET, w, h-Y_OFFSET-SHADOW_SIZE, ARC, ARC));
                 path.moveTo(w-ARC/2, 0);
-                path.lineTo(w-ARC/2-Y_OFFSET/2, Y_OFFSET);
-                path.lineTo(w-ARC/2-Y_OFFSET/2-Y_OFFSET, Y_OFFSET);
+                path.lineTo(w-ARC/2, Y_OFFSET);
+                path.lineTo(w-ARC/2-Y_OFFSET, Y_OFFSET);
                 break;
             case GridBagConstraints.NORTHWEST: 
                 area = new Area(new RoundRectangle2D.Float(0, SHADOW_SIZE, w, h-Y_OFFSET-SHADOW_SIZE, ARC, ARC));
                 path.moveTo(w-ARC/2, h-1);
-                path.lineTo(w-ARC/2-Y_OFFSET/2, h-1-Y_OFFSET);
-                path.lineTo(w-ARC/2-Y_OFFSET/2-Y_OFFSET, h-1-Y_OFFSET);
+                path.lineTo(w-ARC/2-Y_OFFSET, h-1-Y_OFFSET);
+                path.lineTo(w-ARC/2, h-1-Y_OFFSET);
                 break;
             }
                 
@@ -308,12 +406,9 @@ public class BalloonManager {
             Area area = new Area(parentMask);
 
             AffineTransform tx = new AffineTransform();
-            if( arrowLocation == GridBagConstraints.NORTHEAST || arrowLocation == GridBagConstraints.NORTHWEST ) {
-                tx.translate(SHADOW_SIZE, -SHADOW_SIZE );//Math.sin(ANGLE)*(getHeight()+SHADOW_SIZE), 0);
-            } else {
-                tx.translate(SHADOW_SIZE, SHADOW_SIZE );//Math.sin(ANGLE)*(getHeight()+SHADOW_SIZE), 0);
-            }
+            tx.translate(SHADOW_SIZE, SHADOW_SIZE );//Math.sin(ANGLE)*(getHeight()+SHADOW_SIZE), 0);
             area.transform(tx);
+            area.subtract(new Area(parentMask));
             return area;
         }
 
@@ -325,30 +420,54 @@ public class BalloonManager {
         @Override
         protected void paintComponent(Graphics g) {
             Graphics2D g2d = (Graphics2D)g;
+            
+            g2d.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+            
             Composite oldC = g2d.getComposite();
             Shape s = getMask( getWidth(), getHeight() );
 
-            g2d.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER, 0.25f ) );
+            g2d.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER, 0.25f*currentAlpha ) );
             g2d.setColor( Color.black );
             g2d.fill( getShadowMask(s) );
             
             g2d.setColor( UIManager.getColor( "ToolTip.background" ) ); //NOI18N
-//            g2d.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER, 0.5f ) );
-            g2d.setComposite( oldC );
+            g2d.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER, currentAlpha ) );
+            Point2D p1 = s.getBounds().getLocation();
+            Point2D p2 = new Point2D.Double(p1.getX(), p1.getY()+s.getBounds().getHeight());
+            if( isMouseOverEffect )
+                g2d.setPaint( new GradientPaint( p2, mouseOverGradientStartColor, p1, mouseOverGradientFinishColor ) );
+            else
+                g2d.setPaint( new GradientPaint( p2, defaultGradientStartColor, p1, defaultGradientFinishColor ) );
             g2d.fill(s);
             g2d.setColor( Color.black );
             g2d.draw(s);
+            g2d.setComposite( oldC );
         }
+
+        @Override
+        protected void paintChildren(Graphics g) {
+            Graphics2D g2d = (Graphics2D)g;
+            Composite oldC = g2d.getComposite();
+            g2d.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER, currentAlpha ) );
+            super.paintChildren(g);
+            g2d.setComposite( oldC );
+        }
+        
+        private static final Color mouseOverGradientStartColor = new Color(224,224,185);
+        private static final Color mouseOverGradientFinishColor = new Color(255,255,241);
+        
+        private static final Color defaultGradientStartColor = new Color(225,225,225);
+        private static final Color defaultGradientFinishColor = new Color(255,255,255);
     }
     
     private static class DismissButton extends JButton {
 
         public DismissButton() {
-            Image img = Utilities.loadImage( "org/netbeans/modules/autoupdate/ui/resources/dismiss_enabled.png" );
+            Image img = ImageUtilities.loadImage( "org/netbeans/modules/autoupdate/ui/resources/dismiss_enabled.png" );
             setIcon( new ImageIcon( img ) );
-            img = Utilities.loadImage( "org/netbeans/modules/autoupdate/ui/resources/dismiss_rollover.png" );
+            img = ImageUtilities.loadImage( "org/netbeans/modules/autoupdate/ui/resources/dismiss_rollover.png" );
             setRolloverIcon(new ImageIcon( img ));
-            img = Utilities.loadImage( "org/netbeans/modules/autoupdate/ui/resources/dismiss_pressed.png" );
+            img = ImageUtilities.loadImage( "org/netbeans/modules/autoupdate/ui/resources/dismiss_pressed.png" );
             setPressedIcon(new ImageIcon( img ));
 
             setBorder( BorderFactory.createEmptyBorder() );
