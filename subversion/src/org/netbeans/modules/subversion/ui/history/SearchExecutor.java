d7bf28bb8f440e3f3f9e5945f93b53d4314ff3b9
Tomas Stupka <tstupka@netbeans.org>
2008-09-24 14:09:56 +0200

    #148015 - NullPointerException at org.netbeans.modules.subversion.ui.history.SearchExecutor.appendResults

--- subversion/src/org/netbeans/modules/subversion/ui/history/SearchExecutor.java
+++ subversion/src/org/netbeans/modules/subversion/ui/history/SearchExecutor.java
@@ -76,8 +76,6 @@ class SearchExecutor implements Runnable {
     private Map<SVNUrl, Set<File>>      workFiles;
     private Map<String,File>            pathToRoot;
     private final SearchCriteriaPanel   criteria;
-    private boolean                     filterUsername;
-    private boolean                     filterMessage;
     
     private int                         completedSearches;
     private boolean                     searchCanceled;
@@ -86,8 +84,6 @@ class SearchExecutor implements Runnable {
     public SearchExecutor(SearchHistoryPanel master) {
         this.master = master;
         criteria = master.getCriteria();
-        filterUsername = criteria.getUsername() != null;
-        filterMessage = criteria.getCommitMessage() != null;
         
         pathToRoot = new HashMap<String, File>(); 
         try {
@@ -129,7 +125,7 @@ class SearchExecutor implements Runnable {
         
         FileStatusCache cache = Subversion.getInstance().getStatusCache();
         for(File f : files) {
-            if(Subversion.getInstance().isAdministrative(f)) {
+            if(SvnUtils.isAdministrative(f)) {
                 continue;
             }
             int status = cache.getStatus(f).getStatus();
@@ -203,7 +199,7 @@ class SearchExecutor implements Runnable {
         if (progressSupport.isCanceled()) {
             searchCanceled = true;
             return;
-        }        
+        }
         if (searchingUrl()) {
             try {
                 ISVNLogMessage [] messages = client.getLogMessages(rootUrl, null, fromRevision, toRevision, false, true, 0);
@@ -218,13 +214,17 @@ class SearchExecutor implements Runnable {
             int idx = 0;
             try {       
                 for (File file : files) {
-                    paths[idx++] = SvnUtils.getRelativePath(rootUrl, file);
+                    String p = SvnUtils.getRelativePath(file);
+                    if(p != null && p.startsWith("/")) {
+                        p = p.substring(1, p.length());
+                    }
+                    paths[idx++] = p;
                 }                
-                ISVNLogMessage [] messages = client.getLogMessages(rootUrl, paths, fromRevision, toRevision, false, true);
+                ISVNLogMessage [] messages = SvnUtils.getLogMessages(client, rootUrl, paths, fromRevision, toRevision, false, true);
                 appendResults(rootUrl, messages);
             } catch (SVNClientException e) {                
                 try {    
-                    // XXX WORKAROUND issue #110034 
+                    // WORKAROUND issue #110034 
                     // the client.getLogMessages(rootUrl, paths[] ... seems to touch also the repository root even if it's not 
                     // listed in paths[]. This causes problems when the given user has restricted access only to a specific folder.
                     if(SvnClientExceptionHandler.isHTTP403(e.getMessage())) { // 403 forbidden
@@ -261,8 +261,12 @@ class SearchExecutor implements Runnable {
         // traverse in reverse chronological order
         for (int i = logMessages.length - 1; i >= 0; i--) {
             ISVNLogMessage logMessage = logMessages[i];
-            if (filterUsername && !criteria.getUsername().equals(logMessage.getAuthor())) continue;
-            if (filterMessage && logMessage.getMessage().indexOf(criteria.getCommitMessage()) == -1) continue;
+            if(logMessage == null) continue;
+            String username = criteria.getUsername();
+            String msg = criteria.getCommitMessage();
+            String logMsg = logMessage.getMessage();
+            if (username != null && !username.equals(logMessage.getAuthor())) continue;
+            if (msg != null && logMsg != null && logMsg.indexOf(msg) == -1) continue;
             RepositoryRevision rev = new RepositoryRevision(logMessage, rootUrl);
             for (RepositoryRevision.Event event : rev.getEvents()) {
                 if (event.getChangedPath().getAction() == 'A' && event.getChangedPath().getCopySrcPath() != null) {
