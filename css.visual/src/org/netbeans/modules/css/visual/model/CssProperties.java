603658a7ea6e3d1e60617fb008f30c45cb0ea875
Marek Fukala <mfukala@netbeans.org>
2008-09-25 17:01:47 +0200

    #141324 - style builder font-size "picas" units are generated incorrectly

--- css.visual/src/org/netbeans/modules/css/visual/model/CssProperties.java
+++ css.visual/src/org/netbeans/modules/css/visual/model/CssProperties.java
@@ -238,7 +238,7 @@ public final class CssProperties {
         cssLengthUnits.add("mm"); //NOI18N
         cssLengthUnits.add("em"); //NOI18N
         cssLengthUnits.add("ex"); //NOI18N
-        cssLengthUnits.add("picas"); //NOI18N
+        cssLengthUnits.add("pc"); //NOI18N
     }
     
     private static List<String> backgroundRepeatValues = new ArrayList<String>();
@@ -309,6 +309,7 @@ public final class CssProperties {
     
     private static List<String> textAlignValues = new ArrayList<String>();
     private static void setTextAlignValues(){
+        textAlignValues.add("center"); //NOI18N
         textAlignValues.add("left"); //NOI18N
         textAlignValues.add("right"); //NOI18N
         textAlignValues.add("justify"); //NOI18N
@@ -339,6 +340,7 @@ public final class CssProperties {
     
     private static List<String> positionValues = new ArrayList<String>();
     private static void setPositionValues(){
+        positionValues.add("absolute"); //NOI18N
         positionValues.add("static"); //NOI18N
         positionValues.add("relative"); //NOI18N
         positionValues.add("fixed"); //NOI18N
