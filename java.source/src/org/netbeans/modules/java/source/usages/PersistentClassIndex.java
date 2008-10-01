bc33ccdb5dee773a7bbbd3ffc3a250cb9252f15f
Tomas Zezula <tzezula@netbeans.org>
2008-10-01 09:38:04 +0200

    #148703:IllegalStateException: Index already closed

--- java.source/src/org/netbeans/modules/java/source/usages/PersistentClassIndex.java
+++ java.source/src/org/netbeans/modules/java/source/usages/PersistentClassIndex.java
@@ -48,6 +48,7 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 import javax.lang.model.element.ElementKind;
@@ -59,10 +60,8 @@ import org.netbeans.api.java.source.CompilationInfo;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.JavaSource.Phase;
 import org.netbeans.modules.java.source.JavaSourceAccessor;
-import static org.netbeans.modules.java.source.usages.ClassIndexImpl.UsageType.*;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.URLMapper;
-import org.openide.util.Exceptions;
 import org.openide.util.Exceptions;
 
 /**
@@ -111,13 +110,8 @@ public class PersistentClassIndex extends ClassIndexImpl {
         return rootFos;
     }
     
-    public String getSourceName (final String binaryName) {
-        try {
-            return index.getSourceName(binaryName);
-        } catch (IOException ioe) {
-            Exceptions.printStackTrace(ioe);
-            return null;
-        }
+    public String getSourceName (final String binaryName) throws IOException {
+        return index.getSourceName(binaryName);        
     }
     
 
@@ -129,53 +123,52 @@ public class PersistentClassIndex extends ClassIndexImpl {
     }
     
     // Implementation of UsagesQueryImpl ---------------------------------------    
-    public <T> void search (final String binaryName, final Set<UsageType> usageType, final ResultConvertor<T> convertor, final Set<? super T> result) throws InterruptedException {
+    public <T> void search (final String binaryName, final Set<UsageType> usageType, final ResultConvertor<T> convertor, final Set<? super T> result) throws InterruptedException, IOException {
         updateDirty();
         if (BinaryAnalyser.OBJECT.equals(binaryName)) {
             this.getDeclaredTypes("", ClassIndex.NameKind.PREFIX, convertor, result);
             return;
         }
-        try {
-            ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void> () {
-                public Void run () throws IOException, InterruptedException {
-                    usages(binaryName, usageType, convertor, result);
-                    return null;
-                }
-            });
-        } catch (IOException ioe) {
-            Exceptions.printStackTrace(ioe);
-        }
+        
+        ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void> () {
+            public Void run () throws IOException, InterruptedException {
+                usages(binaryName, usageType, convertor, result);
+                return null;
+            }
+        });        
     }
     
     
                
     
-    public <T> void getDeclaredTypes (final String simpleName, final ClassIndex.NameKind kind, final ResultConvertor<T> convertor, final Set<? super T> result) throws InterruptedException {
+    public <T> void getDeclaredTypes (final String simpleName, final ClassIndex.NameKind kind, final ResultConvertor<T> convertor, final Set<? super T> result) throws InterruptedException, IOException {
         updateDirty();
-        try {
-            ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void> () {
-                public Void run () throws IOException, InterruptedException {
-                    index.getDeclaredTypes (simpleName,kind, convertor, result);
-                    return null;
-                }                    
-            });
-        } catch (IOException ioe) {
-            Exceptions.printStackTrace(ioe);
-        }
+        ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void> () {
+            public Void run () throws IOException, InterruptedException {
+                index.getDeclaredTypes (simpleName,kind, convertor, result);
+                return null;
+            }                    
+        });
+    }
+    
+    public <T> void getDeclaredElements (final String ident, final ClassIndex.NameKind kind, final ResultConvertor<T> convertor, final Map<T,Set<String>> result) throws InterruptedException, IOException {
+        updateDirty();
+        ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void>() {
+            public Void run () throws IOException, InterruptedException {
+                index.getDeclaredElements(ident, kind, convertor, result);
+                return null;
+            }
+        });                            
     }
     
     
-    public void getPackageNames (final String prefix, final boolean directOnly, final Set<String> result) throws InterruptedException {
-        try {
-            ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void>() {
-                public Void run () throws IOException, InterruptedException {
-                    index.getPackageNames(prefix, directOnly, result);
-                    return null;
-                }
-            });
-        } catch (IOException ioe) {
-            Exceptions.printStackTrace(ioe);
-        }
+    public void getPackageNames (final String prefix, final boolean directOnly, final Set<String> result) throws InterruptedException, IOException {
+        ClassIndexManager.getDefault().readLock(new ClassIndexManager.ExceptionAction<Void>() {
+            public Void run () throws IOException, InterruptedException {
+                index.getPackageNames(prefix, directOnly, result);
+                return null;
+            }
+        });        
     }
     
     public synchronized void setDirty (final JavaSource js) {        
@@ -216,18 +209,18 @@ public class PersistentClassIndex extends ClassIndexImpl {
                 Iterator<FileObject> files = js.getFileObjects().iterator();
                 FileObject fo = files.hasNext() ? files.next() : null;
                 if (fo != null && fo.isValid()) {                    
-                    if (JavaSourceAccessor.INSTANCE.isDispatchThread()) {
+                    if (JavaSourceAccessor.getINSTANCE().isDispatchThread()) {
                         //Already under javac's lock
                         try {
                             ClassIndexManager.getDefault().writeLock(
                                 new ClassIndexManager.ExceptionAction<Void>() {
                                     public Void run () throws IOException {
-                                        CompilationInfo compilationInfo = JavaSourceAccessor.INSTANCE.getCurrentCompilationInfo (js, JavaSource.Phase.RESOLVED);                                        
+                                        CompilationInfo compilationInfo = JavaSourceAccessor.getINSTANCE().getCurrentCompilationInfo (js, JavaSource.Phase.RESOLVED);                                        
                                         if (compilationInfo != null) {
                                             //Not cancelled
                                             final SourceAnalyser sa = getSourceAnalyser();
                                             long st = System.currentTimeMillis();
-                                            sa.analyseUnitAndStore(compilationInfo.getCompilationUnit(), JavaSourceAccessor.INSTANCE.getJavacTask(compilationInfo),
+                                            sa.analyseUnitAndStore(compilationInfo.getCompilationUnit(), JavaSourceAccessor.getINSTANCE().getJavacTask(compilationInfo),
                                                 ClasspathInfoAccessor.getINSTANCE().getFileManager(compilationInfo.getClasspathInfo()));
                                             long et = System.currentTimeMillis();
                                             }
@@ -253,7 +246,7 @@ public class PersistentClassIndex extends ClassIndexImpl {
                                                     controller.toPhase(Phase.RESOLVED);
                                                     final SourceAnalyser sa = getSourceAnalyser();
                                                     long st = System.currentTimeMillis();
-                                                    sa.analyseUnitAndStore(controller.getCompilationUnit(), JavaSourceAccessor.INSTANCE.getJavacTask(controller),
+                                                    sa.analyseUnitAndStore(controller.getCompilationUnit(), JavaSourceAccessor.getINSTANCE().getJavacTask(controller),
                                                     ClasspathInfoAccessor.getINSTANCE().getFileManager(controller.getClasspathInfo()));
                                                     long et = System.currentTimeMillis();
                                                     return null;
