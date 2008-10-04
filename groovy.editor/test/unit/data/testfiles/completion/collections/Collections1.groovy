a0b94242737ed86f25969b11600c321741980a7d
Petr Hejl <phejl@netbeans.org>
2008-10-04 22:05:13 +0200

    Improved code completion and test coverage.

---
+++ groovy.editor/test/unit/data/testfiles/completion/collections/Collections1.groovy
@@ -0,0 +1,19 @@
+def rangeTest() {
+    // "groovy.lang.Range"
+    // this is buggy in test case
+    (1..10).a
+    // this should offer cc for integer
+    1..10.d
+    // test full list
+    (1..10).
+}
+
+def listTest() {
+    // "java.util.List"
+   ["one","two"].listIter
+}
+
+def mapTest() {
+    // "java.util.Map"
+  [1:"one", 2:"two"].ent
+}
