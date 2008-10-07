3c994c8827d3e325eb6ac0dc6cae60ad111f9edf
David Konecny <dkonecny@netbeans.org>
2008-10-07 15:56:53 +1300

    merge AppClientProjectUtil with Utils

--- j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/Utils.java
+++ j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/Utils.java
@@ -42,16 +42,36 @@
 package org.netbeans.modules.j2ee.clientproject;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
+import org.netbeans.api.java.classpath.ClassPath;
+import org.netbeans.api.java.platform.JavaPlatform;
+import org.netbeans.api.java.platform.JavaPlatformManager;
+import org.netbeans.api.java.platform.Specification;
+import org.netbeans.api.java.source.ClasspathInfo;
+import org.netbeans.api.java.source.SourceUtils;
+import org.netbeans.modules.j2ee.clientproject.ui.customizer.MainClassChooser;
 import org.openide.WizardDescriptor;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.NbBundle;
+import org.openide.util.Parameters;
 
 /**
  * Utility methods for the module.
  */
 public class Utils {
 
-    private static final String WIZARD_PANEL_CONTENT_DATA = "WizardPanel_contentData"; // NOI18N
-    private static final String WIZARD_PANEL_CONTENT_SELECTED_INDEX = "WizardPanel_contentSelectedIndex"; //NOI18N;
+    private static final Logger UI_LOGGER = Logger.getLogger("org.netbeans.ui.j2ee.clientproject"); // NOI18N
+    private static final Logger USG_LOGGER = Logger.getLogger("org.netbeans.ui.metrics.j2ee.clientproject"); // NOI18N
+    
+    private static final String WIZARD_PANEL_CONTENT_DATA = WizardDescriptor.PROP_CONTENT_DATA; // NOI18N
+    private static final String WIZARD_PANEL_CONTENT_SELECTED_INDEX = WizardDescriptor.PROP_CONTENT_SELECTED_INDEX; //NOI18N;
 
     private Utils() {
     }
@@ -69,33 +89,7 @@ public class Utils {
         }
         return classpath.toString();
     }
-    
-//    public static void notifyError(Exception ex) {
-//        NotifyDescriptor ndd = new NotifyDescriptor.Message(ex.getMessage(), NotifyDescriptor.ERROR_MESSAGE);
-//        DialogDisplayer.getDefault().notify(ndd);
-//    }
-//
-//    public static void mergeSteps(WizardDescriptor wizard, WizardDescriptor.Panel[] panels, String[] steps) {
-//        Object prop = wizard.getProperty (WIZARD_PANEL_CONTENT_DATA);
-//        String[] beforeSteps;
-//        int offset;
-//        if (prop instanceof String[]) {
-//            beforeSteps = (String[]) prop;
-//            offset = beforeSteps.length;
-//            if (offset > 0 && ("...".equals(beforeSteps[offset - 1]))) {// NOI18N
-//                offset--;
-//            }
-//        } else {
-//            beforeSteps = null;
-//            offset = 0;
-//        }
-//        String[] resultSteps = new String[ (offset) + panels.length];
-//        for (int i = 0; i < offset; i++) {
-//            resultSteps[i] = beforeSteps[i];
-//        }
-//        setSteps(panels, steps, resultSteps, offset);
-//    }
-//
+
     private static void setSteps(WizardDescriptor.Panel[] panels, String[] steps, String[] resultSteps, int offset) {
         int n = steps == null ? 0 : steps.length;
         for (int i = 0; i < panels.length; i++) {
@@ -115,123 +109,115 @@ public class Utils {
         setSteps(panels, steps, steps, 0);
     }
 
-//    /**
-//     * JMI transaction must be started and JMI classpath must be set to use this method
-//     */
-//    public static void save(JavaClass jc) {
-//        if (jc == null) {
-//            return;
-//        }
-//        DataObject dataObject = null;
-//        try {
-//            DataObject.find(JavaModel.getFileObject(jc.getResource()));
-//        } catch (DataObjectNotFoundException dnfe) {
-//        }
-//        SaveCookie saveCookie = dataObject == null ? null : (SaveCookie) dataObject.getCookie(SaveCookie.class);
-//        // TODO: SaveCookie - is returned if file is not modified?
-////        assert saveCookie != null: ("SaveCookie not found for " + jc.getName());
-//        if (saveCookie != null) {
-//            try {
-//                saveCookie.save();
-//            } catch (IOException ioe) {
-//                ErrorManager.getDefault().notify(ErrorManager.EXCEPTION, ioe);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * JMI transaction must be started and JMI classpath must be set to use this method
-//     */
-//    public static boolean isModified(JavaClass ce) {        
-//        DataObject dataObject = JavaMetamodel.getManager().getDataObject(ce.getResource());
-//        assert dataObject != null: ("DataObject not found for " + ce.getName());
-//        return dataObject.isModified();
-//    }
-//
-//    public static boolean areInSameJ2EEApp(Project p1, Project p2) {
-//        Set globalPath = GlobalPathRegistry.getDefault().getSourceRoots();
-//        Iterator iter = globalPath.iterator();
-//        while (iter.hasNext()) {
-//            FileObject sourceRoot = (FileObject)iter.next();
-//            Project project = FileOwnerQuery.getOwner(sourceRoot);
-//            if (project != null) {
-//                Object j2eeAppProvider = project.getLookup().lookup(J2eeAppProvider.class);
-//                if (j2eeAppProvider != null) { // == it is j2ee app
-//                    J2eeAppProvider j2eeApp = (J2eeAppProvider)j2eeAppProvider;
-//                    J2eeModuleProvider[] j2eeModules = j2eeApp.getChildModuleProviders();
-//                    if ((j2eeModules != null) && (j2eeModules.length > 0)) { // == there are some modules in the j2ee app
-//                        J2eeModuleProvider affectedPrjProvider1 =
-//                                (J2eeModuleProvider)p1.getLookup().lookup(J2eeModuleProvider.class);
-//                        J2eeModuleProvider affectedPrjProvider2 =
-//                                (J2eeModuleProvider)p2.getLookup().lookup(J2eeModuleProvider.class);
-//                        if (affectedPrjProvider1 != null && affectedPrjProvider2 != null) {
-//                            List childModules = Arrays.asList(j2eeModules);
-//                            if (childModules.contains(affectedPrjProvider1) &&
-//                                childModules.contains(affectedPrjProvider2)) {
-//                                return true;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        return false;
-//    }
-//
-//    // =========================================================================
-//
-//    // utils for ejb code synchronization
-//    
-//    
-//    
-//    /** Returns list of all EJB projects that can be called from the caller project.
-//     *
-//     * @param enterpriseProject the caller enterprise project
-//     */
-//    public static Project [] getCallableEjbProjects (Project enterpriseProject) {
-//        Project[] allProjects = OpenProjects.getDefault().getOpenProjects();
-//        
-//        boolean isCallerEJBModule = false;
-//        J2eeModuleProvider callerJ2eeModuleProvider = (J2eeModuleProvider) enterpriseProject.getLookup().lookup(J2eeModuleProvider.class);
-//        if (callerJ2eeModuleProvider != null && callerJ2eeModuleProvider.getJ2eeModule().getModuleType().equals(J2eeModule.CAR)) {
-//            // TODO: HACK - this should be set by calling AntArtifactQuery.findArtifactsByType(p, EjbProjectConstants.ARTIFACT_TYPE_EJBJAR)
-//            // but now freeform doesn't implement this correctly
-//            isCallerEJBModule = true;
-//        }
-//        // TODO: HACK - this must be solved by freeform's own implementation of EnterpriseReferenceContainer, see issue 57003
-//        // call ejb should not make this check, all should be handled in EnterpriseReferenceContainer
-//        boolean isCallerFreeform = enterpriseProject.getClass().getName().equals("org.netbeans.modules.ant.freeform.FreeformProject");
-//        
-//        List /*<Project>*/ filteredResults = new ArrayList(allProjects.length);
-//        for (int i = 0; i < allProjects.length; i++) {
-//            boolean isEJBModule = false;
-//            J2eeModuleProvider j2eeModuleProvider = (J2eeModuleProvider) allProjects[i].getLookup().lookup(J2eeModuleProvider.class);
-//            if (j2eeModuleProvider != null && j2eeModuleProvider.getJ2eeModule().getModuleType().equals(J2eeModule.CAR)) {
-//                isEJBModule = true;
-//            }
-//            if ((isEJBModule && !isCallerFreeform) ||
-//                (isCallerFreeform && enterpriseProject.equals(allProjects[i]))) {
-//                filteredResults.add(allProjects[i]);
-//            }
-//        }
-//        return (Project []) filteredResults.toArray(new Project[filteredResults.size()]);
-//    }
-//
-//    // Copied from j2ee/utilities JMIUtils
-//    public static JavaClass findClass(String className) {
-//        JavaClass result = (JavaClass) resolveType(className);
-//        return result instanceof UnresolvedClass ? null : result;
-//    }
-//
-//    // Copied from j2ee/utilities JMIUtils
-//    public static Type resolveType(String typeName) {
-//        Type type = JavaModel.getDefaultExtent().getType().resolve(typeName);
-//        if (type instanceof UnresolvedClass) {
-//            Type basicType = JavaModel.getDefaultExtent().getType().resolve("java.lang." + typeName);  // NOI18N;
-//            if (!(basicType instanceof UnresolvedClass)) {
-//                return basicType;
-//            }
-//        }
-//        return type;
-//    }
+    /**
+     * Logs the UI gesture.
+     *
+     * @param bundle resource bundle to use for message
+     * @param message message key
+     * @param params message parameters, may be <code>null</code>
+     */
+    public static void logUI(ResourceBundle bundle,String message, Object[] params) {
+        Parameters.notNull("message", message);
+        Parameters.notNull("bundle", bundle);
+
+        LogRecord logRecord = new LogRecord(Level.INFO, message);
+        logRecord.setLoggerName(UI_LOGGER.getName());
+        logRecord.setResourceBundle(bundle);
+        if (params != null) {
+            logRecord.setParameters(params);
+        }
+        UI_LOGGER.log(logRecord);
+    }
+
+    /**
+     * Logs feature usage.
+     *
+     * @param srcClass source class
+     * @param message message key
+     * @param params message parameters, may be <code>null</code>
+     */
+    public static void logUsage(Class srcClass, String message, Object[] params) {
+        Parameters.notNull("message", message);
+
+        LogRecord logRecord = new LogRecord(Level.INFO, message);
+        logRecord.setLoggerName(USG_LOGGER.getName());
+        logRecord.setResourceBundle(NbBundle.getBundle(srcClass));
+        logRecord.setResourceBundleName(srcClass.getPackage().getName() + ".Bundle"); // NOI18N
+        if (params != null) {
+            logRecord.setParameters(params);
+        }
+        USG_LOGGER.log(logRecord);
+    }
+    
+    /** Check if the given file object represents a source with the main method.
+     * 
+     * @param fo source
+     * @return true if the source contains the main method
+     */
+    public static boolean hasMainMethod(FileObject fo) {
+        // support for unit testing
+        if (MainClassChooser.unitTestingSupport_hasMainMethodResult != null) {
+            return MainClassChooser.unitTestingSupport_hasMainMethodResult.booleanValue ();
+        }
+        if (fo == null) {
+            // ??? maybe better should be thrown IAE
+            return false;
+        }
+        return !SourceUtils.getMainClasses(fo).isEmpty();
+    }
+
+    public static boolean isMainClass (final String className, ClassPath bootPath, ClassPath compilePath, ClassPath sourcePath) {
+        ClasspathInfo cpInfo = ClasspathInfo.create(bootPath, compilePath, sourcePath);
+        return SourceUtils.isMainClass(className, cpInfo);
+    }
+    
+    /**
+     * Creates an URL of a classpath or sourcepath root
+     * For the existing directory it returns the URL obtained from {@link File#toUri()}
+     * For archive file it returns an URL of the root of the archive file
+     * For non existing directory it fixes the ending '/'
+     * @param root the file of a root
+     * @param offset a path relative to the root file or null (eg. src/ for jar:file:///lib.jar!/src/)" 
+     * @return an URL of the root
+     * @throws MalformedURLException if the URL cannot be created
+     */
+    public static URL getRootURL (File root, String offset) throws MalformedURLException {
+        URL url = root.toURI().toURL();
+        if (FileUtil.isArchiveFile(url)) {
+            url = FileUtil.getArchiveRoot(url);
+        } else if (!root.exists()) {
+            url = new URL(url.toExternalForm() + "/"); // NOI18N
+        }
+        if (offset != null) {
+            assert offset.endsWith("/");    //NOI18N
+            url = new URL(url.toExternalForm() + offset); // NOI18N
+        }
+        return url;
+    }
+    
+    
+    /**
+     * Returns the active platform used by the project or null if the active
+     * project platform is broken.
+     * @param activePlatformId the name of platform used by Ant script or null
+     * for default platform.
+     * @return active {@link JavaPlatform} or null if the project's platform
+     * is broken
+     */
+    public static JavaPlatform getActivePlatform (final String activePlatformId) {
+        final JavaPlatformManager pm = JavaPlatformManager.getDefault();
+        if (activePlatformId == null) {
+            return pm.getDefaultPlatform();
+        }
+        else {
+            JavaPlatform[] installedPlatforms = pm.getPlatforms(null, new Specification ("j2se",null));   //NOI18N
+            for (int i=0; i<installedPlatforms.length; i++) {
+                String antName = installedPlatforms[i].getProperties().get("platform.ant.name");        //NOI18N
+                if (antName != null && antName.equals(activePlatformId)) {
+                    return installedPlatforms[i];
+                }
+            }
+            return null;
+        }
+    }
 }
