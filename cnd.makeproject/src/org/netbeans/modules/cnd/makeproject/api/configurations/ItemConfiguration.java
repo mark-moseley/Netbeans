948aadb3f5233d57f7899fbfd4207a8e00fa2096
Alexander Simon <alexvsimon@netbeans.org>
2008-10-08 12:48:00 +0400

    fixed: IZ#149397:Renaming source file in  "Important files" caused NPE

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/ItemConfiguration.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/ItemConfiguration.java
@@ -52,8 +52,6 @@ import org.netbeans.modules.cnd.makeproject.configurations.ui.BooleanNodeProp;
 import org.netbeans.modules.cnd.api.utils.IpeUtils;
 import org.netbeans.modules.cnd.api.xml.XMLDecoder;
 import org.netbeans.modules.cnd.api.xml.XMLEncoder;
-import org.netbeans.modules.cnd.api.compilers.CompilerSetManager;
-import org.netbeans.modules.cnd.api.compilers.CompilerSet;
 import org.netbeans.modules.cnd.api.compilers.Tool;
 import org.openide.nodes.Node;
 import org.openide.nodes.PropertySupport;
@@ -148,11 +146,12 @@ public class ItemConfiguration implements ConfigurationAuxObject {
     }
     
     // Tool
-    public void setTool(String genericName) {
-        if (genericName != null) {
-            CompilerSet set = CompilerSetManager.getDefault().getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
-            tool = set.getToolKind(genericName);
-        }
+    public void setTool(String name) {
+//        if (genericName != null) {
+//            CompilerSet set = CompilerSetManager.getDefault(((MakeConfiguration)configuration).getDevelopmentHost().getName()).getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
+//            tool = set.getToolKind(genericName);
+//        }
+        setTool(Tool.getTool(name));
     }
     public void setTool(int tool) {
         this.tool = tool;
@@ -163,13 +162,14 @@ public class ItemConfiguration implements ConfigurationAuxObject {
         }
         return tool;
     }
-    protected String getToolName() {
-        CompilerSet set = CompilerSetManager.getDefault().getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
-        return set.getTool(getTool()).getName();
-    }
+//    protected String getToolName() {
+//        CompilerSet set = CompilerSetManager.getDefault(((MakeConfiguration)configuration).getDevelopmentHost().getName()).getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
+//        return set.getTool(getTool()).getName();
+//    }
     protected String[] getToolNames() {
-        CompilerSet set = CompilerSetManager.getDefault().getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
-        return set.getToolGenericNames();
+//        CompilerSet set = CompilerSetManager.getDefault(((MakeConfiguration)configuration).getDevelopmentHost().getName()).getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
+//        return set.getToolGenericNames();
+        return Tool.getCompilerToolNames();
     }
     
     // Custom Tool
@@ -191,7 +191,12 @@ public class ItemConfiguration implements ConfigurationAuxObject {
     
     public synchronized CCompilerConfiguration getCCompilerConfiguration() {
         if (cCompilerConfiguration == null) {
-            cCompilerConfiguration = new CCompilerConfiguration(((MakeConfiguration)configuration).getBaseDir(), item.getFolder().getFolderConfiguration(configuration).getCCompilerConfiguration());
+            FolderConfiguration folderConfiguration = item.getFolder().getFolderConfiguration(configuration);
+            if (folderConfiguration != null) {
+                cCompilerConfiguration = new CCompilerConfiguration(((MakeConfiguration)configuration).getBaseDir(), folderConfiguration.getCCompilerConfiguration());
+            } else {
+                cCompilerConfiguration = new CCompilerConfiguration(((MakeConfiguration)configuration).getBaseDir(), null);
+            }
         }
         return cCompilerConfiguration;
     }
@@ -203,7 +208,12 @@ public class ItemConfiguration implements ConfigurationAuxObject {
     
     public synchronized CCCompilerConfiguration getCCCompilerConfiguration() {
         if (ccCompilerConfiguration == null) {
-            ccCompilerConfiguration = new CCCompilerConfiguration(((MakeConfiguration)configuration).getBaseDir(), item.getFolder().getFolderConfiguration(configuration).getCCCompilerConfiguration());
+            FolderConfiguration folderConfiguration = item.getFolder().getFolderConfiguration(configuration);
+            if (folderConfiguration != null) {
+                ccCompilerConfiguration = new CCCompilerConfiguration(((MakeConfiguration)configuration).getBaseDir(), folderConfiguration.getCCCompilerConfiguration());
+            } else {
+                ccCompilerConfiguration = new CCCompilerConfiguration(((MakeConfiguration)configuration).getBaseDir(), null);
+            }
         }
         return ccCompilerConfiguration;
     }
@@ -297,6 +307,7 @@ public class ItemConfiguration implements ConfigurationAuxObject {
         ItemConfiguration copy = new ItemConfiguration(makeConfiguration, getItem());
         // safe using
         copy.assign(this);
+        copy.setConfiguration(makeConfiguration);
         return copy;
     }
     
@@ -407,8 +418,9 @@ public class ItemConfiguration implements ConfigurationAuxObject {
         @Override
         public String getAsText() {
             int val = ((Integer)getValue()).intValue();
-            CompilerSet set = CompilerSetManager.getDefault().getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
-            return set.getTool(val).getGenericName();
+            return Tool.getName(val);
+//            CompilerSet set = CompilerSetManager.getDefault(((MakeConfiguration)configuration).getDevelopmentHost().getName()).getCompilerSet(((MakeConfiguration)configuration).getCompilerSet().getValue());
+//            return set.getTool(val).getGenericName();
         }
         
         @Override
