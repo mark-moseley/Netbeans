d6d2c4d5639239400990409ea478a99b2832b5e5
Tomas Holy <t_h@netbeans.org>
2008-10-11 13:55:20 +0200

    #142370: IndexOutOfBoundsException: bitIndex < 0: -14

--- openide.explorer/test/unit/src/org/openide/explorer/view/VisualizerNodeTest.java
+++ openide.explorer/test/unit/src/org/openide/explorer/view/VisualizerNodeTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -42,21 +42,27 @@
 package org.openide.explorer.view;
 
 import java.awt.Image;
+import java.lang.ref.WeakReference;
 import javax.swing.Icon;
 import javax.swing.tree.TreeNode;
 import org.netbeans.junit.NbTestCase;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
 
 /** VisualizerNode tests, mostly based on reported bugs.
  */
 public class VisualizerNodeTest extends NbTestCase {
+    {
+        System.setProperty("org.openide.explorer.VisualizerNode.prefetchCount", "0");
+    }
 
     public VisualizerNodeTest(String name) {
         super(name);
     }
 
+    @Override
     protected boolean runInEQ() {
         return true;
     }
@@ -65,6 +71,7 @@ public class VisualizerNodeTest extends NbTestCase {
         final boolean[] arr = new boolean[1];
         
         AbstractNode a = new AbstractNode(Children.LEAF) {
+            @Override
             public Image getIcon(int type) {
                 arr[0] = true;
                 return null;
@@ -88,5 +95,141 @@ public class VisualizerNodeTest extends NbTestCase {
         TreeNode tm = Visualizer.findVisualizer(m);
         
         assertEquals("Index is 1", 1, ta.getIndex(tm));
+    }
+    
+    public void testIconsAreShared() {
+        AbstractNode a1 = new AbstractNode(Children.LEAF);
+        VisualizerNode v1 = VisualizerNode.getVisualizer(null, a1);
+        Icon icon1 = v1.getIcon(false, false);
+        
+        AbstractNode a2 = new AbstractNode(Children.LEAF);
+        VisualizerNode v2 = VisualizerNode.getVisualizer(null, a2);
+        Icon icon2 = v2.getIcon(false, false);
+        
+        assertSame("Icon instances should be same", icon1, icon2);
+    }
+    
+    public void testLazyVisGet() throws Exception {
+        LazyChildren lch = new LazyChildren();
+        AbstractNode a = new AbstractNode(lch);
+        
+        TreeNode ta = Visualizer.findVisualizer(a);
+        
+        assertEquals("Child check", "c", ta.getChildAt(2).toString());
+        assertEquals("Counter should be 1", 1, lch.cnt);
+    }
+    
+    public void testLazyFilterGet() throws Exception {
+        LazyChildren lch = new LazyChildren();
+        AbstractNode a = new AbstractNode(lch);
+        FilterNode fnode = new FilterNode(a);
+        
+        TreeNode ta = Visualizer.findVisualizer(fnode);
+        
+        assertEquals("Child check", "c", ta.getChildAt(2).toString());
+        assertEquals("Counter should be 1", 1, lch.cnt);
+
+        VisualizerNode vn = (VisualizerNode)ta.getChildAt(2);
+        String msg = ((VisualizerNode)ta).getChildren().dumpIndexes(vn);
+        if (msg.indexOf("'c'") == -1) {
+            fail("Missing note about visualizer node 'c': " + msg);
+        }
+    }
+
+    public void testAddingJavaAndFormAtTheEndOfExistingFolder() throws Exception {
+        LazyChildren lch = new LazyChildren();
+        AbstractNode a = new AbstractNode(lch);
+
+        TreeNode ta = Visualizer.findVisualizer(a);
+
+        assertEquals("Child check", "c", ta.getChildAt(2).toString());
+        assertEquals("Counter should be 1", 1, lch.cnt);
+
+        assertEquals("Child check", "b", ta.getChildAt(1).toString());
+        assertEquals("Counter should be 2", 2, lch.cnt);
+
+        assertEquals("Child check", "a", ta.getChildAt(0).toString());
+        assertEquals("Counter should be all", 3, lch.cnt);
+
+        lch.keys("a", "b", "c", "x", "-x");
+        
+        assertEquals("Counter should still be 3", 3, lch.cnt);
+        assertEquals("Size is 5", 5, ta.getChildCount());
+        
+        lch.keys("a", "b", "c", "x", "-x");
+        
+        assertEquals("Counter should still be 3", 3, lch.cnt);
+        assertEquals("Size is 5", 5, ta.getChildCount());
+
+        assertTrue("Child is empty", isDummyNode(ta.getChildAt(4)));
+        assertEquals("We have still 5 children, no opportunity to update", 5, ta.getChildCount());
+        assertEquals("Three nodes created, still", 3, lch.cnt);
+        
+        assertEquals("x Child check", "x", ta.getChildAt(3).toString());
+        
+        lch.keys("a", "b", "c", "x", "-x", "-y", "y");
+
+        assertEquals("No time to update, should be 5", 5, ta.getChildCount());
+        assertTrue("Nothing removed, -x still present", isDummyNode(ta.getChildAt(4)));
+    }
+
+    public void testVisualizerChildrenGC() throws Exception {
+        LazyChildren ch = new LazyChildren();
+        AbstractNode a = new AbstractNode(ch);
+        VisualizerNode vn = (VisualizerNode) Visualizer.findVisualizer(a);
+        VisualizerChildren vch = vn.getChildren();
+        WeakReference<VisualizerChildren> ref = new WeakReference<VisualizerChildren>(vch);
+        vch = null;
+        boolean gced = true;
+        try {
+            assertGC("", ref);
+        } catch (Error e) {
+            gced = false;
+        }
+        if (gced) {
+            fail("VisualizerChildren should not be GCed.");
+        }
+        TreeNode child = vn.getChildAt(0);
+
+        gced = true;
+        try {
+            assertGC("", ref);
+        } catch (Error e) {
+            gced = false;
+        }
+        if (gced) {
+            fail("VisualizerChildren should not be GCed.");
+        }
+
+        child = null;
+        assertGC("VisualizerChildren should be GCed", ref);
+    }
+
+    final boolean isDummyNode(TreeNode visNode) {
+        Node node = ((VisualizerNode)(visNode)).node;
+        return node.getClass().getName().endsWith("EntrySupport$Lazy$DummyNode");
+    }
+    
+    static class LazyChildren extends Children.Keys<String> {
+        public LazyChildren() {
+            super(true);
+            setKeys(new String[] {"a", "b", "c"});
+        }
+        int cnt;
+        @Override
+        protected Node[] createNodes(String key) {
+            if (key.startsWith("-")) {
+                return null;
+            }
+
+            AbstractNode node = new AbstractNode(LEAF);
+            node.setName(key);
+            cnt++;
+            return new Node[] {node};
+        }
+
+        public void keys(String... arr) {
+            super.setKeys(arr);
+        }
     }
 }
