bd23bdcecbe40ae9625a1c04d41b2d4ab7a46da3
mkuchtiak@netbeans.org <none@none>
2008-10-23 17:38:08 +0200

    moving spi/api to websvc.utilities
    --HG--
    rename : websvc.core/src/META-INF/services/org.netbeans.modules.websvc.core.ServiceCreatorProvider => websvc.core/src/META-INF/services/org.netbeans.modules.websvc.spi.support.ServiceCreatorProvider
    rename : websvc.jaxrpc/src/META-INF/services/org.netbeans.modules.websvc.core.ServiceCreatorProvider => websvc.jaxrpc/src/META-INF/services/org.netbeans.modules.websvc.spi.support.ServiceCreatorProvider

--- websvc.core/src/org/netbeans/modules/websvc/core/dev/wizard/NewWebServiceWizardIterator.java
+++ websvc.core/src/org/netbeans/modules/websvc/core/dev/wizard/NewWebServiceWizardIterator.java
@@ -52,8 +52,8 @@ import javax.swing.JComponent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.SourceGroup;
+import org.netbeans.modules.websvc.api.support.ServiceCreator;
 import org.netbeans.modules.websvc.core.CreatorProvider;
-import org.netbeans.modules.websvc.core.ServiceCreator;
 
 import org.openide.WizardDescriptor;
 
@@ -130,7 +130,7 @@ public class NewWebServiceWizardIterator implements TemplateWizard.Iterator /*, 
         };
         
         // Creating steps.
-        Object prop = this.wiz.getProperty("WizardPanel_contentData"); // NOI18N
+        Object prop = this.wiz.getProperty(WizardDescriptor.PROP_CONTENT_DATA); // NOI18N
         String[] beforeSteps = null;
         if (prop != null && prop instanceof String[]) {
             beforeSteps = (String[]) prop;
@@ -143,9 +143,9 @@ public class NewWebServiceWizardIterator implements TemplateWizard.Iterator /*, 
             if (c instanceof JComponent) { // assume Swing components
                 JComponent jc = (JComponent) c;
                 // Step #.
-                jc.putClientProperty("WizardPanel_contentSelectedIndex", Integer.valueOf(i)); // NOI18N
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, Integer.valueOf(i)); // NOI18N
                 // Step name (actually the whole list for reference).
-                jc.putClientProperty("WizardPanel_contentData", steps); // NOI18N
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA, steps); // NOI18N
             }
         }
     }
