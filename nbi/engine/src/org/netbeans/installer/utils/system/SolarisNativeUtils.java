f6c0df77b918ed6f59ee8042ac2e6e7f6ce7067c
Dmitry Lipin <dlipin@netbeans.org>
2008-10-26 15:04:11 +0300

    Distinguish solaris-x86 and -x64, -sparc and -sparcv9 based on the system bitmode. Make x64 bit compatible with x86 and sparcv9 with sparc

--- nbi/engine/src/org/netbeans/installer/utils/system/SolarisNativeUtils.java
+++ nbi/engine/src/org/netbeans/installer/utils/system/SolarisNativeUtils.java
@@ -36,8 +36,12 @@
 
 package org.netbeans.installer.utils.system;
 
+import java.io.IOException;
+import org.netbeans.installer.utils.LogManager;
+import org.netbeans.installer.utils.StringUtils;
 import org.netbeans.installer.utils.SystemUtils;
-import static org.netbeans.installer.utils.helper.Platform.*;
+import org.netbeans.installer.utils.helper.ExecutionResults;
+import org.netbeans.installer.utils.helper.Platform;
 /**
  *
  * @author Kirill Sorokin
@@ -81,5 +85,28 @@ public class SolarisNativeUtils extends UnixNativeUtils {
         
         loadNativeLibrary(library);
         initializeForbiddenFiles(FORBIDDEN_DELETING_FILES_SOLARIS);
+    }
+    @Override
+
+    protected Platform getPlatform() {
+        final String osArch = System.getProperty("os.arch");
+
+        String systemArch = "";
+        try {
+            ExecutionResults er = SystemUtils.executeCommand("isainfo", "-k");
+            systemArch = StringUtils.splitByLines(er.getStdOut())[0];
+        } catch (IOException e) {
+            LogManager.log(e);
+        }
+
+        if (osArch.equals("sparcv9") || systemArch.equals("sparcv9")) {
+            return Platform.SOLARIS_SPARCV9;
+        } else if (osArch.equals("sparc")) {
+            return Platform.SOLARIS_SPARC;
+        } else if (osArch.equals("amd64") || systemArch.equals("amd64")) { // ????? not sure about that value
+            return Platform.SOLARIS_X64;
+        } else {
+            return Platform.SOLARIS_X86;
+        }
     }
 }
