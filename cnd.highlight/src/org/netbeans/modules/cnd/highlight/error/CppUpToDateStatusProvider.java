4761244315e9225d647a462649fe1b91f65bc225
Sergey Grinev <sergius@netbeans.org>
2008-10-27 17:57:58 +0300

    cleanup

--- cnd.highlight/src/org/netbeans/modules/cnd/highlight/error/CppUpToDateStatusProvider.java
+++ cnd.highlight/src/org/netbeans/modules/cnd/highlight/error/CppUpToDateStatusProvider.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,11 +20,11 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -41,48 +41,54 @@
 
 package org.netbeans.modules.cnd.highlight.error;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.WeakHashMap;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import org.netbeans.modules.cnd.api.project.NativeProject;
-import org.netbeans.modules.cnd.makeproject.api.ui.BrokenIncludes;
+import org.netbeans.editor.BaseDocument;
+import org.netbeans.spi.editor.errorstripe.UpToDateStatus;
+import org.netbeans.spi.editor.errorstripe.UpToDateStatusProvider;
 
 /**
- *
+ *org.netbeans.modules.cnd.highlight.CppUpToDateStatusProvider
  * @author Alexander Simon
  */
-public class BrokenProjectService implements BrokenIncludes, ChangeListener {
-    private static WeakHashMap<ChangeListener,Boolean> listeners = new WeakHashMap<ChangeListener,Boolean>();
-    private final Object lock = new String("BrokenProjectService lock"); // NOI18N
+public class CppUpToDateStatusProvider extends UpToDateStatusProvider {
+
+    private UpToDateStatus current;
+    private static final boolean TRACE = Boolean.getBoolean("cnd.uptodate.trace");
+
+    public CppUpToDateStatusProvider() {
+        current = UpToDateStatus.UP_TO_DATE_DIRTY;
+    }
     
-    public BrokenProjectService() {
+    @Override
+    public UpToDateStatus getUpToDate() {
+        if( TRACE ) System.err.printf("getUpToDate -> %s\n", current);
+        return current;
     }
 
-    public boolean isBroken(NativeProject project) {
-        return BadgeProvider.getInstance().isBroken(project);
-    }
-
-    public void addChangeListener(ChangeListener provider){
-        synchronized(lock) {
-            listeners.put(provider,Boolean.TRUE);
+    //package
+    synchronized  void setUpToDate(UpToDateStatus status) {
+        if( TRACE ) System.err.printf("setUpToDate: %s -> %s\n", current, status);
+        if (current != status) {
+            firePropertyChange(PROP_UP_TO_DATE, current, status);
+            current = status;
         }
     }
 
-    public void removeChangeListener(ChangeListener provider){
-        synchronized(lock) {
-            listeners.remove(provider);
+    public boolean isValid() {
+        return true;
+    }
+    
+    public static synchronized CppUpToDateStatusProvider get(BaseDocument doc) {
+        if (doc == null) {
+            return null;
         }
+        
+        CppUpToDateStatusProvider provider = (CppUpToDateStatusProvider) doc.getProperty(CppUpToDateStatusProvider.class);
+
+        if (provider == null) {
+            doc.putProperty(CppUpToDateStatusProvider.class, provider = new CppUpToDateStatusProvider());
+        }
+
+        return provider;
     }
 
-    public void stateChanged(ChangeEvent e) {
-        List<ChangeListener> list = null;
-        synchronized(lock) {
-            list = new ArrayList<ChangeListener>(listeners.keySet());
-        }
-        for (ChangeListener provider : list){
-            provider.stateChanged(e);
-        }
-    }
 }
