110e48358e84339637646c70544f8d773eb5bc73
Tomas Holy <t_h@netbeans.org>
2008-10-30 18:03:16 +0100

    #30187: Enabling substring search for TreeView in ElementSelectorPanel

--- java.editor/src/org/netbeans/modules/java/editor/codegen/ui/ElementSelectorPanel.java
+++ java.editor/src/org/netbeans/modules/java/editor/codegen/ui/ElementSelectorPanel.java
@@ -65,6 +65,7 @@ public class ElementSelectorPanel extends JPanel implements ExplorerManager.Prov
         setLayout(new BorderLayout());
         elementView = new CheckTreeView();
         elementView.setRootVisible(false);
+        elementView.setUseSubstringInQuickSearch(true);
         add(elementView, BorderLayout.CENTER);
         setRootElement(elementDescription, singleSelection);
         //make sure that the first element is pre-selected
@@ -165,6 +166,10 @@ public class ElementSelectorPanel extends JPanel implements ExplorerManager.Prov
     
     private void getSelectedHandles( ElementNode.Description description,
                                      ArrayList<ElementHandle<? extends Element>> target) {
+
+        //#143049
+        if (description == null)
+            return;
         
         List<ElementNode.Description> subs = description.getSubs();
         
