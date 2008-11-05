e9015f8491065d288b9204f704c88cf7dd5146c7
Dmitry Lipin <dlipin@netbeans.org>
2008-11-05 10:26:38 +0300

    Issue 152303 Uninstaller doesn't provide userID paramater to uninstallation survey

--- installer/engine/src/org/netbeans/installer/wizard/components/actions/netbeans/NbShowUninstallationSurveyAction.java
+++ installer/engine/src/org/netbeans/installer/wizard/components/actions/netbeans/NbShowUninstallationSurveyAction.java
@@ -76,7 +76,7 @@ public class NbShowUninstallationSurveyAction extends WizardAction {
                                 product.getVersion().getMinor();
 
                         LogManager.log("... version : " + version);
-                        final File superId = new File(SystemUtils.getUserHomeDirectory(), ".netbeans" + File.separator + ".superid");
+                        final File superId = new File(SystemUtils.getUserHomeDirectory(), ".netbeans" + File.separator + ".superId");
                         String id = "";
                         if (FileUtils.exists(superId)) {
                             id =  FileUtils.readStringList(superId).get(0);
@@ -105,7 +105,7 @@ public class NbShowUninstallationSurveyAction extends WizardAction {
         return null;
     }
     public static final String UNINSTALLATION_SURVEY_URL =
-            "http://quality.netbeans.org/survey/uninstall.jsp?version={0}&id={1}&life={2}";//NOI18N
+            "http://quality.netbeans.org/survey/uninstall?version={0}&id={1}&life={2}";//NOI18N
     public static final String SHOW_UNINSTALLATION_SURVEY_PROPERTY = 
             "show.uninstallation.survey";
 }
