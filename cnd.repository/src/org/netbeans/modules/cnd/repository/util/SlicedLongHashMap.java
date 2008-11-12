c0e1510ba4003d59887281a043b953a98764d5ba
Alexander Simon <alexvsimon@netbeans.org>
2008-11-12 20:08:02 +0300

    no IZ: cleanup

--- cnd.repository/src/org/netbeans/modules/cnd/repository/util/SlicedLongHashMap.java
+++ cnd.repository/src/org/netbeans/modules/cnd/repository/util/SlicedLongHashMap.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
- *
+ * 
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -38,103 +38,73 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
-package org.netbeans.modules.cnd.repository.sfs.index;
+package org.netbeans.modules.cnd.repository.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import org.netbeans.modules.cnd.repository.spi.Key;
-
+import java.util.Collections;
 
 /**
- * Synchronized index wrapper:
- * Delegate work to the given index, is responsible for synchronization
+ * A LongHashMap that is sliced by several chunks to reduce concurrency
+ * @author Alexander Simon
  * @author Vladimir Kvashin
  */
-class SynchronizedFileIndex implements FileIndex {
-    
-    private FileIndex delegate;
-    private ReadWriteLock indexLock = new ReentrantReadWriteLock();
-    
-    public SynchronizedFileIndex(FileIndex delegate) {
-	this.delegate = delegate;
-    }
-    
-    public int size() {
-	indexLock.readLock().lock();
-	try {
-	    return delegate.size();
-	} finally {
-	    indexLock.readLock().unlock();
-	}
-    }
-    
-    private class SyncRemoveIterator<K> implements Iterator<K> {
-	
-	Iterator<K> delegate;
-	
-	SyncRemoveIterator(Iterator<K> delegate) {
-	    this.delegate = delegate;
-	}
+public class SlicedLongHashMap<K> {
 
-	public void remove() {
-	    indexLock.writeLock().lock();
-	    try {
-		delegate.remove();
-	    } finally {
-		indexLock.writeLock().unlock();
-	    }
+    private final LongHashMap<K>[] instances;
+    private int sliceNumber;
+
+    @SuppressWarnings("unchecked")
+    public SlicedLongHashMap(int sliceNumber, int sliceCapacity) {
+        this.sliceNumber = sliceNumber;
+        instances = new LongHashMap[sliceNumber];
+        for (int i = 0; i < sliceNumber; i++) {
+            instances[i] = new LongHashMap<K>(sliceCapacity);
+        }
+    }
+
+    private LongHashMap<K> getDelegate(K key) {
+	int index = key.hashCode() % sliceNumber;
+	if (index < 0) {
+	    index += sliceNumber;
 	}
-	
-	public K next() {
-	    return delegate.next();
+	return instances[index];
+
+    }
+
+    public long put(K key, long value) {
+	return getDelegate(key).put(key, value);
+    }
+
+    public long get(K key) {
+	return getDelegate(key).get(key);
+    }
+
+    public long remove(K key) {
+	return getDelegate(key).remove(key);
+    }
+
+    public int size() {
+	int size = 0;
+	for (int i = 0; i < sliceNumber; i++) {
+	    size += instances[i].size();
 	}
-	
-	public boolean hasNext() {
-	    return delegate.hasNext();
+	return size;
+    }
+
+    public Collection<K> keySet() {
+	Collection<K> res = new ArrayList<K>(size());
+	for (int i = 0; i < sliceNumber; i++) {
+	    res.addAll(instances[i].keySet());
 	}
+	return Collections.<K>unmodifiableCollection(res);
     }
-    
-    public Iterator<Key> getKeySetIterator() {
-        return new SyncRemoveIterator<Key>(delegate.getKeySetIterator());
+
+    public Collection<LongHashMap.Entry<K>> entrySet() {
+	Collection<LongHashMap.Entry<K>> res = new ArrayList<LongHashMap.Entry<K>>(size());
+	for (int i = 0; i < sliceNumber; i++) {
+	    res.addAll(instances[i].entrySet());
+	}
+	return Collections.<LongHashMap.Entry<K>>unmodifiableCollection(res);
     }
-    
-    public Collection<Key> keySet() {
-        indexLock.readLock().lock();
-        try {
-            return new ArrayList<Key>(delegate.keySet());
-        }  finally {
-            indexLock.readLock().unlock();
-        }
-    }
-    
-    public int put(Key key, long offset, int size) {
-        indexLock.writeLock().lock();
-        try {
-            return delegate.put(key, offset, size);
-        } finally {
-            indexLock.writeLock().unlock();
-        }
-    }
-    
-    public int remove(Key key) {
-        indexLock.writeLock().lock();
-        try {
-            return delegate.remove(key);
-        } finally {
-            indexLock.writeLock().unlock();
-        }
-    }
-    
-    public ChunkInfo get(Key key) {
-        indexLock.readLock().lock();
-        try {
-            return delegate.get(key);
-        } finally {
-            indexLock.readLock().unlock();
-        }
-    }    
 }
