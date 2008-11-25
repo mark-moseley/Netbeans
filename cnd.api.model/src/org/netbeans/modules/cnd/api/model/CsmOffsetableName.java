dfdf22e22e75a4e1f0fbb9cecffc2defd20804c3
Vladimir Voskresensky <vv159170@netbeans.org>
2008-11-25 21:19:20 +0300

    API for parameters

--- cnd.api.model/src/org/netbeans/modules/cnd/api/model/CsmOffsetableName.java
+++ cnd.api.model/src/org/netbeans/modules/cnd/api/model/CsmOffsetableName.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,18 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
@@ -41,22 +52,11 @@
 
 package org.netbeans.modules.cnd.api.model;
 
-import java.util.List;
-
 /**
- * Represents C/C++ macros
- * @author vk155633
+ * 
+ * @author Vladimir Voskresensky
  */
-public interface CsmMacro extends CsmNamedElement, CsmOffsetable, CsmIdentifiable<CsmMacro> {
-
-    List<? extends CharSequence> getParameters();
-    CsmParameterList<CsmParameterList, CsmMacroParameter> getParameterList();
-    
-    CharSequence getBody();
-    
-    /**
-     * flag for system macros, such macro can't be undefined or changed
-     */
-    boolean isSystem();
-    
+public interface CsmOffsetableName extends CsmNamedElement, CsmOffsetable {
+    int getNameStartOffset();
+    int getNameEndOffset();
 }
