120f7d4f0df6004d9c8d4d2496c66f64d0832635
Milos Kleint <mkleint@netbeans.org>
2008-11-26 09:54:41 +0100

    fix various warnings, still a lot to do..

---
+++ maven/src/org/netbeans/modules/maven/execute/model/ActionToGoalMapping.java
@@ -0,0 +1,144 @@
+/*
+ * $Id$
+ */
+
+package org.netbeans.modules.maven.execute.model;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+/**
+ * Class ActionToGoalMapping.
+ * 
+ * @version $Revision$ $Date$
+ */
+public class ActionToGoalMapping implements java.io.Serializable {
+
+
+      //--------------------------/
+     //- Class/Member Variables -/
+    //--------------------------/
+
+    /**
+     * Supported packaging type that this this project maps to. 
+     *                     Useful for custom non-standard
+     * packingings to get the same behaviour as the supported ones.
+     *                     Supported types: jar, war, ejb, ear
+     *                     @deprecated is ethe
+     * netbeans.hint.packaging POM property instead.
+     */
+    private String packaging;
+
+    /**
+     * Field actions.
+     */
+    private java.util.List<NetbeansActionMapping> actions;
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+     * Method addAction.
+     * 
+     * @param netbeansActionMapping
+     */
+    public void addAction(NetbeansActionMapping netbeansActionMapping)
+    {
+        getActions().add( netbeansActionMapping );
+    } //-- void addAction(NetbeansActionMapping) 
+
+    /**
+     * Method getActions.
+     * 
+     * @return java.util.List
+     */
+    public java.util.List<NetbeansActionMapping> getActions()
+    {
+        if ( this.actions == null )
+        {
+            this.actions = new java.util.ArrayList<NetbeansActionMapping>();
+        }
+        
+        return this.actions;
+    } //-- java.util.List getActions() 
+
+    /**
+     * Get supported packaging type that this this project maps to.
+     * 
+     *                     Useful for custom non-standard
+     * packingings to get the same behaviour as the supported ones.
+     *                     Supported types: jar, war, ejb, ear
+     *                     @deprecated is ethe
+     * netbeans.hint.packaging POM property instead.
+     * 
+     * @return String
+     */
+    public String getPackaging()
+    {
+        return this.packaging;
+    } //-- String getPackaging() 
+
+    /**
+     * Method removeAction.
+     * 
+     * @param netbeansActionMapping
+     */
+    public void removeAction(NetbeansActionMapping netbeansActionMapping)
+    {
+        if ( !(netbeansActionMapping instanceof NetbeansActionMapping) )
+        {
+            throw new ClassCastException( "ActionToGoalMapping.removeActions(netbeansActionMapping) parameter must be instanceof " + NetbeansActionMapping.class.getName() );
+        }
+        getActions().remove( netbeansActionMapping );
+    } //-- void removeAction(NetbeansActionMapping) 
+
+    /**
+     * Set the actions field.
+     * 
+     * @param actions
+     */
+    public void setActions(java.util.List<NetbeansActionMapping> actions)
+    {
+        this.actions = actions;
+    } //-- void setActions(java.util.List) 
+
+    /**
+     * Set supported packaging type that this this project maps to.
+     * 
+     *                     Useful for custom non-standard
+     * packingings to get the same behaviour as the supported ones.
+     *                     Supported types: jar, war, ejb, ear
+     *                     @deprecated is ethe
+     * netbeans.hint.packaging POM property instead.
+     * 
+     * @param packaging
+     */
+    public void setPackaging(String packaging)
+    {
+        this.packaging = packaging;
+    } //-- void setPackaging(String) 
+
+
+    private String modelEncoding = "UTF-8";
+
+    /**
+     * Set an encoding used for reading/writing the model.
+     *
+     * @param modelEncoding the encoding used when reading/writing the model.
+     */
+    public void setModelEncoding( String modelEncoding )
+    {
+        this.modelEncoding = modelEncoding;
+    }
+
+    /**
+     * @return the current encoding used when reading/writing this model.
+     */
+    public String getModelEncoding()
+    {
+        return modelEncoding;
+    }
+}
