21969c59f53d7529434be998ada0d1113b87d94a
Vladimir Voskresensky <vv159170@netbeans.org>
2008-12-01 18:53:38 +0300

    fixed IZ#154276: functions are creted instead of variables

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/TypeFunPtrImpl.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/TypeFunPtrImpl.java
@@ -38,203 +38,216 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
 package org.netbeans.modules.cnd.modelimpl.csm;
 
 import java.io.DataInput;
-import org.netbeans.modules.cnd.modelimpl.parser.FakeAST;
-import java.util.*;
 
 import antlr.collections.AST;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import org.netbeans.modules.cnd.api.model.*;
 import org.netbeans.modules.cnd.modelimpl.parser.generated.CPPTokenTypes;
 import org.netbeans.modules.cnd.modelimpl.csm.core.*;
-import org.netbeans.modules.cnd.modelimpl.textcache.NameCache;
+import org.netbeans.modules.cnd.modelimpl.repository.RepositoryUtils;
+import org.netbeans.modules.cnd.modelimpl.uid.UIDCsmConverter;
+import org.netbeans.modules.cnd.modelimpl.uid.UIDObjectFactory;
 
 /**
  * Represent pointer to function type
  * @author Vladimir Kvashin
  */
-public class TypeFunPtrImpl extends TypeImpl {
+public class TypeFunPtrImpl extends TypeImpl implements CsmFunctionPointerType {
 
-    private CharSequence functionPointerParamList;
+    private Collection<CsmUID<CsmParameter>> functionParameters;
     private short functionPointerDepth;
-    
-    private static class Pair {
-	public String paramList;
-	public short pointerDepth;
-    }
-    
-    public TypeFunPtrImpl(AST classifier, CsmFile file, int pointerDepth, boolean reference, int arrayDepth) {
-	super(classifier, file, pointerDepth, reference, arrayDepth);
-	Pair pair = getFunctionPointerParamList(classifier, true);
-	assert pair != null;
-	functionPointerParamList = pair.paramList;
-	functionPointerDepth = pair.pointerDepth;
-    }
-    
-    @Override
-    protected StringBuilder getText(boolean canonical, CharSequence variableNameToInsert) {
-	StringBuilder sb = new StringBuilder();
-	if( isConst() ) {
-	    sb.append("const "); // NOI18N
-	}
-	sb.append(getClassifierText());
-	for( int i = 0; i < getPointerDepth(); i++ ) {
-	    sb.append('*');
-	}
-	if( isReference() ) {
-	    sb.append('&');
-	}
-	for( int i = 0; i < getArrayDepth(); i++ ) {
-	    sb.append(canonical ? "*" : "[]"); // NOI18N
-	}
-	
-	sb.append('(');
-	for (int i = 0; i < functionPointerDepth; i++) {
-	    sb.append('*');
-	    if( variableNameToInsert != null ) {
-		sb.append(variableNameToInsert);
-	    }
-	}
-	sb.append(')');
-	
-	sb.append('(');
-	sb.append(functionPointerParamList);
-	sb.append(')');
-	
-	return sb;
-    }
-    
-    public static boolean isFunctionPointerParamList(AST classifier) {
-	return getFunctionPointerParamList(classifier, false) != null;
-    }
-    
-    private static Pair getFunctionPointerParamList(AST classifier, boolean fillText) {
-	
-	// find opening brace
-	AST brace = AstUtil.findSiblingOfType(classifier, CPPTokenTypes.LPAREN);
-	if( brace == null ) {
-	    return null;
-	}
-	
-	// check whether it's followed by asterisk
-	AST next = brace.getNextSibling();
-	if( next == null || next.getType() != CPPTokenTypes.CSM_PTR_OPERATOR) {
-	    return null;
-	}
-	
-	Pair pair = new Pair();
-	
-	// skip adjacent asterisks
-	do {
-	    next = next.getNextSibling();
-	    pair.pointerDepth++;
-	}
-	while( next != null && next.getType() == CPPTokenTypes.CSM_PTR_OPERATOR );
 
-	if( next == null) {
-	    return null;
-	}
-	
+    TypeFunPtrImpl(CsmFile file, int pointerDepth, boolean reference, int arrayDepth, boolean _const, int startOffset, int endOffset) {
+        super(file, pointerDepth, reference, arrayDepth, _const, startOffset, endOffset);
+    }
+
+    void init(AST ast, boolean inFunctionParameters) {
+        initFunctionPointerParamList(ast, this, inFunctionParameters);
+    }
+
+    public Collection<CsmParameter> getParameters() {
+        if (functionParameters == null) {
+            return Collections.<CsmParameter>emptyList();
+        } else {
+            return UIDCsmConverter.UIDsToDeclarations(functionParameters);
+        }
+    }
+
+    @Override
+    public StringBuilder decorateText(CharSequence classifierText, CsmType decorator, boolean canonical, CharSequence variableNameToInsert) {
+        StringBuilder sb = new StringBuilder();
+        if (decorator.isConst()) {
+            sb.append("const "); // NOI18N
+        }
+        sb.append(classifierText);
+        for (int i = 0; i < decorator.getPointerDepth(); i++) {
+            sb.append('*');
+        }
+        if (decorator.isReference()) {
+            sb.append('&');
+        }
+        for (int i = 0; i < decorator.getArrayDepth(); i++) {
+            sb.append(canonical ? "*" : "[]"); // NOI18N
+        }
+
+        sb.append('(');
+        for (int i = 0; i < functionPointerDepth; i++) {
+            sb.append('*');
+            if (variableNameToInsert != null) {
+                sb.append(variableNameToInsert);
+            }
+        }
+        sb.append(')');
+        sb.append(FunctionImpl.createParametersSignature(getParameters()));
+        return sb;
+    }
+
+    @Override
+    public boolean isPointer() {
+        // function pointer is always pointer
+        return true;
+    }
+
+    public static boolean isFunctionPointerParamList(AST ast, boolean inFunctionParameters) {
+        return initFunctionPointerParamList(ast, null, inFunctionParameters);
+    }
+
+    private static boolean initFunctionPointerParamList(AST ast, TypeFunPtrImpl instance, boolean inFunctionParams) {
+        AST next = null;
+        // find opening brace
+        AST brace = AstUtil.findSiblingOfType(ast, CPPTokenTypes.LPAREN);
+        if (brace != null) {
+            // check whether it's followed by asterisk
+            next = brace.getNextSibling();
+            if (next == null) {
+                return false;
+            }
+            if (inFunctionParams && next.getType() == CPPTokenTypes.CSM_PARMLIST) {
+                // this is start of function params
+                next = AstUtil.findSiblingOfType(ast, CPPTokenTypes.CSM_QUALIFIED_ID);
+            } else {
+                if (next.getType() != CPPTokenTypes.CSM_PTR_OPERATOR) {
+                    return false;
+                }
+                // skip adjacent asterisks
+                do {
+                    next = next.getNextSibling();
+                    if (instance != null) {
+                        ++instance.functionPointerDepth;
+                    }
+                } while (next != null && next.getType() == CPPTokenTypes.CSM_PTR_OPERATOR);
+            }
+        }
+
+        if (inFunctionParams && next == null) {
+            next = AstUtil.findSiblingOfType(ast, CPPTokenTypes.CSM_QUALIFIED_ID);
+        }
+
+        if (next == null) {
+            return false;
+        }
+
         // check that it's followed by exprected token
-        if (next.getType() == CPPTokenTypes.CSM_VARIABLE_DECLARATION) {
+        if (next.getType() == CPPTokenTypes.CSM_VARIABLE_DECLARATION ||
+                next.getType() == CPPTokenTypes.CSM_ARRAY_DECLARATION) {
             // fine. this could be variable of function type
         } else if (next.getType() == CPPTokenTypes.CSM_QUALIFIED_ID) {
-            // check function returns function
-            next = next.getNextSibling();
-            // skip LPAREN (let's not assume it's obligatory)
-            if (next == null || next.getType() != CPPTokenTypes.LPAREN) {
-                return null;
-            }
-            next = next.getNextSibling();
-            if (next == null) {
-                return null;
-            }
-            // skip params of fun itself
-            if (next.getType() == CPPTokenTypes.CSM_PARMLIST) {
+            AST lookahead = next.getNextSibling();
+            AST lookahead2 = lookahead == null ? null : lookahead.getNextSibling();
+            if (lookahead != null && lookahead.getType() == CPPTokenTypes.RPAREN) {
+                // OK. This could be function type in typedef - in this case we get
+                // CSM_QUALIFIED_ID instead of CSM_VARIABLE_DECLARATION.
+            } else if (inFunctionParams && lookahead != null && lookahead.getType() == CPPTokenTypes.LPAREN
+                    && lookahead2 != null && lookahead2.getType() == CPPTokenTypes.CSM_PARMLIST) {
+                // OK. This could be function as a parameter
+                next = lookahead;
+            } else {
+                next = lookahead;
+                // check function returns function
+                // skip LPAREN (let's not assume it's obligatory)
+                if (next == null || next.getType() != CPPTokenTypes.LPAREN) {
+                    return false;
+                }
                 next = next.getNextSibling();
                 if (next == null) {
-                    return null;
+                    return false;
                 }
-            }               
-            // params of fun are closed with RPAREN
-            if (next.getType() != CPPTokenTypes.RPAREN) {
-                return null;
+                // skip params of fun itself
+                if (next.getType() == CPPTokenTypes.CSM_PARMLIST) {
+                    next = next.getNextSibling();
+                    if (next == null) {
+                        return false;
+                    }
+                }
+                // params of fun are closed with RPAREN
+                if (next.getType() != CPPTokenTypes.RPAREN) {
+                    return false;
+                }
             }
         }
         // last step: verify that it's followed with a closing brace
         next = next.getNextSibling();
-        if (next == null || next.getType() != CPPTokenTypes.RPAREN) {
-            return null;
-        }
-
-        next = next.getNextSibling();
-
-        // skip LPAREN (let's not assume it's obligatory)
-        if (next != null && next.getType() == CPPTokenTypes.LPAREN) {
+        if (next != null && next.getType() == CPPTokenTypes.RPAREN) {
             next = next.getNextSibling();
-        }        
-        if (next == null) {
-            return null;
-        }
-        if (next.getType() == CPPTokenTypes.CSM_PARMLIST) {
-            if (fillText) {
-                pair.paramList = gatherChildrenText(next);
+            // skip LPAREN (let's not assume it's obligatory)
+            AST lParent = next;
+            if (next != null && next.getType() == CPPTokenTypes.LPAREN) {
+                next = next.getNextSibling();
             }
-            return pair;
-        } else if (next.getType() == CPPTokenTypes.RPAREN) {
-            pair.paramList = "";
-            return pair;
+            if (next == null) {
+                return false;
+            }
+            if (next.getType() == CPPTokenTypes.CSM_PARMLIST) {
+                if (instance != null) {
+                    instance.functionParameters = RepositoryUtils.put(
+                            AstRenderer.renderParameters(next, instance.getContainingFile(), null));
+                }
+                return true;
+            } else if (next.getType() == CPPTokenTypes.RPAREN) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (inFunctionParams && next != null && next.getType() == CPPTokenTypes.CSM_PARMLIST) {
+            if (instance != null) {
+                instance.functionParameters = RepositoryUtils.put(
+                        AstRenderer.renderParameters(next, instance.getContainingFile(), null));
+            }
+            return true;
         } else {
-            return null;
-        }         
+            return false;
+        }
     }
-    
-    private static String gatherChildrenText(AST ast) {
-	StringBuilder sb = new StringBuilder();
-	for( AST next = ast.getFirstChild(); next != null; next = next.getNextSibling() ) {
-	    if( sb.length() > 0 ) {
-		sb.append(',');
-	    }
-	    StringBuilder param = new StringBuilder();
-	    addText(param, next);
-	    sb.append(param);
-	}
-	return sb.toString();
-    }    
 
-    // copy-pasted from TypeImpl. The difference us in one check: ast.getType() != CPPTokenTypes.STAR
-    // it's so small that parametherising isn't better
-    private static void addText(StringBuilder sb, AST ast) {
-        if( ! (ast instanceof FakeAST) ) {
-            if( sb.length() > 0 && ast.getType() != CPPTokenTypes.STAR ) {
-                sb.append(' ');
-            }
-            sb.append(ast.getText());
-        }
-        for( AST token = ast.getFirstChild(); token != null; token = token.getNextSibling() ) {
-            addText(sb,  token);
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (functionParameters != null) {
+            RepositoryUtils.remove(functionParameters);
         }
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     // impl of persistent
-    
     @Override
     public void write(DataOutput output) throws IOException {
         super.write(output);
-	assert functionPointerParamList != null;
-	output.writeShort(functionPointerDepth);
-	output.writeUTF(this.functionPointerParamList.toString());
+        output.writeShort(functionPointerDepth);
+        UIDObjectFactory factory = UIDObjectFactory.getDefaultFactory();
+        factory.writeUIDCollection(functionParameters, output, false);
     }
 
     public TypeFunPtrImpl(DataInput input) throws IOException {
         super(input);
-	this.functionPointerDepth = input.readShort();
-        this.functionPointerParamList = NameCache.getManager().getString(input.readUTF());
+        functionPointerDepth = input.readShort();
+        UIDObjectFactory factory = UIDObjectFactory.getDefaultFactory();
+        functionParameters = factory.readUIDCollection(new ArrayList<CsmUID<CsmParameter>>(), input);
     }
-    
 }
