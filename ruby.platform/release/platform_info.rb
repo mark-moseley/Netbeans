9bdca4c3fb51e8b6dbd1619e69b298f3e5fb914d
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-12-05 12:37:42 +0100

    #154660: cannot detect native ruby interpreter when there are % in path. Patch by Nahum Dotan.

--- ruby.platform/release/platform_info.rb
+++ ruby.platform/release/platform_info.rb
@@ -4,9 +4,15 @@ require 'rbconfig'
 # properties file format so NetBeans can easily parse it through
 # java.util.Properties
 
+# See issue #154660
+def escape(path)
+  path.gsub('%', '%%')
+end
+
 # Interpreter info
-jruby = defined?(JRUBY_VERSION)
-$stdout.printf "ruby_kind=#{jruby ? "JRuby" : "Ruby"}\n"
+jruby = defined?(JRUBY_VERSION) || (defined?(RUBY_ENGINE) && 'jruby' == RUBY_ENGINE)
+rubinius = defined?(RUBY_ENGINE) && 'rbx' == RUBY_ENGINE
+$stdout.printf "ruby_kind=#{jruby ? "JRuby" : (rubinius ? "Rubinius" : "Ruby")}\n"
 $stdout.printf "ruby_version=#{RUBY_VERSION}\n"
 $stdout.printf "jruby_version=#{JRUBY_VERSION}\n" if jruby
 $stdout.printf "ruby_patchlevel=#{RUBY_PATCHLEVEL}\n" if defined? RUBY_PATCHLEVEL
@@ -14,16 +20,19 @@ $stdout.printf "ruby_release_date=#{RUBY_RELEASE_DATE}\n"
 RbConfig = Config unless defined?(RbConfig) # 1.8.4 support
 ruby = File.join(RbConfig::CONFIG["bindir"], RbConfig::CONFIG["ruby_install_name"])
 ruby << RbConfig::CONFIG["EXEEXT"]
-$stdout.printf "ruby_executable=#{ruby}\n"
+$stdout.printf "ruby_executable=#{escape(ruby)}\n"
 $stdout.printf "ruby_platform=#{RUBY_PLATFORM}\n"
+$stdout.printf "ruby_lib_dir=#{escape(RbConfig::CONFIG['rubylibdir'])}\n"
 
 # RubyGems info
 begin
   require 'rubygems'
-  $stdout.printf "gem_home=#{Gem.dir}\n" 
-  $stdout.printf "gem_path=#{Gem.path.join(":")}\n" 
-  $stdout.printf "gem_version=#{Gem::RubyGemsVersion} (#{Gem::RubyGemsPackageVersion})\n" 
-  $stdout.printf "\n" 
 rescue LoadError
   # no RubyGems installed
+  $stdout.printf "\n"
+  exit 0
 end
+$stdout.printf "gem_home=#{escape(Gem.dir)}\n"
+$stdout.printf "gem_path=#{Gem.path.map{|p| escape(p)}.join(":")}\n"
+$stdout.printf "gem_version=#{Gem::RubyGemsVersion}\n"
+$stdout.printf "\n"
