be945b68c297c4916eb80f52e60b472722ef0e31
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-12-08 16:55:57 +0100

    Implementation of 'Go to Declaration' for constant. Refactoring DeclarationFinder into smaller parts with base class. Fixing IndexedConstant signature, and other related tweaks.

---
+++ ruby/test/unit/data/testfiles/constants.rb
@@ -0,0 +1,22 @@
+module Colors
+
+  module Converter
+    VERSION = 1234
+    # module definition
+  end
+
+  RED   = "#FF0000"
+  GREEN = "#00FF00"
+  BLUE  = "#0000FF"
+
+  def red
+    return RED
+  end
+
+end
+
+Colors::RED.byte
+Colors::Converter::VERSION
+
+require 'rexml/rexml'
+REXML::COPYRIGHT.ls
