52ecba45183f8171c6b712de535e9e2b6396d138
Alexander Simon <alexvsimon@netbeans.org>
2008-12-12 16:44:26 +0300

    fixed IZ#152053:Starting Configure Code Assistance Wizard blocks AWT for a long time

--- cnd.discovery/src/org/netbeans/modules/cnd/discovery/wizard/SelectProviderPanel.java
+++ cnd.discovery/src/org/netbeans/modules/cnd/discovery/wizard/SelectProviderPanel.java
@@ -51,6 +51,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import org.netbeans.api.project.Project;
 import org.netbeans.modules.cnd.api.model.CsmFile;
+import org.netbeans.modules.cnd.api.model.CsmListeners;
 import org.netbeans.modules.cnd.api.model.CsmModelAccessor;
 import org.netbeans.modules.cnd.api.model.CsmProgressListener;
 import org.netbeans.modules.cnd.api.model.CsmProject;
@@ -58,6 +59,7 @@ import org.netbeans.modules.cnd.discovery.api.DiscoveryProvider;
 import org.netbeans.modules.cnd.discovery.api.ProjectProxy;
 import org.netbeans.modules.cnd.discovery.api.ProviderProperty;
 import org.netbeans.modules.cnd.discovery.wizard.api.DiscoveryDescriptor;
+import org.openide.WizardDescriptor;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
@@ -95,7 +97,7 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
             }
         };
         rootFolder.getDocument().addDocumentListener(documentListener);
-        CsmModelAccessor.getModel().addProgressListener(this);
+        CsmListeners.getDefault().addProgressListener(this);
     }
     
     /** This method is called from within the constructor to
@@ -111,6 +113,8 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
         rootFolderButton = new javax.swing.JButton();
         instructionPanel = new javax.swing.JPanel();
         instructionsTextArea = new javax.swing.JTextArea();
+        alertPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         labelForRoot = new javax.swing.JLabel();
         prividersComboBox = new javax.swing.JComboBox();
         labelForProviders = new javax.swing.JLabel();
@@ -140,6 +144,10 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
         gridBagConstraints.insets = new java.awt.Insets(0, 4, 0, 0);
         add(rootFolderButton, gridBagConstraints);
 
+        instructionPanel.setEnabled(false);
+        instructionPanel.setFocusable(false);
+        instructionPanel.setRequestFocusEnabled(false);
+        instructionPanel.setVerifyInputWhenFocusTarget(false);
         instructionPanel.setLayout(new java.awt.GridBagLayout());
 
         instructionsTextArea.setBackground(instructionPanel.getBackground());
@@ -148,12 +156,26 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
         instructionsTextArea.setWrapStyleWord(true);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
         instructionPanel.add(instructionsTextArea, gridBagConstraints);
+
+        alertPanel.setLayout(new java.awt.BorderLayout());
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/netbeans/modules/cnd/discovery/wizard/resources/info.png"))); // NOI18N
+        org.openide.awt.Mnemonics.setLocalizedText(jLabel1, org.openide.util.NbBundle.getMessage(SelectProviderPanel.class, "selectedAdvancedLabel")); // NOI18N
+        alertPanel.add(jLabel1, java.awt.BorderLayout.SOUTH);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTHWEST;
+        gridBagConstraints.weighty = 1.0;
+        instructionPanel.add(alertPanel, gridBagConstraints);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
@@ -258,8 +280,9 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
                 false
                 );
         int ret = fileChooser.showOpenDialog(this);
-        if (ret == JFileChooser.CANCEL_OPTION)
+        if (ret == JFileChooser.CANCEL_OPTION) {
             return;
+        }
         String path = fileChooser.getSelectedFile().getPath();
         //path = FilePathAdaptor.normalize(path);
         rootFolder.setText(path);
@@ -267,8 +290,10 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel alertPanel;
     private javax.swing.JPanel instructionPanel;
     private javax.swing.JTextArea instructionsTextArea;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel labelForProviders;
     private javax.swing.JLabel labelForRoot;
     private javax.swing.JComboBox prividersComboBox;
@@ -371,6 +396,7 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
     }
     
     boolean valid(DiscoveryDescriptor wizardDescriptor) {
+  	    wizardDescriptor.setMessage(null);
         String path = rootFolder.getText();
         File file = new File(path);
         if (!(file.exists() && file.isDirectory())) {
@@ -390,7 +416,13 @@ public final class SelectProviderPanel extends JPanel implements CsmProgressList
         }
         return true;
     }
-    
+
+   void showAlert(DiscoveryDescriptor wizardDescriptor){
+        Object o = ((WizardDescriptor)wizardDescriptor).getProperty("ShowAlert");
+        alertPanel.setVisible(Boolean.TRUE.equals(o));
+    }
+
+
     private String getString(String key) {
         return NbBundle.getBundle(SelectProviderPanel.class).getString(key);
     }
