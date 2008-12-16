7fdd74a828ae989703d1668ec6c62b96f16acaf3
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-12-16 13:25:30 +0100

    Go to Constant declaration does not work for constants declared withing local scope.

---
+++ ruby/test/unit/data/testfiles/constants.rb
@@ -0,0 +1,22 @@
+GLOBAL_CONSTANT = 'Global Constant'
+
+module Colors
+
+  module Converter
+    VERSION = 1234
+    # module definition
+  end
+
+  RED   = "#FF0000"
+  GREEN = "#00FF00"
+  BLUE  = "#0000FF"
+
+  def red
+    return RED
+  end
+
+end
+
+Colors::RED.byte
+Colors::Converter::VERSION
+puts GLOBAL_CONSTANT
