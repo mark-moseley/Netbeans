3e1e36c3248451fee63675de241bd80a2c8178b7
Dongmei Cao <dongmeic@netbeans.org>
2008-12-23 13:47:58 -0800

    Fix for #126642 - Refixed to have the DataFoloder in the Enterprise Beans and Server Resources node lookups so that the getTargetFolder() will never return null.

--- j2ee.api.ejbmodule/src/org/netbeans/modules/j2ee/spi/ejbjar/support/J2eeProjectView.java
+++ j2ee.api.ejbmodule/src/org/netbeans/modules/j2ee/spi/ejbjar/support/J2eeProjectView.java
@@ -60,8 +60,11 @@ import org.openide.loaders.ChangeableDataFilter;
 import org.openide.loaders.DataFilter;
 import org.openide.loaders.DataFolder;
 import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
+import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
@@ -100,11 +103,23 @@ public final class J2eeProjectView {
     }
     
     public static Node createServerResourcesNode (Project p) {
-        return new ServerResourceNode (p);
+        try {
+            return new ServerResourceNode(p);
+        } catch (DataObjectNotFoundException ex) {
+            // Should never happen
+            Exceptions.printStackTrace(ex);
+            return null;
+        }
     }
     
-    public static Node createEjbsView(EjbJar ejbModule, Project p) {
-        return new EjbContainerNode(ejbModule, p, getEjbNodesFactory());
+    public static Node createEjbsView(EjbJar ejbModule, Project p){
+        try {
+            return new EjbContainerNode(ejbModule, p, getEjbNodesFactory());
+        } catch (DataObjectNotFoundException ex) {
+            //Should not happen 
+            Exceptions.printStackTrace(ex);
+            return null;
+        }
     }
     
     public static Node createConfigFilesView (FileObject folder) {
@@ -115,7 +130,7 @@ public final class J2eeProjectView {
         private static final DataFilter VISIBILITY_QUERY_FILTER = new VisibilityQueryDataFilter();
 
         private static Image CONFIGURATION_FILES_BADGE =
-                Utilities.loadImage( "org/netbeans/modules/j2ee/ejbjar/project/ui/ejbjar.gif", true ); // NOI18N
+                ImageUtilities.loadImage( "org/netbeans/modules/j2ee/ejbjar/project/ui/ejbjar.gif", true ); // NOI18N
 
         public DocBaseNode(DataFolder folder) {
             super(folder.getNodeDelegate(), folder.createNodeChildren(VISIBILITY_QUERY_FILTER));
@@ -132,7 +147,7 @@ public final class J2eeProjectView {
         private Image computeIcon( boolean opened, int type ) {
             Node folderNode = getOriginal();
             Image image = opened ? folderNode.getOpenedIcon( type ) : folderNode.getIcon( type );
-            return Utilities.mergeImages( image, CONFIGURATION_FILES_BADGE, 7, 7 );
+            return ImageUtilities.mergeImages( image, CONFIGURATION_FILES_BADGE, 7, 7 );
         }
         
         public String getName() {
