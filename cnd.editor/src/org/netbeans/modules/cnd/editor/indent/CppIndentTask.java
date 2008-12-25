75e08a4967df8cb6e1703efdec0f52b6f4425302
alexvsimon@netbeans.org <none@none>
2008-12-25 11:53:33 +0300

    fixwd: IZ#155684:Comments broke code

--- cnd.editor/src/org/netbeans/modules/cnd/editor/indent/CppIndentTask.java
+++ cnd.editor/src/org/netbeans/modules/cnd/editor/indent/CppIndentTask.java
@@ -40,6 +40,7 @@ package org.netbeans.modules.cnd.editor.indent;
 
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
+import org.netbeans.api.lexer.Token;
 import org.netbeans.cnd.api.lexer.CndLexerUtilities;
 import org.netbeans.cnd.api.lexer.CppTokenId;
 import org.netbeans.modules.cnd.editor.api.CodeStyle;
@@ -107,8 +108,26 @@ public class CppIndentTask extends IndentSupport implements IndentTask {
             // leave untouched for now, (bug#22570)
             return -1;
         }
+        //if ((dotPos >= 1 && DocumentUtilities.getText(doc).charAt(dotPos-1) != '\\')
+        //    || (dotPos >= 2 && DocumentUtilities.getText(doc).charAt(dotPos-2) == '\\')) {
+        if (token.getTokenID() == CppTokenId.STRING_LITERAL || token.getTokenID() == CppTokenId.CHAR_LITERAL) {
+            int start = token.getTokenSequence().offset();
+            Token<CppTokenId> tok = token.getTokenSequence().token();
+            if (start < caretOffset && caretOffset < start + tok.length()) {
+                // if insede literal
+                if (caretOffset >= start + 2 && tok.text().charAt(caretOffset - start - 2) == '\\') {
+                    if (!(caretOffset > start + 2 && tok.text().charAt(caretOffset - start - 3) == '\\')) {
+                        return -1;
+                    }
+                }
+            }
+        }
+
         if (token.getTokenID() == CppTokenId.BLOCK_COMMENT || token.getTokenID() == CppTokenId.DOXYGEN_COMMENT){
             if (isMultiLineComment(token)) {
+                if (caretOffset == token.getTokenSequence().offset()){
+                    return findIndent(token);
+                }
                 // Indent the inner lines of the multi-line comment by one
                 if (!getFormatLeadingStarInComment()) {
                     return findIndent(token) + 1;
@@ -335,8 +354,6 @@ public class CppIndentTask extends IndentSupport implements IndentTask {
                                     indent = getTokenIndent(tt);
                                     break;
                             }
-                        } else {
-                            indent = getTokenIndent(tt);
                         }
                         break;
 
