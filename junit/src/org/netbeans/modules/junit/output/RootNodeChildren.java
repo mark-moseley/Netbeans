74e3dc01c2fcc41236ec606bcc69bafc2c8b6125
Michael Chernyshov <cms@netbeans.org>
2008-12-25 18:09:51 +0300

    #145850 fix, part two (fixed regression in incorrect test numbers calculation)

--- junit/src/org/netbeans/modules/junit/output/RootNodeChildren.java
+++ junit/src/org/netbeans/modules/junit/output/RootNodeChildren.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -112,13 +112,12 @@ final class RootNodeChildren extends Children.Array {
         assert EventQueue.isDispatchThread();
         assert (runningSuiteNode != null)
                == (live && (runningSuiteName != null));
-        
+
         TestsuiteNode correspondingNode;
         
         if (reports == null) {
             reports = new ArrayList<Report>(10);
         }
-        reports.add(report);
 
         final boolean isPassedSuite = updateStatistics(report);
         
@@ -138,7 +137,11 @@ final class RootNodeChildren extends Children.Array {
                 correspondingNode = null;
             }
         } else {
-            if (live && !(filtered && isPassedSuite)) {
+            if (live && 
+                    !(filtered && isPassedSuite)) {
+                if (reports.contains(report)) {
+                    removeSuite(report);
+                }
                 add(new Node[] {
                     correspondingNode = createNode(report)});
             } else {
@@ -148,6 +151,8 @@ final class RootNodeChildren extends Children.Array {
         
         assert runningSuiteName == null;
         assert runningSuiteNode == null;
+
+        reports.add(report);
         
         return correspondingNode;
     }
@@ -231,8 +236,6 @@ final class RootNodeChildren extends Children.Array {
     @Override
     protected void removeNotify() {
         super.removeNotify();
-        
-        remove(getNodes());               //PENDING
         live = false;
     }
     
@@ -248,19 +251,19 @@ final class RootNodeChildren extends Children.Array {
                                ? matchingNodesCount + 1
                                : matchingNodesCount;
         if (nodesCount != 0) {
-            final Node[] nodes = new Node[nodesCount];
+            final Node[] nods = new Node[nodesCount];
             final Iterator<Report> i = reports.iterator();
             int index = 0;
             while (index < matchingNodesCount) {
                 Report report = i.next();
                 if (!filterOn || report.containsFailed()) {
-                    nodes[index++] = createNode(report);
+                    nods[index++] = createNode(report);
                 }
             }
             if (runningSuiteNode != null) {
-                nodes[index++] = runningSuiteNode;
+                nods[index++] = runningSuiteNode;
             }
-            add(nodes);
+            add(nods);
         }
     }
     
@@ -289,9 +292,6 @@ final class RootNodeChildren extends Children.Array {
         if (!live) {
             return;
         }
-        if (passedSuites == 0) {
-            return;
-        }
 
         if (filtered) {
             removePassedSuites();
@@ -299,7 +299,30 @@ final class RootNodeChildren extends Children.Array {
             addPassedSuites();
         }
     }
-    
+
+    /**
+     */
+    private void removeSuite(Report rep) {
+        assert EventQueue.isDispatchThread();
+        assert live;
+
+        final Node[] nods = getNodes();
+        for (int index = 0;
+                    index < nods.length;
+                    index++) {
+            TestsuiteNode node = (TestsuiteNode) nods[index];
+            Report report = node.getReport();
+            if (report == null) {
+                continue;
+            }
+            if (report.equals(rep)) {
+                remove(new Node[] {node});
+                break;
+            }
+        }
+    }
+
+
     /**
      */
     private void removePassedSuites() {
@@ -307,12 +330,12 @@ final class RootNodeChildren extends Children.Array {
         assert live;
         
         final Node[] nodesToRemove = new Node[passedSuites];
-        final Node[] nodes = getNodes();
+        final Node[] nods = getNodes();
         int nodesIndex = 0;
         for (int index = 0;
                     index < nodesToRemove.length;
                     nodesIndex++) {
-            TestsuiteNode node = (TestsuiteNode) nodes[nodesIndex];
+            TestsuiteNode node = (TestsuiteNode) nods[nodesIndex];
             Report report = node.getReport();
             if (report == null) {
                 continue;
@@ -323,12 +346,12 @@ final class RootNodeChildren extends Children.Array {
                 node.setFiltered(filtered);
             }
         }
-        while (nodesIndex < nodes.length) {
+        while (nodesIndex < nods.length) {
             Report report;
-            assert (report = ((TestsuiteNode) nodes[nodesIndex]).getReport())
+            assert (report = ((TestsuiteNode) nods[nodesIndex]).getReport())
                            == null
                    || report.containsFailed();
-            ((TestsuiteNode) nodes[nodesIndex++]).setFiltered(filtered);
+            ((TestsuiteNode) nods[nodesIndex++]).setFiltered(filtered);
         }
         remove(nodesToRemove);
     }
