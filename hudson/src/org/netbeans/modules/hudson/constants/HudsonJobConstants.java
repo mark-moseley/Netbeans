b5eb0581c25df42ba4a62ed6286de613fb66275d
Jesse Glick <jglick@netbeans.org>
2009-01-07 16:40:41 -0500

    Stylistic changes to how constants are defined. Use static imports rather than implementing interfaces. Remove excessive HUDSON_ prefixes on constant names and hudson_ prefix on values.

---
+++ hudson/src/org/netbeans/modules/hudson/constants/HudsonJobConstants.java
@@ -0,0 +1,55 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.hudson.constants;
+
+/**
+ * Constants provider for HudsonJob
+ *
+ * @author Michal Mocnak
+ */
+public class HudsonJobConstants {
+
+    private HudsonJobConstants() {}
+    
+    public static final String JOB_DISPLAY_NAME = "job_display_name";
+    
+    public static final String JOB_NAME = "job_name";
+    
+    public static final String JOB_DESCRIPTION = "job_description";
+    
+    public static final String JOB_URL = "job_url";
+    
+    public static final String JOB_VCS_ROOT = "job_vcs_root";
+    
+    public static final String JOB_COLOR = "job_color";
+    
+    public static final String JOB_IN_QUEUE = "job_in_queue";
+    
+    public static final String JOB_BUILDABLE = "job_buildable";
+    
+    public static final String JOB_LAST_BUILD = "job_last_build";
+    
+    public static final String JOB_LAST_STABLE_BUILD = "job_last_stable_build";
+    
+    public static final String JOB_LAST_SUCCESSFUL_BUILD = "job_last_successful_build";
+    
+    public static final String JOB_LAST_FAILED_BUILD = "job_last_failed_build";
+
+}
