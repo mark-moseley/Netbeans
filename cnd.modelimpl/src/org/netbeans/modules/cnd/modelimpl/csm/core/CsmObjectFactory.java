26dd7b09834ee9d04cfe2780877ad8507510b58a
Alexander Simon <alexvsimon@netbeans.org>
2009-01-15 19:48:52 +0300

    no IZ: reduce storage consumption for parameters and types

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/CsmObjectFactory.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/CsmObjectFactory.java
@@ -47,6 +47,7 @@ import java.io.IOException;
 import org.netbeans.modules.cnd.api.model.CsmFriendFunction;
 import org.netbeans.modules.cnd.modelimpl.csm.ClassForwardDeclarationImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.ClassImpl;
+import org.netbeans.modules.cnd.modelimpl.csm.ClassImplSpecialization;
 import org.netbeans.modules.cnd.modelimpl.csm.ConstructorDDImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.ConstructorDefinitionImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.ConstructorImpl;
@@ -56,6 +57,7 @@ import org.netbeans.modules.cnd.modelimpl.csm.DestructorImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.EnumImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.EnumeratorImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.FieldImpl;
+import org.netbeans.modules.cnd.modelimpl.csm.ForwardClass;
 import org.netbeans.modules.cnd.modelimpl.csm.FriendClassImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.FriendFunctionDDImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.FriendFunctionDefinitionImpl;
@@ -65,6 +67,7 @@ import org.netbeans.modules.cnd.modelimpl.csm.FunctionDDImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.FunctionDefinitionImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.FunctionImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.FunctionImplEx;
+import org.netbeans.modules.cnd.modelimpl.csm.FunctionParameterListImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.IncludeImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.MacroImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.MethodDDImpl;
@@ -72,7 +75,9 @@ import org.netbeans.modules.cnd.modelimpl.csm.MethodImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.NamespaceAliasImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.NamespaceDefinitionImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.NamespaceImpl;
+import org.netbeans.modules.cnd.modelimpl.csm.ParameterEllipsisImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.ParameterImpl;
+import org.netbeans.modules.cnd.modelimpl.csm.ParameterListImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.TypedefImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.UsingDeclarationImpl;
 import org.netbeans.modules.cnd.modelimpl.csm.UsingDirectiveImpl;
@@ -88,6 +93,9 @@ import org.netbeans.modules.cnd.repository.support.SelfPersistent;
  * objects factory
  * @author Vladimir Voskresensky
  */
+/* XXX typo in interface name?
+@org.openide.util.lookup.ServiceProvider(service=org.netbeans.modules.cnd.repository.spi.PersistentObjectFactory.class)
+*/
 public final class CsmObjectFactory extends AbstractObjectFactory implements PersistentFactory {
     
     private static final CsmObjectFactory instance = new CsmObjectFactory();
@@ -127,7 +135,13 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
         } else if (object instanceof EnumImpl) {
             aHandler = ENUM_IMPL;
         } else if (object instanceof ClassImpl) {
-            aHandler = CLASS_IMPL;
+            if (object instanceof ClassImplSpecialization) {
+                aHandler = CLASS_IMPL_SPECIALIZATION;
+            } else if (object instanceof ForwardClass) {
+                aHandler = FORWARD_CLASS;
+            } else {
+                aHandler = CLASS_IMPL;
+            }
         } else if (object instanceof TypedefImpl) {
             if (object instanceof ClassImpl.MemberTypedef) {
                 aHandler = MEMBER_TYPEDEF;
@@ -145,7 +159,11 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
         } else if (object instanceof UsingDirectiveImpl) {
             aHandler = USING_DIRECTIVE_IMPL;
         } else if (object instanceof ClassForwardDeclarationImpl) {
-            aHandler = CLASS_FORWARD_DECLARATION_IMPL;
+            if (object instanceof ClassImpl.ClassMemberForwardDeclaration) {
+                aHandler = CLASS_MEMBER_FORWARD_DECLARATION;
+            } else {
+                aHandler = CLASS_FORWARD_DECLARATION_IMPL;
+            }
         } else if (object instanceof FunctionImpl) {
             // we have several FunctionImpl subclasses
             if (object instanceof FunctionImplEx) {
@@ -206,6 +224,8 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
                 aHandler = VARIABLE_DEF_IMPL;
             } else if (object instanceof FieldImpl) {
                 aHandler = FIELD_IMPL;
+            } else if (object instanceof ParameterEllipsisImpl) {
+                aHandler = PARAMETER_ELLIPSIS_IMPL;
             } else if (object instanceof ParameterImpl) {
                 aHandler = PARAMETER_IMPL;
             } else {
@@ -215,6 +235,14 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
             aHandler = ENUMERATOR_IMPL;
         } else if (object instanceof IncludeImpl) {
             aHandler = INCLUDE_IMPL;
+        } else if (object instanceof ParameterListImpl) {
+            aHandler = PARAM_LIST_IMPL;
+            if (object instanceof FunctionParameterListImpl) {
+                aHandler = FUNCTION_PARAM_LIST_IMPL;
+                if (object instanceof FunctionParameterListImpl.FunctionKnRParameterListImpl) {
+                    aHandler = FUNCTION_KR_PARAM_LIST_IMPL;
+                }
+            }
         } else if (object instanceof MacroImpl) {
             aHandler = MACRO_IMPL;
         } else if (object instanceof FriendClassImpl) {
@@ -266,6 +294,14 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
             case CLASS_IMPL:
                 obj = new ClassImpl(stream);
                 break;
+
+            case CLASS_IMPL_SPECIALIZATION:
+                obj = new ClassImplSpecialization(stream);
+                break;
+
+            case FORWARD_CLASS:
+                obj = new ForwardClass(stream);
+                break;
                 
             case TYPEDEF_IMPL:
                 obj = new TypedefImpl(stream);
@@ -297,6 +333,10 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
                 
             case CLASS_FORWARD_DECLARATION_IMPL:
                 obj = new ClassForwardDeclarationImpl(stream);
+                break;
+
+            case CLASS_MEMBER_FORWARD_DECLARATION:
+                obj = new ClassImpl.ClassMemberForwardDeclaration(stream);
                 break;
                 
             case FUNCTION_IMPL:
@@ -358,6 +398,10 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
             case PARAMETER_IMPL:
                 obj = new ParameterImpl(stream);
                 break;
+
+            case PARAMETER_ELLIPSIS_IMPL:
+                obj = new ParameterEllipsisImpl(stream);
+                break;
                 
             case VARIABLE_IMPL:
                 obj = new VariableImpl(stream);
@@ -371,6 +415,18 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
                 obj = new IncludeImpl(stream);
                 break;
                 
+            case PARAM_LIST_IMPL:
+                obj = new ParameterListImpl(stream);
+                break;
+
+            case FUNCTION_PARAM_LIST_IMPL:
+                obj = new FunctionParameterListImpl(stream);
+                break;
+
+            case FUNCTION_KR_PARAM_LIST_IMPL:
+                obj = new FunctionParameterListImpl.FunctionKnRParameterListImpl(stream);
+                break;
+
             case MACRO_IMPL:
                 obj = new MacroImpl(stream);
                 break;
@@ -429,7 +485,9 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
     private static final int DECLARATION_CONTAINER	    = GRAPH_CONTAINER + 1;
     private static final int FILE_IMPL                      = DECLARATION_CONTAINER + 1;
     private static final int ENUM_IMPL                      = FILE_IMPL + 1;
-    private static final int CLASS_IMPL                     = ENUM_IMPL + 1;
+    private static final int CLASS_IMPL_SPECIALIZATION      = ENUM_IMPL + 1;
+    private static final int FORWARD_CLASS                  = CLASS_IMPL_SPECIALIZATION + 1;
+    private static final int CLASS_IMPL                     = FORWARD_CLASS + 1;
 //    private static final int UNRESOLVED_FILE                = CLASS_IMPL + 1;
 //    private static final int UNRESOLVED_CLASS               = UNRESOLVED_FILE + 1;
 //    private static final int TYPEDEF_IMPL                   = UNRESOLVED_CLASS + 1;
@@ -441,7 +499,8 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
     private static final int USING_DECLARATION_IMPL         = NAMESPACE_ALIAS_IMPL + 1;
     private static final int USING_DIRECTIVE_IMPL           = USING_DECLARATION_IMPL + 1;
     private static final int CLASS_FORWARD_DECLARATION_IMPL = USING_DIRECTIVE_IMPL + 1;   
-    private static final int FRIEND_CLASS_IMPL              = CLASS_FORWARD_DECLARATION_IMPL + 1;   
+    private static final int CLASS_MEMBER_FORWARD_DECLARATION = CLASS_FORWARD_DECLARATION_IMPL + 1;   
+    private static final int FRIEND_CLASS_IMPL              = CLASS_MEMBER_FORWARD_DECLARATION + 1;   
                 
     // functions
     private static final int FUNCTION_IMPL                  = FRIEND_CLASS_IMPL + 1;
@@ -474,11 +533,15 @@ public final class CsmObjectFactory extends AbstractObjectFactory implements Per
     private static final int VARIABLE_DEF_IMPL              = VARIABLE_IMPL + 1;
     private static final int FIELD_IMPL                     = VARIABLE_DEF_IMPL + 1;
     private static final int PARAMETER_IMPL                 = FIELD_IMPL + 1;
+    private static final int PARAMETER_ELLIPSIS_IMPL        = PARAMETER_IMPL + 1;
     
-    private static final int ENUMERATOR_IMPL                = PARAMETER_IMPL + 1;
+    private static final int ENUMERATOR_IMPL                = PARAMETER_ELLIPSIS_IMPL + 1;
 
     private static final int INCLUDE_IMPL                   = ENUMERATOR_IMPL + 1;
-    private static final int MACRO_IMPL                     = INCLUDE_IMPL + 1;
+    private static final int PARAM_LIST_IMPL                = INCLUDE_IMPL + 1;
+    private static final int FUNCTION_PARAM_LIST_IMPL       = PARAM_LIST_IMPL + 1;
+    private static final int FUNCTION_KR_PARAM_LIST_IMPL    = FUNCTION_PARAM_LIST_IMPL + 1;
+    private static final int MACRO_IMPL                     = FUNCTION_KR_PARAM_LIST_IMPL + 1;
     
     // index to be used in another factory (but only in one) 
     // to start own indeces from the next after LAST_INDEX        
