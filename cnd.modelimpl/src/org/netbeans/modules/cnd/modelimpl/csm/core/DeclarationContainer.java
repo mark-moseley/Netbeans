e5d058f38eb8e5a098ef88e1cef82f9c825d003a
Egor Ushakov <gorrus@netbeans.org>
2009-01-15 12:11:56 +0300

    fixing IZ:155936 (unchecked warnings)

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/DeclarationContainer.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/DeclarationContainer.java
@@ -63,10 +63,13 @@ import org.netbeans.modules.cnd.api.model.CsmFriend;
 import org.netbeans.modules.cnd.api.model.CsmFriendClass;
 import org.netbeans.modules.cnd.api.model.CsmFriendFunction;
 import org.netbeans.modules.cnd.api.model.CsmFunction;
+import org.netbeans.modules.cnd.api.model.CsmNamespace;
 import org.netbeans.modules.cnd.api.model.CsmOffsetableDeclaration;
 import org.netbeans.modules.cnd.api.model.CsmUID;
 import org.netbeans.modules.cnd.api.model.util.CsmKindUtilities;
+import org.netbeans.modules.cnd.modelimpl.debug.DiagnosticExceptoins;
 import org.netbeans.modules.cnd.modelimpl.repository.DeclarationContainerKey;
+import org.netbeans.modules.cnd.modelimpl.repository.NamespaceDeclararationContainerKey;
 import org.netbeans.modules.cnd.modelimpl.repository.RepositoryUtils;
 import org.netbeans.modules.cnd.modelimpl.uid.UIDCsmConverter;
 import org.netbeans.modules.cnd.modelimpl.uid.UIDObjectFactory;
@@ -76,20 +79,39 @@ import org.netbeans.modules.cnd.utils.cache.CharSequenceKey;
 import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
 
 /**
- * Storage for project declarations. Class was extracted from ProjectBase.
+ * Storage for project or namespace declarations.
  * @author Alexander Simon
  */
-/*package-local*/ class DeclarationContainer extends ProjectComponent implements Persistent, SelfPersistent {
+public class DeclarationContainer extends ProjectComponent implements Persistent, SelfPersistent {
     
     private SortedMap<CharSequence, Object> declarations = new TreeMap<CharSequence, Object>(CharSequenceKey.Comparator);
     private ReadWriteLock declarationsLock = new ReentrantReadWriteLock();
     
     private Map<CharSequence, Set<CsmUID<? extends CsmFriend>>> friends = new ConcurrentHashMap<CharSequence, Set<CsmUID<? extends CsmFriend>>>();
+    
+    // empty stub
+    private static final DeclarationContainer EMPTY = new DeclarationContainer() {
+
+        @Override
+        public void put() {
+        }
+
+        @Override
+        public void putDeclaration(CsmOffsetableDeclaration decl) {
+        }
+
+    };
 
     /** Creates a new instance of ProjectDeclarations */
     public DeclarationContainer(ProjectBase project) {
 	super(new DeclarationContainerKey(project.getUniqueName().toString()));
 	put();
+    }
+
+    /** Creates a new instance of ProjectDeclarations */
+    public DeclarationContainer(CsmNamespace ns) {
+        super(new NamespaceDeclararationContainerKey(ns));
+        put();
     }
     
     public DeclarationContainer(DataInput input) throws IOException {
@@ -97,9 +119,17 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
 	read(input);
     }
 
+    // only for EMPTY static field
+    private DeclarationContainer() {
+        super((org.netbeans.modules.cnd.repository.spi.Key)null);
+    }
+
+    public static DeclarationContainer empty() {
+        return EMPTY;
+    }
+
     public void removeDeclaration(CsmDeclaration decl) {
 	CharSequence uniqueName = CharSequenceKey.create(decl.getUniqueName());
-//	synchronized(declarations){
 	Object o = null;
 	try {
 	    declarationsLock.writeLock().lock();
@@ -144,7 +174,6 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
 	    declarationsLock.writeLock().unlock();
 	}
 	removeFriend(decl);
-//	}
 	put();
     }
     
@@ -177,7 +206,6 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
         @SuppressWarnings("unchecked")
 	CsmUID<CsmOffsetableDeclaration> uid = RepositoryUtils.put(decl);
 	assert uid != null;
-//            synchronized(declarations) {
 	try {
 	    declarationsLock.writeLock().lock();
 
@@ -218,7 +246,6 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
 	    declarationsLock.writeLock().unlock();
 	}
 	putFriend(decl);
-//            }
     }
     
     private void putFriend(CsmDeclaration decl){
@@ -244,29 +271,60 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
 	put();
     }
     
-    public Collection<CsmOffsetableDeclaration> getDeclarationsRange(CharSequence from, CharSequence to) {
-        List<CsmUID<CsmOffsetableDeclaration>> list = new ArrayList<CsmUID<CsmOffsetableDeclaration>>();
+    public Collection<CsmUID<CsmOffsetableDeclaration>> getUIDsRange(CharSequence from, CharSequence to) {
+        Collection<CsmUID<CsmOffsetableDeclaration>> list = new ArrayList<CsmUID<CsmOffsetableDeclaration>>();
         from = CharSequenceKey.create(from);
         to = CharSequenceKey.create(to);
         try {
             declarationsLock.readLock().lock();
             for (Map.Entry<CharSequence, Object> entry : declarations.subMap(from, to).entrySet()){
-                Object o = entry.getValue();
-                if (o instanceof CsmUID[]) {
-                    CsmUID[] uids = (CsmUID[])o;
-                    for(CsmUID uid:uids){
-                        list.add(uid);
-                    }
-                } else if (o instanceof CsmUID){
-                    list.add((CsmUID)o);
-                }
+                addAll(list, entry.getValue());
             }
         } finally {
             declarationsLock.readLock().unlock();
         }
-        return UIDCsmConverter.UIDsToDeclarations(list);
+        return list;
+    }
+
+    /**
+     * Adds ether object to the collection or array of objects
+     * @param list
+     * @param o - can be CsmUID or CsmUID[]
+     */
+    private static void addAll(Collection<CsmUID<CsmOffsetableDeclaration>> list, Object o) {
+        if (o instanceof CsmUID<?>[]) {
+            // we know the template type to be CsmOffsetableDeclaration
+            @SuppressWarnings("unchecked") // checked
+            final CsmUID<CsmOffsetableDeclaration>[] uids = (CsmUID<CsmOffsetableDeclaration>[]) o;
+            for (CsmUID<CsmOffsetableDeclaration> uid : uids) {
+                list.add(uid);
+            }
+        } else if (o instanceof CsmUID<?>) {
+            // we know the template type to be CsmOffsetableDeclaration
+            @SuppressWarnings("unchecked") // checked
+            final CsmUID<CsmOffsetableDeclaration> uid = (CsmUID<CsmOffsetableDeclaration>) o;
+            list.add(uid);
+        }
     }
     
+    public Collection<CsmOffsetableDeclaration> getDeclarationsRange(CharSequence from, CharSequence to) {
+        return UIDCsmConverter.UIDsToDeclarations(getUIDsRange(from, to));
+    }
+
+    public Collection<CsmUID<CsmOffsetableDeclaration>> getDeclarationsUIDs() {
+        // add all declarations
+        Collection<CsmUID<CsmOffsetableDeclaration>> list = new ArrayList<CsmUID<CsmOffsetableDeclaration>>();
+        try {
+            declarationsLock.readLock().lock();
+            for (Object o : declarations.values()) {
+                addAll(list, o);
+            }
+        } finally {
+            declarationsLock.readLock().unlock();
+        }
+        return list;
+    }
+
     public Collection<CsmFriend> findFriends(CsmOffsetableDeclaration decl){
         CharSequence name = null;
         if (CsmKindUtilities.isClass(decl)) {
@@ -311,19 +369,11 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
     }
     
     public Collection<CsmOffsetableDeclaration> findDeclarations(CharSequence uniqueName) {
-        List<CsmUID<CsmOffsetableDeclaration>> list = new ArrayList<CsmUID<CsmOffsetableDeclaration>>();
+        Collection<CsmUID<CsmOffsetableDeclaration>> list = new ArrayList<CsmUID<CsmOffsetableDeclaration>>();
         uniqueName = CharSequenceKey.create(uniqueName);
         try {
             declarationsLock.readLock().lock();
-            Object o = declarations.get(uniqueName);
-            if (o instanceof CsmUID[]) {
-                CsmUID[] uids = (CsmUID[])o;
-                for(CsmUID uid:uids){
-                    list.add(uid);
-                }
-            } else if (o instanceof CsmUID){
-                list.add((CsmUID)o);
-            }
+            addAll(list, declarations.get(uniqueName));
         } finally {
             declarationsLock.readLock().unlock();
         }
@@ -337,16 +387,24 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
         try {
             declarationsLock.readLock().lock();
             Object o = declarations.get(uniqueName);
-            if (o instanceof CsmUID[]) {
-                uid = ((CsmUID[])o)[0];
-            } else if (o instanceof CsmUID){
-                uid = (CsmUID)o;
+            if (o instanceof CsmUID<?>[]) {
+                // we know the template type to be CsmDeclaration
+                @SuppressWarnings("unchecked") // checked
+                final CsmUID<CsmDeclaration>[] uids = (CsmUID<CsmDeclaration>[]) o;
+                uid = uids[0];
+            } else if (o instanceof CsmUID<?>) {
+                // we know the template type to be CsmDeclaration
+                @SuppressWarnings("unchecked") // checked
+                final CsmUID<CsmDeclaration> uidt = (CsmUID<CsmDeclaration>) o;
+                uid = uidt;
             }
         } finally {
             declarationsLock.readLock().unlock();
         }
         result = UIDCsmConverter.UIDtoDeclaration(uid);
-        assert result != null || uid == null : "no declaration for UID " + uid;
+        if (uid != null && result == null) {
+            DiagnosticExceptoins.register(new IllegalStateException("no declaration for UID " + uid)); // NOI18N
+        }
         return result;
     }
     
@@ -388,7 +446,7 @@ import org.netbeans.modules.cnd.modelimpl.textcache.UniqueNameCache;
         if (decl1 != null && decl2 != null){
             CsmFile file1 = decl1.getContainingFile();
             CsmFile file2 = decl2.getContainingFile();
-            if (file1 != null && file1 != null) {
+            if (file1 != null && file2 != null) {
                 return file1.equals(file2);
             }
         }
