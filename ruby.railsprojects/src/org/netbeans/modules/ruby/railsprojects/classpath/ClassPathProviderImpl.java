45b0e8c6f5a8053b7086fb68bf3831a9e20ab314
Martin Krauskopf <mkrauskopf@netbeans.org>
2009-01-26 18:21:49 +0100

    GSF -> CSL: do not use ClassPath's IDs

--- ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/classpath/ClassPathProviderImpl.java
+++ ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/classpath/ClassPathProviderImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -43,23 +43,22 @@ package org.netbeans.modules.ruby.railsprojects.classpath;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
-import java.util.Map;
 import java.util.HashMap;
-import org.netbeans.api.gsfpath.classpath.ClassPath;
+import java.util.Map;
+import org.netbeans.api.java.classpath.ClassPath;
 import org.netbeans.api.project.SourceGroup;
+import org.netbeans.modules.ruby.RubyLanguage;
 import org.netbeans.modules.ruby.railsprojects.SourceRoots;
-import org.netbeans.modules.ruby.railsprojects.classpath.SourcePathImplementation;
 import org.netbeans.modules.ruby.spi.project.support.rake.PropertyEvaluator;
-import org.netbeans.spi.gsfpath.classpath.ClassPathFactory;
-import org.netbeans.spi.gsfpath.classpath.ClassPathProvider;
 import org.netbeans.modules.ruby.spi.project.support.rake.RakeProjectHelper;
-import org.netbeans.spi.gsfpath.classpath.support.ClassPathSupport;
+import org.netbeans.spi.java.classpath.ClassPathFactory;
+import org.netbeans.spi.java.classpath.ClassPathProvider;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.WeakListeners;
 
 /**
- * Defines the various class paths for a J2SE project.
+ * Defines the various load paths for a Rails project.
  */
 public final class ClassPathProviderImpl implements ClassPathProvider, PropertyChangeListener {
 
@@ -78,7 +77,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     private final PropertyEvaluator evaluator;
     private final SourceRoots sourceRoots;
     private final SourceRoots testSourceRoots;
-    private final ClassPath[] cache = new ClassPath[8];
+    private final ClassPath[] cache = new ClassPath[9];
 
     private final Map<String,FileObject> dirCache = new HashMap<String,FileObject>();
 
@@ -94,7 +93,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     }
 
     private synchronized FileObject getDir(String propname) {
-        FileObject fo = (FileObject) this.dirCache.get (propname);
+        FileObject fo = this.dirCache.get(propname);
         if (fo == null ||  !fo.isValid()) {
             String prop = evaluator.getProperty(propname);
             if (prop != null) {
@@ -108,6 +107,29 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     
     private FileObject[] getPrimarySrcPath() {
         return this.sourceRoots.getRoots();
+    }
+
+    private ClassPath getPublicWebClassPath() {
+        ClassPath cp = cache[8];
+        if (cp == null) {
+            cp = ClassPathFactory.createClassPath(new PublicClassPathImplementation(projectDirectory));
+            cache[8] = cp;
+            return cp;
+        } else {
+            return cp;
+        }
+    }
+ 
+    private ClassPath getJavascriptsWebClassPath() {
+        ClassPath cp = cache[8];
+        if (cp == null) {
+            cp = ClassPathFactory.createClassPath(new JavascriptsClassPathImplementation(projectDirectory));
+            cache[8] = cp;
+            
+            return cp;
+        } else {
+            return cp;
+        }
     }
     
     private FileObject[] getTestSrcDir() {
@@ -143,98 +165,8 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         // separate test roots etc.
         // TODO - check if files are even within the project?
         return 0;
-//        FileObject[] srcPath = getPrimarySrcPath();
-//        for (int i=0; i < srcPath.length; i++) {
-//            FileObject root = srcPath[i];
-//            if (root.equals(file) || FileUtil.isParentOf(root, file)) {
-//                return 0;
-//            }
-//        }        
-//        srcPath = getTestSrcDir();
-//        for (int i=0; i< srcPath.length; i++) {
-//            FileObject root = srcPath[i];
-//            if (root.equals(file) || FileUtil.isParentOf(root, file)) {
-//                return 1;
-//            }
-//        }
-//        FileObject dir = getBuildClassesDir();
-//        if (dir != null && (dir.equals(file) || FileUtil.isParentOf(dir, file))) {
-//            return 2;
-//        }
-//        dir = getDistJar(); // not really a dir at all, of course
-//        if (dir != null && dir.equals(FileUtil.getArchiveFile(file))) {
-//            // XXX check whether this is really the root
-//            return 4;
-//        }
-//        dir = getBuildTestClassesDir();
-//        if (dir != null && (dir.equals(file) || FileUtil.isParentOf(dir,file))) {
-//            return 3;
-//        }
-//        return -1;
     }
     
-//    private synchronized ClassPath getCompileTimeClasspath(FileObject file) {
-//        int type = getType(file);
-//        return this.getCompileTimeClasspath(type);
-//    }
-//    
-//    private ClassPath getCompileTimeClasspath(int type) {        
-//        if (type < 0 || type > 1) {
-//            // Not a source file.
-//            return null;
-//        }
-//        ClassPath cp = cache[2+type];
-//        if ( cp == null) {            
-//            if (type == 0) {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {JAVAC_CLASSPATH})); // NOI18N
-//            }
-//            else {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {JAVAC_TEST_CLASSPATH})); // NOI18N
-//            }
-//            cache[2+type] = cp;
-//        }
-//        return cp;
-//    }
-//    
-//    private synchronized ClassPath getRunTimeClasspath(FileObject file) {
-//        int type = getType(file);
-//        if (type < 0 || type > 4) {
-//            // Unregistered file, or in a JAR.
-//            // For jar:file:$projdir/dist/*.jar!/**/*.class, it is misleading to use
-//            // run.classpath since that does not actually contain the file!
-//            // (It contains file:$projdir/build/classes/ instead.)
-//            return null;
-//        } else if (type > 1) {
-//            type-=2;            //Compiled source transform into source
-//        }
-//        ClassPath cp = cache[4+type];
-//        if ( cp == null) {
-//            if (type == 0) {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {RUN_CLASSPATH})); // NOI18N
-//            }
-//            else if (type == 1) {
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {RUN_TEST_CLASSPATH})); // NOI18N
-//            }
-//            else if (type == 2) {
-//                //Only to make the CompiledDataNode hapy
-//                //Todo: Strictly it should return ${run.classpath} - ${build.classes.dir} + ${dist.jar}
-//                cp = ClassPathFactory.createClassPath(
-//                    ProjectClassPathSupport.createPropertyBasedClassPathImplementation(
-//                    projectDirectory, evaluator, new String[] {DIST_JAR})); // NOI18N
-//            }
-//            cache[4+type] = cp;
-//        }
-//        return cp;
-//    }
-//    
     private synchronized ClassPath getSourcepath(FileObject file) {
         int type = getType(file);
         return this.getSourcepath(type);
@@ -251,7 +183,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
                     cp = ClassPathFactory.createClassPath(new SourcePathImplementation (this.sourceRoots, helper, evaluator));
                     break;
                 case 1:
-                    cp = ClassPathFactory.createClassPath(new SourcePathImplementation (this.testSourceRoots));
+                    cp = ClassPathFactory.createClassPath(new SourcePathImplementation(this.testSourceRoots));
                     break;
             }
         }
@@ -261,7 +193,7 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     
     private synchronized ClassPath getBootClassPath() {
         ClassPath cp = cache[7];
-        if ( cp== null ) {
+        if (cp == null) {
             cp = ClassPathFactory.createClassPath(new BootClassPathImplementation(projectDirectory, evaluator));
             cache[7] = cp;
         }
@@ -269,15 +201,17 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
     }
     
     public ClassPath findClassPath(FileObject file, String type) {
-        /*if (type.equals(ClassPath.EXECUTE)) {
+        /*if (type.equals(RubyLanguage.EXECUTE)) {
             return getRunTimeClasspath(file);
-        } else */ if (type.equals(ClassPath.SOURCE)) {
+        } else */ if (type.equals(RubyLanguage.SOURCE)) {
             return getSourcepath(file);
-        } else if (type.equals(ClassPath.BOOT)) {
+        } else if (type.equals(RubyLanguage.BOOT)) {
             return getBootClassPath();
-        } else if (type.equals(ClassPath.COMPILE)) {
+        } else if (type.equals(RubyLanguage.COMPILE)) {
             // Bogus
             return getBootClassPath();
+        } else if (type.equals("js/library")) { // NOI18N
+            return getJavascriptsWebClassPath();
         } else {
             return null;
         }
@@ -288,22 +222,15 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
      * The result is used for example for GlobalPathRegistry registrations.
      */
     public ClassPath[] getProjectClassPaths(String type) {
-        if (ClassPath.BOOT.equals(type)) {
+        if (RubyLanguage.BOOT.equals(type)) {
             return new ClassPath[]{getBootClassPath()};
         }
-//        if (ClassPath.COMPILE.equals(type)) {
-//            ClassPath[] l = new ClassPath[2];
-//            l[0] = getCompileTimeClasspath(0);
-//            l[1] = getCompileTimeClasspath(1);
-//            return l;
-//        }
-        if (ClassPath.SOURCE.equals(type)) {
+        if (RubyLanguage.SOURCE.equals(type)) {
             ClassPath[] l = new ClassPath[2];
             l[0] = getSourcepath(0);
             l[1] = getSourcepath(1);
             return l;
         }
-//        assert false;
         return null;
     }
 
@@ -312,16 +239,12 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
      * (i.e., excluding tests roots). Valid types are BOOT, SOURCE and COMPILE.
      */
     public ClassPath getProjectSourcesClassPath(String type) {
-        if (ClassPath.BOOT.equals(type)) {
+        if (RubyLanguage.BOOT.equals(type)) {
              return getBootClassPath();
         }
-        if (ClassPath.SOURCE.equals(type)) {
+        if (RubyLanguage.SOURCE.equals(type)) {
             return getSourcepath(0);
         }
-//        if (ClassPath.COMPILE.equals(type)) {
-//            return getCompileTimeClasspath(0);
-//        }
-//        assert false;
         return null;
     }
 
@@ -334,10 +257,10 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         FileObject[] path = getPrimarySrcPath();
         for (int i=0; i<path.length; i++) {
             if (root.equals(path[i])) {
-                if (ClassPath.COMPILE.equals(type)) {
+                if (RubyLanguage.COMPILE.equals(type)) {
                     return JAVAC_CLASSPATH;
                 }
-                else if (ClassPath.EXECUTE.equals(type)) {
+                else if (RubyLanguage.EXECUTE.equals(type)) {
                     return RUN_CLASSPATH;
                 }
                 else {
@@ -348,10 +271,10 @@ public final class ClassPathProviderImpl implements ClassPathProvider, PropertyC
         path = getTestSrcDir();
         for (int i=0; i<path.length; i++) {
             if (root.equals(path[i])) {
-                if (ClassPath.COMPILE.equals(type)) {
+                if (RubyLanguage.COMPILE.equals(type)) {
                     return JAVAC_TEST_CLASSPATH;
                 }
-                else if (ClassPath.EXECUTE.equals(type)) {
+                else if (RubyLanguage.EXECUTE.equals(type)) {
                     return RUN_TEST_CLASSPATH;
                 }
                 else {
