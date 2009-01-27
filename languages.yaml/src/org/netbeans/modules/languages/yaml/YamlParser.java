cffd122f47b1e6332fc7f4b01458d957773cdcf5
phejl@netbeans.org <none@none>
2009-01-27 15:13:59 +0100

    Yaml rewritten to CSL.

---
+++ languages.yaml/src/org/netbeans/modules/languages/yaml/YamlParser.java
@@ -0,0 +1,268 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.languages.yaml;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.event.ChangeListener;
+import org.jruby.util.ByteList;
+import org.jvyamlb.Composer;
+import org.jvyamlb.PositioningComposerImpl;
+import org.jvyamlb.PositioningParserImpl;
+import org.jvyamlb.PositioningScannerImpl;
+import org.jvyamlb.ResolverImpl;
+import org.jvyamlb.exceptions.PositionedParserException;
+import org.jvyamlb.nodes.Node;
+import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenHierarchy;
+import org.netbeans.api.lexer.TokenId;
+import org.netbeans.api.lexer.TokenSequence;
+import org.netbeans.modules.csl.api.Severity;
+import org.netbeans.modules.csl.spi.DefaultError;
+import org.netbeans.modules.parsing.api.Snapshot;
+import org.netbeans.modules.parsing.api.Task;
+import org.netbeans.modules.parsing.spi.ParseException;
+import org.netbeans.modules.parsing.spi.Parser;
+import org.netbeans.modules.parsing.spi.SourceModificationEvent;
+import org.openide.util.NbBundle;
+
+/**
+ * Parser for YAML. Delegates to the YAML parser shipped with JRuby (jvyamlb)
+ * @author Tor Norbye
+ */
+public class YamlParser extends Parser {
+
+    private YamlParserResult lastResult;
+
+    @Override
+    public void addChangeListener(ChangeListener changeListener) {
+        // FIXME parsing API
+    }
+
+    @Override
+    public void removeChangeListener(ChangeListener changeListener) {
+        // FIXME parsing API
+    }
+    
+    @Override
+    public void cancel() {
+        // FIXME parsing API
+    }
+
+    @Override
+    public Result getResult(Task task) throws ParseException {
+        assert lastResult != null : "getResult() called prior parse()"; //NOI18N
+        return lastResult;
+    }
+
+    private static String asString(CharSequence sequence) {
+        if (sequence instanceof String) {
+            return (String)sequence;
+        } else {
+            return sequence.toString();
+        }
+    }
+
+    // for test package private
+    YamlParserResult parse(String source, Snapshot snapshot) {
+        try {
+            if (source.length() > 512*1024) {
+                YamlParserResult result = new YamlParserResult(Collections.<Node>emptyList(), this, snapshot, false, null, null);
+                // FIXME this can violate contract of DefaultError (null fo)
+                DefaultError error = new DefaultError(null, NbBundle.getMessage(YamlParser.class, "TooLarge"), null,
+                        snapshot.getSource().getFileObject(), 0, 0, Severity.WARNING);
+                result.addError(error);
+                return result;
+            }
+            source.length();
+            ByteList byteList = null;
+            int[] byteToUtf8 = null;
+            int[] utf8toByte = null;
+
+            byte[] bytes = source.getBytes("UTF-8"); // NOI18N
+            if (bytes.length == source.length()) {
+                // No position translations necessary - this should be fast
+                byteList = new ByteList(bytes);
+            } else {
+                // There's some encoding happening of unicode characters.
+                // I need to produce functions to translate between a byte offset
+                // and a unicode offset.
+                // I couldn't find an API for this in the various Charset functions.
+                // So for now, here is a fantastically lame but functional way to do it:
+                // I'm encoding the string, one character at a time, flushing after
+                // each operation to compute the current byte offset. I then build
+                // up an array of these offsets such that I can do quick translations.
+                ByteArrayOutputStream out = new ByteArrayOutputStream(2*source.length());
+                OutputStreamWriter writer = new OutputStreamWriter(out, "UTF-8"); // NOI18N
+                utf8toByte = new int[source.length()];
+                int currentPos = 0;
+                for (int i = 0, n = source.length(); i < n; i++) {
+                    writer.write(source.charAt(i));
+                    writer.flush(); // flush because otherwise we don't know the correct offset
+                    utf8toByte[i] = currentPos;
+                    currentPos = out.size();
+                }
+
+                if (currentPos > 0) {
+                    byteToUtf8 = new int[currentPos];
+                    for (int i = 0, n = utf8toByte.length; i < n; i++) {
+                        byteToUtf8[utf8toByte[i]] = i;
+                    }
+                    // Fill in holes - these are the middles of unicode encodings.
+                    int last = 0;
+                    for (int i = 0, n = byteToUtf8.length; i < n; i++) {
+                        int p = byteToUtf8[i];
+                        if (p == 0) {
+                            byteToUtf8[i] = last;
+                        } else {
+                            last = p;
+                        }
+                    }
+                } else {
+                    byteToUtf8 = new int[0];
+                }
+
+                byteList = new ByteList(out.toByteArray());
+            }
+
+            Composer composer = new PositioningComposerImpl(new PositioningParserImpl(new PositioningScannerImpl(byteList)), new ResolverImpl());
+            List<Node> nodes = new ArrayList<Node>();
+            Iterator iterator = composer.eachNode();
+            while (iterator.hasNext()) {
+                Node node = (Node) iterator.next();
+                if (node == null) {
+                    break;
+                }
+                nodes.add(node);
+            }
+
+            //Object yaml = YAML.load(stream);
+            return new YamlParserResult(nodes, this, snapshot, true, byteToUtf8, utf8toByte);
+        } catch (Exception ex) {
+            int pos = 0;
+            if (ex instanceof PositionedParserException) {
+                PositionedParserException ppe = (PositionedParserException)ex;
+                pos = ppe.getPosition().offset;
+            }
+
+            YamlParserResult result = new YamlParserResult(Collections.<Node>emptyList(), this, snapshot, false, null, null);
+            String message = ex.getMessage();
+            if (message != null && message.length() > 0) {
+                // Strip off useless prefixes to make errors more readable
+                if (message.startsWith("ScannerException null ")) { // NOI18N
+                    message = message.substring(22);
+                } else if (message.startsWith("ParserException ")) { // NOI18N
+                    message = message.substring(16);
+                }
+                // Capitalize sentences
+                char firstChar = message.charAt(0);
+                char upcasedChar = Character.toUpperCase(firstChar);
+                if (firstChar != upcasedChar) {
+                    message = upcasedChar + message.substring(1);
+                }
+
+                // FIXME this can violate contract of DefaultError (null fo)
+                DefaultError error = new DefaultError(null, message, null, snapshot.getSource().getFileObject(),
+                        pos, pos, Severity.ERROR);
+                result.addError(error);
+            }
+            
+            return result;
+        }
+    }
+
+    @Override
+    public void parse(Snapshot snapshot, Task task, SourceModificationEvent event) throws ParseException {
+        try {
+            String source = asString(snapshot.getText());
+//                int caretOffset = reader.getCaretOffset(file);
+//                if (caretOffset != -1 && job.translatedSource != null) {
+//                    caretOffset = job.translatedSource.getAstOffset(caretOffset);
+//                }
+
+            // Construct source by removing <% %> tokens etc.
+            StringBuilder sb = new StringBuilder();
+            TokenHierarchy hi = TokenHierarchy.create(source, YamlTokenId.language());
+
+            TokenSequence ts = hi.tokenSequence();
+
+            // If necessary move ts to the requested offset
+            int offset = 0;
+            ts.move(offset);
+
+//                int adjustedOffset = 0;
+//                int adjustedCaretOffset = -1;
+            while (ts.moveNext()) {
+                Token t = ts.token();
+                TokenId id = t.id();
+
+                if (id == YamlTokenId.RUBY_EXPR) {
+                    String marker = "__"; // NOI18N
+                    // Marker
+                    sb.append(marker);
+                    // Replace with spaces to preserve offsets
+                    for (int i = 0, n = t.length()-marker.length(); i < n; i++) { // -2: account for the __
+                        sb.append(' ');
+                    }
+                } else if (id == YamlTokenId.RUBY || id == YamlTokenId.RUBYCOMMENT || id == YamlTokenId.DELIMITER) {
+                    // Replace with spaces to preserve offsets
+                    for (int i = 0; i < t.length(); i++) {
+                        sb.append(' ');
+                    }
+                } else {
+                    sb.append(t.text().toString());
+                }
+
+//                    adjustedOffset += t.length();
+            }
+
+            source = sb.toString();
+
+            lastResult = parse(source, snapshot);
+        } catch (Exception ioe) {
+            lastResult = new YamlParserResult(Collections.<Node>emptyList(), this, snapshot, false, null, null);
+        }
+    }
+ 
+}
---
+++ languages.yaml/test/unit/src/org/netbeans/modules/languages/yaml/YamlParserTest.java
@@ -0,0 +1,121 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.languages.yaml;
+
+import java.util.Collections;
+import java.util.List;
+import org.netbeans.modules.parsing.api.ParserManager;
+import org.netbeans.modules.parsing.api.ResultIterator;
+import org.netbeans.modules.parsing.api.Snapshot;
+import org.netbeans.modules.parsing.api.Source;
+import org.netbeans.modules.parsing.api.UserTask;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+
+/**
+ *
+ * @author Tor Norbye
+ */
+public class YamlParserTest extends YamlTestBase {
+    public YamlParserTest(String testName) {
+        super(testName);
+    }
+
+    public void testErrors1() throws Exception {
+        checkErrors("testfiles/error.yaml");
+    }
+
+    public void testErrors2() throws Exception {
+        checkErrors("testfiles/error2.yaml");
+    }
+
+    public void testErrors3() throws Exception {
+        checkErrors("testfiles/error3.yaml");
+    }
+
+    public void testHuge() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        String s = readFile(getTestFile("testfiles/database.yml"));
+        while (sb.length() < 1024*1024) {
+            sb.append(s);
+        }
+        String huge = sb.toString();
+        final String relFilePath = "generated-huge.yml";
+        FileObject workDir = FileUtil.toFileObject(FileUtil.normalizeFile(getWorkDir()));
+        createFile(workDir, relFilePath, huge);
+
+        FileObject f = workDir.getFileObject(relFilePath);
+        Source source = Source.create(f);
+
+        ParserManager.parse(Collections.singleton(source), new UserTask() {
+            public @Override void run(ResultIterator resultIterator) throws Exception {
+                YamlParserResult result = (YamlParserResult) resultIterator.getParserResult();
+
+                assertNotNull(result);
+
+                String text = result.getSnapshot().getText().toString();
+                assertNotNull(text);
+
+                List<? extends org.netbeans.modules.csl.api.Error> diagnostics = result.getDiagnostics();
+                String annotatedSource = annotateErrors(diagnostics);
+                assertDescriptionMatches("testfiles/" + relFilePath, annotatedSource, false, ".errors", false);
+                // Make sure we actually skipped parsing this large document!
+                assertTrue(result.getRootNodes().size() == 0);
+            }
+        });
+    }
+
+    public void testValidResult() throws Exception {
+        // Make sure we get a valid parse result out of an aborted parse
+        FileObject fo = getTestFile("testfiles/error3.yaml");
+
+        Source source = Source.create(fo);
+
+        YamlParser parser = new YamlParser() {
+            YamlParserResult parse(String source, Snapshot snapshot) {
+                throw new RuntimeException("Very bad thing");
+            }
+        };
+
+        parser.parse(source.createSnapshot(), null, null);
+
+        assertNotNull("Parser result must be nonnull", parser.getResult(null));
+    }
+}
