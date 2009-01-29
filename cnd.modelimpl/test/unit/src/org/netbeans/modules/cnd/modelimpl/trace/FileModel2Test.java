2acbd70ca667d9c46aa1beb1e5f3376dc3062072
Nick N. Krasilnikov <nnnnnk@netbeans.org>
2009-01-29 19:58:22 +0300

    tests for IZ#157603 : Code model does not understand __attribute, constructor, destructor keywords (GNU)

--- cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModel2Test.java
+++ cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModel2Test.java
@@ -68,6 +68,21 @@ public class FileModel2Test extends TraceModelTestBase {
         getTraceModel().setDumpPPState(true);
     }
 
+    public void testIZ156061() throws Exception {
+        // IZ156061: structure typedefs are highlighted as error
+        performTest("iz156061.cc");
+    }
+    
+    public void testIZ154276() throws Exception {
+        // IZ154276: functions are creted instead of fields
+        performTest("iz154276.cc");
+    }
+
+    public void testIZ154196() throws Exception {
+        // IZ154196: Regression in LiteSQL (Error Highlighting)
+        performTest("iz154196.cc");
+    }
+
     public void testIZ136887() throws Exception {
         // IZ136887: Model do not support bit fields
         performTest("iz136887.cc");
@@ -165,4 +180,13 @@ public class FileModel2Test extends TraceModelTestBase {
         performTest("iz151957_namespace_alias.cc");
     }
 
+    public void testIZ154349() throws Exception {
+        // IZ#154349: wrongly flagged errors for destructor during template specialization
+        performTest("iz154349.cc");
+    }
+
+    public void testIZ157603() throws Exception {
+        // IZ#157603 : Code model does not understand __attribute, constructor, destructor keywords (GNU)
+        performTest("iz157603.cc");
+    }
 }
