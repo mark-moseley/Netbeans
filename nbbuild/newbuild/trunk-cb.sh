432d0de317e13cf871841f41014023c85a121783
Jesse Glick <jglick@netbeans.org>
2009-02-02 18:44:29 -0500

    Removing some visualweb-related things which did not need to be here.

--- nbbuild/newbuild/trunk-cb.sh
+++ nbbuild/newbuild/trunk-cb.sh
@@ -8,46 +8,15 @@ TRUNK_CB_DIRNAME=`pwd`
 export BUILD_DESC=trunk-cb
 source init.sh
 
-cd $TRUNK_CB_DIRNAME
-bash clean-purge.sh
-ERROR_CODE=$?
-
-if [ $ERROR_CODE != 0 ]; then
-    echo "ERROR: $ERROR_CODE - Clean failed"
-    exit $ERROR_CODE;
-fi
-
 ###################################################################
 #
-# Update all the required NB modules
+# Clean remains from last build
 #
 ###################################################################
 
-cd $TRUNK_CB_DIRNAME
-bash update-all-components.sh
-ERROR_CODE=$?
-
-if [ $ERROR_CODE != 0 ]; then
-    echo "ERROR: $ERROR_CODE - Update failed"
-    exit $ERROR_CODE;
-fi
-
-###################################################################
-#
-# Check if the build is required
-#
-###################################################################
-
-cd $TRUNK_CB_DIRNAME
-bash check-for-update.sh
-RETURN_CODE=$?
-
-if [ $RETURN_CODE -eq 2 ]; then
-    #There were no update, sleep for 10 min and exit
-    sleep 360
-    exit 0;
-fi
-
+cd $BASE_DIR
+rm -rf dist
+ant clean
 
 ###################################################################
 #
@@ -63,24 +32,6 @@ if [ $ERROR_CODE != 0 ]; then
     echo "ERROR: $ERROR_CODE - Build failed"
     exit $ERROR_CODE;
 fi
-
-###################################################################
-#
-# Build all the components
-#
-###################################################################
-
-cd $TRUNK_CB_DIRNAME
-bash run-vw-sanity.sh
-ERROR_CODE=$?
-
-if [ $ERROR_CODE != 0 ]; then
-    echo "ERROR: $ERROR_CODE - VW sanity failed"
-#    exit $ERROR_CODE;
-fi
-
-
-
 
 ###################################################################
 #
