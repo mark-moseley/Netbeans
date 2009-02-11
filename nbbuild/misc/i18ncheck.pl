4e5457bf852945b3503ba667182c513f6d793bcd
Vladimir Kvashin <vkvashin@netbeans.org>
2009-02-11 22:33:39 +0300

    exclude annotations from I18N check

--- nbbuild/misc/i18ncheck.pl
+++ nbbuild/misc/i18ncheck.pl
@@ -221,6 +221,9 @@ sub checkline {
             && ! m,loadImage\s*\(,
             && ! m,getResource(AsStream)?\s*\(,
             && ! m,findResource ?\s*\(,
+            && ! m,Parameters\.\s*not,
+            && ! m,Parameters\.\s*java,
+            && ! m,^\@.*,
             && m,".+", 
            ); 
 }
