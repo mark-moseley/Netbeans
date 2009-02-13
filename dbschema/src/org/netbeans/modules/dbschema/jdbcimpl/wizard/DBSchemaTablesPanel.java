0063fd0ee4ca813b44d6080ce2fd7a68ec24d998
Jiri Rechtacek <jrechtacek@netbeans.org>
2009-02-13 12:10:01 +0100

    #158465: Clash in using isValid method

--- dbschema/src/org/netbeans/modules/dbschema/jdbcimpl/wizard/DBSchemaTablesPanel.java
+++ dbschema/src/org/netbeans/modules/dbschema/jdbcimpl/wizard/DBSchemaTablesPanel.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -64,6 +64,7 @@ import org.openide.util.NbBundle;
 
 import org.netbeans.modules.dbschema.jdbcimpl.DDLBridge;
 import org.netbeans.modules.dbschema.jdbcimpl.ConnectionProvider;
+import org.openide.WizardDescriptor;
 import org.openide.util.Exceptions;
 import org.openide.util.Mutex;
 import org.openide.util.RequestProcessor;
@@ -98,7 +99,7 @@ public class DBSchemaTablesPanel extends JPanel implements ListDataListener {
         views = new LinkedList();
         cp = null;
 
-        putClientProperty("WizardPanel_contentSelectedIndex", new Integer(2)); //NOI18N
+        putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, new Integer(2)); //NOI18N
         setName(bundle.getString("TablesChooser")); //NOI18N
 
         initComponents();
@@ -187,7 +188,21 @@ public class DBSchemaTablesPanel extends JPanel implements ListDataListener {
         handlers.add(new Handler() {
             public void handle(Parameters params) {
                 ConnectionManager.getDefault().showConnectionDialog(dbconn);
+                
+                // TODO - This is a workaround until we add an API to show
+                // connection dialog on the event thread that guarantees
+                // the connection will not be null
                 conn = dbconn.getJDBCConnection();
+                int count = 0;
+                while ( conn == null && count < 10 ) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch ( InterruptedException e ) {
+                        break;
+                    }
+                    conn = dbconn.getJDBCConnection();
+                    count++;
+                }
             }
             public boolean getRunInEDT() {
                 return true;
@@ -672,7 +687,7 @@ public class DBSchemaTablesPanel extends JPanel implements ListDataListener {
         jButtonRemoveAll.setEnabled(((SortedListModel) jListSelectedTables.getModel()).isEmpty() ? false : true);
     }
 
-    public boolean isValid() {
+    public boolean isInputValid() {
         if (jListSelectedTables.getModel().getSize() > 0)
             return true;
         else
