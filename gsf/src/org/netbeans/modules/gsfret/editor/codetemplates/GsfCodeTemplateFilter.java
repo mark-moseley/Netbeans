8b0ee95cdb5cdb6ad448403aca9cd405ab8bcd0a
phejl@netbeans.org <none@none>
2009-02-13 15:25:24 +0100

    #152894 Edit/Highlight loop causes 5s-10s EDT pauses

--- gsf/src/org/netbeans/modules/gsfret/editor/codetemplates/GsfCodeTemplateFilter.java
+++ gsf/src/org/netbeans/modules/gsfret/editor/codetemplates/GsfCodeTemplateFilter.java
@@ -43,17 +43,21 @@ package org.netbeans.modules.gsfret.editor.codetemplates;
 
 import java.awt.Toolkit;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import javax.swing.text.JTextComponent;
-import org.netbeans.api.gsf.CancellableTask;
-import org.netbeans.api.gsf.Completable;
+import org.netbeans.modules.gsf.api.CancellableTask;
+import org.netbeans.modules.gsf.api.CodeCompletionHandler;
 import org.netbeans.napi.gsfret.source.CompilationController;
 import org.netbeans.napi.gsfret.source.Phase;
 import org.netbeans.napi.gsfret.source.Source;
 import org.netbeans.napi.gsfret.source.SourceUtils;
 import org.netbeans.lib.editor.codetemplates.api.CodeTemplate;
 import org.netbeans.lib.editor.codetemplates.spi.CodeTemplateFilter;
+import org.netbeans.modules.gsfret.editor.completion.GsfCompletionProvider;
 import org.openide.awt.StatusDisplayer;
+import org.openide.filesystems.FileObject;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 
@@ -72,13 +76,16 @@ public class GsfCodeTemplateFilter implements CodeTemplateFilter, CancellableTas
     
     private GsfCodeTemplateFilter(JTextComponent component, int offset) {
         this.startOffset = offset;
-        this.endOffset = component.getSelectionStart() == offset ? component.getSelectionEnd() : -1;            
+        this.endOffset = component.getSelectionStart() == offset ? component.getSelectionEnd() : -1;
         Source js = Source.forDocument(component.getDocument());
         if (js != null) {
             try {
                 if (SourceUtils.isScanInProgress()) {
                     StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(GsfCodeTemplateFilter.class, "JCT-scanning-in-progress")); //NOI18N
                     Toolkit.getDefaultToolkit().beep();
+                // temporary (until CSL available) workaround for issue 152894; see below
+                } else if (isDisabled(js)) {
+                    templates = Collections.emptySet();
                 } else {
                     js.runUserActionTask(this, true);
                 }
@@ -106,12 +113,10 @@ public class GsfCodeTemplateFilter implements CodeTemplateFilter, CancellableTas
     public synchronized void run(CompilationController controller) throws IOException {
         controller.toPhase(Phase.PARSED);
         
-        if (controller.getLanguage().getParser() != null) {
-            Completable completer = controller.getLanguage().getCompletionProvider();
+        CodeCompletionHandler completer = GsfCompletionProvider.getCompletable(controller,  startOffset);
             
-            if (completer != null) {
-                templates = completer.getApplicableTemplates(controller, startOffset, endOffset);
-            }
+        if (completer != null) {
+            templates = completer.getApplicableTemplates(controller, startOffset, endOffset);
         }
     }
 
@@ -120,5 +125,27 @@ public class GsfCodeTemplateFilter implements CodeTemplateFilter, CancellableTas
         public CodeTemplateFilter createFilter(JTextComponent component, int offset) {
             return new GsfCodeTemplateFilter(component, offset);
         }
+    }
+
+    // THIS IS TEMPORARY HACK DISABLING TEMPLATE FILTER FOR GROOVY
+    // THERE IS ISSUE WITH JAVA/GSF SCAN - RESOLVED BY CSL
+    // see http://www.netbeans.org/issues/show_bug.cgi?id=152894
+
+    private static final Set<String> DISABLED_TYPES = new HashSet<String>();
+
+    static {
+        Collections.addAll(DISABLED_TYPES, "text/x-groovy", "text/x-gsp"); // NOI18N
+    }
+
+    private static boolean isDisabled(Source source) {
+        boolean disabled = false;
+        for (FileObject file : source.getFileObjects()) {
+            if (DISABLED_TYPES.contains(file.getMIMEType())) {
+                disabled = true;
+                break;
+            }
+        }
+
+        return disabled;
     }
 }
