cca66375d40072794237d33abbde2738f0cb580b
Maria Tishkova <mromashova@netbeans.org>
2009-02-13 17:38:52 +0300

    No IZ: real time visualizer  doesn't  ask data if it is not showing
    --HG--
    rename : dlight.visualizers/src/org/netbeans/modules/dlight/visualizers/DefaultTreeTableNode.java => dlight/src/org/netbeans/modules/dlight/api/impl/DefaultTreeTableNode.java

---
+++ dlight.visualizers/src/org/netbeans/modules/dlight/visualizers/OnTimerTask.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.dlight.visualizers;
+
+/**
+ *
+ * @author masha
+ */
+interface OnTimerTask {
+
+    int onTimer();
+    void timerStopped();
+}
