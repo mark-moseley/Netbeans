5b8159e45f97f2afc7979eba36be26c746b18eca
Vladimir Voskresensky <vv159170@netbeans.org>
2009-02-16 20:54:33 +0300

    macro expansion in tooltips

--- cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/CsmHyperlinkProvider.java
+++ cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/CsmHyperlinkProvider.java
@@ -38,29 +38,35 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
-
 package org.netbeans.modules.cnd.completion.cplusplus.hyperlink;
 
+import java.awt.event.InputEvent;
 import java.util.Collection;
+import java.util.prefs.Preferences;
+import javax.swing.text.Document;
 import org.netbeans.modules.cnd.api.model.CsmFile;
 import org.netbeans.modules.cnd.api.model.CsmFunction;
 import org.netbeans.modules.cnd.api.model.CsmFunctionDefinition;
 import org.netbeans.modules.cnd.api.model.CsmVariable;
 import org.netbeans.modules.cnd.api.model.CsmVariableDefinition;
-import org.netbeans.modules.cnd.completion.cplusplus.utils.Token;
 import org.netbeans.modules.cnd.modelutil.CsmUtilities;
 import org.netbeans.modules.cnd.api.model.CsmObject;
 import org.netbeans.modules.cnd.api.model.CsmOffsetable;
 import org.netbeans.modules.cnd.api.model.util.CsmKindUtilities;
 import javax.swing.text.JTextComponent;
-import org.netbeans.editor.BaseDocument;
+import org.netbeans.api.editor.mimelookup.MimeLookup;
+import org.netbeans.api.editor.settings.SimpleValueNames;
+import org.netbeans.cnd.api.lexer.CppTokenId;
+import org.netbeans.cnd.api.lexer.TokenItem;
+import org.netbeans.lib.editor.hyperlink.spi.HyperlinkType;
 import org.netbeans.modules.cnd.api.model.CsmNamespace;
 import org.netbeans.modules.cnd.api.model.CsmNamespaceDefinition;
 import org.netbeans.modules.cnd.api.model.services.CsmFunctionDefinitionResolver;
 import org.netbeans.modules.cnd.api.model.xref.CsmReference;
 import org.netbeans.modules.cnd.completion.impl.xref.ReferencesSupport;
-import org.netbeans.modules.cnd.editor.cplusplus.CCTokenContext;
+import org.netbeans.modules.cnd.modelutil.CsmDisplayUtilities;
+import org.netbeans.modules.editor.NbEditorUtilities;
+import org.openide.util.NbBundle;
 
 /**
  * Implementation of the hyperlink provider for C/C++ language.
@@ -72,100 +78,110 @@ import org.netbeans.modules.cnd.editor.cplusplus.CCTokenContext;
  * @author Jan Lahoda, Vladimir Voskresensky
  */
 public final class CsmHyperlinkProvider extends CsmAbstractHyperlinkProvider {
+
     public CsmHyperlinkProvider() {
     }
-    
-    protected void performAction(final BaseDocument doc, final JTextComponent target, final int offset) {
-        goToDeclaration(doc, target, offset);
+
+    protected void performAction(final Document doc, final JTextComponent target, final int offset, final HyperlinkType type) {
+        goToDeclaration(doc, target, offset, type);
     }
-    
-    protected boolean isValidToken(Token token) {
-        return isSupportedToken(token);
+
+    protected boolean isValidToken(TokenItem<CppTokenId> token, HyperlinkType type) {
+        return isSupportedToken(token, type);
     }
-    
-    public static boolean isSupportedToken(Token token) {
-        if ((token != null) && (token.getTokenID() == CCTokenContext.IDENTIFIER ||
-                token.getTokenID() == CCTokenContext.OPERATOR)) {
-            return true;
-        } else {
-            return false;
-        }        
+
+    public static boolean isSupportedToken(TokenItem<CppTokenId> token, HyperlinkType type) {
+        if (token != null) {
+            if (type == HyperlinkType.ALT_HYPERLINK) {
+                return !CppTokenId.WHITESPACE_CATEGORY.equals(token.id().primaryCategory()) &&
+                        !CppTokenId.COMMENT_CATEGORY.equals(token.id().primaryCategory());
+            }
+            switch (token.id()) {
+                case IDENTIFIER:
+                case PREPROCESSOR_IDENTIFIER:
+                case OPERATOR:
+                    return true;
+            }
+        }
+        return false;
     }
-    
-    public boolean goToDeclaration(BaseDocument doc, JTextComponent target, int offset) {
-        if (!preJump(doc, target, offset, "opening-csm-element")) { //NOI18N
+
+    public boolean goToDeclaration(Document doc, JTextComponent target, int offset, HyperlinkType type) {
+        if (!preJump(doc, target, offset, "opening-csm-element", type)) { //NOI18N
             return false;
         }
-        Token jumpToken = getJumpToken();
-        CsmOffsetable item = findTargetObject(target, doc, jumpToken, offset);
+        TokenItem<CppTokenId> jumpToken = getJumpToken();
+        CsmOffsetable item = (CsmOffsetable) findTargetObject(doc, jumpToken, offset, true);
         return postJump(item, "goto_source_source_not_found", "cannot-open-csm-element"); //NOI18N
     }
-    
-    /*package*/ CsmOffsetable findTargetObject(final JTextComponent target, final BaseDocument doc, final Token jumpToken, final int offset) {
-        CsmOffsetable item = null;
+
+    /*package*/ CsmObject findTargetObject(final Document doc, final TokenItem<CppTokenId> jumpToken, final int offset, boolean toOffsetable) {
+        CsmObject item = null;
         assert jumpToken != null;
         CsmFile file = CsmUtilities.getCsmFile(doc, true);
         CsmObject csmObject = file == null ? null : ReferencesSupport.findDeclaration(file, doc, jumpToken, offset);
         if (csmObject != null) {
             // convert to jump object
-            item = toJumpObject(csmObject, file, offset);
+            item = toOffsetable ? toJumpObject(csmObject, file, offset) : csmObject;
         }
         return item;
     }
-    
+
     private CsmOffsetable toJumpObject(CsmObject csmObject, CsmFile csmFile, int offset) {
         CsmOffsetable item = null;
         if (CsmKindUtilities.isOffsetable(csmObject)) {
-            item = (CsmOffsetable)csmObject;
+            item = (CsmOffsetable) csmObject;
             if (CsmKindUtilities.isFunctionDeclaration(csmObject)) {
                 // check if we are in function definition name => go to declaration
                 // else it is more useful to jump to definition of function
-                CsmFunctionDefinition definition = ((CsmFunction)csmObject).getDefinition();
+                CsmFunctionDefinition definition = ((CsmFunction) csmObject).getDefinition();
                 if (definition != null) {
                     if (csmFile.equals(definition.getContainingFile()) &&
                             (definition.getStartOffset() <= offset &&
-                            offset <= definition.getBody().getStartOffset())
-                            ) {
+                            offset <= definition.getBody().getStartOffset())) {
                         // it is ok to jump to declaration
                         if (definition.getDeclaration() != null) {
                             item = definition.getDeclaration();
                         } else if (csmObject.equals(definition)) {
-                            item = (CsmOffsetable)csmObject;
+                            item = (CsmOffsetable) csmObject;
                         }
                     } else {
                         // it's better to jump to definition
                         item = definition;
                     }
                 } else {
-                    CsmReference ref = CsmFunctionDefinitionResolver.getDefault().getFunctionDefinition((CsmFunction)csmObject);
-                    if (ref != null){
+                    CsmReference ref = CsmFunctionDefinitionResolver.getDefault().getFunctionDefinition((CsmFunction) csmObject);
+                    if (ref != null) {
                         item = ref;
                     }
                 }
-            }else if (CsmKindUtilities.isFunctionDefinition(csmObject)) {
-                CsmFunctionDefinition definition = (CsmFunctionDefinition)csmObject;
+            } else if (CsmKindUtilities.isFunctionDefinition(csmObject)) {
+                CsmFunctionDefinition definition = (CsmFunctionDefinition) csmObject;
                 if (csmFile.equals(definition.getContainingFile()) &&
                         (definition.getStartOffset() <= offset &&
-                        offset <= definition.getBody().getStartOffset())
-                        ) {
+                        offset <= definition.getBody().getStartOffset())) {
                     // it is ok to jump to declaration
                     if (definition.getDeclaration() != null) {
                         item = definition.getDeclaration();
-                    } else if (csmObject.equals(definition)) {
-                        item = (CsmOffsetable)csmObject;
+                    } else {
+                        item = definition;
                     }
-                }                
+                }
             } else if (CsmKindUtilities.isVariableDeclaration(csmObject)) {
-                // check if we are in function definition name => go to declaration
-                // else it is more useful to jump to definition of function
-                CsmVariableDefinition definition = ((CsmVariable)csmObject).getDefinition();
+                // check if we are in variable definition name => go to declaration
+                CsmVariableDefinition definition = ((CsmVariable) csmObject).getDefinition();
                 if (definition != null) {
                     item = definition;
+                    if (csmFile.equals(definition.getContainingFile()) &&
+                            (definition.getStartOffset() <= offset &&
+                            offset <= definition.getEndOffset())) {
+                        item = (CsmVariable) csmObject;
+                    }
                 }
             }
         } else if (CsmKindUtilities.isNamespace(csmObject)) {
             // get all definitions of namespace, but prefer the definition in this file
-            CsmNamespace nmsp = (CsmNamespace)csmObject;
+            CsmNamespace nmsp = (CsmNamespace) csmObject;
             Collection<CsmNamespaceDefinition> defs = nmsp.getDefinitions();
             CsmNamespaceDefinition bestDef = null;
             for (CsmNamespaceDefinition def : defs) {
@@ -183,5 +199,20 @@ public final class CsmHyperlinkProvider extends CsmAbstractHyperlinkProvider {
             item = bestDef;
         }
         return item;
+    }
+
+    protected String getTooltipText(Document doc, TokenItem<CppTokenId> token, int offset, HyperlinkType type) {
+        CsmObject item = findTargetObject(doc, token, offset, false);
+        CharSequence msg = item == null ? null : CsmDisplayUtilities.getTooltipText(item);
+        if (msg != null && CsmKindUtilities.isMacro(item)) {
+            msg = getAlternativeHyperlinkTip(doc, msg);
+        }
+        return msg == null ? null : msg.toString();
+    }
+
+    private CharSequence getAlternativeHyperlinkTip(Document doc, CharSequence tooltip) {
+        Preferences prefs = MimeLookup.getLookup(NbEditorUtilities.getMimeType(doc)).lookup(Preferences.class);
+        int shortCut = prefs.getInt(SimpleValueNames.ALT_HYPERLINK_ACTIVATION_MODIFIERS, InputEvent.CTRL_DOWN_MASK | InputEvent.ALT_DOWN_MASK);
+        return NbBundle.getMessage(CsmHyperlinkProvider.class, "AltHyperlinkHint", tooltip, InputEvent.getModifiersExText(shortCut)); // NOI18N
     }
 }
