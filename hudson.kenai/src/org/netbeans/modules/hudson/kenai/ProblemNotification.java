9eb9b73e7bc618a530beb047d98dc5e72e025fd1
Jesse Glick <jglick@netbeans.org>
2009-02-18 16:43:06 -0500

    Rudimentary integration of Hudson into the notiifications popup.

---
+++ hudson.kenai/src/org/netbeans/modules/hudson/kenai/ProblemNotification.java
@@ -0,0 +1,104 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.hudson.kenai;
+
+import java.io.CharConversionException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.Icon;
+import org.netbeans.modules.hudson.api.HudsonJob;
+import org.netbeans.modules.notifications.spi.Notification;
+import org.openide.awt.HtmlBrowser.URLDisplayer;
+import org.openide.util.Exceptions;
+import org.openide.util.ImageUtilities;
+import org.openide.xml.XMLUtil;
+
+/**
+ * Build failed or was unstable.
+ */
+class ProblemNotification extends Notification {
+
+    private final HudsonJob job;
+    private final boolean failed;
+    private final boolean running;
+
+    ProblemNotification(HudsonJob job, boolean failed, boolean running) {
+        this.job = job;
+        this.failed = failed;
+        this.running = running;
+    }
+
+    public String getTitle() {
+        try {
+            return XMLUtil.toElementContent(job.getDisplayName()) + (failed ? " <em>failed</em>" : " is <em>unstable</em>"); // XXX I18N
+        } catch (CharConversionException ex) {
+            Exceptions.printStackTrace(ex);
+            return "";
+        }
+    }
+
+    public String getDescription() {
+        // XXX should perhaps summarize last lines of failed build, or list some failed tests...
+        return failed ? "The build failed." : "Some tests failed."; // XXX I18N
+    }
+
+    public String getLinkTitle() {
+        return "Show Job"; // XXX I18N
+    }
+
+    public void showDetails() {
+        // XXX could also show console, show test failures, etc.
+        try {
+            URLDisplayer.getDefault().showURL(new URL(job.getUrl()));
+        } catch (MalformedURLException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+    public Priority getPriority() {
+        return failed ? Priority.HIGH : Priority.NORMAL;
+    }
+
+    public Icon getIcon() {
+        return ImageUtilities.loadImageIcon("/org/netbeans/modules/hudson/ui/resources/" + // NOI18N
+                (failed ? "red" : "yellow") + (running ? "_run" : "") + ".png", true); // NOI18N
+    }
+
+}
