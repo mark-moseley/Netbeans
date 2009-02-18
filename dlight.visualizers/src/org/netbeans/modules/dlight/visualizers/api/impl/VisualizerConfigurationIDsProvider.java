ad4ce7cc47b2c382bf39eaeff4da45e3f46238c7
Maria Tishkova <mromashova@netbeans.org>
2009-02-18 10:49:05 +0300

    IZ #158200 : Provide Go to Source UI feature in Table Visualizer and Callees/Callees Visualizer

---
+++ dlight.visualizers/src/org/netbeans/modules/dlight/visualizers/api/impl/VisualizerConfigurationIDsProvider.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.dlight.visualizers.api.impl;
+
+/**
+ *
+ * @author masha
+ */
+public interface VisualizerConfigurationIDsProvider {
+    String CALLERS_CALLEES_VISUALIZER = "callers-calees"; // NOI18N
+    String TREE_TABLE_VISUALIZER = "treeTable"; // NOI18N
+    String TABLE_VISUALIZER = "tableVisualizer"; // NOI18N
+    String ADVANCED_TABLE_VISUALIZER = "advancedTableVisualizer"; // NOI18N
+}
