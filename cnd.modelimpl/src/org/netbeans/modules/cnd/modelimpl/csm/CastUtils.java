cf0bb9ba9d39b7ade809e0030df87fb73c7e3183
Vladimir Voskresensky <vv159170@netbeans.org>
2009-02-19 14:31:29 +0300

    use char seq instead of strings

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/CastUtils.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/CastUtils.java
@@ -86,6 +86,8 @@ public class CastUtils {
 		case CPPTokenTypes.AMPERSAND:
 		case CPPTokenTypes.STAR:
 		case CPPTokenTypes.LITERAL_const:
+                case CPPTokenTypes.LITERAL___const:    
+                case CPPTokenTypes.LITERAL___const__:
 		    sb.append(next.getText());
 		    break;
 		default:
@@ -118,32 +120,6 @@ public class CastUtils {
 		sb.append(text);
 	    }
 	}
-    }
-    
-    public static String[] getClassOrNspNames(AST ast) {
-	assert isCast(ast);
-	AST child = ast.getFirstChild();
-	if( child != null && child.getType() == CPPTokenTypes.ID ) {
-	    AST next = child.getNextSibling();
-	    if( next != null && next.getType() == CPPTokenTypes.SCOPE ) {
-		List<String> l = new ArrayList<String>();
-		l.add(child.getText());
-		begin:
-		for( next = next.getNextSibling(); next != null; next = next.getNextSibling() ) {
-		    switch( next.getType() ) {
-			case CPPTokenTypes.ID:
-			    l.add(next.getText());
-                            break;
-			case CPPTokenTypes.SCOPE:
-			    break; // do nothing
-			default:
-			    break begin;
-		    }
-		}
-		return (String[]) l.toArray(new String[l.size()]);
-	    }
-	}
-	return null;
     }
     
     public static boolean isMemberDefinition(AST ast) {
