6786cf77caeab1b9800453a9498af72ed086f9db
Jaroslav Tulach <jtulach@netbeans.org>
2009-02-20 18:34:41 +0100

    Updating static analysis and dynamic verification to work with @AntBasedProjectRegistration

---
+++ ide.ergonomics/project-wizard.xsl
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <xsl:output method="xml" omit-xml-declaration="yes"/>
+    <xsl:param name="cluster.name"/>
+
+    <xsl:template match="filesystem/folder[@name='Templates']">
+        <xsl:element name="folder">
+            <xsl:attribute name="name">Templates</xsl:attribute>
+            <xsl:apply-templates mode="project-wizard"/>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="filesystem/folder[@name='Services']/folder[@name='MIMEResolver']">
+        <xsl:element name="folder">
+            <xsl:attribute name="name">Services</xsl:attribute>
+            <xsl:element name="folder">
+                <xsl:attribute name="name">MIMEResolver</xsl:attribute>
+                <xsl:apply-templates mode="mime-resolvers"/>
+            </xsl:element>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="filesystem/folder[@name='Debugger']">
+        <xsl:element name="folder">
+            <xsl:attribute name="name">Debugger</xsl:attribute>
+                <xsl:apply-templates mode="attach-types"/>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="filesystem/folder[@name='Services']/folder[@name='AntBasedProjectTypes']">
+        <xsl:element name="folder">
+            <xsl:attribute name="name">Ergonomics</xsl:attribute>
+            <xsl:element name="folder">
+                <xsl:attribute name="name">AntBasedProjectTypes</xsl:attribute>
+                <xsl:apply-templates mode="project-types"/>
+            </xsl:element>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="filesystem/folder[@name='Loaders']/folder/folder/folder[@name='Factories']">
+        <xsl:element name="folder">
+            <xsl:attribute name="name">Loaders</xsl:attribute>
+            <xsl:element name="folder">
+                <xsl:attribute name="name"><xsl:value-of select="../../@name"/></xsl:attribute>
+                <xsl:element name="folder">
+                    <xsl:attribute name="name"><xsl:value-of select="../@name"/></xsl:attribute>
+                    <xsl:element name="folder">
+                        <xsl:attribute name="name">Factories</xsl:attribute>
+                        <xsl:element name="file">
+                            <xsl:attribute name="name">Ergonomics.instance</xsl:attribute>
+                            <xsl:element name="attr">
+                                <xsl:attribute name="name">instanceCreate</xsl:attribute>
+                                <xsl:attribute name="methodvalue">org.netbeans.modules.ide.ergonomics.fod.FodDataObjectFactory.create</xsl:attribute>
+                            </xsl:element>
+                            <xsl:element name="attr">
+                                <xsl:attribute name="name">position</xsl:attribute>
+                                <xsl:attribute name="intvalue">999999</xsl:attribute>
+                            </xsl:element>
+                        </xsl:element>
+                    </xsl:element>
+                </xsl:element>
+            </xsl:element>
+        </xsl:element>
+    </xsl:template>
+
+    <!-- project wizard -->
+    <xsl:template match="file" mode="project-wizard">
+        <xsl:element name="file">
+            <xsl:attribute name="name"><xsl:value-of select="@name"/></xsl:attribute>
+            <xsl:apply-templates mode="project-wizard"/>
+        </xsl:element>
+    </xsl:template>
+    <xsl:template match="folder" mode="project-wizard">
+        <xsl:element name="folder">
+            <xsl:attribute name="name"><xsl:value-of select="@name"/></xsl:attribute>
+            <xsl:apply-templates mode="project-wizard"/>
+        </xsl:element>
+    </xsl:template>
+    <xsl:template match="attr[@name='instantiatingIterator']" mode="project-wizard">
+        <xsl:element name="attr">
+            <xsl:attribute name="name">instantiatingIterator</xsl:attribute>
+            <xsl:attribute name="methodvalue">org.netbeans.modules.ide.ergonomics.api.Factory.newProject</xsl:attribute>
+        </xsl:element>
+    </xsl:template>
+    <xsl:template match="attr[@name='templateWizardIterator']" mode="project-wizard">
+        <xsl:element name="attr">
+            <xsl:attribute name="name">instantiatingIterator</xsl:attribute>
+            <xsl:attribute name="methodvalue">org.netbeans.modules.ide.ergonomics.api.Factory.newProject</xsl:attribute>
+        </xsl:element>
+    </xsl:template>
+    <xsl:template match="attr" mode="project-wizard">
+        <xsl:copy-of select="."/>
+    </xsl:template>
+
+    <!-- mime-resolvers -->
+    <xsl:template match="file" mode="mime-resolvers">
+        <xsl:element name="file">
+            <xsl:attribute name="name"><xsl:value-of select="@name"/></xsl:attribute>
+            <xsl:if test="@url">
+                <xsl:attribute name="url"><xsl:value-of select="@url"/></xsl:attribute>
+            </xsl:if>
+            <xsl:apply-templates mode="mime-resolvers"/>
+        </xsl:element>
+    </xsl:template>
+    <xsl:template match="attr" mode="mime-resolvers">
+        <xsl:copy-of select="."/>
+    </xsl:template>
+
+    <!-- attach type -->
+    <xsl:template match="file" mode="attach-types">
+        <xsl:element name="file">
+            <xsl:attribute name="name"><xsl:value-of select="@name"/></xsl:attribute>
+            <xsl:if test="@url">
+                <xsl:attribute name="url"><xsl:value-of select="@url"/></xsl:attribute>
+            </xsl:if>
+            <xsl:apply-templates mode="attach-types"/>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="attr[@name='instanceCreate']" mode="attach-types">
+        <xsl:element name="attr">
+            <xsl:attribute name="name">instanceCreate</xsl:attribute>
+            <xsl:attribute name="methodvalue">org.netbeans.modules.ide.ergonomics.debugger.AttachTypeProxy.create</xsl:attribute>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="attr" mode="attach-types">
+        <xsl:copy-of select="."/>
+    </xsl:template>
+
+    <!-- project type -->
+    <xsl:template match="file" mode="project-types">
+        <xsl:element name="file">
+            <xsl:attribute name="name"><xsl:value-of select="@name"/></xsl:attribute>
+            <xsl:if test="@url">
+                <xsl:attribute name="url"><xsl:value-of select="@url"/></xsl:attribute>
+            </xsl:if>
+            <xsl:apply-templates mode="project-types"/>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="attr[@name='instanceCreate']" mode="project-types">
+        <xsl:element name="attr">
+            <xsl:attribute name="name">instanceCreate</xsl:attribute>
+            <xsl:attribute name="methodvalue">org.netbeans.modules.ide.ergonomics.fod.FeatureProjectFactory.create</xsl:attribute>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="attr[@name='instanceClass']" mode="project-types">
+        <xsl:element name="attr">
+            <xsl:attribute name="name">instanceClass</xsl:attribute>
+            <xsl:attribute name="methodvalue">org.netbeans.spi.project.ProjectFactory</xsl:attribute>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="attr" mode="project-types">
+        <xsl:copy-of select="."/>
+    </xsl:template>
+</xsl:stylesheet>
---
+++ ide.ergonomics/src/org/netbeans/modules/ide/ergonomics/fod/FeatureInfo.java
@@ -0,0 +1,304 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.ide.ergonomics.fod;
+
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathExpression;
+import org.openide.filesystems.FileObject;
+import org.w3c.dom.Document;
+import org.openide.filesystems.XMLFileSystem;
+import org.openide.modules.ModuleInfo;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+
+/** Description of <em>Feature On Demand</em> capabilities and a 
+ * factory to create new instances.
+ *
+ * @author Jaroslav Tulach <jtulach@netbeans.org>, Jirka Rechtacek <jrechtacek@netbeans.org>
+ */
+public final class FeatureInfo {
+    private final URL delegateLayer;
+    private XMLFileSystem fs;
+    private final Set<String> cnbs;
+    private final Map<String,String> nbproject = new HashMap<String,String>();
+    private final Map<Object[],String> files = new HashMap<Object[],String>();
+    private Properties properties;
+    final String clusterName;
+
+    private FeatureInfo(String clusterName, Set<String> cnbs, URL delegateLayer, Properties p) {
+        this.cnbs = cnbs;
+        this.delegateLayer = delegateLayer;
+        this.properties = p;
+        this.clusterName = clusterName;
+    }
+    
+
+    public static FeatureInfo create(String clusterName, URL delegateLayer, URL bundle) throws IOException {
+        Properties p = new Properties();
+        p.load(bundle.openStream());
+        String cnbs = p.getProperty("cnbs");
+        assert cnbs != null : "Error loading from " + bundle; // NOI18N
+        TreeSet<String> s = new TreeSet<String>();
+        s.addAll(Arrays.asList(cnbs.split(",")));
+
+        FeatureInfo info = new FeatureInfo(clusterName, s, delegateLayer, p);
+        final String prefix = "nbproject.";
+        final String prefFile = "project.file.";
+        final String prefXPath = "project.xpath.";
+        for (Object k : p.keySet()) {
+            String key = (String) k;
+            if (key.startsWith(prefix)) {
+                info.nbproject(
+                    key.substring(prefix.length()),
+                    p.getProperty(key)
+                );
+            }
+            if (key.startsWith(prefFile)) {
+                try {
+                    info.projectFile(key.substring(prefFile.length()), null, p.getProperty(key));
+                } catch (XPathExpressionException ex) {
+                    IOException e = new IOException(ex.getMessage());
+                    e.initCause(ex);
+                    throw e;
+                }
+            }
+            if (key.startsWith(prefXPath)) {
+                try {
+                    String xpaths = p.getProperty(key);
+                    for (String xp : xpaths.split(",")) {
+                        info.projectFile(key.substring(prefXPath.length()), xp, "");
+                    }
+                } catch (XPathExpressionException ex) {
+                    IOException e = new IOException(ex.getMessage());
+                    e.initCause(ex);
+                    throw e;
+                }
+            }
+        }
+        FileObject fo = info.getXMLFileSystem().findResource("Ergonomics/AntBasedProjectTypes"); // NOI18N
+        if (fo != null) {
+            for (FileObject o : fo.getChildren()) {
+                String type = (String)o.getAttribute("type"); // NOI18N
+                info.nbproject(type, "org.netbeans.modules.project.ant.AntBasedGenericType"); // NOI18N
+            }
+        }
+        return info;
+    }
+
+    public String getProfilerAttachName() {
+        return properties.getProperty("profilerAttachName");
+    }
+
+    public Object getProjectImporter() {
+        return properties.getProperty("projectImporter");
+    }
+
+    String getPreferredCodeNameBase() {
+        return properties.getProperty("mainModule");
+    }
+
+    boolean isEnabled() {
+        for (ModuleInfo mi : Lookup.getDefault().lookupAll(ModuleInfo.class)) {
+            if (cnbs.contains(mi.getCodeNameBase())) {
+                return mi.isEnabled();
+            }
+        }
+        return false;
+    }
+
+    public synchronized XMLFileSystem getXMLFileSystem() {
+        if (fs == null) {
+            URL url = delegateLayer;
+            fs = new XMLFileSystem();
+            if (url != null) {
+                try {
+                    fs.setXmlUrl(url);
+                } catch (IOException ex) {
+                    FoDFileSystem.LOG.log(Level.SEVERE, "Cannot parse: " + url, ex);Exceptions.printStackTrace(ex);
+                } catch (PropertyVetoException ex) {
+                    FoDFileSystem.LOG.log(Level.SEVERE, "Cannot parse: " + url, ex);Exceptions.printStackTrace(ex);
+                }
+            }
+        }
+        return fs;
+    }
+
+    /** @return 0 = no
+     *          1 = yes
+     *          2 = I am interested to be turned on when this project is opened
+     */
+    int isProject(FeatureProjectFactory.Data data) {
+        FoDFileSystem.LOG.log(Level.FINE, "Checking project {0}", data);
+        int toRet;
+        if (isNbProject(data)) {
+            toRet = 1;
+        } else {
+            if (files.isEmpty()) {
+                toRet = 0;
+            } else {
+                toRet = 0;
+                for (Object[] required : files.keySet()) {
+                    String s = (String)required[0];
+                    FoDFileSystem.LOG.log(Level.FINER, "    checking file {0}", s);
+                    if (data.hasFile(s)) {
+                        FoDFileSystem.LOG.log(Level.FINER, "    found", s);
+                        if (data.isDeepCheck() && required[1] != null) {
+                            XPathExpression e = (XPathExpression)required[1];
+                            Document content = data.dom(s);
+                            try {
+                                String res = e.evaluate(content);
+                                FoDFileSystem.LOG.log(
+                                    Level.FINER,
+                                    "Parsed result {0} of type {1}",
+                                    new Object[] {
+                                        res, res == null ? null : res.getClass()
+                                    }
+                                );
+                                if (res != null && res.length() > 0) {
+                                    toRet = 2;
+                                }
+                            } catch (XPathExpressionException ex) {
+                                Exceptions.printStackTrace(ex);
+                            }
+                        } else {
+                            toRet = 1;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        FoDFileSystem.LOG.log(Level.FINE, "  isProject: {0}", toRet);
+        return toRet;
+    }
+
+    public final Set<String> getCodeNames() {
+        return Collections.unmodifiableSet(cnbs);
+    }
+
+    public boolean isPresent() {
+        Set<String> codeNames = new HashSet<String>(getCodeNames());
+        for (ModuleInfo moduleInfo : Lookup.getDefault().lookupAll(ModuleInfo.class)) {
+            codeNames.remove(moduleInfo.getCodeNameBase());
+        }
+        return codeNames.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "FeatureInfo[" + clusterName + "]";
+    }
+    
+    private boolean isNbProject(FeatureProjectFactory.Data data) {
+        if (nbproject.isEmpty()) {
+            return false;
+        } else {
+            if (!data.hasFile("nbproject/project.xml")) { // NOI18N
+                FoDFileSystem.LOG.log(Level.FINEST, "    nbproject/project.xml not found"); // NOI18N
+                return false;
+            }
+            if (!data.isDeepCheck()) {
+                FoDFileSystem.LOG.log(Level.FINEST, "    no deep check, OK"); // NOI18N
+                return true;
+            }
+            String text = data.is("nbproject/project.xml"); // NOI18N
+            if (text == null) {
+                return false;
+            }
+            for (String t : nbproject.keySet()) {
+                final String pattern = "<type>" + t + "</type>"; // NOI18N
+                if (text.indexOf(pattern) >= 0) { // NOI18N
+                    FoDFileSystem.LOG.log(Level.FINEST, "    '" + pattern + "' found, OK"); // NOI18N
+                    return true;
+                } else {
+                    FoDFileSystem.LOG.log(Level.FINEST, "    '" + pattern + "' not found"); // NOI18N
+                }
+            }
+            FoDFileSystem.LOG.log(Level.FINEST, "    not accepting"); // NOI18N
+            return false;
+        }
+    }
+
+    final void nbproject(String prjType, String clazz) {
+        nbproject.put(prjType, clazz);
+    }
+    final void projectFile(String file, String xpath, String clazz) throws XPathExpressionException {
+        XPathExpression e = null;
+        if (xpath != null) {
+            e = XPathFactory.newInstance().newXPath().compile(xpath);
+        }
+
+        files.put(new Object[] { file, e }, clazz);
+    }
+    static Map<String,String> nbprojectTypes() {
+        Map<String,String> map = new HashMap<String, String>();
+
+        for (FeatureInfo info : FeatureManager.features()) {
+            map.putAll(info.nbproject);
+        }
+        return map;
+    }
+
+    static Map<String,String> projectFiles() {
+        Map<String,String> map = new HashMap<String, String>();
+
+        for (FeatureInfo info : FeatureManager.features()) {
+            for (Map.Entry<Object[], String> e : info.files.entrySet()) {
+                if (e.getValue().length() > 0) {
+                    map.put((String)(e.getKey()[0]), e.getValue());
+                }
+            }
+        }
+        return map;
+    }
+}
