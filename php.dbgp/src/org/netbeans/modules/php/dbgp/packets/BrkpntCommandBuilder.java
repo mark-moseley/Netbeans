e93fccd379a6988588dc1e5cc644870f803e78ca
Radek Matous <rmatous@netbeans.org>
2009-02-23 13:40:18 +0100

    source file and web server URI mapping related to #156220, #152390

--- php.dbgp/src/org/netbeans/modules/php/dbgp/packets/BrkpntCommandBuilder.java
+++ php.dbgp/src/org/netbeans/modules/php/dbgp/packets/BrkpntCommandBuilder.java
@@ -40,7 +40,7 @@
  */
 package org.netbeans.modules.php.dbgp.packets;
 
-import org.netbeans.modules.php.dbgp.api.SessionId;
+import org.netbeans.modules.php.dbgp.SessionId;
 import org.netbeans.modules.php.dbgp.breakpoints.FunctionBreakpoint;
 import org.netbeans.modules.php.dbgp.breakpoints.LineBreakpoint;
 import org.netbeans.modules.php.dbgp.packets.BrkpntSetCommand.Types;
@@ -57,26 +57,12 @@ public class BrkpntCommandBuilder {
     private BrkpntCommandBuilder() {
         // avoid inst-n
     }
-    
-    public static BrkpntSetCommand buildLineBreakpoint( SessionId id , 
-            String transactionId, String localFileName , int lineNumber ) 
-    {
-        BrkpntSetCommand command = new BrkpntSetCommand( transactionId );
-        String uri = id.getFileUriByLocal(localFileName);
-        if ( uri == null ) {
-            return null;
-        }
-        command.setType( Types.LINE );
-        command.setFile( uri );
-        command.setLineNumber(lineNumber);
-        return command;
-    }
-    
+        
     public static BrkpntSetCommand buildLineBreakpoint( SessionId id , 
             String transactionId, FileObject localFile, int lineNumber ) 
     {
         BrkpntSetCommand command = new BrkpntSetCommand( transactionId );
-        String uri = id.getFileUriByLocal(localFile);
+        String uri = id.toWebServerURI(localFile);
         if ( uri == null ) {
             return null;
         }
@@ -94,7 +80,7 @@ public class BrkpntCommandBuilder {
         
         BrkpntSetCommand command =  buildLineBreakpoint(id, transactionId, 
                 fileObject , line.getLineNumber() );
-        command.setBreakpoint( breakpoint );
+        if (command != null) command.setBreakpoint( breakpoint );
         return command;
     }
     
@@ -143,18 +129,7 @@ public class BrkpntCommandBuilder {
         command.setException( excName );
         return command;
     }
-    
-    public static BrkpntSetCommand buildConditionalBreakpoint( SessionId id , 
-            String transactionId, String localFileName , String expression ) 
-    {
-        BrkpntSetCommand command = new BrkpntSetCommand( transactionId );
-        command.setType( Types.CONDITIONAL );
-        String uri = id.getFileUriByLocal(localFileName);
-        command.setFile( uri );
-        command.setExpression( expression );
-        return command;
-    }
-    
+
     public static BrkpntSetCommand buildConditionalBreakpoint(  
             String transactionId, String expression ) 
     {
@@ -163,4 +138,29 @@ public class BrkpntCommandBuilder {
         command.setExpression( expression );
         return command;
     }
+    /*
+       public static BrkpntSetCommand buildLineBreakpoint( SessionId id ,
+            String transactionId, String localFileName , int lineNumber )
+    {
+        BrkpntSetCommand command = new BrkpntSetCommand( transactionId );
+        String uri = id.getFileUriByLocal(localFileName);
+        if ( uri == null ) {
+            return null;
+        }
+        command.setType( Types.LINE );
+        command.setFile( uri );
+        command.setLineNumber(lineNumber);
+        return command;
+    }
+
+    public static BrkpntSetCommand buildConditionalBreakpoint( SessionId id ,
+            String transactionId, String localFileName , String expression )
+    {
+        BrkpntSetCommand command = new BrkpntSetCommand( transactionId );
+        command.setType( Types.CONDITIONAL );
+        String uri = id.getFileUriByLocal(localFileName);
+        command.setFile( uri );
+        command.setExpression( expression );
+        return command;
+    }*/
 }
