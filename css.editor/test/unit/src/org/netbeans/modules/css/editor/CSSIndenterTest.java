bd983489d2c5bf1af68ef286978427bc14bab0c1
David Konecny <dkonecny@netbeans.org>
2009-02-25 12:30:21 +1300

    more tests and fixes

--- css.editor/test/unit/src/org/netbeans/modules/css/editor/CSSIndenterTest.java
+++ css.editor/test/unit/src/org/netbeans/modules/css/editor/CSSIndenterTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -34,66 +34,62 @@
  *
  * Contributor(s):
  *
- * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.web.core.syntax.formatting;
+package org.netbeans.modules.css.editor;
 
-import java.io.File;
+import java.util.List;
+import java.util.concurrent.Semaphore;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import org.netbeans.api.editor.mimelookup.MimePath;
 import org.netbeans.api.editor.mimelookup.test.MockMimeLookup;
 import org.netbeans.api.html.lexer.HTMLTokenId;
 import org.netbeans.api.java.lexer.JavaTokenId;
 import org.netbeans.api.jsp.lexer.JspTokenId;
 import org.netbeans.api.lexer.Language;
+import org.netbeans.api.lexer.LanguagePath;
 import org.netbeans.editor.BaseDocument;
+import org.netbeans.editor.ext.html.parser.SyntaxElement;
+import org.netbeans.editor.ext.html.parser.SyntaxParser;
 import org.netbeans.junit.MockServices;
 import org.netbeans.lib.lexer.test.TestLanguageProvider;
 import org.netbeans.modules.css.editor.indent.CssIndentTaskFactory;
+import org.netbeans.modules.css.editor.test.TestBase;
 import org.netbeans.modules.css.formatting.api.support.AbstractIndenter;
 import org.netbeans.modules.css.lexer.api.CSSTokenId;
+import org.netbeans.modules.gsf.GsfTestBase.IndentPrefs;
 import org.netbeans.modules.gsf.api.CompilationInfo;
 import org.netbeans.modules.gsf.api.Formatter;
 import org.netbeans.modules.html.editor.HTMLKit;
 import org.netbeans.modules.html.editor.NbReaderProvider;
+import org.netbeans.modules.html.editor.coloring.EmbeddingUpdater;
 import org.netbeans.modules.html.editor.indent.HtmlIndentTaskFactory;
-import org.netbeans.modules.java.source.parsing.ClassParserFactory;
-import org.netbeans.modules.java.source.parsing.JavacParserFactory;
 import org.netbeans.modules.java.source.save.Reformatter;
 import org.netbeans.modules.web.core.syntax.EmbeddingProviderImpl;
 import org.netbeans.modules.web.core.syntax.JSPKit;
 import org.netbeans.modules.web.core.syntax.indent.JspIndentTaskFactory;
-import org.netbeans.test.web.core.syntax.TestBase2;
 import org.openide.cookies.EditorCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.util.Lookup;
 
-/**
- *
- * @author david
- */
-public class JspIndenterTest extends TestBase2 {
+public class CSSIndenterTest extends TestBase {
 
-    public JspIndenterTest(String name) {
+    public CSSIndenterTest(String name) {
         super(name);
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        initParserJARs();
-        copyWebProjectJarsTo(new File(getDataDir(), "FormattingProject/lib"));
         NbReaderProvider.setupReaders();
         AbstractIndenter.inUnitTestRun = true;
 
-        
-        
         MockServices.setServices(TestLanguageProvider.class, MockMimeLookup.class);
         // init TestLanguageProvider
         Lookup.getDefault().lookup(TestLanguageProvider.class);
-
         TestLanguageProvider.register(CSSTokenId.language());
         TestLanguageProvider.register(HTMLTokenId.language());
         TestLanguageProvider.register(JspTokenId.language());
@@ -106,13 +102,12 @@ public class JspIndenterTest extends TestBase2 {
         HtmlIndentTaskFactory htmlReformatFactory = new HtmlIndentTaskFactory();
         MockMimeLookup.setInstances(MimePath.parse("text/html"), htmlReformatFactory, new HTMLKit("text/x-jsp"));
         Reformatter.Factory factory = new Reformatter.Factory();
-        MockMimeLookup.setInstances(MimePath.parse("text/x-java"), factory, new JavacParserFactory(), new ClassParserFactory());
+        MockMimeLookup.setInstances(MimePath.parse("text/x-java"), factory);
     }
 
     @Override
-    protected void configureIndenters(final BaseDocument document, final Formatter formatter,
-            final CompilationInfo compilationInfo, boolean indentOnly, String mimeType) throws BadLocationException {
-        // override it because I've already done in setUp()
+    protected boolean runInEQ() {
+        return true;
     }
 
     @Override
@@ -136,76 +131,90 @@ public class JspIndenterTest extends TestBase2 {
     }
 
     @Override
-    protected boolean runInEQ() {
-        return true;
+    protected void configureIndenters(final BaseDocument document, final Formatter formatter,
+            final CompilationInfo compilationInfo, boolean indentOnly, String mimeType) throws BadLocationException {
+        // override it because I've already done in setUp()
     }
 
-    public void testFormattingCase001() throws Exception {
-        reformatFileContents("FormattingProject/web/case001.jsp",new IndentPrefs(4,4));
+    private static class Listener extends EmbeddingUpdater {
+        private Semaphore s;
+        public Listener(Document doc, Semaphore s) throws InterruptedException {
+            super(doc);
+            this.s = s;
+            s.acquire();
+        }
+        @Override
+        public void parsingFinished(List<SyntaxElement> elements) {
+            super.parsingFinished(elements);
+            s.release();
+        }
+
     }
 
-    public void testFormattingCase002() throws Exception {
-        reformatFileContents("FormattingProject/web/case002.jsp",new IndentPrefs(4,4));
+    public void testFormatting() throws Exception {
+        format("a{\nbackground: red,\nblue;\n  }\n",
+               "a{\n    background: red,\n        blue;\n}\n", null);
+        format("a{     background: green,\nyellow;\n      }\n",
+               "a{     background: green,\n           yellow;\n}\n", null);
+
+        // comments formatting:
+        format("a{\n/*comme\n  *dddsd\n nt*/\nbackground: red;\n}",
+               "a{\n    /*comme\n      *dddsd\n     nt*/\n    background: red;\n}", null);
+
+        // even though lines are preserved they will be indented according to indent of previous line;
+        // I'm not sure it is OK but leaving like that for now:
+        format("a{\ncolor: red; /* start\n   comment\n end*/ background: blue;\n}",
+               "a{\n    color: red; /* start\n       comment\n     end*/ background: blue;\n}", null);
+
+        // formatting of last line:
+        format("a{\nbackground: red,",
+               "a{\n    background: red,", null);
+        format("a{\nbackground: red,\n",
+               "a{\n    background: red,\n", null);
     }
 
-    public void testFormattingCase003() throws Exception {
-        reformatFileContents("FormattingProject/web/case003.jsp",new IndentPrefs(4,4));
+    private void forceHTMLParsingAndWait(String file, String mimeType, Language language) throws Exception {
+        FileObject fo = getTestFile(file);
+        BaseDocument doc = getDocument(fo, mimeType, language);
+        LanguagePath htmlLP = LanguagePath.get(language);
+        Semaphore s = new Semaphore(1);
+        Listener l = new Listener(doc, s);
+        SyntaxParser.get(doc, htmlLP).addSyntaxParserListener(l);
+        s.acquire();
+        s.release();
     }
 
-    public void testFormattingCase004() throws Exception {
-        /*Project p = ProjectManager.getDefault().findProject(getTestFile("FormattingProject"));
-        DataObject dobj = DataObject.find(getTestFile("FormattingProject/web/simple.jsp"));
-        System.err.println("dobj="+dobj);
-        EditorCookie ec = dobj.getCookie(EditorCookie.class);
-        System.err.println("ec="+ec);
-        StyledDocument doc = ec.openDocument();*/
-
-        reformatFileContents("FormattingProject/web/case004.jsp",new IndentPrefs(4,4));
+    public void testNativeEmbeddingFormattingCase1() throws Exception {
+        forceHTMLParsingAndWait("testfiles/format1.html", "text/html", HTMLTokenId.language());
+        reformatFileContents("testfiles/format1.html", "text/html", HTMLTokenId.language(), new IndentPrefs(4,4));
     }
 
-    public void testFormattingCase005() throws Exception {
-        reformatFileContents("FormattingProject/web/case005.jsp",new IndentPrefs(4,4));
+    public void testNativeEmbeddingFormattingCase2() throws Exception {
+        forceHTMLParsingAndWait("testfiles/format2.html", "text/html", HTMLTokenId.language());
+        reformatFileContents("testfiles/format2.html", "text/html", HTMLTokenId.language(), new IndentPrefs(4,4));
     }
 
-    public void testFormattingIssue121102() throws Exception {
-        reformatFileContents("FormattingProject/web/issue121102.jsp",new IndentPrefs(4,4));
-    }
-
-    public void testFormattingIssue129778() throws Exception {
-        reformatFileContents("FormattingProject/web/issue129778.jsp",new IndentPrefs(4,4));
-    }
-
-    public void testFormattingIssue89174() throws Exception {
-        reformatFileContents("FormattingProject/web/issue89174.jsp",new IndentPrefs(4,4));
+    public void testFormattingNetBeansCSS() throws Exception {
+        reformatFileContents("testfiles/netbeans.css",new IndentPrefs(4,4));
     }
 
     public void testIndentation() throws Exception {
-        insertNewline("<jsp:useBean>^", "<jsp:useBean>\n    ^", null);
-        insertNewline("^<jsp:body>", "\n^<jsp:body>", null);
-
-        insertNewline("<jsp:body>\n    <html>^", "<jsp:body>\n    <html>\n        ^", null);
-        insertNewline("<jsp:body>\n^<html>", "<jsp:body>\n\n    ^<html>", null);
-
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>^", "<jsp:body>\n    <html>\n        <jsp:useBean>\n            ^", null);
-        insertNewline("<jsp:body>\n    <html>\n^<jsp:useBean>", "<jsp:body>\n    <html>\n\n        ^<jsp:useBean>", null);
-
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n            <table>^", "<jsp:body>\n    <html>\n        <jsp:useBean>\n            <table>\n                ^", null);
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n^<table>", "<jsp:body>\n    <html>\n        <jsp:useBean>\n\n            ^<table>", null);
-
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>^", "<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>\n        ^", null);
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n^</jsp:useBean>", "<jsp:body>\n    <html>\n        <jsp:useBean>\n\n        ^</jsp:useBean>", null);
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>^</jsp:useBean>", "<jsp:body>\n    <html>\n        <jsp:useBean>\n        ^</jsp:useBean>", null);
-
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>\n    </html>^", "<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>\n    </html>\n    ^", null);
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>^</html>", "<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>\n    ^</html>", null);
-        insertNewline("<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>^<table>", "<jsp:body>\n    <html>\n        <jsp:useBean>\n        </jsp:useBean>\n        ^<table>", null);
-
-        insertNewline("<%@ taglib uri=\"http://java.sun.com/jsp/jstl/core\"\n        prefix=\"c\" %>^",
-                      "<%@ taglib uri=\"http://java.sun.com/jsp/jstl/core\"\n        prefix=\"c\" %>\n^",null);
-        
-        // TODO: impl matching of INDENT/RETURN and use it to properly match incorrect document:
-        //insertNewline("<jsp:body>\n    <html>^</jsp:body>", "<jsp:body>\n    <html>\n^</jsp:body>", null);
-
+        // property indentation:
+        insertNewline("a{^background: red;\n  }\n", "a{\n    ^background: red;\n  }\n", null);
+        insertNewline("a{\n    background: red;\n}\na{\n    background: red;^\n}", "a{\n    background: red;\n}\na{\n    background: red;\n    ^\n}", null);
+        insertNewline("a{background: red;}\nb{^", "a{background: red;}\nb{\n    ^", null);
+        insertNewline("a {       background: red;^a: b;", "a {       background: red;\n          ^a: b;", null);
+        insertNewline("a {       background: red,^blue", "a {       background: red,\n              ^blue", null);
+        // value indentation:
+        insertNewline("a{\n    background:^red;\n  }\n", "a{\n    background:\n        ^red;\n  }\n", null);
+        insertNewline("a { background: red,^blue; }", "a { background: red,\n        ^blue; }", null);
+        // indentation of end of line:
+        insertNewline("a { background: red,^", "a { background: red,\n        ^", null);
+        insertNewline("a { background: red,^\n", "a { background: red,\n        ^\n", null);
+        // property indentation:
+        insertNewline("a{\n    background: red;^\n  }\n", "a{\n    background: red;\n    ^\n  }\n", null);
+        // new rule indentation:
+        insertNewline("a{\n    background: red;\n  }^", "a{\n    background: red;\n  }\n  ^", null);
     }
 
 }
