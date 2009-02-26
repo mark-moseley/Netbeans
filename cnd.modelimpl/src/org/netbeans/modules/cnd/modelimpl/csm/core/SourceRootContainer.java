04f117b20aff5bc15bff9823f95b5469332224c2
Vladimir Voskresensky <vv159170@netbeans.org>
2009-02-26 19:55:00 +0300

    commented out unused code

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/SourceRootContainer.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/SourceRootContainer.java
@@ -61,12 +61,30 @@ public class SourceRootContainer {
     }
     
     public boolean isMySource(String includePath){
-        return projectRoots.containsKey(includePath);
+        if (projectRoots.containsKey(includePath)){
+            return true;
+        }
+        while (true){
+            int i = includePath.lastIndexOf('\\');
+            if (i <= 0) {
+                i = includePath.lastIndexOf('/');
+            }
+            if (i <= 0) {
+                return false;
+            }
+            includePath = includePath.substring(0,i);
+            Integer val = projectRoots.get(includePath);
+            if (val != null) {
+                if (val > Integer.MAX_VALUE/4) {
+                    return true;
+                }
+            }
+        }
     }
     
     public void fixFolder(String path){
         if (path != null) {
-            projectRoots.put(path,new Integer(Integer.MAX_VALUE/2));
+            projectRoots.put(path, Integer.MAX_VALUE / 2);
         }
     }
     
@@ -76,7 +94,7 @@ public class SourceRootContainer {
         }
     }
     
-    public void addFile(File file){
+    private void addFile(File file){
         File parentFile = FileUtil.normalizeFile(file).getParentFile();
         String path = parentFile.getAbsolutePath();
         addPath(path);
@@ -93,28 +111,28 @@ public class SourceRootContainer {
     
     private void addPath(final String path) {
         Integer integer = projectRoots.get(path);
-        if (integer == null){
-            projectRoots.put(path,new Integer(1));
+        if (integer == null) {
+            projectRoots.put(path, 1);
         } else {
-            projectRoots.put(path, new Integer(integer.intValue()+1));
+            projectRoots.put(path, integer + 1);
         }
     }
     
-    public void removeSources(List<NativeFileItem> items){
-        for( NativeFileItem nativeFileItem : items ) {
-            removeFile(nativeFileItem.getFile());
-        }
-    }
-    
-    public void removeFile(File file){
-        String path = FileUtil.normalizeFile(file).getParent();
-        Integer integer = projectRoots.get(path);
-        if (integer != null){
-            if (integer.intValue()>1) {
-                projectRoots.put(path, new Integer(integer.intValue()-1));
-            } else {
-                projectRoots.remove(path);
-            }
-        }
-    }
+//    public void removeSources(List<NativeFileItem> items){
+//        for( NativeFileItem nativeFileItem : items ) {
+//            removeFile(nativeFileItem.getFile());
+//        }
+//    }
+//
+//    private void removeFile(File file){
+//        String path = FileUtil.normalizeFile(file).getParent();
+//        Integer integer = projectRoots.get(path);
+//        if (integer != null) {
+//            if (integer.intValue() > 1) {
+//                projectRoots.put(path, integer - 1);
+//            } else {
+//                projectRoots.remove(path);
+//            }
+//        }
+//    }
 }
