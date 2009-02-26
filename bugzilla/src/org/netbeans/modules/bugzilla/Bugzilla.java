9a7f89026385d0c34ffdabc53211ebf5e2eaa13a
Tomas Stupka <tstupka@netbeans.org>
2009-02-26 23:57:18 +0100

    no need to cache the repository conection

--- bugzilla/src/org/netbeans/modules/bugzilla/Bugzilla.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/Bugzilla.java
@@ -40,12 +40,14 @@
 package org.netbeans.modules.bugzilla;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.mylyn.internal.bugzilla.core.BugzillaClient;
 import org.eclipse.mylyn.internal.bugzilla.core.BugzillaCorePlugin;
 import org.eclipse.mylyn.internal.bugzilla.core.BugzillaCustomField;
 import org.eclipse.mylyn.internal.bugzilla.core.BugzillaRepositoryConnector;
@@ -66,8 +68,6 @@ public class Bugzilla {
 
     private RequestProcessor rp;
 
-    private Map<String, RepositoryConfiguration> repoToRepoconf = new HashMap<String, RepositoryConfiguration>();
-    
     private Bugzilla() {
         BugzillaCorePlugin bcp = new BugzillaCorePlugin();
         try {
@@ -90,6 +90,17 @@ public class Bugzilla {
             brc = new BugzillaRepositoryConnector();
         }
         return brc;
+    }
+
+    /**
+     * Returns a BugzillaClient for the given repository
+     * @param repository
+     * @return
+     * @throws java.net.MalformedURLException
+     * @throws org.eclipse.core.runtime.CoreException
+     */
+    public BugzillaClient getClient(BugzillaRepository repository) throws MalformedURLException, CoreException {
+        return getRepositoryConnector().getClientManager().getClient(repository.getTaskRepository(), new NullProgressMonitor());
     }
 
     /**
@@ -259,12 +270,7 @@ public class Bugzilla {
     }
 
     private RepositoryConfiguration getRepositoryConfiguration(BugzillaRepository repository) throws CoreException, IOException {
-        RepositoryConfiguration rc = repoToRepoconf.get(repository.getDisplayName());
-        if(rc == null) {
-            rc = getRepositoryConnector().getClientManager().getClient(repository.getTaskRepository(), new NullProgressMonitor()).getRepositoryConfiguration(new NullProgressMonitor());
-            repoToRepoconf.put(repository.getDisplayName(), rc);
-        }
-        return rc;
+        return getClient(repository).getRepositoryConfiguration(new NullProgressMonitor());
     }
 
 }
