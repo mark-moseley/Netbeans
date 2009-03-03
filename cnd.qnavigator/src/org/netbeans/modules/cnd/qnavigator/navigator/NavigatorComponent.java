ea282de5918f13765c0461518460737eda3123dc
Vladimir Voskresensky <vv159170@netbeans.org>
2009-03-03 16:40:03 +0300

    changed logic to use pure data objects

--- cnd.qnavigator/src/org/netbeans/modules/cnd/qnavigator/navigator/NavigatorComponent.java
+++ cnd.qnavigator/src/org/netbeans/modules/cnd/qnavigator/navigator/NavigatorComponent.java
@@ -41,16 +41,13 @@
 
 package org.netbeans.modules.cnd.qnavigator.navigator;
 
-import java.util.Collection;
-import java.util.List;
 import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
+import org.netbeans.modules.cnd.api.model.CsmListeners;
 import org.netbeans.modules.cnd.api.model.CsmModelAccessor;
-import org.netbeans.modules.cnd.loaders.CCDataObject;
-import org.netbeans.modules.cnd.loaders.CDataObject;
-import org.netbeans.modules.cnd.loaders.CndDataObject;
-import org.netbeans.modules.cnd.loaders.HDataObject;
+import org.netbeans.modules.cnd.utils.MIMENames;
 import org.netbeans.spi.navigator.NavigatorPanel;
+import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
@@ -64,12 +61,7 @@ import org.openide.util.NbBundle;
 public class NavigatorComponent implements NavigatorPanel, LookupListener {
     
     /** Lookup template to search for java data objects. shared with InheritanceTreePanel */
-    private Lookup.Template CTemplate = new Lookup.Template(CDataObject.class);
-    private Lookup.Result CContext;
-    private Lookup.Template CCTemplate = new Lookup.Template(CCDataObject.class);
-    private Lookup.Result CCContext;
-    private Lookup.Template HTemplate = new Lookup.Template(HDataObject.class);
-    private Lookup.Result HContext;
+    private Lookup.Result<DataObject> doContext;
     /** UI of this navigator panel */
     private NavigatorPanelUI panelUI;
     /** model actually containing content of this panel */
@@ -89,37 +81,21 @@ public class NavigatorComponent implements NavigatorPanel, LookupListener {
     public JComponent getComponent() {
         return getPanelUI();
     }
-    
+
+    private String getMime(DataObject dobj) {
+        FileObject fo = (dobj == null) ? null : dobj.getPrimaryFile();
+        String mime = (fo == null) ? "" : fo.getMIMEType();
+        return mime;
+    }
     /** Called when this panel's component is about to being displayed.
      * Right place to attach listeners to current navigation data context.
      *
      * @param context Lookup instance representing current context
      */
     public synchronized void panelActivated(Lookup context) {
-        CContext = context.lookup(CTemplate);
-        CCContext = context.lookup(CCTemplate);
-        HContext = context.lookup(HTemplate);
-        
-        Collection col = CContext.allInstances();
-        if (CContext.allInstances().size()>0 ){
-            curData = (DataObject)(((List)col).get(0));
-        }
-        
-        col = CCContext.allInstances();
-        if (CCContext.allInstances().size()>0 && curData == null){
-            curData = (DataObject)(((List)col).get(0));
-        }
-        
-        col = HContext.allInstances();
-        if (HContext.allInstances().size()>0 && curData == null){
-            curData = (DataObject)(((List)col).get(0));
-        }
-        
-        CContext.addLookupListener(this);
-        CCContext.addLookupListener(this);
-        HContext.addLookupListener(this);
-        
-        setNewContent(curData);
+        doContext = context.lookupResult(DataObject.class);
+        doContext.addLookupListener(this);
+        resultChanged(null);
     }
     
     
@@ -127,12 +103,8 @@ public class NavigatorComponent implements NavigatorPanel, LookupListener {
      * Right place to detach, remove listeners from data context.
      */
     public synchronized void panelDeactivated() {
-        CContext.removeLookupListener(this);
-        CCContext.removeLookupListener(this);
-        HContext.removeLookupListener(this);
-        CContext = null;
-        CCContext = null;
-        HContext = null;
+        doContext.removeLookupListener(this);
+        doContext = null;
         detachFromModel(curModel);
         curModel = null;
         curData = null;
@@ -140,20 +112,20 @@ public class NavigatorComponent implements NavigatorPanel, LookupListener {
     
     /** Impl of LookupListener, reacts to changes of context */
     public synchronized void resultChanged(LookupEvent ev) {
-        Collection data = ((Lookup.Result)ev.getSource()).allInstances();
-        if (!data.isEmpty()) {
-            CndDataObject cdo = (CndDataObject)data.iterator().next();
-            if (!cdo.equals(curData)) {
-		detachFromModel(curModel);
-                curData = cdo;
-                setNewContent(cdo);
+        for (DataObject dob : doContext.allInstances()) {
+            if (MIMENames.isHeaderOrCppOrC(getMime(dob))) {
+                if (!dob.equals(curData)) {
+                    detachFromModel(curModel);
+                    curData = dob;
+                    setNewContent(dob);
+                }
+                break;
             }
         }
     }
     
-    /** Default activated Node strategy is enough for now */
     public Lookup getLookup() {
-        return null;
+        return this.panelUI.getLookup();
     }
     
     // ModelBusyListener impl - sets wait cursor on content during computing
@@ -211,8 +183,8 @@ public class NavigatorComponent implements NavigatorPanel, LookupListener {
     
     private void setNewContentImpl(DataObject cdo, NavigatorPanelUI ui) {
         curModel = new NavigatorModel(cdo, ui, this);
-        CsmModelAccessor.getModel().addProgressListener(curModel);
-        CsmModelAccessor.getModel().addModelListener(curModel);
+        CsmListeners.getDefault().addProgressListener(curModel);
+        CsmListeners.getDefault().addModelListener(curModel);
         ui.getContent().setModel(curModel);
         try {
             curModel.addBusyListener(this);
@@ -224,8 +196,8 @@ public class NavigatorComponent implements NavigatorPanel, LookupListener {
     
     private void detachFromModel(NavigatorModel model) {
 	if( model != null ) {
-	    CsmModelAccessor.getModel().removeProgressListener(model);
-	    CsmModelAccessor.getModel().removeModelListener(model);
+	    CsmListeners.getDefault().removeProgressListener(model);
+	    CsmListeners.getDefault().removeModelListener(model);
 	    model.removeBusyListener(this);
 	    model.removeNotify();
 	}
