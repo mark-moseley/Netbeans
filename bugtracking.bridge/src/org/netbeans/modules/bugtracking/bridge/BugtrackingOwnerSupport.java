bff6cbec65156079732c7d59fbe072bdff57291d
Marian Petras <mpetras@netbeans.org>
2009-03-05 21:04:29 +0100

    display the login dialog if the user clicks on a bug ID and he/she is not logged to Kenai yet

--- bugtracking.bridge/src/org/netbeans/modules/bugtracking/bridge/BugtrackingOwnerSupport.java
+++ bugtracking.bridge/src/org/netbeans/modules/bugtracking/bridge/BugtrackingOwnerSupport.java
@@ -39,21 +39,48 @@
 
 package org.netbeans.modules.bugtracking.bridge;
 
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+import java.util.regex.Pattern;
+import javax.security.auth.login.LoginException;
+import org.netbeans.modules.bugtracking.spi.BugtrackingConnector;
 import org.netbeans.modules.bugtracking.spi.Repository;
 import org.netbeans.modules.bugtracking.util.BugtrackingUtil;
+import org.netbeans.modules.kenai.api.Kenai;
+import org.netbeans.modules.kenai.api.KenaiException;
+import org.netbeans.modules.kenai.api.KenaiProject;
+import org.netbeans.modules.kenai.ui.spi.UIUtils;
+import org.netbeans.modules.versioning.spi.VersioningSupport;
+import org.netbeans.modules.versioning.spi.VersioningSystem;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.NbBundle;
+import org.openide.util.NbPreferences;
 
 /**
  *
  * @author Tomas Stupka
+ * @author Marian Petras
  */
 public class BugtrackingOwnerSupport {
 
     private static Logger LOG = Logger.getLogger("org.netbeans.modules.bugtracking.bridge.BugtrackingOwnerSupport");   // NOI18N
+
+    private static final String REPOSITORY_FOR_FILE_PREFIX = "repository for "; //NOI18N
+
+    private static final String KENAI_PROJECT_URI_REGEXP
+       = "(https|http)://(testkenai|kenai)\\.com/(svn|hg)/(\\S*)~(.*)"; //NOI18N
+    private static Pattern repositoryPattern = Pattern.compile(KENAI_PROJECT_URI_REGEXP);
 
     private Map<File, Repository> fileToRepo = new HashMap<File, Repository>(10);
     private static BugtrackingOwnerSupport instance;
@@ -68,44 +95,240 @@ public class BugtrackingOwnerSupport {
     }
     
     public Repository getRepository(File file) {
-        // check cached values firts
-        // XXX - todo
-        // 1.) cache repository for a VCS topmostanagedParent
-        // 2.) persist cache
+        return getRepository(file, null);
+    }
+
+    public Repository getRepository(File file, String issueId) {
+        //TODO - synchronization/threading
         Repository repo = fileToRepo.get(file);
         if(repo != null) {
             LOG.log(Level.FINER, " found cached repository [" + repo + "] for file " + file); // NOI18N
         }
 
-        // XXX todo
-        // 1.) check if file belongs to a kenai project and create repository from
-        // its metadata eventually
-        // 2.) store in cache
         if(repo == null) {
-            repo = getBugtrackingOwner(file);
+            repo = getBugtrackingOwner(file, issueId);
             LOG.log(Level.FINER, " caching repository [" + repo + "] for file " + file); // NOI18N
             fileToRepo.put(file, repo);
         }
 
-        // XXX
-        // 1.) there was no repository assotiated with the file
-        // neither does it belong to a kenai project ->
-        // -> ask user if he want's to create a new one
-        // 2.) store in cache
         return repo;
     }
 
-    // XXX dummy
-    private Repository getBugtrackingOwner(File file) {
-        Repository[] repos = BugtrackingUtil.getKnownRepositories();
-        
-        for (Repository r : repos) {
-            if(r.getDisplayName().indexOf("office") > -1) { // XXX
-                return r;
+    private Repository getBugtrackingOwner(File file, String issueId) {
+        FileObject fileObject = FileUtil.toFileObject(file);
+        if (fileObject == null) {
+            return null;
+        }
+
+        Object attValue = fileObject.getAttribute(
+                                   "ProvidedExtensions.RemoteLocation");//NOI18N
+        if (attValue instanceof String) {
+            Repository repository;
+            try {
+                repository = getKenaiBugtrackingRepository((String) attValue);
+            } catch (LoginException ex) {
+                return null;
+            }
+
+            if (repository != null) {
+                return repository;
+            }
+        }
+
+        VersioningSystem owner = VersioningSupport.getOwner(file);
+        if (owner == null) {
+            return null;
+        }
+
+        //XXX: should be the nearest managed ancestor (rather than topmost)
+        File topmostManagedAncestor = owner.getTopmostManagedAncestor(file);
+        if (topmostManagedAncestor == null) {
+            assert false;       //this should not happen
+            return null;
+        }
+
+        Repository repo = fileToRepo.get(topmostManagedAncestor);
+        if (repo != null) {
+            LOG.log(Level.FINER, " found cached repository [" + repo    //NOI18N
+                                 + "] for directory " + topmostManagedAncestor); //NOI18N
+            return repo;
+        }
+
+        repo = getRepositoryFromPrefs(topmostManagedAncestor);
+        if (repo != null) {
+            LOG.log(Level.FINER, " found stored repository [" + repo    //NOI18N
+                                 + "] for directory " + topmostManagedAncestor); //NOI18N
+            fileToRepo.put(topmostManagedAncestor, repo);
+            return repo;
+        }
+
+        repo = askUserToSpecifyRepository(fileObject, issueId);
+        if (repo != null) {
+            fileToRepo.put(topmostManagedAncestor, repo);
+            storeRepositoryMappingToPrefs(topmostManagedAncestor, repo);
+            return repo;
+        }
+
+        return null;
+    }
+
+    /**
+     * 
+     * @throws  javax.security.auth.login.LoginException
+     *          if the user failed to log into Kenai
+     */
+    private Repository getKenaiBugtrackingRepository(String remoteLocation) throws LoginException {
+
+        if (!isKenaiProjectLocation(remoteLocation)) {
+            return null;
+        }
+
+        boolean loggedToKenai = checkUserIsLoggedToKenai();
+        if (!loggedToKenai) {
+            throw new LoginException();
+        }
+
+        try {
+            KenaiProject project = KenaiProject.forRepository(remoteLocation);
+            Repository repository = (project != null) ? getKenaiBugtrackingRepository(project)
+                                                      : null;
+            return repository;
+        } catch (KenaiException ex) {
+            LOG.throwing(getClass().getName(),                    //class name
+                         "getKenaiBugtrackingRepository(String)", //method name //NOI18N
+                         ex);
+            return null;
+        }
+    }
+
+    private boolean isKenaiProjectLocation(String remoteLocation) {
+        return repositoryPattern.matcher(remoteLocation).matches();
+    }
+
+    /**
+     * Checks whether the user is logged to Kenai. If he/she is not logged in,
+     * displays the login dialog and allows the user is logged in.
+     * 
+     * @return  {@code true} if the user has already been logged in
+     *          or if he/she has successfuly logged in with the dialog;
+     *          {@code false} otherwise
+     */
+    private boolean checkUserIsLoggedToKenai() {
+        if (Kenai.getDefault().getPasswordAuthentication() != null) {
+            return true;
+        } else {
+            return UIUtils.showLogin();
+        }
+    }
+
+    private Repository getKenaiBugtrackingRepository(KenaiProject project) {
+        return BugtrackingUtil.getKenaiBugtrackingRepository(project);
+    }
+
+    private Repository getRepositoryFromPrefs(File file) {
+        String filePath;
+        try {
+            filePath = file.getCanonicalPath();
+        } catch (IOException ex) {
+            LOG.throwing(getClass().getCanonicalName(),
+                         "getRepositoryForFile",                        //NOI18N
+                         ex);
+            filePath = file.getAbsolutePath();
+        }
+
+        String preferencesKey = REPOSITORY_FOR_FILE_PREFIX + filePath;
+        String requestedUrl = getPreferences().get(preferencesKey, null);
+        if ((requestedUrl == null) || (requestedUrl.length() == 0)) {
+            return null;
+        }
+
+        Repository[] repositories = BugtrackingUtil.getKnownRepositories();
+        for (Repository repository : repositories) {
+            if (repository.getUrl().equals(requestedUrl)) {
+                return repository;
             }
         }
 
         return null;
+    }
+
+    private void storeRepositoryMappingToPrefs(File root, Repository repository) {
+        String key, value;
+
+        try {
+            key = root.getCanonicalPath();
+        } catch (IOException ex) {
+            LOG.throwing(getClass().getCanonicalName(), "storeMapping", //NOI18N
+                         ex);
+            return;
+        }
+        value = repository.getUrl();
+
+        getPreferences().put(REPOSITORY_FOR_FILE_PREFIX + key, value);
+    }
+
+    private Repository askUserToSpecifyRepository(FileObject file, String issueId) {
+        Repository[] repos = BugtrackingUtil.getKnownRepositories();
+        BugtrackingConnector[] connectors = BugtrackingUtil.getBugtrackingConnectors();
+
+        final RepositorySelectorPanel selectorPanel = new RepositorySelectorPanel(repos, connectors);
+
+        final String dialogTitle = getMessage("LBL_BugtrackerSelectorTitle"); //NOI18N
+        final String selectButtonLabel = getMessage("CTL_Select");            //NOI18N
+
+        class ButtonActionListener implements ActionListener {
+            private Dialog dialog;
+            void setDialog(Dialog dialog) {
+                this.dialog = dialog;
+            }
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == selectButtonLabel) {
+                    boolean valuesAreValid = selectorPanel.validateValues();
+                    if (valuesAreValid) {
+                        assert dialog != null;
+                        dialog.setVisible(false);
+                        dialog.dispose();
+                    }
+                }
+            }
+        }
+
+        final ButtonActionListener actionListener = new ButtonActionListener();
+
+        DialogDescriptor dialogDescriptor = new DialogDescriptor(selectorPanel,
+                             dialogTitle,
+                             true,
+                             new Object[] {selectButtonLabel, DialogDescriptor.CANCEL_OPTION},
+                             selectButtonLabel,
+                             DialogDescriptor.DEFAULT_ALIGN,
+                             null,      //HelpCtx
+                             actionListener);
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(dialogDescriptor);
+        actionListener.setDialog(dialog);
+
+        dialog.pack();
+        dialog.setVisible(true);
+
+        if (dialogDescriptor.getValue() == selectButtonLabel) {
+            Repository repository = selectorPanel.getSelectedRepository();
+            try {
+                repository.getController().applyChanges();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+                repository = null;
+            }
+            return repository;
+        } else {
+            return null;
+        }
+    }
+
+    public Preferences getPreferences() {
+        return NbPreferences.forModule(BugtrackingOwnerSupport.class);
+    }
+
+    private String getMessage(String msgKey) {
+        return NbBundle.getMessage(BugtrackingOwnerSupport.class, msgKey);
     }
 
 }
