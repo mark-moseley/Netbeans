f93b1d64feea2e5b71a1e1481604e2d7c0330170
Vita Stejskal <vstejskal@netbeans.org>
2009-03-06 12:35:33 +0100

    Various fixes in tests, commit-validation, etc

--- java.source/test/unit/src/org/netbeans/modules/java/source/usages/NeverEndingScanTest.java
+++ java.source/test/unit/src/org/netbeans/modules/java/source/usages/NeverEndingScanTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,22 +31,40 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.java.source.usages;
 
-import java.io.File;
-import org.netbeans.modules.parsing.impl.indexing.CacheFolder;
-import org.openide.filesystems.FileUtil;
+import org.netbeans.junit.NbTestCase;
+import org.netbeans.modules.parsing.api.indexing.IndexingManager;
 
 /**
  *
- * @author Tomas Zezula
+ * @author Pavel Flaska
  */
-public class IndexUtil {
+public class NeverEndingScanTest extends NbTestCase {
 
-    public static void setCacheFolder (final File cacheFolder) {
-        CacheFolder.setCacheFolder(FileUtil.toFileObject(cacheFolder));
+    public NeverEndingScanTest(String name) {
+        super(name);
+    }
+
+    /**
+     * When no projects are being opened, scan shouldn't be in progress.
+     * (#149727)
+     *
+     * @throws java.lang.InterruptedException
+     */
+    public void testNeverendingScan() throws InterruptedException {
+        int counter = 0;
+        while (IndexingManager.getDefault().isIndexing() && counter < 8) {
+            Thread.sleep(1000);
+            counter++;
+        }
+        assertFalse("Scan does not end", IndexingManager.getDefault().isIndexing());
     }
 
 }
