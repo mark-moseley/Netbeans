e94c9c86a12016ae14125641f242052025519e24
Vladimir Voskresensky <vv159170@netbeans.org>
2009-03-06 15:45:27 +0300

    hashCode && equals && some abbrevs and final classes

--- cnd.apt/src/org/netbeans/modules/cnd/apt/impl/structure/APTFileNode.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/impl/structure/APTFileNode.java
@@ -53,7 +53,7 @@ import org.netbeans.modules.cnd.apt.support.APTWalker;
 public final class APTFileNode extends APTContainerNode 
                                 implements APTFile, Serializable {
     private static final long serialVersionUID = -6182803432699849825L;
-    private String path;
+    private final String path;
     transient private boolean tokenized;
     
     /** Copy constructor */
@@ -61,11 +61,6 @@ public final class APTFileNode extends APTContainerNode
         super(orig);
         this.path = orig.path;
         this.tokenized = false;
-    }
-    
-    /** Constructor for serialization */
-    protected APTFileNode() {
-        tokenized = false;
     }
     
     /** Creates a new instance of APTFileNode */
@@ -99,6 +94,7 @@ public final class APTFileNode extends APTContainerNode
         return path;
     }
 
+    @Override
     public void dispose() {
         if (isTokenized()) {
             new CleanTokensWalker(this).visit();
@@ -108,7 +104,30 @@ public final class APTFileNode extends APTContainerNode
 
     public boolean isTokenized() {
         return tokenized;
-    }    
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof APTFileNode)) {
+            return false;
+        }
+        final APTFileNode other = (APTFileNode) obj;
+        if (!this.path.equals(other.path)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + this.path.hashCode();
+        return hash;
+    }
+
     ////////////////////////////////////////////////////////////////////////////
     // implementation details
     
@@ -168,16 +187,18 @@ public final class APTFileNode extends APTContainerNode
             // do nothing
         }
 
-//        protected Token onToken(Token token) {
+//        protected APTToken onToken(APTToken token) {
 //            // do nothing
 //            return token;
 //        }    
 
+        @Override
         protected void onStreamNode(APT apt) {
             // clean node's stream
             apt.dispose();
         }
         
+        @Override
         protected void onOtherNode(APT apt) {
             // clean tokens for 
             //APT.Type.INVALID:
--- cnd.apt/src/org/netbeans/modules/cnd/apt/impl/structure/APTMacroBaseNode.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/impl/structure/APTMacroBaseNode.java
@@ -41,11 +41,11 @@
 
 package org.netbeans.modules.cnd.apt.impl.structure;
 
-import antlr.Token;
 import java.io.Serializable;
 import java.util.logging.Level;
 import org.netbeans.modules.cnd.apt.debug.DebugUtils;
 import org.netbeans.modules.cnd.apt.structure.APT;
+import org.netbeans.modules.cnd.apt.support.APTToken;
 import org.netbeans.modules.cnd.apt.support.APTTokenAbstact;
 import org.netbeans.modules.cnd.apt.utils.APTUtils;
 
@@ -56,7 +56,7 @@ import org.netbeans.modules.cnd.apt.utils.APTUtils;
 public abstract class APTMacroBaseNode extends APTTokenBasedNode
                                         implements Serializable {
     private static final long serialVersionUID = 1315417078059538898L;
-    private Token macroName = EMPTY_NAME;
+    private APTToken macroName = EMPTY_NAME;
     
     /** Copy constructor */
     /**package*/APTMacroBaseNode(APTMacroBaseNode orig) {
@@ -69,7 +69,7 @@ public abstract class APTMacroBaseNode extends APTTokenBasedNode
     }
     
     /** Creates a new instance of APTMacroBaseNode */
-    public APTMacroBaseNode(Token token) {
+    public APTMacroBaseNode(APTToken token) {
         super(token);
     }
 
@@ -83,7 +83,7 @@ public abstract class APTMacroBaseNode extends APTTokenBasedNode
         assert (false) : "define/undef doesn't support children"; // NOI18N        
     }
 
-    public boolean accept(Token token) {
+    public boolean accept(APTToken token) {
         if (APTUtils.isID(token)) {
             if (macroName != EMPTY_NAME) {
                 // init macro name only once
@@ -113,19 +113,32 @@ public abstract class APTMacroBaseNode extends APTTokenBasedNode
         return retValue;
     }
     
-    public Token getName() {
+    public APTToken getName() {
         return macroName;
     }
     
     private static final NotHandledMacroName EMPTY_NAME = new NotHandledMacroName();
     
     //TODO: what about Serializable
-    private static class NotHandledMacroName extends APTTokenAbstact {
+    private static final class NotHandledMacroName extends APTTokenAbstact {
         public NotHandledMacroName() {
         }
         
+        @Override
         public String getText() {
             return "<<DUMMY>>"; // NOI18N
-        }        
+        }
+
+        @Override
+        public int hashCode() {
+            return -1;
+        }
+
+        @Override
+        @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+        public boolean equals(Object obj) {
+            return this == obj;
+        }
+
     };    
 }
