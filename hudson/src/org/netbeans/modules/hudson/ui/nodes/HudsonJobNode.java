5ac71e0bc725a7b5db8d386f5ce5237c5a08cb17
Jesse Glick <jglick@netbeans.org>
2009-03-06 19:14:07 -0500

    Removing job details page at jrojcek's request.

--- hudson/src/org/netbeans/modules/hudson/ui/nodes/HudsonJobNode.java
+++ hudson/src/org/netbeans/modules/hudson/ui/nodes/HudsonJobNode.java
@@ -52,7 +52,6 @@ import org.netbeans.modules.hudson.ui.actions.OpenUrlAction;
 import org.netbeans.modules.hudson.ui.actions.ShowBuildConsole;
 import org.netbeans.modules.hudson.ui.actions.ShowChanges;
 import org.netbeans.modules.hudson.ui.actions.ShowFailures;
-import org.netbeans.modules.hudson.ui.actions.ShowJobDetailAction;
 import org.netbeans.modules.hudson.ui.actions.StartJobAction;
 import org.openide.actions.PropertiesAction;
 import org.openide.nodes.AbstractNode;
@@ -114,7 +113,6 @@ public class HudsonJobNode extends AbstractNode {
     @Override
     public Action[] getActions(boolean context) {
         List<Action> actions = new ArrayList<Action>();
-        actions.add(SystemAction.get(ShowJobDetailAction.class));
         actions.add(SystemAction.get(StartJobAction.class));
         int last = job.getLastBuild();
         if (last >= 0) {
@@ -130,11 +128,6 @@ public class HudsonJobNode extends AbstractNode {
         actions.add(null);
         actions.add(SystemAction.get(PropertiesAction.class));
         return actions.toArray(new Action[actions.size()]);
-    }
-    
-    @Override
-    public Action getPreferredAction() {
-        return SystemAction.get(ShowJobDetailAction.class);
     }
     
     @Override
@@ -191,13 +184,12 @@ public class HudsonJobNode extends AbstractNode {
             setIconBaseWithExtension(ICON_BASE_YELLOW_RUN);
             htmlDisplayName = "<b>"+job.getDisplayName()+"</b>";
             break;
-        case grey:
-            setIconBaseWithExtension(ICON_BASE_GREY);
-            break;
         case grey_anime:
             setIconBaseWithExtension(ICON_BASE_GREY_RUN);
             htmlDisplayName = "<b>"+job.getDisplayName()+"</b>";
             break;
+        default: // grey, disabled, aborted
+            setIconBaseWithExtension(ICON_BASE_GREY);
         }
         
         // Fire changes if any
