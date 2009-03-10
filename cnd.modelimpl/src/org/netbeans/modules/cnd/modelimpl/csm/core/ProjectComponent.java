9963df10ca32af646c8977d2dd29901e20632a76
Vladimir Voskresensky <vv159170@netbeans.org>
2009-03-10 16:13:40 +0300

    put declaration container of namespace, instead of hanging it in memory

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/ProjectComponent.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/ProjectComponent.java
@@ -62,17 +62,18 @@ import org.netbeans.modules.cnd.repository.support.SelfPersistent;
  * @author Vladimir Kvashin
  */
 
-//package-local
-abstract class ProjectComponent implements Persistent, SelfPersistent {
+public abstract class ProjectComponent implements Persistent, SelfPersistent {
     
-    private Key key;
-    
-    public ProjectComponent(Key key) {
+    private final Key key;
+    private final boolean hangInRepository;
+    public ProjectComponent(Key key, boolean hangInRepository) {
 	this.key = key;
+        this.hangInRepository = hangInRepository;
     }
     
     public ProjectComponent(DataInput in) throws IOException {
 	key = KeyFactory.getDefaultFactory().readKey(in);
+        hangInRepository = in.readBoolean();
 	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) System.err.printf("< ProjectComponent: Reading %s key %s\n", this, key);
     }
     
@@ -81,18 +82,23 @@ abstract class ProjectComponent implements Persistent, SelfPersistent {
     }
     
     public void put() {
-	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) System.err.printf("> ProjectComponent: Hanging %s by key %s\n", this, key);
-	RepositoryUtils.hang(key, this);
+	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) {System.err.printf("> ProjectComponent: Hanging %s by key %s\n", this, key);}
+        if (hangInRepository) {
+            RepositoryUtils.hang(key, this);
+        } else {
+            RepositoryUtils.put(key, this);
+        }
     }
 
-    private void putImpl() {
-	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) System.err.printf("> ProjectComponent: Putting %s by key %s\n", this, key);
-	RepositoryUtils.put(key, this);
-    }
+//    private void putImpl() {
+//	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) System.err.printf("> ProjectComponent: Putting %s by key %s\n", this, key);
+//	RepositoryUtils.put(key, this);
+//    }
     
     public void write(DataOutput out) throws IOException {
-	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) System.err.printf("> ProjectComponent: Writing %s by key %s\n", this, key);
+	if( TraceFlags.TRACE_PROJECT_COMPONENT_RW ) {System.err.printf("> ProjectComponent: Writing %s by key %s\n", this, key);}
 	writeKey(key, out);
+        out.writeBoolean(hangInRepository);
     }
     
     public static Key readKey(DataInput in) throws IOException {
@@ -107,8 +113,9 @@ abstract class ProjectComponent implements Persistent, SelfPersistent {
 	Persistent p = RepositoryUtils.tryGet(key);
 	if( p != null ) {
 	    assert p instanceof  ProjectComponent;
-	    ProjectComponent pc = (ProjectComponent) p;
-	    pc.putImpl();
+	    //ProjectComponent pc = (ProjectComponent) p;
+            // A workaround for #131701
+	    //pc.putImpl();
 	}
     }
 }
