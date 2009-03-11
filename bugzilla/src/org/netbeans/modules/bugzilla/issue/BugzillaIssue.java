0c4fc3474251745f05bfd9c1c15509ed11cc224b
Tomas Stupka <tstupka@netbeans.org>
2009-03-11 12:23:07 +0100

    use bugzilla executor

--- bugzilla/src/org/netbeans/modules/bugzilla/issue/BugzillaIssue.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/issue/BugzillaIssue.java
@@ -72,6 +72,8 @@ import org.netbeans.modules.bugtracking.spi.BugtrackingController;
 import org.netbeans.modules.bugtracking.spi.Issue;
 import org.netbeans.modules.bugtracking.spi.Query.ColumnDescriptor;
 import org.netbeans.modules.bugzilla.BugzillaRepository;
+import org.netbeans.modules.bugzilla.commands.BugzillaCommand;
+import org.netbeans.modules.bugzilla.commands.BugzillaExecutor;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 
@@ -179,6 +181,18 @@ public class BugzillaIssue extends Issue {
         this.repository = repo;
     }
 
+    @Override
+    public String getDisplayName() {
+        return data.isNew() ?
+                NbBundle.getMessage(BugzillaIssue.class, "CTL_NewIssue") :
+                NbBundle.getMessage(BugzillaIssue.class, "CTL_Issue", new Object[] {getID(), getSummary()});
+    }
+
+    @Override
+    public String getTooltip() {
+        return getDisplayName();
+    }
+
     public static ColumnDescriptor[] getColumnDescriptors() {
         if(DESCRIPTORS == null) {
             ResourceBundle loc = NbBundle.getBundle(BugzillaIssue.class);
@@ -207,7 +221,7 @@ public class BugzillaIssue extends Issue {
     }
 
     @Override
-    public BugtrackingController getControler() {
+    public BugtrackingController getController() {
         if (controller == null) {
             controller = new IssueController(this);
         }
@@ -299,40 +313,40 @@ public class BugzillaIssue extends Issue {
                 for (IssueField changedField : changedFields) {
                     switch(changedField) {
                         case SUMMARY :
-                            ret = changedCount + " changes, inclusive summary";
+                            ret = changedCount + " changes, incl. summary";
                             break;
                         case PRIORITY :
-                            ret = changedCount + " changes, inclusive priority";
+                            ret = changedCount + " changes, incl. priority";
                             break;
                         case SEVERITY :
-                            ret = changedCount + " changes, inclusive severity";
+                            ret = changedCount + " changes, incl. severity";
                             break;
                         case PRODUCT :
-                            ret = changedCount + " changes, inclusive product";
+                            ret = changedCount + " changes, incl. product";
                             break;
                         case COMPONENT :
-                            ret = changedCount + " changes, inclusive component";
+                            ret = changedCount + " changes, incl. component";
                             break;
                         case PLATFORM :
-                            ret = changedCount + " changes, inclusive platform";
+                            ret = changedCount + " changes, incl. platform";
                             break;
                         case VERSION :
-                            ret = changedCount + " changes, inclusive version";
+                            ret = changedCount + " changes, incl. version";
                             break;
                         case MILESTONE :
-                            ret = changedCount + " changes, inclusive milestone";
+                            ret = changedCount + " changes, incl. milestone";
                             break;
                         case KEYWORDS :
-                            ret = changedCount + " changes, inclusive keywords";
+                            ret = changedCount + " changes, incl. keywords";
                             break;
                         case URL :
-                            ret = changedCount + " changes, inclusive url";
+                            ret = changedCount + " changes, incl. url";
                             break;
                         case ASSIGNED_TO :
-                            ret = changedCount + " changes, inclusive Assignee";
+                            ret = changedCount + " changes, incl. Assignee";
                             break;
                         case QA_CONTACT :
-                            ret = changedCount + " changes, inclusive qa contact";
+                            ret = changedCount + " changes, incl. qa contact";
                             break;
                         case DEPENDS_ON :
                         case BLOCKS :
@@ -348,8 +362,16 @@ public class BugzillaIssue extends Issue {
         return "";
     }
 
+    /**
+     * Returns the id from the given taskData or null if taskData.isNew()
+     * @param taskData
+     * @return id or null
+     */
     public static String getID(TaskData taskData) {
         try {
+            if(taskData.isNew()) {
+                return null;
+            }
             return Integer.toString(BugzillaRepositoryConnector.getBugId(taskData.getTaskId()));
         } catch (CoreException ex) {
             Bugzilla.LOG.log(Level.SEVERE, null, ex);
@@ -563,28 +585,26 @@ public class BugzillaIssue extends Issue {
     }
 
 
-    void addAttachment(File f, String comment, String desc, String contentType) throws HttpException, IOException, CoreException  {
+    void addAttachment(final File file, final String comment, final String desc, final String contentType, final boolean patch) throws HttpException, IOException, CoreException  {
         assert !SwingUtilities.isEventDispatchThread() : "Accesing remote host. Do not call in awt";
-        FileTaskAttachmentSource attachmentSource = new FileTaskAttachmentSource(f);
+        final FileTaskAttachmentSource attachmentSource = new FileTaskAttachmentSource(file);
         attachmentSource.setContentType(contentType);
-        BugzillaTaskAttachmentHandler.AttachmentPartSource source = new BugzillaTaskAttachmentHandler.AttachmentPartSource(attachmentSource);
+        final BugzillaTaskAttachmentHandler.AttachmentPartSource source = new BugzillaTaskAttachmentHandler.AttachmentPartSource(attachmentSource);
 
-//        try {
-            Bugzilla.getInstance().getClient(repository).postAttachment(
-                    getID(), 
-                    comment,
-                    desc,
-                    attachmentSource.getContentType(), 
-                    false, 
-                    source, 
-                    new NullProgressMonitor());
-//        } catch (HttpException ex) {
-//            Bugzilla.LOG.log(Level.SEVERE, null, ex);
-//        } catch (IOException ex) {
-//            Bugzilla.LOG.log(Level.SEVERE, null, ex);
-//        } catch (CoreException ex) {
-//            Bugzilla.LOG.log(Level.SEVERE, null, ex);
-//        }
+        BugzillaCommand cmd = new BugzillaCommand() {
+            @Override
+            public void execute() throws CoreException, IOException, MalformedURLException {
+                Bugzilla.getInstance().getClient(repository).postAttachment(
+                                getID(),
+                                comment,
+                                desc,
+                                attachmentSource.getContentType(),
+                                patch,
+                                source,
+                                new NullProgressMonitor());
+            }
+        };
+        repository.getExecutor().execute(cmd);
     }
 
     Comment[] getComments() {
@@ -614,11 +634,14 @@ public class BugzillaIssue extends Issue {
         if(comment != null) {
             addComment(comment);
         }
-        try {
-            submit();
-        } catch (CoreException ex) {
-            Bugzilla.LOG.log(Level.SEVERE, null, ex);
-        }
+        
+        BugzillaCommand submitCmd = new BugzillaCommand() {
+            @Override
+            public void execute() throws CoreException, IOException, MalformedURLException {
+                submit();
+            }
+        };
+        repository.getExecutor().execute(submitCmd);
     }
 
     public void addComment(String comment) {
@@ -626,6 +649,19 @@ public class BugzillaIssue extends Issue {
             TaskAttribute ta = data.getRoot().createMappedAttribute(TaskAttribute.COMMENT_NEW);
             ta.setValue(comment);
         }
+    }
+
+    @Override
+    public void attachPatch(File file, String description) {
+        refresh();
+        try {
+            addAttachment(file, null, description, null, true);
+        refresh();
+        } catch (IOException ex) {
+            Bugzilla.LOG.log(Level.SEVERE, null, ex);
+        } catch (CoreException ex) {
+            Bugzilla.LOG.log(Level.SEVERE, null, ex);
+    }
     }
 
     void submit() throws CoreException {
