7baa2bebff91d439dec978183842862894303468
Erno Mononen <emononen@netbeans.org>
2009-03-12 12:24:19 +0100

    Trivial, added JavaDoc

--- ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/RailsProjectUtil.java
+++ ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/RailsProjectUtil.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -45,6 +45,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
@@ -60,7 +61,8 @@ import org.openide.util.Exceptions;
 
 /**
  * Miscellaneous utilities for the Rails project module.
- * @author  Jiri Rechtacek
+ *
+ * @author Jiri Rechtacek
  */
 public class RailsProjectUtil {
     
@@ -69,9 +71,8 @@ public class RailsProjectUtil {
     /** Get the version string out of a ruby version.rb file */
     public static String getVersionString(File versionFile) {
         try {
-            Pattern VERSION_ELEMENT = Pattern.compile("\\s*[A-Z]+\\s*=\\s*(\\d+)\\s*");
+            Pattern VERSION_ELEMENT = Pattern.compile("\\s*[A-Z]+\\s*=\\s*(\\d+)\\s*"); // NOI18N
             BufferedReader br = new BufferedReader(new FileReader(versionFile));
-            StringBuilder sb = new StringBuilder();
             int major = 0;
             int minor = 0;
             int tiny = 0;
@@ -81,17 +82,17 @@ public class RailsProjectUtil {
                     break;
                 }
 
-                if (s.indexOf("MAJOR") != -1) {
+                if (s.indexOf("MAJOR") != -1) { // NOI18N
                     Matcher m = VERSION_ELEMENT.matcher(s);
                     if (m.matches()) {
                         major = Integer.parseInt(m.group(1));
                     }
-                } else if (s.indexOf("MINOR") != -1) {
+                } else if (s.indexOf("MINOR") != -1) { // NOI18N
                     Matcher m = VERSION_ELEMENT.matcher(s);
                     if (m.matches()) {
                         minor = Integer.parseInt(m.group(1));
                     }
-                } else if (s.indexOf("TINY") != -1) {
+                } else if (s.indexOf("TINY") != -1) { // NOI18N
                     Matcher m = VERSION_ELEMENT.matcher(s);
                     if (m.matches()) {
                         tiny = Integer.parseInt(m.group(1));
@@ -101,19 +102,26 @@ public class RailsProjectUtil {
             br.close();
             
         
-            return major + "." + minor + "." + tiny;
+            return major + "." + minor + "." + tiny; // NOI18N
         } catch (IOException ioe) {
             Exceptions.printStackTrace(ioe);
         }
         
         return null;
     }
-    
+
+    /**
+     * Gets the rails version the given <code>project</code> uses. Returns
+     * <code>null</code> if the version could not be determined.
+     *
+     * @param project
+     * @return
+     */
     public static String getRailsVersion(Project project) {
         GemManager gemManager = RubyPlatform.gemManagerFor(project);
         // Add in the builtins first (since they provide some more specific
         // UI configuration for known generators (labelling the arguments etc.)
-        String railsVersion = gemManager.getVersion("rails"); // NOI18N
+        String railsVersion = gemManager.getLatestVersion("rails"); // NOI18N
 
         FileObject railsPlugin = project.getProjectDirectory().getFileObject("vendor/rails/railties"); // NOI18N
         if (railsPlugin != null) {
@@ -126,10 +134,55 @@ public class RailsProjectUtil {
                 }
             }
         }
-        
+
+        FileObject environment = project.getProjectDirectory().getFileObject("config/environment.rb"); // NOI18N
+        if (environment != null && environment.isValid()) {
+            String specifiedVersion = getSpecifiedRailsVersion(environment);
+            if (specifiedVersion != null) {
+                railsVersion = specifiedVersion;
+            }
+        }
+
         return railsVersion;
     }
-    
+
+    /** Return the version of Rails requested in environment.rb */
+    public static String getSpecifiedRailsVersion(final FileObject environment) {
+        BufferedReader br = null;
+        try {
+            // Look for version specifications like
+            //    RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+            // in environment.rb
+            br = new BufferedReader(new InputStreamReader(environment.getInputStream()));
+
+            Pattern VERSION_PATTERN = Pattern.compile("\\s*RAILS_GEM_VERSION\\s*=\\s*['\"]((\\d+)\\.(\\d+)\\.(\\d+))['\"].*"); // NOI18N
+            for (int line = 0; line < 20; line++) {
+                String s = br.readLine();
+                if (s == null) {
+                    break;
+                }
+                if (s.indexOf("RAILS_GEM_VERSION") != -1) { // NOI18N
+                    Matcher m = VERSION_PATTERN.matcher(s);
+                    if (m.matches()) {
+                        return m.group(1);
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+            Exceptions.printStackTrace(ioe);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }
+        
+        return null;
+    }
+
     /**
      * Returns the property value evaluated by RailsProject's PropertyEvaluator.
      *
@@ -150,10 +203,10 @@ public class RailsProjectUtil {
         }
     }
     
-    public static void getAllScripts(String prefix, FileObject sourcesRoot, List/*<String>*/ result) {
+    public static void getAllScripts(String prefix, FileObject sourcesRoot, List<String> result) {
         FileObject children[] = sourcesRoot.getChildren();
         if (!"".equals(prefix)) {
-            prefix += "/";
+            prefix += "/"; // NOI18N
             //prefix += ".";
         }
         for (int i = 0; i < children.length; i++) {
