2418f2d291ac36ee7cdbe68be200e6114bf8efc6
Nick N. Krasilnikov <nnnnnk@netbeans.org>
2009-03-12 18:26:50 +0300

    test for IZ#151530 : Variable is resolved as function

--- cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest/functions_and_variables.cc
+++ cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest/functions_and_variables.cc
@@ -40,6 +40,59 @@ using namespace qwe;
 using namespace qwe::asd;
 using namespace qwe::asd::zxc;
 
+// IZ 136165 : Parameter type is not resolved in contained class
+class NewClass {
+public:
+    static int AAA;
+    static int BBB;
+private:
+
+};
+
+int NewClass::AAA = 10;
+int NewClass::BBB(AAA);
+
+
+
+// IZ 139425 : Wrong rendering of declarations with const qualifier
+class C1{
+public:
+    C1(int i){
+        
+    }
+};
+
+int t = 1;
+
+C1 c1(t);
+const C1 c2(t);
+int foo() {
+    C1 c3(t);
+    const C1 c4(t);
+}
+
+
+// IZ 146030 : set of problems for declarations in Loki (usecase 4)
+static void write_five() {
+    global_int = 5;
+}
+function<void() > static_func2(write_five);
+
+// IZ#151530 : Variable is resolved as function
+class T {
+public:
+    int i;
+    T(int) {}
+};
+template <class P> P foo() {
+    return 0;
+};
+T v(foo<int>()); // resolved as function declaration
+int main() {
+    v.i = 0; // unresolved i
+    return 0;
+}
+
 
 //
 // Functions
@@ -57,3 +110,7 @@ int main(int argc, char** argv) {
     foo = zxczxc;
     return foo;
 }
+
+//Some more variables
+int pBa(*bbbb);
+int ba(&bbbbb);
