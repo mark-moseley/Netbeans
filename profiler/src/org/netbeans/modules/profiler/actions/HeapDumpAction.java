a85b24f4b3d52212f802e58235172995d195eada
Tomas Hurka <thurka@netbeans.org>
2009-03-13 18:32:37 +0100

    use Platform.getJDKMinorNumber()

--- profiler/src/org/netbeans/modules/profiler/actions/HeapDumpAction.java
+++ profiler/src/org/netbeans/modules/profiler/actions/HeapDumpAction.java
@@ -46,6 +46,7 @@ import org.netbeans.lib.profiler.ProfilerLogger;
 import org.netbeans.lib.profiler.TargetAppRunner;
 import org.netbeans.lib.profiler.common.Profiler;
 import org.netbeans.lib.profiler.global.CommonConstants;
+import org.netbeans.lib.profiler.global.Platform;
 import org.netbeans.modules.profiler.NetBeansProfiler;
 import org.netbeans.modules.profiler.ProfilerControlPanel2;
 import org.netbeans.modules.profiler.ProfilerIDESettings;
@@ -349,7 +350,7 @@ public final class HeapDumpAction extends ProfilingAwareAction {
             String fileName = TAKEN_HEAPDUMP_PREFIX + System.currentTimeMillis();
             FileObject folder = (targetFolder == null)
                                 ? IDEUtils.getProjectSettingsFolder(NetBeansProfiler.getDefaultNB().getProfiledProject(), true)
-                                : FileUtil.toFileObject(new File(targetFolder));
+                                : FileUtil.toFileObject(FileUtil.normalizeFile(new File(targetFolder)));
 
             return FileUtil.toFile(folder).getAbsolutePath() + File.separator
                    + FileUtil.findFreeFileName(folder, fileName, ResultsManager.HEAPDUMP_EXTENSION) + "."
@@ -370,17 +371,12 @@ public final class HeapDumpAction extends ProfilingAwareAction {
 
         if (CommonConstants.JDK_15_STRING.equals(jdkVersion)) {
             String fullJDKString = targetApp.getProfilingSessionStatus().fullTargetJDKVersionString;
-            int minorIndex = fullJDKString.lastIndexOf('_'); // NOI18N
-
-            if ((minorIndex > 0) && (minorIndex < (fullJDKString.length() - 1))) {
-                String minorString = fullJDKString.substring(minorIndex + 1);
-
-                if (Integer.parseInt(minorString) >= 12) {
-                    return true;
-                }
+            int minorNumber = Platform.getJDKMinorNumber(fullJDKString);
+            
+            if (minorNumber >= 12) {
+                return true;
             }
         }
-
         return false;
     }
 
