b3a9237b840a3b432a54417c0eb70ff62e5a24aa
Tomas Hurka <thurka@netbeans.org>
2009-03-17 15:19:41 +0100

    Heap.getJavaClassesByRegExp() added

--- lib.profiler/src/org/netbeans/lib/profiler/heap/ClassDumpSegment.java
+++ lib.profiler/src/org/netbeans/lib/profiler/heap/ClassDumpSegment.java
@@ -42,10 +42,13 @@ package org.netbeans.lib.profiler.heap;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 
 /**
@@ -73,6 +76,7 @@ class ClassDumpSegment extends TagBounds {
     final int stackTraceSerialNumberOffset;
     final int superClassIDOffset;
     ClassDump java_lang_Class;
+    Map /*<JavaClass,List<Field>>*/ fieldsCache;
     private List /*<JavaClass>*/ classes;
     private Map /*<Byte,JavaClass>*/ primitiveArrayMap;
 
@@ -102,6 +106,8 @@ class ClassDumpSegment extends TagBounds {
         fieldSize = fieldTypeOffset + 1;
 
         minimumInstanceSize = 2 * idSize;
+        
+        fieldsCache = Collections.synchronizedMap(new FieldsCache());
     }
 
     //~ Methods ------------------------------------------------------------------------------------------------------------------
@@ -115,7 +121,7 @@ class ClassDumpSegment extends TagBounds {
 
         if (entry != null) {
             try {
-                return (ClassDump) classes.get(entry.getIndex() - 1);
+                return (ClassDump) createClassCollection().get(entry.getIndex() - 1);
             } catch (ArrayIndexOutOfBoundsException ex) { // classObjectID do not reffer to ClassDump, its instance number is > classes.size()
 
                 return null;
@@ -129,7 +135,7 @@ class ClassDumpSegment extends TagBounds {
     }
 
     JavaClass getJavaClassByName(String fqn) {
-        Iterator classIt = classes.iterator();
+        Iterator classIt = createClassCollection().iterator();
 
         while (classIt.hasNext()) {
             ClassDump cls = (ClassDump) classIt.next();
@@ -140,6 +146,21 @@ class ClassDumpSegment extends TagBounds {
         }
 
         return null;
+    }
+
+    Collection getJavaClassesByRegExp(String regexp) {
+        Iterator classIt = createClassCollection().iterator();
+        Collection result = new ArrayList(256);
+        Pattern pattern = Pattern.compile(regexp);
+        
+        while (classIt.hasNext()) {
+            ClassDump cls = (ClassDump) classIt.next();
+
+            if (pattern.matcher(cls.getName()).matches()) {
+                result.add(cls);
+            }
+        }
+        return result;
     }
 
     int getMinimumInstanceSize() {
@@ -157,8 +178,9 @@ class ClassDumpSegment extends TagBounds {
     }
 
     Map getClassIdToClassMap() {
-        Map map = new HashMap(classes.size()*4/3);
-        Iterator classIt = classes.iterator();
+        Collection allClasses = createClassCollection();
+        Map map = new HashMap(allClasses.size()*4/3);
+        Iterator classIt = allClasses.iterator();
         
         while(classIt.hasNext()) {
             ClassDump cls = (ClassDump) classIt.next();
@@ -170,8 +192,8 @@ class ClassDumpSegment extends TagBounds {
     
     void addInstanceSize(ClassDump cls, int tag, long instanceOffset) {
         if ((tag == HprofHeap.OBJECT_ARRAY_DUMP) || (tag == HprofHeap.PRIMITIVE_ARRAY_DUMP)) {
-            Integer sizeInt = (Integer) arrayMap.get(cls);
-            int size = 0;
+            Long sizeInt = (Long) arrayMap.get(cls);
+            long size = 0;
             HprofByteBuffer dumpBuffer = hprofHeap.dumpBuffer;
             int idSize = dumpBuffer.getIDSize();
             long elementsOffset = instanceOffset + 1 + idSize + 4;
@@ -189,12 +211,12 @@ class ClassDumpSegment extends TagBounds {
                 elSize = idSize;
             }
 
-            size += (getMinimumInstanceSize() + (elements * elSize));
-            arrayMap.put(cls, Integer.valueOf(size));
+            size += (getMinimumInstanceSize() + (((long)elements) * elSize));
+            arrayMap.put(cls, Long.valueOf(size));
         }
     }
 
-    List /*<JavaClass>*/ createClassCollection() {
+    synchronized List /*<JavaClass>*/ createClassCollection() {
         if (classes != null) {
             return classes;
         }
@@ -210,10 +232,10 @@ class ClassDumpSegment extends TagBounds {
             if (tag == HprofHeap.CLASS_DUMP) {
                 ClassDump classDump = new ClassDump(this, start);
                 long classId = classDump.getJavaClassId();
+                LongMap.Entry classEntry = hprofHeap.idToOffsetMap.put(classId, start);
 
                 classes.add(classDump);
-                hprofHeap.idToOffsetMap.put(classId, start);
-                hprofHeap.idToOffsetMap.get(classId).setIndex(classes.size());
+                classEntry.setIndex(classes.size());
             }
         }
 
@@ -222,19 +244,6 @@ class ClassDumpSegment extends TagBounds {
         extractSpecialClasses();
 
         return classes;
-    }
-
-    List findStaticReferencesFor(long instanceId) {
-        List refs = new ArrayList();
-        Iterator classIt = classes.iterator();
-
-        while (classIt.hasNext()) {
-            ClassDump cls = (ClassDump) classIt.next();
-
-            cls.findStaticReferencesFor(instanceId, refs);
-        }
-
-        return refs;
     }
 
     private void extractSpecialClasses() {
@@ -288,6 +297,21 @@ class ClassDumpSegment extends TagBounds {
             if (typeObj != null) {
                 primitiveArrayMap.put(typeObj, jcls);
             }
+        }
+    }
+    
+    private static class FieldsCache extends LinkedHashMap {
+        private static final int SIZE = 500;
+        
+        FieldsCache() {
+            super(SIZE,0.75f,true);
+        }
+
+        protected boolean removeEldestEntry(Map.Entry eldest) {
+            if (size() > SIZE) {
+                return true;
+            }
+            return false;
         }
     }
 }
