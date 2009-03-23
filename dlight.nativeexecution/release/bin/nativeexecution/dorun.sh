e4c2788d0569d61e7ec96b29b8a46579e9b52863
Andrew Krasny <akrasny@netbeans.org>
2009-03-23 19:57:26 +0300

    noIZ: late environment setup; better (and simplier) environment vars handling

---
+++ dlight.nativeexecution/release/bin/nativeexecution/dorun.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+PROG=`basename $0`
+USAGE="usage: ${PROG} -p pidfile [-x prompt] ..."
+PROMPT=NO
+
+fail() {
+  echo $@ >&2
+  exit 1
+}
+
+doExit() {
+  test -f ${PIDFILE} && rm ${PIDFILE}
+  test -f ${ENVFILE} && rm ${ENVFILE}
+  exit ${STATUS}
+}
+
+[ $# -lt 1 ] && fail $USAGE
+
+while getopts w:e:p:x: opt; do
+  case $opt in
+    p) PIDFILE=$OPTARG
+       ;;
+    x) PROMPT=$OPTARG
+       ;;
+    e) ENVFILE=$OPTARG
+       ;;
+    w) WDIR=$OPTARG
+       ;;
+  esac
+done
+
+shift `expr $OPTIND - 1`
+
+trap "doExit" 1 2 15 EXIT
+
+if [ "${WDIR}" = "" ]; then
+  WDIR=.
+fi
+
+STATUS=-1
+/bin/sh -c "echo \$\$>${PIDFILE} && . ${ENVFILE} && cd ${WDIR} && exec $@"
+STATUS=$?
+echo ${STATUS} > ${PIDFILE}.res
+
+
+if [ "${PROMPT}" != "NO" ]; then
+  /bin/echo "${PROMPT}"
+  read X
+fi
+
