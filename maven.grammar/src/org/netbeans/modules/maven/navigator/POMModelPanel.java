21f8a1822390969b3fc1f9708fb3e8338a5d7bd6
Milos Kleint <mkleint@netbeans.org>
2009-03-27 10:17:27 +0100

    open files in local repository as read only

--- maven.grammar/src/org/netbeans/modules/maven/navigator/POMModelPanel.java
+++ maven.grammar/src/org/netbeans/modules/maven/navigator/POMModelPanel.java
@@ -95,7 +95,6 @@ import org.openide.loaders.DataObject;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
-import org.openide.nodes.NodeOp;
 import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
@@ -205,6 +204,7 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
                 if (dobj == null) {
                     return;
                 }
+                dobj = ROUtil.checkPOMFileObjectReadOnly(dobj);
                 EditorCookie.Observable ec = dobj.getLookup().lookup(EditorCookie.Observable.class);
                 if (ec == null) {
                     return;
@@ -227,14 +227,6 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
                         if (panes != null && panes.length > 0) {
                             JTextComponent component = panes[0];
                             component.setCaretPosition(pos);
-                            if ("pom".equals(dobj.getPrimaryFile().getExt())) {
-                                //make temporarily non-editable??
-//                                component.setEditable(false);
-//                                TopComponent tc = NbEditorUtilities.getOuterTopComponent(component);
-//                                String dn = tc.getDisplayName();
-//                                tc.setDisplayName(dn + " r/o");
-
-                            }
                         }
                     } catch (IOException ioe) {
                     }
@@ -266,8 +258,9 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
     }
     
     public void run() {
-        if (current != null) {
-            File file = FileUtil.toFile(current.getPrimaryFile());
+        DataObject currentFile = current;
+        if (currentFile != null) {
+            File file = FileUtil.toFile(currentFile.getPrimaryFile());
             // can be null for stuff in jars?
             if (file != null) {
                 try {
@@ -324,7 +317,7 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
             }
             
             //now attach the listener to the textcomponent
-            final EditorCookie.Observable ec = current.getLookup().lookup(EditorCookie.Observable.class);
+            final EditorCookie.Observable ec = currentFile.getLookup().lookup(EditorCookie.Observable.class);
             if (ec == null) {
                 //how come?
                 return;
