4216a86730903735e2ce0db46bb03831954055d5
Marian Petras <mpetras@netbeans.org>
2009-04-02 10:51:34 +0200

    slight refactoring of class IssueAction: removed methods openQuery(Issue), openQuery(Issue, Repository); added methods openQuery(), openQuery(Repository)

--- bugtracking/src/org/netbeans/modules/bugtracking/ui/issue/IssueAction.java
+++ bugtracking/src/org/netbeans/modules/bugtracking/ui/issue/IssueAction.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -44,9 +44,12 @@ import org.openide.util.actions.SystemAction;
 import org.openide.util.HelpCtx;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 import javax.swing.SwingUtilities;
 import org.netbeans.modules.bugtracking.spi.Issue;
 import org.netbeans.modules.bugtracking.spi.Repository;
+import org.netbeans.modules.bugtracking.util.BugtrackingUtil;
+import org.netbeans.modules.bugtracking.util.FileToRepoMappingStorage;
 import org.openide.util.NbBundle;
 
 /**
@@ -69,14 +72,44 @@ public class IssueAction extends SystemAction {
     }
 
     public void actionPerformed(ActionEvent ev) {
+        openQuery();
+    }
+
+    public static void openQuery() {
         openQuery(null);
     }
 
-    public static void openQuery(Issue issue) {
-        openIssue(issue, null);
+    public static void openQuery(Repository givenRepository) {
+        final Repository repository;
+        final boolean repositoryGiven;
+
+        if (givenRepository != null) {
+            repository = givenRepository;
+            repositoryGiven = true;
+        } else {
+            Repository suggestedRepository = null;
+            File context = BugtrackingUtil.getLargerContext();
+            if (context != null) {
+                suggestedRepository = FileToRepoMappingStorage.getInstance()
+                                      .getRepository(context);
+            }
+
+            repository = suggestedRepository;
+            repositoryGiven = false;
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                IssueTopComponent tc = new IssueTopComponent();
+                tc.initNewIssue(repository, !repositoryGiven);
+                tc.open();
+                tc.requestActive();
+            }
+        });
     }
 
-    public static void openIssue(final Issue issue, final Repository repository) {
+    public static void openIssue(final Issue issue, final Repository repository,
+                                 final boolean suggestedSelectionOnly) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 IssueTopComponent tc = null;
@@ -86,7 +119,7 @@ public class IssueAction extends SystemAction {
                 if(tc == null) {
                     tc = new IssueTopComponent();
                 }
-                tc.initNewIssue(repository);
+                tc.initNewIssue(repository, suggestedSelectionOnly);
                 if(!tc.isOpened()) {
                     tc.open();
                 }
