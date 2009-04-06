2f02428a4a6fbbd7986085bc4e3170f1f9c5669e
Egor Ushakov <gorrus@netbeans.org>
2009-04-06 17:22:01 +0400

    no IZ: fixed several IDE hangs

--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/actions/KillActionProvider.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/actions/KillActionProvider.java
@@ -50,8 +50,6 @@ package org.netbeans.modules.cnd.debugger.gdb.actions;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 import org.openide.util.RequestProcessor;
@@ -62,7 +60,6 @@ import org.netbeans.spi.debugger.ActionsProviderListener;
 import org.netbeans.spi.debugger.ContextProvider;
 
 import org.netbeans.modules.cnd.debugger.gdb.GdbDebugger;
-import org.netbeans.modules.cnd.debugger.gdb.GdbDebugger;
 
 /**
  * Termination of a debugging session.
@@ -70,14 +67,11 @@ import org.netbeans.modules.cnd.debugger.gdb.GdbDebugger;
  */
 public class KillActionProvider extends ActionsProvider {
     
-    private ContextProvider lookupProvider;
     private GdbDebugger debuggerImpl;
     
     public KillActionProvider(ContextProvider lookupProvider) {
-        debuggerImpl = (GdbDebugger) lookupProvider.lookupFirst
-                (null, GdbDebugger.class);
+        debuggerImpl = lookupProvider.lookupFirst(null, GdbDebugger.class);
         //super (debuggerImpl);
-        this.lookupProvider = lookupProvider;
     }
     
     /**
@@ -86,9 +80,7 @@ public class KillActionProvider extends ActionsProvider {
      * @return set of actions supported by this ActionsProvider
      */
     public Set getActions() {
-        return new HashSet(Arrays.asList(new Object[] {
-            ActionsManager.ACTION_KILL
-        }));
+        return Collections.singleton(ActionsManager.ACTION_KILL);
     }
     
     /**
@@ -126,6 +118,7 @@ public class KillActionProvider extends ActionsProvider {
      *        done.
      * @since 1.5
      */
+    @Override
     public void postAction(final Object action,
             final Runnable actionPerformedNotifier) {
         RequestProcessor.getDefault().post(new Runnable() {
@@ -146,9 +139,10 @@ public class KillActionProvider extends ActionsProvider {
      */
     public boolean isEnabled(Object action) {
         if (debuggerImpl != null) {
-            synchronized (debuggerImpl.LOCK) {
+            // synchronization removed for now, it causes IDE hang
+            //synchronized (debuggerImpl.LOCK) {
                 return true;
-            }
+            //}
         }
         return false;
     }
