8a9b9f952ae92184c5262242a5b9da2a8773f5c3
Andrew Krasny <akrasny@netbeans.org>
2009-04-13 13:36:24 +0400

    noIZ: minor changes

---
+++ dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/LocalNativeProcess.java
@@ -0,0 +1,222 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.nativeexecution;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
+import org.netbeans.modules.nativeexecution.api.util.HostInfoUtils;
+import org.netbeans.modules.nativeexecution.support.EnvWriter;
+import org.netbeans.modules.nativeexecution.support.MacroMap;
+import org.netbeans.modules.nativeexecution.support.UnbufferSupport;
+import org.netbeans.modules.nativeexecution.support.WindowsSupport;
+import org.openide.util.NbBundle;
+import org.openide.util.Utilities;
+
+public final class LocalNativeProcess extends AbstractNativeProcess {
+
+    private final static String shell;
+    private final static boolean isWindows;
+    private Process process = null;
+    private InputStream processOutput = null;
+    private OutputStream processInput = null;
+    private InputStream processError = null;
+
+    static {
+        String sh = null;
+
+        try {
+            sh = HostInfoUtils.getShell(new ExecutionEnvironment());
+        } catch (ConnectException ex) {
+        }
+
+        shell = sh;
+
+        isWindows = Utilities.isWindows();
+    }
+
+    public LocalNativeProcess(NativeProcessInfo info) {
+        super(info);
+    }
+
+    protected void create() throws Throwable {
+        try {
+            if (Utilities.isWindows() && shell == null) {
+                throw new IOException(loc("LocalNativeProcess.shellNotFound.text")); // NOI18N
+            }
+
+            // Get working directory ....
+            String workingDirectory = info.getWorkingDirectory(true);
+
+            if (workingDirectory != null) {
+                workingDirectory = new File(workingDirectory).getAbsolutePath();
+                if (isWindows) {
+                    workingDirectory = WindowsSupport.getInstance().normalizePath(workingDirectory);
+                }
+            }
+
+            final MacroMap env = info.getEnvVariables();
+
+            UnbufferSupport.initUnbuffer(info, env);
+
+            // On windows add /bin to PATH in case cygwin is not in
+            // the Path environment variable ...
+            if (isWindows) {
+                env.put("PATH", "/bin:$PATH"); // NOI18N
+            }
+
+            final ProcessBuilder pb = new ProcessBuilder(shell, "-s"); // NOI18N
+
+            if (isInterrupted()) {
+                throw new InterruptedException();
+            }
+
+            process = pb.start();
+
+            processInput = process.getOutputStream();
+            processError = process.getErrorStream();
+            processOutput = process.getInputStream();
+
+            processInput.write("/bin/echo $$\n".getBytes()); // NOI18N
+            processInput.flush();
+
+            EnvWriter ew = new EnvWriter(processInput);
+            ew.write(env);
+
+            if (workingDirectory != null) {
+                processInput.write(("cd \"" + workingDirectory + "\"\n").getBytes()); // NOI18N
+            }
+
+            String cmd = "exec " + info.getCommandLine() + "\n"; // NOI18N
+
+            if (isWindows) {
+                cmd = cmd.replaceAll("\\\\", "/"); // NOI18N
+            }
+
+            processInput.write(cmd.getBytes());
+            processInput.flush();
+
+            readPID(processOutput);
+        } catch (Throwable ex) {
+            String msg = ex.getMessage() == null ? ex.toString() : ex.getMessage();
+            processOutput = new ByteArrayInputStream(new byte[0]);
+            processError = new ByteArrayInputStream(msg.getBytes());
+            processInput = new ByteArrayOutputStream();
+            throw ex;
+        }
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+        return processInput;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return processOutput;
+    }
+
+    @Override
+    public InputStream getErrorStream() {
+        return processError;
+    }
+
+    @Override
+    public final int waitResult() throws InterruptedException {
+        if (process == null) {
+            throw new InterruptedException();
+        }
+
+        /*
+         * Why not just process.waitResult()...
+         * This is to avoid a problem with short-running tasks, when
+         * this Thread (that waits for process' termination) doesn't see
+         * that it has been interrupted....
+         * TODO: describe situation in details... 
+         */
+
+        int result = -1;
+
+//        // Get lock on process not to take it on every itteration
+//        // (in process.exitValue())
+//
+//        synchronized (process) {
+        // Why this synchronized is commented-out..
+        // This is because ProcessReaper is also synchronized on this...
+        // And it should be able to react on process' termination....
+
+        while (true) {
+            // This sleep is to avoid lost interrupted exception...
+            try {
+                Thread.sleep(200);
+            // 200 - to make this check not so often...
+            // actually, to avoid the problem, 1 is OK.
+            } catch (InterruptedException ex) {
+                throw ex;
+            }
+
+            try {
+                result = process.exitValue();
+            } catch (IllegalThreadStateException ex) {
+                continue;
+            }
+
+            break;
+        }
+//        }
+
+        return result;
+    }
+
+    @Override
+    protected final synchronized void cancel() {
+        if (process != null) {
+            process.destroy();
+        }
+    }
+
+    private static String loc(String key, String... params) {
+        return NbBundle.getMessage(LocalNativeProcess.class, key, params);
+    }
+}
---
+++ dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/RemoteNativeProcess.java
@@ -0,0 +1,175 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.nativeexecution;
+
+import org.netbeans.modules.nativeexecution.api.util.ConnectionManager;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
+import org.netbeans.modules.nativeexecution.api.util.HostInfoUtils;
+import org.netbeans.modules.nativeexecution.support.EnvWriter;
+import org.netbeans.modules.nativeexecution.support.Logger;
+import org.netbeans.modules.nativeexecution.support.MacroMap;
+import org.netbeans.modules.nativeexecution.support.UnbufferSupport;
+
+public final class RemoteNativeProcess extends AbstractNativeProcess {
+
+    private final static java.util.logging.Logger log = Logger.getInstance();
+    private ChannelStreams cstreams = null;
+    private Integer exitValue = null;
+
+    public RemoteNativeProcess(NativeProcessInfo info) {
+        super(info);
+    }
+
+    protected void create() throws Throwable {
+        try {
+            final String commandLine = info.getCommandLine();
+            final ConnectionManager mgr = ConnectionManager.getInstance();
+            final ExecutionEnvironment execEnv = info.getExecutionEnvironment();
+            final Session session = ConnectionManagerAccessor.getDefault().
+                    getConnectionSession(mgr, execEnv, true);
+
+            final String sh = HostInfoUtils.getShell(execEnv);
+            final MacroMap envVars = info.getEnvVariables();
+
+            // Setup LD_PRELOAD to load unbuffer library...
+            UnbufferSupport.initUnbuffer(info, envVars);
+
+            cstreams = execCommand(session, sh + " -s"); // NOI18N
+            cstreams.in.write("echo $$\n".getBytes()); // NOI18N
+            cstreams.in.flush();
+
+            final String workingDirectory = info.getWorkingDirectory(true);
+
+            if (workingDirectory != null) {
+                cstreams.in.write(("cd " + workingDirectory + "\n").getBytes()); // NOI18N
+                cstreams.in.flush();
+            }
+
+            EnvWriter ew = new EnvWriter(cstreams.in);
+            ew.write(envVars);
+
+            cstreams.in.write(("exec " + commandLine + "\n").getBytes()); // NOI18N
+            cstreams.in.flush();
+
+            readPID(cstreams.out);
+        } catch (Throwable ex) {
+            String msg = ex.getMessage() == null ? ex.toString() : ex.getMessage();
+
+            cstreams = new ChannelStreams(
+                    cstreams == null ? null : cstreams.channel,
+                    new ByteArrayInputStream(new byte[0]),
+                    new ByteArrayInputStream(msg.getBytes()),
+                    new ByteArrayOutputStream());
+            throw ex;
+        }
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+        return cstreams.in;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return cstreams.out;
+    }
+
+    @Override
+    public InputStream getErrorStream() {
+        return cstreams.err;
+    }
+
+    @Override
+    public int waitResult() throws InterruptedException {
+        if (cstreams.channel == null) {
+            return -1;
+        }
+
+        while (cstreams.channel.isConnected()) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                interrupt();
+                throw ex;
+            }
+        }
+
+        exitValue = Integer.valueOf(cstreams.channel.getExitStatus());
+
+        return exitValue.intValue();
+    }
+
+    @Override
+    public void cancel() {
+        ChannelExec channel;
+
+        synchronized (this) {
+            channel = cstreams == null ? null : cstreams.channel;
+        }
+
+        if (channel != null && channel.isConnected()) {
+            channel.disconnect();
+//          NativeTaskSupport.kill(execEnv, 9, getPID());
+        }
+    }
+
+    private ChannelStreams execCommand(
+            final Session session,
+            final String command) throws IOException, JSchException {
+        int retry = 2;
+
+        while (retry-- > 0) {
+            try {
+                ChannelExec echannel;
+
+                synchronized (session) {
+                    echannel = (ChannelExec) session.openChannel("exec"); // NOI18N
+                    echannel.setCommand(command);
+                    echannel.connect();
+                }
+
+                return new ChannelStreams(echannel,
+                        echannel.getInputStream(),
+                        echannel.getErrStream(),
+                        echannel.getOutputStream());
+            } catch (JSchException ex) {
+                Throwable cause = ex.getCause();
+                if (cause != null && cause instanceof NullPointerException) {
+                    // Jsch bug... retry? ;)
+                } else {
+                    throw ex;
+                }
+            } catch (NullPointerException npe) {
+                // Jsch bug... retry? ;)
+            }
+        }
+
+        throw new IOException("Failed to execute " + command); // NOI18N
+    }
+
+    private static class ChannelStreams {
+
+        final InputStream out;
+        final InputStream err;
+        final OutputStream in;
+        final ChannelExec channel;
+
+        public ChannelStreams(ChannelExec channel, InputStream out,
+                InputStream err, OutputStream in) {
+            this.channel = channel;
+            this.out = out;
+            this.err = err;
+            this.in = in;
+        }
+    }
+}
