4daa96c99bce6095704b58a5680b5133fff0131c
Vladimir Voskresensky <vv159170@netbeans.org>
2009-04-20 17:13:23 +0400

    fixed IZ#163114: deadlock  - reduce sync block

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeSources.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeSources.java
@@ -42,14 +42,11 @@
 package org.netbeans.modules.cnd.makeproject;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.project.FileOwnerQuery;
-import org.netbeans.api.project.ProjectManager;
 import org.netbeans.api.project.SourceGroup;
 import org.netbeans.api.project.Sources;
 import org.netbeans.modules.cnd.api.utils.IpeUtils;
@@ -60,10 +57,12 @@ import org.netbeans.modules.cnd.makeproject.api.configurations.Item;
 import org.netbeans.modules.cnd.makeproject.api.configurations.MakeConfiguration;
 import org.netbeans.modules.cnd.makeproject.api.configurations.MakeConfigurationDescriptor;
 import org.netbeans.modules.cnd.makeproject.api.configurations.MakefileConfiguration;
+import org.netbeans.modules.cnd.makeproject.api.remote.FilePathAdaptor;
 import org.netbeans.spi.project.support.ant.AntProjectEvent;
 import org.netbeans.spi.project.support.ant.AntProjectHelper;
 import org.netbeans.spi.project.support.ant.AntProjectListener;
 import org.netbeans.spi.project.support.ant.SourcesHelper;
+import org.openide.util.ChangeSupport;
 
 /**
  * Handles source dir list for a freeform project.
@@ -78,112 +77,131 @@ public class MakeSources implements Sources, AntProjectListener {
         this.project = project;
         this.helper = helper;
         helper.addAntProjectListener(this);
+        changeSupport = new ChangeSupport(this);
     }
     private Sources delegate;
-    private final List<ChangeListener> listeners = new ArrayList<ChangeListener>();
-
-    public synchronized SourceGroup[] getSourceGroups(String str) {
+    private final ChangeSupport changeSupport;
+    private long eventID = 0;
+    
+    public SourceGroup[] getSourceGroups(String str) {
         if (!str.equals("generic")) { // NOI18N
             return new SourceGroup[0];
         }
-        if (delegate == null) {
-            delegate = initSources();
+        Sources srcs;
+        long curEvent;
+        synchronized (this) {
+            srcs = delegate;
+            curEvent = eventID;
         }
-        SourceGroup[] sg = delegate.getSourceGroups(str);
+
+        if (srcs == null) {
+            srcs = initSources();
+        }
+
+        synchronized (this){
+            if (curEvent == eventID) {
+                delegate = srcs;
+            }
+        }
+        SourceGroup[] sg = srcs.getSourceGroups(str);
         return sg;
     }
 
     private Sources initSources() {
-        final SourcesHelper h = new SourcesHelper(helper, project.evaluator());
+        SourcesHelper h = new SourcesHelper(project, helper, project.evaluator());
         ConfigurationDescriptorProvider pdp = project.getLookup().lookup(ConfigurationDescriptorProvider.class);
-        ConfigurationDescriptor pd = pdp.getConfigurationDescriptor();
+        ConfigurationDescriptor pd = pdp.getConfigurationDescriptor(!MakeProjectConfigurationProvider.ASYNC_LOAD);
         if (pd != null) {
             MakeConfigurationDescriptor epd = (MakeConfigurationDescriptor) pd;
-            Set<String> set = new HashSet<String>();
-            Item[] projectItems = epd.getProjectItems();
+            Set<String> set = new LinkedHashSet<String>();
+            
             // Add external folders to sources.
-            if (projectItems != null) {
-                for (int i = 0; i < projectItems.length; i++) {
-                    Item item = projectItems[i];
-                    String name = item.getPath();
-                    if (!IpeUtils.isPathAbsolute(name)) {
-                        continue;
+            if (epd.getVersion() < 41) {
+                Item[] projectItems = epd.getProjectItems();
+                if (projectItems != null) {
+                    for (int i = 0; i < projectItems.length; i++) {
+                        Item item = projectItems[i];
+                        String name = item.getPath();
+                        if (!IpeUtils.isPathAbsolute(name)) {
+                            continue;
+                        }
+                        File file = new File(name);
+                        if (!file.exists()) {
+                            continue;
+                        }
+                        if (!file.isDirectory()) {
+                            file = file.getParentFile();
+                        }
+                        name = file.getPath();
+                        set.add(name);
+                        epd.addSourceRootRaw(IpeUtils.toRelativePath(epd.getBaseDir(), name));
                     }
-                    File file = new File(name);
-                    if (!file.exists()) {
-                        continue;
-                    }
-                    if (!file.isDirectory()) {
-                        file = file.getParentFile();
-                    }
-                    name = file.getPath();
-                    set.add(name);
                 }
             }
+            // Add source roots to set (>= V41)
+            List<String> list = epd.getAbsoluteSourceRoots();
+            for (String sr : list) {
+                set.add(sr);
+            }
+            
             // Add buildfolder from makefile projects to sources. See IZ 90190.
-            Configuration[] confs = epd.getConfs().getConfs();
-            for (int i = 0; i < confs.length; i++) {
-                MakeConfiguration makeConfiguration = (MakeConfiguration) confs[i];
-                if (makeConfiguration.isMakefileConfiguration()) {
-                    MakefileConfiguration makefileConfiguration = makeConfiguration.getMakefileConfiguration();
-                    String path = makefileConfiguration.getAbsBuildCommandWorkingDir();
-                    set.add(path);
-                }
-            }
-            for (String name : set) {
-                String displayName = name;
-                int index1 = displayName.lastIndexOf(File.separatorChar);
-                if (index1 > 0) {
-                    int index2 = displayName.substring(0, index1).lastIndexOf(File.separatorChar);
-                    if (index2 > 0) {
-                        displayName = "..." + displayName.substring(index2); // NOI18N
+            if (epd.getVersion() < 41) {
+                Configuration[] confs = epd.getConfs().getConfs();
+                for (int i = 0; i < confs.length; i++) {
+                    MakeConfiguration makeConfiguration = (MakeConfiguration) confs[i];
+                    if (makeConfiguration.isMakefileConfiguration()) {
+                        MakefileConfiguration makefileConfiguration = makeConfiguration.getMakefileConfiguration();
+                        String path = makefileConfiguration.getAbsBuildCommandWorkingDir();
+                        set.add(path);
+                        epd.addSourceRootRaw(IpeUtils.toRelativePath(epd.getBaseDir(), path));
                     }
                 }
+            }
+            
+            for (String name : set) {
+                String displayName = IpeUtils.toRelativePath(epd.getBaseDir(), name);
+//                int index1 = displayName.lastIndexOf(File.separatorChar);
+//                if (index1 > 0) {
+//                    int index2 = displayName.substring(0, index1).lastIndexOf(File.separatorChar);
+//                    if (index2 > 0) {
+//                        displayName = "..." + displayName.substring(index2); // NOI18N
+//                    }
+//                }
+                displayName = FilePathAdaptor.naturalize(displayName);
                 h.addPrincipalSourceRoot(name, displayName, null, null);
                 h.addTypedSourceRoot(name, "generic", displayName, null, null); // NOI18N
             }
         }
-        ProjectManager.mutex().postWriteRequest(new Runnable() {
-
-            public void run() {
-                h.registerExternalRoots(FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT);
-            }
-        });
+        h.registerExternalRoots(FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT);
         return h.createSources();
     }
 
-    public synchronized void addChangeListener(ChangeListener changeListener) {
-        listeners.add(changeListener);
+    public void addChangeListener(ChangeListener changeListener) {
+        changeSupport.addChangeListener(changeListener);
     }
 
-    public synchronized void removeChangeListener(ChangeListener changeListener) {
-        listeners.remove(changeListener);
+    public void removeChangeListener(ChangeListener changeListener) {
+        changeSupport.removeChangeListener(changeListener);
     }
 
     private void fireChange() {
-        ChangeListener[] _listeners;
-        synchronized (this) {
-            delegate = null;
-            if (listeners.isEmpty()) {
-                return;
-            }
-            _listeners = listeners.toArray(new ChangeListener[listeners.size()]);
-        }
-        ChangeEvent ev = new ChangeEvent(this);
-        for (int i = 0; i < _listeners.length; i++) {
-            _listeners[i].stateChanged(ev);
-        }
+        delegate = null;
+        changeSupport.fireChange();
     }
 
     public void configurationXmlChanged(AntProjectEvent ev) {
-        fireChange();
+        // fireChange(); // ignore - cnd projects don't keep source file info in project.xml
     }
 
     public void descriptorChanged() {
-        fireChange();
+        // fireChange(); // ignore
     }
 
     public void propertiesChanged(AntProjectEvent ev) {
         // ignore
+    }
+    
+    public void sourceRootsChanged() {
+        fireChange();
     }
 }
