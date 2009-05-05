2e8a48c4d24ff080250cdbdd09ce131132d9756e
Alexander Simon <alexvsimon@netbeans.org>
2009-05-05 18:59:56 +0400

    fixingIZ#164442:problems with new projects

--- cnd/src/org/netbeans/modules/cnd/ui/options/VersionCommand.java
+++ cnd/src/org/netbeans/modules/cnd/ui/options/VersionCommand.java
@@ -44,123 +44,103 @@ package org.netbeans.modules.cnd.ui.options;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import org.netbeans.modules.cnd.api.compilers.CompilerSet.CompilerFlavor;
-import org.openide.DialogDisplayer;
-import org.openide.NotifyDescriptor;
-import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Collections;
+import org.netbeans.modules.cnd.api.compilers.Tool;
+import org.netbeans.modules.cnd.api.execution.LinkSupport;
+import org.netbeans.modules.cnd.api.remote.CommandProvider;
+import org.openide.util.Lookup;
 
 /**
  *
  * @author gordonp
+ * @author Alexey Vladykin
  */
-public class VersionCommand implements Runnable {
-    
-    private ProcessBuilder pb;
-    private String name;
+/*package-local*/ final class VersionCommand {
+
+    private final Tool tool;
     private String path;
-    private String version = null;
-    private static HashMap<String, String> cygmap;
-    
-    static {
-        cygmap = new HashMap();
-        cygmap.put("cc.exe", "gcc.exe"); // NOI18N
-        cygmap.put("i686-pc-cygwin-gcc.exe", "gcc.exe"); // NOI18N
-        cygmap.put("c++.exe", "g++.exe"); // NOI18N
-        cygmap.put("i686-pc-cygwin-g++.exe", "g++.exe"); // NOI18N
-        cygmap.put("i686-pc-cygwin-c++.exe", "g++.exe"); // NOI18N
-    }
-    
+    private boolean alreadyRun;
+    private String version;
+
     /**
      * Creates a new instance of VersionCommand
      */
-    public VersionCommand(CompilerFlavor flavor, String name, String path) {
-        String option = null;
-        
-        this.name = name;
+    public VersionCommand(Tool tool, String path) {
+        this.tool = tool;
         this.path = path;
-        try {
-            path = new File(path).getCanonicalPath();
-        } catch (IOException ex) {
+    }
+
+    public String getVersion() {
+        if (!alreadyRun) {
+            run();
         }
-        
-        if (flavor.isGnuCompiler()) { 
-            option = "--version"; // NOI18N
-            path = cygwinPath(path);
-        } else if (flavor.isSunCompiler()) {
-            option = "-V"; // NOI18N
-        } else if (Utilities.getOperatingSystem() == Utilities.OS_SOLARIS) {
-            if (path.endsWith("/sfw/bin/gmake")) { // NOI18N
-                option = "--version"; // NOI18N
-            } else if (path.equals("/usr/ccs/bin/make") || path.equals("/usr/xpg4/bin/make")) { // NOI18N
-                path = "/sbin/uname"; // NOI18N
-                option = "-sr"; // NOI18N
-            } else if (name.equals("dmake")) { // NOI18N
-                File inv;
-                String base = path.substring(0, path.length() - 10);
-                
-                inv = new File(base + "/inventory"); // NOI18N
-                if (inv.exists() && inv.isDirectory()) {
-                    String[] files = inv.list();
-                    for (int i = 0; i < files.length; i++) {
-                        if (files[i].equals("v14n1")) { // NOI18N
-                            version = NbBundle.getMessage(VersionCommand.class, "DMAKE10"); // NOI18N
-                        } else if (files[i].equals("v15n1")) { // NOI18N
-                            version = NbBundle.getMessage(VersionCommand.class, "DMAKE11"); // NOI18N
-                        } else if (files[i].equals("v16n1")) { // NOI18N
-                            version = NbBundle.getMessage(VersionCommand.class, "DMAKE12"); // NOI18N
-                        }
-                    }
+        return version;
+    }
+
+    private void run() {
+        if (tool.getExecutionEnvironment().isLocal()) {
+            // we're dealing with a local toolchain
+            path = LinkSupport.resolveWindowsLinkik(path);
+            File file = new File(path);
+            if (file.exists()) {
+                ProcessBuilder pb = new ProcessBuilder(path, getVersionFlags());
+                pb.redirectErrorStream(true);
+                try {
+                    Process process = pb.start();
+                    version = extractVersion(process.getInputStream());
+                } catch (IOException ioe) {
+                    // silently drop
+                }
+            }
+        } else {
+            // it's a remote toolchain
+            CommandProvider provider = Lookup.getDefault().lookup(CommandProvider.class);
+            if (provider != null) {
+                provider.run(tool.getExecutionEnvironment(),
+                        path + " " + getVersionFlags() + " 2>&1", // NOI18N
+                        Collections.<String, String>emptyMap());
+                version = extractVersion(new StringReader(provider.getOutput()));
+            }
+        }
+        alreadyRun = true;
+    }
+
+    private String getVersionFlags() {
+        String flags = null;
+        if (tool.getDescriptor() != null) {
+            flags = tool.getDescriptor().getVersionFlags();
+        }
+        if (flags == null) {
+            return "--version"; // NOI18N
+        } else {
+            return flags;
+        }
+    }
+
+    private String extractVersion(InputStream is) {
+        return extractVersion(new InputStreamReader(is));
+    }
+
+    private String extractVersion(Reader reader) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(reader);
+            return br.readLine();
+        } catch (IOException ioe) {
+            return null;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    // silently drop
                 }
             }
         }
-        if (option == null) {
-            option = "--version"; // NOI18N - Guessing its GNU ...
-        }
-        
-        if (version == null) {
-            pb = new ProcessBuilder(path, option);
-            pb.redirectErrorStream(true);
-        }
     }
-    
-    public void run() {
-        
-        if (pb != null) {
-            try {
-                Process process = pb.start();
-                BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                version = br.readLine(); // just read 1st line...
-                br.close();
-            } catch (IOException ioe) {
-            }
-        }
-        if (version != null) {
-            String message = NbBundle.getMessage(VersionCommand.class, "LBL_VersionInfo", name, path, version);
-            NotifyDescriptor nd = new NotifyDescriptor.Message(message);
-            
-            nd.setTitle(NbBundle.getMessage(VersionCommand.class, "LBL_VersionInfo_Title"));
-            DialogDisplayer.getDefault().notify(nd);
-        }
-    }
-    
-    /**
-     * Replace Cygwin symlinks with what they point to.
-     *
-     * @param orig The orignal path of a compiler/tool
-     * @returns The possibly modifued path of a real file
-     */
-    private String cygwinPath(String orig) {
-        int pos = orig.lastIndexOf(File.separatorChar);
-        String dir = orig.substring(0, pos);
-        String name = orig.substring(pos + 1);
-        String nuename = cygmap.get(name);
-        if (nuename != null) {
-            return dir + File.separator + nuename;
-        } else {
-            return orig;
-        }
-    }
+
 }
