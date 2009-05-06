db29eb9e4cb48c7a0c3f071ee06839d31b80b714
psuchomel@netbeans.org <none@none>
2009-05-06 12:25:20 +0200

    Fixed problem with URL formating after using default emulator install

--- j2me.cdc.platform/src/org/netbeans/modules/j2me/cdc/platform/platformdefinition/PlatformConvertor.java
+++ j2me.cdc.platform/src/org/netbeans/modules/j2me/cdc/platform/platformdefinition/PlatformConvertor.java
@@ -166,7 +166,9 @@ public class PlatformConvertor implements Environment.Provider, InstanceCookie.O
                 ex.printStackTrace();
                 if (x instanceof java.io.IOException)
                     throw (IOException)x;
-                throw new java.io.IOException(ex.getMessage());
+                IOException ioe = new IOException(ex.getMessage());
+                ioe.initCause(x);
+                throw ioe;
             }
 
             CDCPlatform inst = createPlatform(handler);
@@ -624,7 +626,9 @@ public class PlatformConvertor implements Environment.Provider, InstanceCookie.O
             }
             else if (ELEMENT_RESOURCE.equals(qName)) {
                 try {
-                    this.path.add (new URL(this.buffer.toString()));                    
+                    //make sure, that after install URL is resolved correctly
+                    File f = new File(new URL(this.buffer.toString()).getPath());
+                    this.path.add (f.toURI().toURL());
                 } catch (MalformedURLException mue) {
                     ErrorManager.getDefault().notify(mue); 
                 }
