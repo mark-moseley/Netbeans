b2cdf2d5f02f408c778ec3fc364b6cbfdab683ce
Jiri Sedlacek <jis@netbeans.org>
2009-05-07 12:34:04 +0200

    Charts: initial axes appearance, improvements, fixes

---
+++ lib.profiler.ui/src/org/netbeans/lib/profiler/ui/graphs/GraphsUI.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * 
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package org.netbeans.lib.profiler.ui.graphs;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jiri Sedlacek
+ */
+public interface GraphsUI {
+
+    // --- General colors definition -------------------------------------------
+
+    public static final Color  PROFILER_BLUE = new Color(127, 63, 191);
+    public static final Color  PROFILER_RED = new Color(255, 127, 127);
+    public static final Color  PROFILER_GREEN = new Color(30, 157, 68);
+
+
+    // --- Charts colors definition --------------------------------------------
+
+    public static final Color  CHART_BACKGROUND_COLOR = Color.WHITE;
+    public static final Color  SMALL_LEGEND_BACKGROUND_COLOR = Color.WHITE;
+    public static final Color  SMALL_LEGEND_BORDER_COLOR = new Color(235, 235, 235);
+
+    public static final float  TOOLTIP_OVERLAY_LINE_WIDTH = 2.1f;
+    public static final Color  TOOLTIP_OVERLAY_LINE_COLOR = Color.DARK_GRAY;
+    public static final Color  TOOLTIP_OVERLAY_FILL_COLOR = new Color(250, 250, 255);
+    
+    
+    // --- Class History graphs ------------------------------------------------
+
+    public static final String A_ALLOC_OBJECTS_NAME = "Allocated Objects";
+    public static final float  A_ALLOC_OBJECTS_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  A_ALLOC_OBJECTS_PAINTER_LINE_COLOR = PROFILER_RED;
+    public static final Color  A_ALLOC_OBJECTS_PAINTER_FILL_COLOR = null;
+    public static final int    A_ALLOC_OBJECTS_MARKER_RADIUS = 5;
+    public static final float  A_ALLOC_OBJECTS_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  A_ALLOC_OBJECTS_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  A_ALLOC_OBJECTS_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  A_ALLOC_OBJECTS_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  A_ALLOC_OBJECTS_MARKER_FILL_COLOR = PROFILER_RED;
+    public static final long   A_ALLOC_OBJECTS_INITIAL_VALUE = 100l;
+
+    public static final String A_ALLOC_BYTES_NAME = "Allocated Bytes";
+    public static final float  A_ALLOC_BYTES_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  A_ALLOC_BYTES_PAINTER_LINE_COLOR = PROFILER_BLUE;
+    public static final Color  A_ALLOC_BYTES_PAINTER_FILL_COLOR = null;
+    public static final int    A_ALLOC_BYTES_MARKER_RADIUS = 5;
+    public static final float  A_ALLOC_BYTES_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  A_ALLOC_BYTES_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  A_ALLOC_BYTES_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  A_ALLOC_BYTES_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  A_ALLOC_BYTES_MARKER_FILL_COLOR = PROFILER_BLUE;
+    public static final long   A_ALLOC_BYTES_INITIAL_VALUE = 102400l;
+
+    public static final String L_LIVE_OBJECTS_NAME = "Live Objects";
+    public static final float  L_LIVE_OBJECTS_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  L_LIVE_OBJECTS_PAINTER_LINE_COLOR = PROFILER_RED;
+    public static final Color  L_LIVE_OBJECTS_PAINTER_FILL_COLOR = null;
+    public static final int    L_LIVE_OBJECTS_MARKER_RADIUS = 5;
+    public static final float  L_LIVE_OBJECTS_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  L_LIVE_OBJECTS_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  L_LIVE_OBJECTS_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  L_LIVE_OBJECTS_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  L_LIVE_OBJECTS_MARKER_FILL_COLOR = PROFILER_RED;
+    public static final long   L_LIVE_OBJECTS_INITIAL_VALUE = 100l;
+
+    public static final String L_LIVE_BYTES_NAME = "Live Bytes";
+    public static final float  L_LIVE_BYTES_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  L_LIVE_BYTES_PAINTER_LINE_COLOR = PROFILER_BLUE;
+    public static final Color  L_LIVE_BYTES_PAINTER_FILL_COLOR = null;
+    public static final int    L_LIVE_BYTES_MARKER_RADIUS = 5;
+    public static final float  L_LIVE_BYTES_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  L_LIVE_BYTES_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  L_LIVE_BYTES_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  L_LIVE_BYTES_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  L_LIVE_BYTES_MARKER_FILL_COLOR = PROFILER_BLUE;
+    public static final long   L_LIVE_BYTES_INITIAL_VALUE = 102400l;
+
+    public static final String L_ALLOC_OBJECTS_NAME = "Allocated Objects";
+    public static final float  L_ALLOC_OBJECTS_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  L_ALLOC_OBJECTS_PAINTER_LINE_COLOR = PROFILER_GREEN;
+    public static final Color  L_ALLOC_OBJECTS_PAINTER_FILL_COLOR = null;
+    public static final int    L_ALLOC_OBJECTS_MARKER_RADIUS = 5;
+    public static final float  L_ALLOC_OBJECTS_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  L_ALLOC_OBJECTS_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  L_ALLOC_OBJECTS_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  L_ALLOC_OBJECTS_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  L_ALLOC_OBJECTS_MARKER_FILL_COLOR = PROFILER_GREEN;
+    public static final long   L_ALLOC_OBJECTS_INITIAL_VALUE = 100l;
+
+
+    // --- VM Telemetry graphs -------------------------------------------------
+
+    public static final String HEAP_SIZE_NAME = "Heap Size";
+    public static final float  HEAP_SIZE_PAINTER_LINE_WIDTH = 0f;
+    public static final Color  HEAP_SIZE_PAINTER_LINE_COLOR = null;
+    public static final Color  HEAP_SIZE_PAINTER_FILL_COLOR = PROFILER_RED;
+    public static final int    HEAP_SIZE_MARKER_RADIUS = 5;
+    public static final float  HEAP_SIZE_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  HEAP_SIZE_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  HEAP_SIZE_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  HEAP_SIZE_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  HEAP_SIZE_MARKER_FILL_COLOR = PROFILER_RED;
+    public static final long   HEAP_SIZE_INITIAL_VALUE = 5177344l;
+
+    public static final String USED_HEAP_NAME = "Used Heap";
+    public static final float  USED_HEAP_PAINTER_LINE_WIDTH = 0f;
+    public static final Color  USED_HEAP_PAINTER_LINE_COLOR = null;
+    public static final Color  USED_HEAP_PAINTER_FILL_COLOR = PROFILER_BLUE;
+    public static final int    USED_HEAP_MARKER_RADIUS = 5;
+    public static final float  USED_HEAP_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  USED_HEAP_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  USED_HEAP_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  USED_HEAP_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  USED_HEAP_MARKER_FILL_COLOR = PROFILER_BLUE;
+    public static final long   USED_HEAP_INITIAL_VALUE = 1048576l;
+
+    public static final Color  HEAP_LIMIT_FILL_COLOR = new Color(220, 220, 220);
+    
+    public static final String SURVGEN_NAME = "Surviving Generations";
+    public static final float  SURVGEN_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  SURVGEN_PAINTER_LINE_COLOR = PROFILER_RED;
+    public static final Color  SURVGEN_PAINTER_FILL_COLOR = null;
+    public static final int    SURVGEN_MARKER_RADIUS = 5;
+    public static final float  SURVGEN_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  SURVGEN_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  SURVGEN_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  SURVGEN_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  SURVGEN_MARKER_FILL_COLOR = PROFILER_RED;
+    public static final long   SURVGEN_INITIAL_VALUE = 2;
+
+    public static final String GC_TIME_NAME = "Relative Time Spent in GC";
+    public static final float  GC_TIME_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  GC_TIME_PAINTER_LINE_COLOR = PROFILER_BLUE;
+    public static final Color  GC_TIME_PAINTER_FILL_COLOR = null;
+    public static final int    GC_TIME_MARKER_RADIUS = 5;
+    public static final float  GC_TIME_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  GC_TIME_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  GC_TIME_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  GC_TIME_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  GC_TIME_MARKER_FILL_COLOR = PROFILER_BLUE;
+
+    public static final Color  GC_ACTIVITY_FILL_COLOR = new Color(250, 230, 230);
+
+    public static final String THREADS_NAME = "Threads";
+    public static final float  THREADS_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  THREADS_PAINTER_LINE_COLOR = PROFILER_RED;
+    public static final Color  THREADS_PAINTER_FILL_COLOR = null;
+    public static final int    THREADS_MARKER_RADIUS = 5;
+    public static final float  THREADS_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  THREADS_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  THREADS_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  THREADS_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  THREADS_MARKER_FILL_COLOR = PROFILER_RED;
+    public static final long   THREADS_INITIAL_VALUE = 3;
+
+    public static final String LOADED_CLASSES_NAME = "Loaded Classes";
+    public static final float  LOADED_CLASSES_PAINTER_LINE_WIDTH = 3f;
+    public static final Color  LOADED_CLASSES_PAINTER_LINE_COLOR = PROFILER_BLUE;
+    public static final Color  LOADED_CLASSES_PAINTER_FILL_COLOR = null;
+    public static final int    LOADED_CLASSES_MARKER_RADIUS = 5;
+    public static final float  LOADED_CLASSES_MARKER_LINE1_WIDTH = 0.75f;
+    public static final Color  LOADED_CLASSES_MARKER_LINE1_COLOR = Color.BLACK;
+    public static final float  LOADED_CLASSES_MARKER_LINE2_WIDTH = 3.5f;
+    public static final Color  LOADED_CLASSES_MARKER_LINE2_COLOR = Color.WHITE;
+    public static final Color  LOADED_CLASSES_MARKER_FILL_COLOR = PROFILER_BLUE;
+    public static final long   LOADED_CLASSES_INITIAL_VALUE = 732;
+
+}
---
+++ lib.profiler.ui/src/org/netbeans/lib/profiler/ui/monitor/VMTelemetryModels.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * 
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package org.netbeans.lib.profiler.ui.monitor;
+
+import org.netbeans.lib.profiler.charts.LongRect;
+import org.netbeans.lib.profiler.charts.Timeline;
+import org.netbeans.lib.profiler.results.DataManagerListener;
+import org.netbeans.lib.profiler.results.monitor.VMTelemetryDataManager;
+import org.netbeans.lib.profiler.ui.charts.xy.ProfilerGCXYItem;
+import org.netbeans.lib.profiler.ui.charts.xy.ProfilerXYItem;
+import org.netbeans.lib.profiler.ui.charts.xy.ProfilerXYItemsModel;
+import org.netbeans.lib.profiler.ui.graphs.GraphsUI;
+
+/**
+ *
+ * @author Jiri Sedlacek
+ */
+public final class VMTelemetryModels {
+
+    // --- Instance variables --------------------------------------------------
+
+    private final VMTelemetryDataManager dataManager;
+
+    private final Timeline timeline;
+    private final ProfilerXYItemsModel memoryItemsModel;
+    private final ProfilerXYItemsModel generationsItemsModel;
+    private final ProfilerXYItemsModel threadsItemsModel;
+
+
+    // --- Constructor ---------------------------------------------------------
+
+    public VMTelemetryModels(VMTelemetryDataManager dataManager) {
+        this.dataManager = dataManager;
+
+        timeline = createTimeline();
+        memoryItemsModel = createMemoryItemsModel(timeline);
+        generationsItemsModel = createGenerationsItemsModel(timeline);
+        threadsItemsModel = createThreadsItemsModel(timeline);
+
+        dataManager.addDataListener(new DataManagerListener() {
+            public void dataChanged() { dataChangedImpl(); }
+            public void dataReset() { dataResetImpl(); }
+        });
+    }
+
+
+    // --- Public interface ----------------------------------------------------
+
+    public VMTelemetryDataManager getDataManager() {
+        return dataManager;
+    }
+
+    public ProfilerXYItemsModel memoryItemsModel() {
+        return memoryItemsModel;
+    }
+
+    public ProfilerXYItemsModel generationsItemsModel() {
+        return generationsItemsModel;
+    }
+
+    public ProfilerXYItemsModel threadsItemsModel() {
+        return threadsItemsModel;
+    }
+
+
+    // --- DataManagerListener implementation ----------------------------------
+
+    private void dataChangedImpl() {
+        memoryItemsModel.valuesAdded();
+        generationsItemsModel.valuesAdded();
+        threadsItemsModel.valuesAdded();
+    }
+
+    private void dataResetImpl() {
+        memoryItemsModel.valuesReset();
+        generationsItemsModel.valuesReset();
+        threadsItemsModel.valuesReset();
+    }
+
+
+    // --- Private implementation ----------------------------------------------
+
+    private Timeline createTimeline() {
+        return new Timeline() {
+            public int getTimestampsCount() { return dataManager.getItemCount(); }
+            public long getTimestamp(int index) { return dataManager.timeStamps[index]; }
+        };
+    }
+
+    private ProfilerXYItemsModel createMemoryItemsModel(Timeline timeline) {
+        // Heap size
+        ProfilerXYItem heapSizeItem = new ProfilerXYItem(GraphsUI.HEAP_SIZE_NAME, 0) {
+            public long getYValue(int index) {
+                return dataManager.totalMemory[index];
+            }
+        };
+        heapSizeItem.setInitialBounds(new LongRect(0, 0, 0, GraphsUI.HEAP_SIZE_INITIAL_VALUE));
+
+        // Used heap
+        ProfilerXYItem usedHeapItem = new ProfilerXYItem(GraphsUI.USED_HEAP_NAME, 0) {
+            public long getYValue(int index) {
+                return dataManager.usedMemory[index];
+            }
+        };
+        usedHeapItem.setInitialBounds(new LongRect(0, 0, 0, GraphsUI.USED_HEAP_INITIAL_VALUE));
+
+        // Model
+        ProfilerXYItemsModel model = new ProfilerXYItemsModel(timeline,
+                           new ProfilerXYItem[] { heapSizeItem, usedHeapItem });
+
+        return model;
+    }
+
+    private ProfilerXYItemsModel createGenerationsItemsModel(Timeline timeline) {
+        // Surviving generations
+        ProfilerXYItem survivingGenerationsItem = new ProfilerXYItem(GraphsUI.SURVGEN_NAME, 0) {
+            public long getYValue(int index) {
+                return dataManager.nSurvivingGenerations[index];
+            }
+        };
+        survivingGenerationsItem.setInitialBounds(new LongRect(0, 0, 0, GraphsUI.SURVGEN_INITIAL_VALUE));
+
+        // Relative time spent in GC
+        ProfilerXYItem gcTimeItem = new ProfilerXYItem(GraphsUI.GC_TIME_NAME, 0, 1000) {
+            public long getYValue(int index) {
+                return dataManager.relativeGCTimeInPerMil[index];
+            }
+        };
+        gcTimeItem.setInitialBounds(new LongRect(0, 0, 0, 1000));
+
+        // GC intervals
+        ProfilerGCXYItem gcIntervalsItem = new ProfilerGCXYItem("") { // NOI18N
+
+            public long[] getGCStarts(int index) {
+                return dataManager.gcStarts[index];
+            }
+
+            public long[] getGCEnds(int index) {
+                return dataManager.gcFinishs[index];
+            }
+
+        };
+
+        // Model
+        ProfilerXYItemsModel model = new ProfilerXYItemsModel(timeline,
+                 new ProfilerXYItem[] { gcIntervalsItem,
+                                        survivingGenerationsItem,
+                                        gcTimeItem });
+
+        return model;
+    }
+
+    private ProfilerXYItemsModel createThreadsItemsModel(Timeline timeline) {
+        // Threads
+        ProfilerXYItem threadsItem = new ProfilerXYItem(GraphsUI.THREADS_NAME, 0) {
+            public long getYValue(int index) {
+                return dataManager.nTotalThreads[index];
+            }
+        };
+        threadsItem.setInitialBounds(new LongRect(0, 0, 0, GraphsUI.THREADS_INITIAL_VALUE));
+
+        // Loaded classes
+        ProfilerXYItem loadedClassesItem = new ProfilerXYItem(GraphsUI.LOADED_CLASSES_NAME, 0) {
+            public long getYValue(int index) {
+                return dataManager.loadedClassesCount[index];
+            }
+        };
+        loadedClassesItem.setInitialBounds(new LongRect(0, 0, 0, GraphsUI.LOADED_CLASSES_INITIAL_VALUE));
+
+        // Model
+        ProfilerXYItemsModel model = new ProfilerXYItemsModel(timeline,
+                       new ProfilerXYItem[] { threadsItem, loadedClassesItem });
+
+        return model;
+    }
+
+}
