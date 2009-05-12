d7c2a1101ba91ecd30f437a7e695d682db2f259b
mentlicher@netbeans.org <none@none>
2009-05-12 16:49:34 +0200

    #154425 - Disable collection of objects during evaluation.

--- debugger.jpda/src/org/netbeans/modules/debugger/jpda/models/WatchesModel.java
+++ debugger.jpda/src/org/netbeans/modules/debugger/jpda/models/WatchesModel.java
@@ -41,6 +41,7 @@
 
 package org.netbeans.modules.debugger.jpda.models;
 
+import com.sun.jdi.ObjectReference;
 import com.sun.jdi.PrimitiveValue;
 import com.sun.jdi.Value;
 
@@ -68,6 +69,8 @@ import org.netbeans.modules.debugger.jpda.JPDADebuggerImpl;
 import org.netbeans.modules.debugger.jpda.expr.Expression;
 import org.netbeans.modules.debugger.jpda.expr.ParseException;
 
+import org.netbeans.modules.debugger.jpda.jdi.ObjectReferenceWrapper;
+import org.openide.util.NbBundle;
 import org.openide.util.RequestProcessor.Task;
 import org.openide.util.WeakListeners;
 
@@ -87,14 +90,14 @@ public class WatchesModel implements TreeModel {
     private ContextProvider     lookupProvider;
     // Watch to Expression or Exception
     private final Map<Watch, JPDAWatchEvaluating>  watchToValue = new WeakHashMap<Watch, JPDAWatchEvaluating>(); // <node (expression), JPDAWatch>
-    //private final JPDAWatch EMPTY_WATCH;
+    private final JPDAWatch EMPTY_WATCH;
 
     
     public WatchesModel (ContextProvider lookupProvider) {
         debugger = (JPDADebuggerImpl) lookupProvider.
             lookupFirst (null, JPDADebugger.class);
         this.lookupProvider = lookupProvider;
-        //EMPTY_WATCH = new EmptyWatch();
+        EMPTY_WATCH = new EmptyWatch();
     }
     
     /** 
@@ -113,7 +116,7 @@ public class WatchesModel implements TreeModel {
     throws UnknownTypeException {
         if (parent == ROOT) {
             
-            // 1) ger Watches
+            // 1) get Watches
             Watch[] ws = DebuggerManager.getDebuggerManager ().
                 getWatches ();
             to = Math.min(ws.length, to);
@@ -123,9 +126,8 @@ public class WatchesModel implements TreeModel {
             
             // 2) create JPDAWatches for Watches
             int i, k = fws.length;
-            JPDAWatch[] jws = new JPDAWatch [k];// + 1];
+            JPDAWatch[] jws = new JPDAWatch [k + 1];
             for (i = 0; i < k; i++) {
-                
                 
                 JPDAWatchEvaluating jw = watchToValue.get(fws[i]);
                 if (jw == null) {
@@ -136,7 +138,7 @@ public class WatchesModel implements TreeModel {
                 
                 // The actual expressions are computed on demand in JPDAWatchEvaluating
             }
-            //jws[k] = EMPTY_WATCH;
+            jws[k] = EMPTY_WATCH;
             
             if (listener == null)
                 listener = new Listener (this, debugger);
@@ -180,7 +182,7 @@ public class WatchesModel implements TreeModel {
                 return ((JPDAWatchImpl) jw).isPrimitive ();
             }
         }
-        //if (node == EMPTY_WATCH) return true;
+        if (node == EMPTY_WATCH) return true;
         return getLocalsTreeModel ().isLeaf (node);
     }
 
@@ -405,6 +407,14 @@ public class WatchesModel implements TreeModel {
                     JPDAObjectWatchImpl jwi = new JPDAObjectWatchImpl (debugger, w, v);
                     jwi.addPropertyChangeListener(this);
                     jw = jwi;
+                    /* Uncomment if evaluator returns variables with disabled collection
+                    if (v instanceof ObjectReference) {
+                        // Returned variable with disabled collection. When not used any more,
+                        // it's collection must be enabled again.
+                        try {
+                            ObjectReferenceWrapper.enableCollection((ObjectReference) v);
+                        } catch (Exception ex) {}
+                    }*/
                 }
             } catch (InvalidExpressionException e) {
                 JPDAWatchImpl jwi = new JPDAWatchImpl (debugger, w, e, this);
@@ -600,18 +610,23 @@ public class WatchesModel implements TreeModel {
         }
     }
     
-    /*
+    /**
      * The last empty watch, that can be used to enter new watch expressions.
-     *
+     */
     private final class EmptyWatch implements JPDAWatch {
         
     
         public String getExpression() {
-            return "";
+            return ""; // NOI18N
         }
 
-        public void setExpression(String expression) {
-            DebuggerManager.getDebuggerManager().createWatch(expression);
+        public void setExpression(String expr) {
+            String infoStr = NbBundle.getBundle (WatchesModel.class).getString("CTL_WatchesModel_Empty_Watch_Hint");
+            infoStr = "<" + infoStr + ">";
+            if (expr == null || expr.trim().length() == 0 || infoStr.equals(expr)) {
+                return; // cancel action
+            }
+            DebuggerManager.getDebuggerManager().createWatch(expr);
             
             Vector v = (Vector) listeners.clone ();
             int i, k = v.size ();
@@ -626,11 +641,11 @@ public class WatchesModel implements TreeModel {
         }
 
         public String getType() {
-            return "";
+            return ""; // NOI18N
         }
 
         public String getValue() {
-            return "";
+            return ""; // NOI18N
         }
 
         public String getExceptionDescription() {
@@ -642,8 +657,7 @@ public class WatchesModel implements TreeModel {
         }
 
         public String getToStringValue() throws InvalidExpressionException {
-            return "";
+            return ""; // NOI18N
         }
     }
-     */
 }
