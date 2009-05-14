efea1d7751058c2b5075f7d34094507ff48f3096
Radek Matous <rmatous@netbeans.org>
2009-05-14 16:59:56 +0200

    #155563 Missing UI for switching on/off mark occurrences

--- php.editor/src/org/netbeans/modules/php/editor/nav/OccurrencesFinderImpl.java
+++ php.editor/src/org/netbeans/modules/php/editor/nav/OccurrencesFinderImpl.java
@@ -42,14 +42,18 @@ package org.netbeans.modules.php.editor.nav;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import org.netbeans.modules.gsf.api.ColoringAttributes;
-import org.netbeans.modules.gsf.api.CompilationInfo;
-import org.netbeans.modules.gsf.api.OccurrencesFinder;
-import org.netbeans.modules.gsf.api.OffsetRange;
+import java.util.prefs.Preferences;
+import org.netbeans.modules.csl.api.ColoringAttributes;
+import org.netbeans.modules.csl.api.OccurrencesFinder;
+import org.netbeans.modules.csl.api.OffsetRange;
+import org.netbeans.modules.csl.spi.GsfUtilities;
+import org.netbeans.modules.csl.spi.ParserResult;
+import org.netbeans.modules.parsing.spi.Parser.Result;
+import org.netbeans.modules.parsing.spi.Scheduler;
+import org.netbeans.modules.parsing.spi.SchedulerEvent;
 import org.netbeans.modules.php.editor.model.CodeMarker;
 import org.netbeans.modules.php.editor.model.Model;
 import org.netbeans.modules.php.editor.model.ModelElement;
@@ -57,12 +61,13 @@ import org.netbeans.modules.php.editor.model.ModelFactory;
 import org.netbeans.modules.php.editor.model.Occurence;
 import org.netbeans.modules.php.editor.model.OccurencesSupport;
 import org.netbeans.modules.php.editor.model.PhpKind;
+import org.netbeans.modules.php.editor.options.MarkOccurencesSettings;
 
 /**
  *
  * @author Radek Matous
  */
-public class OccurrencesFinderImpl implements OccurrencesFinder {
+public class OccurrencesFinderImpl extends OccurrencesFinder {
     private int offset;
     private Map<OffsetRange, ColoringAttributes> range2Attribs;
 
@@ -76,15 +81,20 @@ public class OccurrencesFinderImpl implements OccurrencesFinder {
     }
 
     public void cancel() {
+        //TODO: implement me 
     }
     
-    public void run(CompilationInfo parameter) throws Exception {
-        for (OffsetRange r : compute(parameter, offset)) {
-            range2Attribs.put(r, ColoringAttributes.MARK_OCCURRENCES);
+    public void run(Result result, SchedulerEvent event) {
+        Preferences node = MarkOccurencesSettings.getCurrentNode();
+
+        if (node.getBoolean(MarkOccurencesSettings.ON_OFF, true)) {
+            for (OffsetRange r : compute((ParserResult) result, GsfUtilities.getLastKnownCaretOffset(result.getSnapshot(), event))) {
+                range2Attribs.put(r, ColoringAttributes.MARK_OCCURRENCES);
+            }
         }
     }
     
-    static Collection<OffsetRange> compute(final CompilationInfo parameter, final int offset) {
+    static Collection<OffsetRange> compute(final ParserResult parameter, final int offset) {
         Set<OffsetRange> result = new TreeSet<OffsetRange>(new Comparator<OffsetRange>() {
             public int compare(OffsetRange o1, OffsetRange o2) {
                 return o1.compareTo(o2);
@@ -96,7 +106,7 @@ public class OccurrencesFinderImpl implements OccurrencesFinder {
         if (caretOccurence != null) {
             ModelElement decl = caretOccurence.getDeclaration();
             if (decl != null && !decl.getPhpKind().equals(PhpKind.INCLUDE)) {
-                List<Occurence> allOccurences = caretOccurence.getAllOccurences();
+                Collection<Occurence> allOccurences = caretOccurence.getAllOccurences();
                 for (Occurence occurence : allOccurences) {
                     result.add(occurence.getOccurenceRange());
                 }
@@ -104,12 +114,22 @@ public class OccurrencesFinderImpl implements OccurrencesFinder {
         } else  {
             CodeMarker codeMarker = occurencesSupport.getCodeMarker();
             if (codeMarker != null) {
-                List<? extends CodeMarker> allMarkers = codeMarker.getAllMarkers();
+                Collection<? extends CodeMarker> allMarkers = codeMarker.getAllMarkers();
                 for (CodeMarker marker : allMarkers) {
                     result.add(marker.getOffsetRange());
                 }
             }
         }
         return result;
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public Class<? extends Scheduler> getSchedulerClass() {
+        return Scheduler.CURSOR_SENSITIVE_TASK_SCHEDULER;
     }
 }
