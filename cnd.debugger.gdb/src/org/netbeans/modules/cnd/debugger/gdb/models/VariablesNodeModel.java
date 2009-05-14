1ca7723a6fe09c2bbf8750a04c60b6652cadbd60
Egor Ushakov <gorrus@netbeans.org>
2009-05-14 17:01:14 +0400

    no IZ: removing unused interfaces

--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/models/VariablesNodeModel.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/models/VariablesNodeModel.java
@@ -41,56 +41,57 @@
 
 package org.netbeans.modules.cnd.debugger.gdb.models;
 
+import java.awt.datatransfer.Transferable;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import org.netbeans.api.debugger.Watch;
 import org.netbeans.spi.debugger.ContextProvider;
 import org.netbeans.spi.viewmodel.ModelEvent;
-import org.netbeans.spi.viewmodel.NodeModel;
 import org.netbeans.spi.viewmodel.TreeModel;
 import org.netbeans.spi.viewmodel.ModelListener;
 import org.netbeans.spi.viewmodel.UnknownTypeException;
+import org.netbeans.modules.cnd.debugger.gdb.Field;
+import org.netbeans.modules.cnd.debugger.gdb.Variable;
+import org.netbeans.spi.viewmodel.ExtendedNodeModel;
+import org.openide.util.datatransfer.PasteType;
 import org.openide.util.NbBundle;
 import org.openide.util.RequestProcessor;
-
-import org.netbeans.modules.cnd.debugger.gdb.LocalVariable;
-import org.netbeans.modules.cnd.debugger.gdb.Field;
 
 /*
  * VariablesNodeModel.java
  *
  * @author Nik Molchanov (copied from Jan Jancura's JPDA implementation)
  */
-public class VariablesNodeModel implements NodeModel {
+public class VariablesNodeModel implements ExtendedNodeModel {
     
     public static final String FIELD =
-            "org/netbeans/modules/debugger/resources/watchesView/Field"; // NOI18N
+            "org/netbeans/modules/debugger/resources/watchesView/Field.gif"; // NOI18N
     public static final String LOCAL =
-            "org/netbeans/modules/debugger/resources/localsView/LocalVariable"; // NOI18N
-    public static final String FIXED_WATCH =
-            "org/netbeans/modules/debugger/resources/watchesView/FixedWatch"; // NOI18N
-    public static final String STATIC_FIELD =
-            "org/netbeans/modules/debugger/resources/watchesView/StaticField"; // NOI18N
-    public static final String SUPER =
-            "org/netbeans/modules/debugger/resources/watchesView/SuperVariable"; // NOI18N
+            "org/netbeans/modules/debugger/resources/localsView/LocalVariable.gif"; // NOI18N
+    public static final String WATCH =
+            "org/netbeans/modules/debugger/resources/watchesView/watch_16.png"; // NOI18N
+    public static final String ERROR =
+            "org/netbeans/modules/cnd/debugger/gdb/resources/error_small_16.png"; // NOI18N
     
-    private RequestProcessor evaluationRP = new RequestProcessor();
+    private final RequestProcessor evaluationRP = new RequestProcessor();
     private final Collection<ModelListener> modelListeners = new HashSet<ModelListener>();
     
     // Localizable messages
-    private String LC_NoInfo = NbBundle.getMessage(VariablesNodeModel.class, "CTL_No_Info"); // NOI18N
-    private String LC_NoCurrentThreadVar = NbBundle.getMessage(VariablesNodeModel.class,
+    private final String LC_NoInfo = NbBundle.getMessage(VariablesNodeModel.class, "CTL_No_Info"); // NOI18N
+    private final String LC_NoCurrentThreadVar = NbBundle.getMessage(VariablesNodeModel.class,
             "NoCurrentThreadVar"); // NOI18N
-    private String LC_LocalsModelColumnNameName = NbBundle.getMessage(VariablesNodeModel.class,
+    private final String LC_LocalsModelColumnNameName = NbBundle.getMessage(VariablesNodeModel.class,
             "CTL_LocalsModel_Column_Name_Name"); // NOI18N
-    private String LC_LocalsModelColumnNameDesc = NbBundle.getMessage(VariablesNodeModel.class,
+    private final String LC_LocalsModelColumnNameDesc = NbBundle.getMessage(VariablesNodeModel.class,
             "CTL_LocalsModel_Column_Name_Desc"); // NOI18N
     
     // Non-localized magic strings
-    private final String strNoInfo = "NoInfo"; // NOI18N
-    private final String strSubArray = "SubArray"; // NOI18N
-    private final String strNoCurrentThread = "No current thread"; // NOI18N
+    private static final String strNoInfo = "NoInfo"; // NOI18N
+    private static final String strSubArray = "SubArray"; // NOI18N
+    private static final String strNoCurrentThread = "No current thread"; // NOI18N
     
     public VariablesNodeModel(ContextProvider lookupProvider) {
     }
@@ -99,11 +100,8 @@ public class VariablesNodeModel implements NodeModel {
         if (o == TreeModel.ROOT) {
             return LC_LocalsModelColumnNameName;
         }
-        if (o instanceof Field) {
-            return ((Field) o).getName();
-        }
-        if (o instanceof LocalVariable) {
-            return ((LocalVariable) o).getName();
+        if (o instanceof Variable) {
+            return ((Variable) o).getName();
         }
         
         String str = o.toString();
@@ -124,7 +122,7 @@ public class VariablesNodeModel implements NodeModel {
         throw new UnknownTypeException(o);
     }
     
-    private Map<Object, String> shortDescriptionMap = new HashMap<Object, String>();
+    private final Map<Object, String> shortDescriptionMap = new HashMap<Object, String>();
     
     public String getShortDescription(final Object o) throws UnknownTypeException {
         synchronized (shortDescriptionMap) {
@@ -155,15 +153,12 @@ public class VariablesNodeModel implements NodeModel {
     private String getShortDescriptionSync(Object o) {
         if (o == TreeModel.ROOT) {
             return LC_LocalsModelColumnNameDesc;
-        } else if (o instanceof Field) {
-            Field f = (Field) o;
-            if (f.getType().length() == 0 && f.getValue().equals("...")) { // NOI18N
+        } else if (o instanceof Variable) {
+            Variable v = (Variable) o;
+            if (o instanceof Field && v.getType().length() == 0 && v.getValue().equals("...")) { // NOI18N
                 return NbBundle.getMessage(VariablesNodeModel.class, "LBL_TruncatedByGdb"); // NOI18N
-            } else {
-                return "(" + ((Field) o).getType() + ") " + ((Field) o).getValue(); // NOI18N
             }
-        } else if (o instanceof LocalVariable) {
-            return "(" + ((LocalVariable) o).getType() + ") " + ((LocalVariable) o).getValue(); // NOI18N
+            return "(" + v.getType() + ") " + v.getValue(); // NOI18N
         }
         
         String str = o.toString();
@@ -182,7 +177,7 @@ public class VariablesNodeModel implements NodeModel {
     }
     
     private void testKnown(Object o) throws UnknownTypeException {
-        if (o == TreeModel.ROOT || o instanceof Field || o instanceof LocalVariable) {
+        if (o == TreeModel.ROOT || o instanceof Variable) {
             return;
         }
         String str = o.toString();
@@ -193,28 +188,32 @@ public class VariablesNodeModel implements NodeModel {
     }
     
     public String getIconBase(Object o) throws UnknownTypeException {
-        if (o == TreeModel.ROOT)
-            return FIELD;
-        if (o instanceof Field) {
-            if (((Field) o).isStatic())
-                return STATIC_FIELD;
-            else
+        throw new UnsupportedOperationException();
+    }
+
+    public String getIconBaseWithExtension(Object node) throws UnknownTypeException {
+        if (node == TreeModel.ROOT || node instanceof GdbWatchVariable || node instanceof Watch) {
+            return WATCH;
+        } else if (node instanceof Field) {
+            if (node instanceof AbstractVariable.ErrorField) {
+                return ERROR;
+            } else {
                 return FIELD;
+            }
+        } else if (node instanceof AbstractVariable) {
+            return LOCAL;
+        } else {
+            // FIXME - I think the following is obsolete, but FCS code freeze is in 4
+            // days so I'm not willing to remove this code. Better to check in the future
+            // (or just leave it).
+            String str = node.toString();
+            if (str.startsWith(strSubArray)) {
+                return LOCAL;
+            } else if (str.equals(strNoInfo) || str.equals(strNoCurrentThread)) {
+                return null;
+            }
         }
-        if (o instanceof LocalVariable)
-            return LOCAL;
-        /*NM TEMPORARY COMMENTED OUT
-        if (o instanceof Super)
-            return SUPER;
-        if (o instanceof This)
-            return FIELD;
-         */
-        String str = o.toString();
-        if (str.startsWith(strSubArray))
-            return LOCAL;
-        if (str.equals(strNoInfo) || str.equals(strNoCurrentThread))
-            return null;
-        throw new UnknownTypeException(o);
+        throw new UnknownTypeException(node);
     }
     
     public void addModelListener(ModelListener l) {
@@ -237,5 +236,36 @@ public class VariablesNodeModel implements NodeModel {
         for (int i = 0; i < listeners.length; i++) {
             ((ModelListener) listeners[i]).modelChanged(me);
         }
+    } 
+    
+    // implement methods from ExtendedNodeModel
+    
+    public boolean canRename(Object node) throws UnknownTypeException {
+        return false;
+    }
+
+    public boolean canCopy(Object node) throws UnknownTypeException {
+        return false;
+    }
+
+    public boolean canCut(Object node) throws UnknownTypeException {
+        return false;
+    }
+
+    public Transferable clipboardCopy(Object node) throws IOException, UnknownTypeException {
+        throw new UnsupportedOperationException();
+    }
+
+    public Transferable clipboardCut(Object node) throws IOException,
+                                                         UnknownTypeException {
+        throw new UnsupportedOperationException();
+    }
+
+    public PasteType[] getPasteTypes(Object node, Transferable t) throws UnknownTypeException {
+        return null;
+    }
+
+    public void setName(Object node, String name) throws UnknownTypeException {
+        throw new UnsupportedOperationException();
     }
 }
--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/models/VariablesTableModel.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/models/VariablesTableModel.java
@@ -50,14 +50,9 @@ import org.netbeans.spi.debugger.ui.Constants;
 import org.netbeans.spi.viewmodel.TableModel;
 import org.netbeans.spi.viewmodel.ModelListener;
 import org.netbeans.spi.viewmodel.UnknownTypeException;
-import org.openide.DialogDisplayer;
-import org.openide.NotifyDescriptor;
 import org.openide.util.NbBundle;
 
-import org.netbeans.modules.cnd.debugger.gdb.Field;
-import org.netbeans.modules.cnd.debugger.gdb.InvalidExpressionException;
 import org.netbeans.modules.cnd.debugger.gdb.GdbDebugger;
-import org.netbeans.modules.cnd.debugger.gdb.LocalVariable;
 import org.netbeans.modules.cnd.debugger.gdb.Variable;
 
 /*
@@ -68,30 +63,28 @@ import org.netbeans.modules.cnd.debugger.gdb.Variable;
 import org.netbeans.modules.cnd.debugger.gdb.utils.GdbUtils;
 public class VariablesTableModel implements TableModel, Constants {
     
-    private GdbDebugger      debugger;
-    private ContextProvider  lookupProvider;
-    private static Logger log = Logger.getLogger("gdb.logger"); // NOI18N
+    private final GdbDebugger      debugger;
+    private static final Logger log = Logger.getLogger("gdb.logger"); // NOI18N
     
     public VariablesTableModel(ContextProvider lookupProvider) {
-        this.lookupProvider = lookupProvider;
-        debugger = (GdbDebugger) lookupProvider.lookupFirst(null, GdbDebugger.class);
+        debugger = lookupProvider.lookupFirst(null, GdbDebugger.class);
     }
     
     public Object getValueAt(Object row, String columnID) throws UnknownTypeException {
         
-        if (debugger == null || !debugger.getState().equals(GdbDebugger.STATE_STOPPED)) {
+        if (debugger == null || !debugger.isStopped()) {
                 return "";
         } else if (columnID.equals(LOCALS_TO_STRING_COLUMN_ID) || columnID.equals(WATCH_TO_STRING_COLUMN_ID)) {
             if (row instanceof Variable) {
-                return ((Variable) row).getValue();
+                return ValuePresenter.getValue((Variable) row);
             }
         } else if (columnID.equals(LOCALS_TYPE_COLUMN_ID) || columnID.equals(WATCH_TYPE_COLUMN_ID)) {
-            if (row instanceof AbstractVariable) {
-                return ((AbstractVariable) row).getType();
+            if (row instanceof Variable) {
+                return ((Variable) row).getType();
             }
         } else if ( columnID.equals(LOCALS_VALUE_COLUMN_ID) || columnID.equals(WATCH_VALUE_COLUMN_ID)) {
-            if (row instanceof AbstractVariable) {
-                return ((AbstractVariable) row).getValue();
+            if (row instanceof Variable) {
+                return ValuePresenter.getValue((Variable) row);
             }
         }
         if (row instanceof JToolTip) {
@@ -111,7 +104,7 @@ public class VariablesTableModel implements TableModel, Constants {
     public boolean isReadOnly(Object row, String columnID) throws UnknownTypeException {
         if (row instanceof AbstractVariable) {
             AbstractVariable var = (AbstractVariable) row;
-            if (debugger == null || !debugger.getState().equals(GdbDebugger.STATE_STOPPED)) {
+            if (debugger == null || !debugger.isStopped()) {
                 return true;
             } else if (columnID.equals(LOCALS_TO_STRING_COLUMN_ID) ||
                     columnID.equals(WATCH_TO_STRING_COLUMN_ID) ||
@@ -121,7 +114,7 @@ public class VariablesTableModel implements TableModel, Constants {
             } else if (columnID.equals(LOCALS_VALUE_COLUMN_ID) || columnID.equals(WATCH_VALUE_COLUMN_ID)) {
                 String t = var.waitForType();
                 if (t == null) {
-                    if (log.isLoggable(Level.FINE) && debugger.getState().equals(GdbDebugger.STATE_STOPPED) &&
+                    if (log.isLoggable(Level.FINE) && debugger.isStopped() &&
                             !SwingUtilities.isEventDispatchThread()) {
                         log.fine("VTM.isReadOnly: null type for " + var.getName() + " (state is " + debugger.getState() + ")"); // NOI18N
                     }
@@ -136,32 +129,19 @@ public class VariablesTableModel implements TableModel, Constants {
             }
         } else if (row.toString().startsWith("No current thread")) { // NOI18N
             return true;
+        } else if (row instanceof AbstractVariable.ErrorField) {
+            return true;
         }
         throw new UnknownTypeException(row);
     }
     
     public void setValueAt(Object row, String columnID, Object value) throws UnknownTypeException {
         
-        if (debugger == null || !debugger.getState().equals(GdbDebugger.STATE_STOPPED)) {
+        if (debugger == null || !debugger.isStopped()) {
             return;
-        } else if (row instanceof LocalVariable) {
+        } else if (row instanceof Variable) {
             if (columnID.equals(LOCALS_VALUE_COLUMN_ID) || columnID.equals(WATCH_VALUE_COLUMN_ID)) {
-                if (row instanceof GdbWatchVariable) {
-                    ((GdbWatchVariable) row).setValueAt((String) value);
-                } else {
-                    ((LocalVariable) row).setValue((String) value);
-                }
-                return;
-            }
-        } else if (row instanceof Field) {
-            if (columnID.equals (LOCALS_VALUE_COLUMN_ID) || columnID.equals (WATCH_VALUE_COLUMN_ID)) {
-                try {
-                    ((Field) row).setValue((String) value);
-                } catch (InvalidExpressionException e) {
-                    NotifyDescriptor.Message descriptor = new NotifyDescriptor.Message(
-                            e.getLocalizedMessage(), NotifyDescriptor.WARNING_MESSAGE);
-                    DialogDisplayer.getDefault().notify(descriptor);
-                }
+                ((Variable) row).setValue((String) value);
                 return;
             }
         }
