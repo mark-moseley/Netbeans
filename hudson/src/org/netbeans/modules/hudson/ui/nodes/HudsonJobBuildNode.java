896710f3310613bdcb1e18ac7cf189295fca4022
Jesse Glick <jglick@netbeans.org>
2009-05-15 16:13:54 -0400

    Factoring some useful things into a new api.UI class for use from hudson.kenai.

---
+++ hudson/src/org/netbeans/modules/hudson/ui/nodes/HudsonJobBuildNode.java
@@ -0,0 +1,137 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.hudson.ui.nodes;
+
+import java.io.CharConversionException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.swing.Action;
+import org.netbeans.modules.hudson.api.HudsonJob.Color;
+import org.netbeans.modules.hudson.api.HudsonJobBuild;
+import org.netbeans.modules.hudson.api.HudsonMavenModuleBuild;
+import org.netbeans.modules.hudson.api.UI;
+import org.netbeans.modules.hudson.ui.actions.OpenUrlAction;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.ChildFactory;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.SystemAction;
+import org.openide.util.lookup.Lookups;
+import org.openide.xml.XMLUtil;
+
+class HudsonJobBuildNode extends AbstractNode {
+
+    private final HudsonJobBuild build;
+    private String htmlDisplayName;
+
+    public HudsonJobBuildNode(HudsonJobBuild build) {
+        super(makeChildren(build), Lookups.singleton(build));
+        setName(Integer.toString(build.getNumber()));
+        setDisplayName(NbBundle.getMessage(HudsonJobBuildNode.class, "HudsonJobBuildNode.displayName", build.getNumber()));
+        Color effectiveColor;
+        if (build.isBuilding()) {
+            effectiveColor = build.getJob().getColor();
+        } else {
+            switch (build.getResult()) {
+            case SUCCESS:
+                effectiveColor = Color.blue;
+                break;
+            case UNSTABLE:
+                effectiveColor = Color.yellow;
+                break;
+            case FAILURE:
+                effectiveColor = Color.red;
+                break;
+            default:
+                effectiveColor = Color.grey;
+            }
+        }
+        try {
+            htmlDisplayName = effectiveColor.colorizeDisplayName(XMLUtil.toElementContent(getDisplayName()));
+        } catch (CharConversionException x) {
+            htmlDisplayName = null;
+        }
+        setIconBaseWithExtension(effectiveColor.iconBase());
+        this.build = build;
+    }
+
+    public @Override String getHtmlDisplayName() {
+        return htmlDisplayName;
+    }
+
+    public @Override Action[] getActions(boolean context) {
+        List<Action> actions = new ArrayList<Action>();
+        actions.add(UI.showChangesAction(build));
+        actions.add(UI.showConsoleAction(build));
+        if (build.getResult() == HudsonJobBuild.Result.UNSTABLE && build.getMavenModules().isEmpty()) {
+            actions.add(UI.showFailuresAction(build));
+        }
+        actions.add(null);
+        actions.add(SystemAction.get(OpenUrlAction.class));
+        return actions.toArray(new Action[actions.size()]);
+    }
+
+    private static Children makeChildren(final HudsonJobBuild build) {
+        return Children.create(new ChildFactory<Object>() {
+            final Object ARTIFACTS = new Object();
+            protected boolean createKeys(List<Object> toPopulate) {
+                Collection<? extends HudsonMavenModuleBuild> modules = build.getMavenModules();
+                if (modules.isEmpty()) {
+                    // XXX is it possible to cheaply check in advance if the build has any artifacts?
+                    toPopulate.add(ARTIFACTS);
+                } else {
+                    toPopulate.addAll(modules);
+                }
+                return true;
+            }
+            protected @Override Node createNodeForKey(Object key) {
+                if (key instanceof HudsonMavenModuleBuild) {
+                    return new HudsonMavenModuleBuildNode((HudsonMavenModuleBuild) key);
+                } else {
+                    assert key == ARTIFACTS : key;
+                    return new HudsonArtifactsNode(build);
+                }
+            }
+        }, false);
+    }
+
+}
---
+++ hudson/src/org/netbeans/modules/hudson/ui/notification/ProblemNotification.java
@@ -0,0 +1,149 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.hudson.ui.notification;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Icon;
+import org.netbeans.modules.hudson.api.HudsonJob;
+import org.netbeans.modules.hudson.api.HudsonJobBuild;
+import org.netbeans.modules.hudson.api.HudsonMavenModuleBuild;
+import org.netbeans.modules.hudson.api.UI;
+import org.openide.awt.Notification;
+import org.openide.awt.NotificationDisplayer;
+import org.openide.awt.NotificationDisplayer.Priority;
+import org.openide.util.ImageUtilities;
+import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
+
+/**
+ * Build failed or was unstable.
+ */
+class ProblemNotification implements ActionListener {
+
+    private static final Logger LOG = Logger.getLogger(ProblemNotification.class.getName());
+
+    final HudsonJob job;
+    private final int build;
+    private final boolean failed;
+    private Notification notification;
+
+    ProblemNotification(HudsonJob job, int build, boolean failed) {
+        this.job = job;
+        this.build = build;
+        this.failed = failed;
+    }
+
+    private String getTitle() {
+        // XXX use HudsonJobBuild.getDisplayName
+        return NbBundle.getMessage(ProblemNotification.class, failed ? "ProblemNotification.title.failed" : "ProblemNotification.title.unstable",
+                job.getDisplayName(), build);
+    }
+
+    private String getDescription() {
+        return NbBundle.getMessage(ProblemNotification.class, failed ? "ProblemNotification.description.failed" : "ProblemNotification.description.unstable");
+    }
+
+    public void actionPerformed(final ActionEvent e) {
+        UI.selectNode(job.getInstance().getUrl(), job.getName(), Integer.toString(build));
+        RequestProcessor.getDefault().post(new Runnable() {
+            public void run() {
+                for (HudsonJobBuild b : job.getBuilds()) {
+                    if (b.getNumber() == build) {
+                        if (failed) {
+                            UI.showConsoleAction(b).actionPerformed(e);
+                        } else if (b.getMavenModules().isEmpty()) {
+                            UI.showFailuresAction(b).actionPerformed(e);
+                        } else {
+                            for (HudsonMavenModuleBuild module : b.getMavenModules()) {
+                                switch (module.getColor()) {
+                                case yellow:
+                                case yellow_anime:
+                                    UI.showFailuresAction(module).actionPerformed(e);
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    private Priority getPriority() {
+        return failed ? Priority.HIGH : Priority.NORMAL;
+    }
+
+    private Icon getIcon() {
+        return ImageUtilities.loadImageIcon("org/netbeans/modules/hudson/ui/resources/notification.png", true); // NOI18N
+    }
+
+    void add() {
+        LOG.log(Level.FINE, "Adding {0}", this);
+        notification = NotificationDisplayer.getDefault().notify(getTitle(), getIcon(), getDescription(), this, getPriority());
+    }
+
+    void remove() {
+        if (notification != null) {
+            LOG.log(Level.FINE, "Removing {0}", this);
+            notification.clear();
+            notification = null;
+        }
+    }
+
+    public @Override boolean equals(Object obj) {
+        if (!(obj instanceof ProblemNotification)) {
+            return false;
+        }
+        ProblemNotification other = (ProblemNotification) obj;
+        return job.getName().equals(other.job.getName()) && build == other.build;
+    }
+
+    public @Override int hashCode() {
+        return job.getName().hashCode() ^ build;
+    }
+
+    public @Override String toString() {
+        return "ProblemNotification[" + job.getName() + "#" + build + "]"; // NOI18N
+    }
+
+}
