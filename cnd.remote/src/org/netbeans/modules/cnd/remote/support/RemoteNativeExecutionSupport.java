0eee1713564e911acac6b255ad5d159a8d161fe5
Andrew Krasny <akrasny@netbeans.org>
2009-05-20 16:56:46 +0400

    #165602: REGRESSION: Compilers errors are not visible (remote execution)

--- cnd.remote/src/org/netbeans/modules/cnd/remote/support/RemoteNativeExecutionSupport.java
+++ cnd.remote/src/org/netbeans/modules/cnd/remote/support/RemoteNativeExecutionSupport.java
@@ -75,10 +75,14 @@ public class RemoteNativeExecutionSupport extends RemoteConnectionSupport {
             //String cmd = makeCommand(dirf, exe, args, envp);
             NativeProcessBuilder pb = NativeProcessBuilder.newProcessBuilder(executionEnvironment);
             pb.setExecutable(cmd); //NOI18N
-            pb = pb.addEnvironmentVariables(env);
+            pb.addEnvironmentVariables(env);
+
             if (args != null) {
-                pb = pb.setArguments(Utilities.parseParameters(args));
+                pb.setArguments(Utilities.parseParameters(args));
             }
+
+            pb.redirectError();
+
             String path = null;
             if (dirf != null) {
                 path = RemotePathMap.getPathMap(executionEnvironment).getRemotePath(dirf.getAbsolutePath(),true);
@@ -143,41 +147,6 @@ public class RemoteNativeExecutionSupport extends RemoteConnectionSupport {
 //            disconnect();
         }
     }
-
-//    private String makeCommand(File dirf, String exe, String args, String[] envp) {
-//        String dircmd;
-//        String path = RemotePathMap.getPathMap(executionEnvironment).getRemotePath(dirf.getAbsolutePath());
-//
-//        if (path != null) {
-//            dircmd = "cd \"" + path + "\"; "; // NOI18N
-//        } else {
-//            dircmd = "";
-//        }
-//
-//        StringBuilder command = new StringBuilder(); // NOI18N
-//
-//        // if (enableDisplayVariable) {
-//        if (envp == null ) {
-//            envp = new String[1];
-//            envp[0] = getDisplayString();
-//        } else {
-//            String[] envp2 = new String[envp.length + 1 ];
-//            for (int i = 0; i < envp.length; i++) {
-//                envp2[i] = envp[i];
-//            }
-//            envp2[envp.length] = getDisplayString();
-//            envp = envp2;
-//        }
-//
-//        if (envp != null) {
-//            command.append(ShellUtils.prepareExportString(envp));
-//        }
-//        command.append(exe).append(" ").append(args).append(" 2>&1"); // NOI18N
-//        command.insert(0, dircmd);
-//
-//        String theCommand = ShellUtils.wrapCommand(executionEnvironment, command.toString());
-//        return theCommand;
-//    }
 
     private static String displayString ;
 
