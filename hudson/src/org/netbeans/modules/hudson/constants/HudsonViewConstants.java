88989b8154c0f82bddd56f51afcec9dc554e7e5d
Jesse Glick <jglick@netbeans.org>
2009-05-21 23:51:58 -0400

    #161828 (part): speed up synchronization, i.e. retrieval of $root/api/xml. Ask server to not send XML elements which will not be parsed anyway. Omit <description> which is unused, and <view><name>All</name></view>. Warn if any unexpected elements do appear. (Can only specify excludes, not includes.) Simplify parsing logic somewhat. Avoid using XPath.

---
+++ hudson/src/org/netbeans/modules/hudson/constants/HudsonViewConstants.java
@@ -0,0 +1,24 @@
+/*
+ * HudsonViewConstants.java
+ * 
+ * Created on May 5, 2007, 5:40:32 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.hudson.constants;
+
+/**
+ *
+ * @author marigan
+ */
+public class HudsonViewConstants {
+
+    private HudsonViewConstants() {}
+
+    public static final String VIEW_NAME = "view_name"; // NOI18N
+    
+    public static final String VIEW_URL = "view_url"; // NOI18N
+
+}
