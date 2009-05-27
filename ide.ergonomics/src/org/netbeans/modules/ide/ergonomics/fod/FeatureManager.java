32ad276731b28179f980af4e54fa25f1b082bd92
Jaroslav Tulach <jtulach@netbeans.org>
2009-05-27 10:34:09 +0200

    #166077: Caching isEnabled and isPresent state of the FeatureInfo

--- ide.ergonomics/src/org/netbeans/modules/ide/ergonomics/fod/FeatureManager.java
+++ ide.ergonomics/src/org/netbeans/modules/ide/ergonomics/fod/FeatureManager.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,9 +31,9 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
+ *
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
@@ -45,6 +45,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -68,18 +70,23 @@ import org.openide.util.lookup.InstanceContent;
  */
 public final class FeatureManager
 implements PropertyChangeListener, LookupListener {
-    private static FeatureManager INSTANCE = new FeatureManager ();
+    private static FeatureManager INSTANCE;
     private static Logger UILOG = Logger.getLogger("org.netbeans.ui.ergonomics"); // NOI18N
     private final Lookup.Result<ModuleInfo> result;
     private final ChangeSupport support;
+    private Set<String> enabledCnbs = Collections.emptySet();
 
     private FeatureManager() {
         support = new ChangeSupport(this);
         result = Lookup.getDefault().lookupResult(ModuleInfo.class);
         result.addLookupListener(this);
+        resultChanged(null);
     }
 
-    public static FeatureManager getInstance () {
+    public static synchronized FeatureManager getInstance () {
+        if (INSTANCE == null) {
+            INSTANCE = new FeatureManager();
+        }
         return INSTANCE;
     }
 
@@ -105,7 +112,7 @@ implements PropertyChangeListener, LookupListener {
     public static Map<String,String> projectFiles() {
         return FeatureInfo.projectFiles();
     }
-    
+
     public static Collection<? extends FeatureInfo> features() {
         return featureTypesLookup().lookupAll(FeatureInfo.class);
     }
@@ -166,7 +173,7 @@ implements PropertyChangeListener, LookupListener {
         }
         return cnt;
     }
-    
+
     /** Used from tests */
     public static synchronized void assignFeatureTypesLookup(Lookup lkp) {
         boolean eaOn = false;
@@ -175,9 +182,11 @@ implements PropertyChangeListener, LookupListener {
             throw new IllegalStateException();
         }
         featureTypesLookup = lkp;
+        noCnbCheck = true;
     }
 
     private static Lookup featureTypesLookup;
+    private static boolean noCnbCheck;
     private static synchronized Lookup featureTypesLookup() {
         if (featureTypesLookup != null) {
             return featureTypesLookup;
@@ -214,9 +223,6 @@ implements PropertyChangeListener, LookupListener {
     }
 
     public void addChangeListener(ChangeListener l) {
-        if (!support.hasListeners()) {
-            resultChanged(null);
-        }
         support.addChangeListener(l);
     }
     public void removeChangeListener(ChangeListener l) {
@@ -228,14 +234,37 @@ implements PropertyChangeListener, LookupListener {
             m.removePropertyChangeListener(this);
             m.addPropertyChangeListener(this);
         }
+        Set<String> tmp = new HashSet<String>();
+        for (ModuleInfo mi : result.allInstances()) {
+            if (mi.isEnabled()) {
+                tmp.add(mi.getCodeNameBase());
+            }
+        }
+        enabledCnbs = tmp;
         if (ev != null) {
-            support.fireChange();
+            fireChange();
         }
     }
     public void propertyChange(PropertyChangeEvent evt) {
         if (ModuleInfo.PROP_ENABLED.equals(evt.getPropertyName())) {
-            support.fireChange();
+            ModuleInfo mi = (ModuleInfo)evt.getSource();
+            if (!noCnbCheck && enabledCnbs.contains(mi.getCodeNameBase()) && mi.isEnabled()) {
+                return;
+            }
+            fireChange();
+            if (mi.isEnabled()) {
+                enabledCnbs.add(mi.getCodeNameBase());
+            } else {
+                enabledCnbs.remove(mi.getCodeNameBase());
+            }
         }
+    }
+
+    private void fireChange() {
+        for (FeatureInfo f : features()) {
+            f.clearCache();
+        }
+        support.fireChange();
     }
 
 }
