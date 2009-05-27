78ca2b848e1abbae12be52e500ae8c04bb864940
mgrebac@netbeans.org <none@none>
2009-05-27 16:51:18 +0200

    Fix for GF bug 1030 - Wrong RM header namespace generated.

--- websvc.wsitmodelext/src/org/netbeans/modules/websvc/wsitmodelext/rm/RMQName.java
+++ websvc.wsitmodelext/src/org/netbeans/modules/websvc/wsitmodelext/rm/RMQName.java
@@ -42,9 +42,12 @@
 
 package org.netbeans.modules.websvc.wsitmodelext.rm;
 
+import java.util.HashMap;
 import javax.xml.namespace.QName;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import org.netbeans.modules.websvc.wsitmodelext.versioning.ConfigVersion;
 
 /**
  *
@@ -52,37 +55,97 @@ import java.util.Set;
  */
 public enum RMQName {
     RMASSERTION(createRMQName("RMAssertion")),                                  //NOI18N
-    BASERETRANSMISSIONINTERVAL(createRMQName("BaseRetransmissionInterval")),    //NOI18N
-    EXPONENTIALBACKOFF(createRMQName("ExponentialBackoff")),                    //NOI18N
     ACKNOWLEDGEMENTINTERVAL(createRMQName("AcknowledgementInterval")),          //NOI18N
-    INACTIVITYTIMEOUT(createRMQName("InactivityTimeout"));                      //NOI18N
+    DELIVERYASSURANCE(createRMQName("DeliveryAssurance")),                    //NOI18N
+    INORDER(createRMQName("InOrder")),                                        //NOI18N
+    EXACTLYONCE(createRMQName("ExactlyOnce")),                                //NOI18N
+    ATLEASTONCE(createRMQName("AtLeastOnce")),                                //NOI18N
+    ATMOSTONCE(createRMQName("AtMostOnce")),                                //NOI18N
+    SEQUENCETRANSPORTSECURITY(createRMQName("SequenceTransportSecurity")),    //NOI18N
+    SEQUENCESTR(createRMQName("SequenceSTR"));                                //NOI18N
 
-    public static final String RM_HEADERS_NS_URI = "http://schemas.xmlsoap.org/ws/2005/02/rm";    //NOI18N
-    
-    public static final String RM_NS_URI = "http://schemas.xmlsoap.org/ws/2005/02/rm/policy";    //NOI18N
-    public static final String RM_NS_PREFIX = "wsrm";                                            //NOI18N
-    
-    public static QName createRMQName(String localName){
+
+    static final String RM_NS_PREFIX = "wsrm";                                            //NOI18N
+
+    static final String RM_NS_URI = "http://schemas.xmlsoap.org/ws/2005/02/rm/policy";    //NOI18N
+    static final String RM_NS_URI_EXT = "http://schemas.xmlsoap.org/ws/2005/02/rm/wsrm-policy.xsd";    //NOI18N
+    static final String RM_NS_URI_LOCAL = "nbres:/org/netbeans/modules/websvc/wsitmodelext/catalog/resources/wsrm-policy-200502.xsd";    //NOI18N
+    static final String RM_NS_HEADERS_URI = "http://schemas.xmlsoap.org/ws/2005/02/rm";    //NOI18
+
+    static final String RM_12_NS_URI = "http://docs.oasis-open.org/ws-rx/wsrmp/200702";    //NOI18N
+    static final String RM_12_NS_URI_EXT = "http://docs.oasis-open.org/ws-rx/wsrmp/200702/wsrmp-1.2-schema-200702.xsd";    //NOI18N
+    static final String RM_12_NS_URI_LOCAL = "nbres:/org/netbeans/modules/websvc/wsitmodelext/catalog/resources/wsrmp-1.2-schema-200702.xsd";    //NOI18N
+    static final String RM_12_NS_HEADERS_URI = "http://docs.oasis-open.org/ws-rx/wsrm/200702";    //NOI18
+
+    static QName createRMQName(String localName){
         return new QName(RM_NS_URI, localName, RM_NS_PREFIX);
     }
-    
+
     RMQName(QName name) {
         qName = name;
     }
-    
-    public QName getQName(){
-        return qName;
+
+    public QName getQName(ConfigVersion cfgVersion) {
+        return new QName(getNamespaceUri(cfgVersion), qName.getLocalPart(), qName.getPrefix());
     }
-    private static Set<QName> qnames = null;
-    public static Set<QName> getQNames() {
-        if (qnames == null) {
-            qnames = new HashSet<QName>();
-            for (RMQName wq : values()) {
-                qnames.add(wq.getQName());
+
+    public static String getNamespaceUri(ConfigVersion cfgVersion) {
+        switch (cfgVersion) {
+            case CONFIG_1_0 : return RM_NS_URI;
+            case CONFIG_1_3 : return RM_12_NS_URI;
+        }
+        return null;
+    }
+
+    public static String getHeaderNamespaceUri(ConfigVersion cfgVersion) {
+        switch (cfgVersion) {
+            case CONFIG_1_0 : return RM_NS_HEADERS_URI;
+            case CONFIG_1_3 : return RM_12_NS_HEADERS_URI;
+        }
+        return null;
+    }
+
+    public static ConfigVersion getConfigVersion(QName q) {
+        for (ConfigVersion cfgVersion : ConfigVersion.values()) {
+            if (getQNames(cfgVersion).contains(q)) {
+                return cfgVersion;
             }
+        }
+        return null;
+    }
+
+    public static Set<QName> getQNames(ConfigVersion cfgVersion) {
+        Set<QName> qnames = new HashSet<QName>();
+        for (RMQName wq : values()) {
+            qnames.add(wq.getQName(cfgVersion));
         }
         return qnames;
     }
     private final QName qName;
+
+    public Map<String, String> getSchemaLocations(boolean local) {
+        HashMap<String, String> hmap = new HashMap<String, String>();
+        for (ConfigVersion cfg : ConfigVersion.values()) {
+            try {
+                String nsUri = getNamespaceUri(cfg);
+                if (nsUri != null) {
+                    hmap.put(nsUri, getSchemaLocation(nsUri, local));
+                }
+            } catch (IllegalArgumentException iae) {
+                // ignore - just skip this
+            }
+        }
+        return hmap;
+    }
+
+    public String getSchemaLocation(String namespace, boolean local) {
+        if (RM_NS_URI.equals(namespace)) {
+            return local ? RM_NS_URI_LOCAL : RM_NS_URI_EXT;
+        }
+        if (RM_12_NS_URI.equals(namespace)) {
+            return local ? RM_12_NS_URI_LOCAL : RM_12_NS_URI_EXT;
+        }
+        return null;
+    }
 
 }
