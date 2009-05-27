e4dd771357da4150d464414b7a9f737204d46c2b
Dmitry Lipin <dlipin@netbeans.org>
2009-05-27 20:54:28 +0400

    Add support for nbi.utils.log.file system property to set log file

--- nbi/engine/src/org/netbeans/installer/utils/LogManager.java
+++ nbi/engine/src/org/netbeans/installer/utils/LogManager.java
@@ -81,19 +81,18 @@ public final class LogManager {
             logLevel = DEFAULT_LOG_LEVEL;
         }
         
-        // check whether we should log to console as well
-        if (System.getProperty(LOG_TO_CONSOLE_PROPERTY) != null) {
-            logToConsole = new Boolean(System.getProperty(LOG_TO_CONSOLE_PROPERTY));
-        } else {
-            logToConsole = DEFAULT_LOG_TO_CONSOLE;
-        }
+        initializeConsoleLogging();
         
         // init the log file and streams
         try {
             if(logFile!=null) {
                 logFile.getParentFile().mkdirs();
-                logFile.createNewFile();
+                if(logFile.exists()) {
+                   logFile.delete();
+                }
+                logFile.createNewFile();                
                 logWriter = new PrintWriter(new FileWriter(logFile));
+                System.setProperty(LOG_FILE_PROPERTY, logFile.getAbsolutePath());
             }
             
             // here is a small assumption that there will be no calls to log*(*)
@@ -102,6 +101,7 @@ public final class LogManager {
             for (String string: logCache) {
                 write(string);
             }
+            logCache.clear();
         } catch (IOException e) {
             e.printStackTrace(System.err);
             logWriter = null;
@@ -109,11 +109,43 @@ public final class LogManager {
         
         started = true;
     }
+    private static void initializeConsoleLogging () {
+        // check whether we should log to console as well
+        if (System.getProperty(LOG_TO_CONSOLE_PROPERTY) != null) {
+            logToConsole = new Boolean(System.getProperty(LOG_TO_CONSOLE_PROPERTY));
+        } else {
+            logToConsole = DEFAULT_LOG_TO_CONSOLE;
+        }
+    }
     
     public static synchronized void stop() {
         started = false;
-        
-        logWriter.close();
+        stopFileLog();
+        stopConsoleLog();
+    }
+
+    private static final void stopConsoleLog() {
+        // can happen if log manager haven`t started yet - dump to console everything cached
+        if (!logCache.isEmpty()) {
+            initializeConsoleLogging();
+            if (logToConsole) {
+                for (String string : logCache) {
+                    try {
+                        write(string);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                logCache.clear();
+            }
+        }
+    }
+
+    private static synchronized void stopFileLog() {
+        if (logWriter != null) {
+            logWriter.close();
+            logWriter = null;
+        }
     }
     
     public static synchronized void indent() {
@@ -139,6 +171,7 @@ public final class LogManager {
                     } else {
                         logCache.add(string);
                     }
+		    //System.out.println(message);
                 }
             } catch (IOException e) {
                 logWriter = null;
@@ -242,6 +275,8 @@ public final class LogManager {
             "nbi.utils.log.level"; // NOI18N
     public static final String LOG_TO_CONSOLE_PROPERTY =
             "nbi.utils.log.to.console"; // NOI18N
+    public static final String LOG_FILE_PROPERTY =
+            "nbi.utils.log.file"; // NOI18N
     
     public static final String INDENT =
             "    "; // NOI18N
