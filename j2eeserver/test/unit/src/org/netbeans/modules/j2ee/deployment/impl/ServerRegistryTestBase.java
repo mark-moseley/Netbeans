07cac2a9dc18e49ca7ee0b128ad361b70113d021
phejl@netbeans.org <none@none>
2009-06-08 16:47:11 +0200

    J2eeserver tests fixed.

--- j2eeserver/test/unit/src/org/netbeans/modules/j2ee/deployment/impl/ServerRegistryTestBase.java
+++ j2eeserver/test/unit/src/org/netbeans/modules/j2ee/deployment/impl/ServerRegistryTestBase.java
@@ -41,105 +41,105 @@
 
 package org.netbeans.modules.j2ee.deployment.impl;
 
-import junit.framework.*;
-import org.netbeans.junit.*;
-import org.openide.filesystems.*;
-import org.openide.util.Lookup;
-import org.openide.util.lookup.InstanceContent;
-import org.openide.util.lookup.Lookups;
-import org.openide.util.lookup.ProxyLookup;
-import java.util.*;
-import java.io.*;
-import java.util.logging.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import org.netbeans.junit.NbTestCase;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.test.MockLookup;
 
 /**
  *
  * @author Pavel Buzek
  */
 public class ServerRegistryTestBase extends NbTestCase {
-    static {
-        System.setProperty("org.openide.util.Lookup", Lkp.class.getName());
-    }
-    
+
     protected ServerRegistryTestBase (String name) {
         super (name);
     }
-    
-    public static final class Lkp extends ProxyLookup {
-        public Lkp() {
-            super(new Lookup[] {
-                Lookups.fixed(new Object[] {"repo"}, new Conv()),
-                Lookups.metaInfServices(Lkp.class.getClassLoader()),
-                Lookups.singleton(Lkp.class.getClassLoader()),
-            });
-        }
-        private static final class Conv implements InstanceContent.Convertor {
-            public Conv() {}
-            public Object convert(Object obj) {
-                assert obj == "repo";
-                try {
-                    return new Repo();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            public String displayName(Object obj) {
-                return obj.toString();
-            }
-            public String id(Object obj) {
-                return obj.toString();
-            }
-            public Class type(Object obj) {
-                assert obj == "repo";
-                return Repository.class;
-            }
-        }
-    }
+
     private File scratchF;
-    
+
     private void mkdir(String path) {
 //        System.out.println ("mkdir:"+path);
         new File(scratchF, path.replace('/', File.separatorChar)).mkdirs();
     }
+
+    @Override
     protected boolean runInEQ() {
         return true;
     }
+
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
-        clearWorkDir();
-        scratchF = getWorkDir();
-        mkdir("system/J2EE/InstalledServers");
-        mkdir("system/J2EE/DeploymentPlugins");
-        System.setProperty("SYSTEMDIR", new File(scratchF, "system").getAbsolutePath());
-        FileObject sfs = Repository.getDefault().getDefaultFileSystem().getRoot();
+        MockLookup.setLayersAndInstances();
+
+        FileObject sfs = FileUtil.getConfigRoot();
         assertNotNull("no default FS", sfs);
         FileObject j2eeFolder = sfs.getFileObject("J2EE");
         assertNotNull("have J2EE", j2eeFolder);
-//        Enumeration enumDest = sfs.getChildren (true);
-//        while (enumDest.hasMoreElements()) {
-//            FileObject f = (FileObject) enumDest.nextElement();
-//            System.out.println ("    dest file:" + f.getPath ());
-//        }
     }
-    
-     private static final class Repo extends Repository {
-        
-        public Repo() throws Exception {
-            super(mksystem());
+
+    protected static File getProjectAsFile(NbTestCase test, String projectFolderName) throws IOException {
+        File f = new File(test.getDataDir(), projectFolderName);
+        if (!f.exists()) {
+            // maybe it's zipped
+            File archive = new File(test.getDataDir(), projectFolderName + ".zip");
+            if (archive.exists() && archive.isFile()) {
+                unZip(archive, test.getWorkDir());
+                f = new File(test.getWorkDir(), projectFolderName);
+            }
         }
-        
-        private static FileSystem mksystem() throws Exception {
-            LocalFileSystem lfs = new LocalFileSystem();
-            lfs.setRootDirectory(new File(System.getProperty("SYSTEMDIR")));
-            java.net.URL layerFile = Repo.class.getClassLoader().getResource ("org/netbeans/tests/j2eeserver/plugin/layer.xml");
-            assert layerFile != null;
-            XMLFileSystem layer = new XMLFileSystem (layerFile);
-            FileSystem layers [] = new FileSystem [] {lfs, layer};
-            MultiFileSystem mfs = new MultiFileSystem (layers);
-            return mfs;
-        }
-        
+        NbTestCase.assertTrue("project directory has to exists: " + f, f.exists());
+        FileUtil.refreshFor(test.getDataDir());
+        return f;
     }
-   
+
+    private static void unZip(File archive, File destination) throws IOException {
+        if (!archive.exists()) {
+            throw new FileNotFoundException(archive + " does not exist.");
+        }
+        ZipFile zipFile = new ZipFile(archive);
+        Enumeration<? extends ZipEntry> all = zipFile.entries();
+        while (all.hasMoreElements()) {
+            extractFile(zipFile, all.nextElement(), destination);
+        }
+    }
+
+    private static void extractFile(ZipFile zipFile, ZipEntry e, File destination) throws IOException {
+        String zipName = e.getName();
+        if (zipName.startsWith("/")) {
+            zipName = zipName.substring(1);
+        }
+        if (zipName.endsWith("/")) {
+            return;
+        }
+        int ix = zipName.lastIndexOf('/');
+        if (ix > 0) {
+            String dirName = zipName.substring(0, ix);
+            File d = new File(destination, dirName);
+            if (!(d.exists() && d.isDirectory())) {
+                if (!d.mkdirs()) {
+                    NbTestCase.fail("Warning: unable to mkdir " + dirName);
+                }
+            }
+        }
+        FileOutputStream os = new FileOutputStream(destination.getAbsolutePath() + "/" + zipName);
+        InputStream is = zipFile.getInputStream(e);
+        int n = 0;
+        byte[] buff = new byte[8192];
+        while ((n = is.read(buff)) > 0) {
+            os.write(buff, 0, n);
+        }
+        is.close();
+        os.close();
+    }
+
 }
