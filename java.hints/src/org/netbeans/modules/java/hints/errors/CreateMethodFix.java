12b0ce69956b1ab8a9a5a37eab65f7b69509e75a
Max Sauer <msauer@netbeans.org>
2009-06-08 16:25:37 +0200

    #166139: Poor variable generation for enums

--- java.hints/src/org/netbeans/modules/java/hints/errors/CreateMethodFix.java
+++ java.hints/src/org/netbeans/modules/java/hints/errors/CreateMethodFix.java
@@ -30,9 +30,11 @@ package org.netbeans.modules.java.hints.errors;
 import com.sun.source.tree.BlockTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.Tree;
 import com.sun.source.tree.TypeParameterTree;
 import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
@@ -44,8 +46,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
+import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
@@ -53,6 +58,7 @@ import org.netbeans.api.java.source.Task;
 import org.netbeans.api.java.source.ClasspathInfo;
 import org.netbeans.api.java.source.CompilationInfo;
 import org.netbeans.api.java.source.ElementHandle;
+import org.netbeans.api.java.source.GeneratorUtilities;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.JavaSource.Phase;
 import org.netbeans.api.java.source.ModificationResult;
@@ -60,7 +66,6 @@ import org.netbeans.api.java.source.SourceUtils;
 import org.netbeans.api.java.source.TreeMaker;
 import org.netbeans.api.java.source.TypeMirrorHandle;
 import org.netbeans.api.java.source.WorkingCopy;
-import org.netbeans.modules.java.editor.codegen.GeneratorUtils;
 import org.netbeans.modules.java.hints.infrastructure.ErrorHintsProvider;
 import org.netbeans.spi.editor.hints.ChangeInfo;
 import org.netbeans.spi.editor.hints.Fix;
@@ -87,7 +92,18 @@ public final class CreateMethodFix implements Fix {
     
     public CreateMethodFix(CompilationInfo info, String name, Set<Modifier> modifiers, TypeElement target, TypeMirror returnType, List<? extends TypeMirror> argumentTypes, List<String> argumentNames, FileObject targetFile) {
         this.name = name;
-        this.inFQN = target.getQualifiedName().toString();
+        final Name qualifiedName = target.getQualifiedName(); //#130759
+        if (qualifiedName == null) {
+            this.inFQN = ""; //NOI18N
+            Logger.getLogger(CreateMethodFix.class.getName()).warning("Target qualified name could not be resolved."); //NOI18N
+        } else {
+            String qnString = qualifiedName.toString();
+            if (qnString.length() == 0) {
+                //probably an anonymous class
+                qnString = target.asType().toString();
+            }
+            this.inFQN = qnString;
+        }
         this.cpInfo = info.getClasspathInfo();
         this.modifiers = modifiers;
         this.targetFile = targetFile;
@@ -141,6 +157,8 @@ public final class CreateMethodFix implements Fix {
     public ChangeInfo implement() throws IOException {
         //use the original cp-info so it is "sure" that the proposedType can be resolved:
         JavaSource js = JavaSource.create(cpInfo, targetFile);
+        // tag used for selection
+        final String methodBodyTag = "mbody"; //NOI18N
         
         ModificationResult diff = js.runModificationTask(new Task<WorkingCopy>() {
             public void run(final WorkingCopy working) throws IOException {
@@ -175,20 +193,34 @@ public final class CreateMethodFix implements Fix {
                 
                 while (typeIt.hasNext() && nameIt.hasNext()) {
                     TypeMirrorHandle tmh = typeIt.next();
-                    String           argName = nameIt.next();
+                    TypeMirror tm = tmh.resolve(working);
+                    String argName;
+
+                    Element elem = working.getTypes().asElement(tm);
+                    if (elem != null && elem.getKind() == ElementKind.ENUM) {
+                        StringBuffer buf = new StringBuffer(elem.getSimpleName().toString());
+                        buf.setCharAt(0, Character.toLowerCase(buf.charAt(0)));
+                        argName = buf.toString();
+                    } else {
+                        argName = nameIt.next();
+                    }
                     
-                    argTypes.add(make.Variable(make.Modifiers(EnumSet.noneOf(Modifier.class)), argName, make.Type(tmh.resolve(working)), null));
+                    argTypes.add(make.Variable(make.Modifiers(EnumSet.noneOf(Modifier.class)), argName, make.Type(tm), null));
                 }
                 
                 BlockTree body = targetType.getKind().isClass() ? createDefaultMethodBody(working, returnType) : null;
-                MethodTree mt = make.Method(make.Modifiers(modifiers), name, returnType != null ? make.Type(returnType) : null, Collections.<TypeParameterTree>emptyList(), argTypes, Collections.<ExpressionTree>emptyList(), body, null);
-                ClassTree decl = GeneratorUtils.insertClassMember(working, targetTree, mt);
                 
+                if(body != null && !body.getStatements().isEmpty()) {
+                    working.tag(body.getStatements().get(0), methodBodyTag);
+                }
+                
+                MethodTree mt = make.Method(make.Modifiers(modifiers), name, returnType != null ? make.Type(returnType) : null, Collections.<TypeParameterTree>emptyList(), argTypes, Collections.<ExpressionTree>emptyList(), body, null);
+                ClassTree decl = GeneratorUtilities.get(working).insertClassMember((ClassTree)targetTree.getLeaf(), mt);
                 working.rewrite(targetTree.getLeaf(), decl);
             }
         });
         
-        return Utilities.commitAndComputeChangeInfo(targetFile, diff);
+        return Utilities.commitAndComputeChangeInfo(targetFile, diff, methodBodyTag);
     }
     
     private void addArguments(CompilationInfo info, StringBuilder value) {
@@ -245,6 +277,6 @@ public final class CreateMethodFix implements Fix {
         }
         return make.Block(blockStatements, false);
     }
-    
+
 }
 
--- java.hints/test/unit/src/org/netbeans/modules/java/hints/errors/CreateMethodTest.java
+++ java.hints/test/unit/src/org/netbeans/modules/java/hints/errors/CreateMethodTest.java
@@ -122,13 +122,12 @@ public class CreateMethodTest extends ErrorHintsTestBase {
                        "package test; import java.util.Collection; public class Test {public static void test() {fff(getStrings());} private static Collection<String> getStrings() {return null;}}",
                        116 - 25,
                        "CreateMethodFix:fff(java.util.Collection<java.lang.String> strings)void:test.Test",
-                       "package test; import java.util.Collection; public class Test {public static void test() {fff(getStrings());} private static void fff(Collection<String> strings) { throw new UnsupportedOperationException(\"Not yet implemented\"); } private static Collection<String> getStrings() {return null;}}");
+                       "package test; import java.util.Collection; public class Test {public static void test() {fff(getStrings());} private static Collection<String> getStrings() {return null;} private static void fff(Collection<String> strings) { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
     }
     
     public void testCreateMethod74129() throws Exception {
         performFixTest("test/Test.java",
-                       "package test; public class Test {public void test() {TopLevel.fff();}} class TopLevel {}",
-                       89 - 25,
+                       "package test; public class Test {public void test() {TopLevel.f|ff();}} class TopLevel {}",
                        "CreateMethodFix:fff()void:test.TopLevel",
                        "package test; public class Test {public void test() {TopLevel.fff();}} class TopLevel { static void fff() { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
     }
@@ -138,7 +137,7 @@ public class CreateMethodTest extends ErrorHintsTestBase {
                        "package test; public class Test {public static class T extends Test {public void test() {super.fff();}}}",
                        122 - 25,
                        "CreateMethodFix:fff()void:test.Test",
-                       "package test; public class Test {public static class T extends Test {public void test() {super.fff();}} private void fff() { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
+                       "package test; public class Test { private void fff() { throw new UnsupportedOperationException(\"Not yet implemented\"); } public static class T extends Test {public void test() {super.fff();}}}");
     }
     
     public void testCreateMethod75069() throws Exception {
@@ -154,6 +153,20 @@ public class CreateMethodTest extends ErrorHintsTestBase {
                        "package test; public class Test {static {f|ff();}}",
                        "CreateMethodFix:fff()void:test.Test",
                        "package test; public class Test {static {fff();} private static void fff() { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
+    }
+
+    public void testCreateMethodWithAnonymousParameter104820() throws Exception {
+        performFixTest("test/Test.java",
+                       "package test;public class Test {public static void method() {final Test ac = new Test();new Runnable() {public void run() {ac.a|ction(this);}};}}",
+                       "CreateMethodFix:action(java.lang.Runnable aThis)void:test.Test",
+                       "package test;public class Test {public static void method() {final Test ac = new Test();new Runnable() {public void run() {ac.action(this);}};} private void action(Runnable aThis) { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
+    }
+
+    public void testCreateMethodWithEnumParam() throws Exception {
+        performFixTest("test/Test.java",
+                       "package test; public class Test { enum Paddle{UP, DOWN} public void foo() {f|ff(Paddle.UP);}}",
+                       "CreateMethodFix:fff(test.Test.Paddle UP)void:test.Test",
+                       "package test; public class Test { private void fff(Paddle paddle) { throw new UnsupportedOperationException(\"Not yet implemented\"); } enum Paddle{UP, DOWN} public void foo() {fff(Paddle.UP);}}");
     }
     
     protected List<Fix> computeFixes(CompilationInfo info, int pos, TreePath path) throws IOException {
