4fe04684076065b0ca6ade9fc7b71fdb0e94ab9d
Alexander Simon <alexvsimon@netbeans.org>
2009-06-10 20:05:34 +0400

    improve discovery: support qmake and cmake scripts

--- cnd.discovery/test/unit/src/org/netbeans/modules/cnd/discovery/project/MakeProjectBase.java
+++ cnd.discovery/test/unit/src/org/netbeans/modules/cnd/discovery/project/MakeProjectBase.java
@@ -41,7 +41,10 @@ package org.netbeans.modules.cnd.discovery.project;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -51,39 +54,58 @@ import org.netbeans.junit.MockServices;
 import org.netbeans.junit.NbTestCase;
 import org.netbeans.modules.cnd.api.execution.ExecutionListener;
 import org.netbeans.modules.cnd.api.execution.NativeExecutor;
+import org.netbeans.modules.cnd.api.model.CsmFile;
+import org.netbeans.modules.cnd.api.model.CsmInclude;
 import org.netbeans.modules.cnd.api.model.CsmModel;
 import org.netbeans.modules.cnd.api.model.CsmModelAccessor;
 import org.netbeans.modules.cnd.api.model.CsmProject;
 import org.netbeans.modules.cnd.api.project.NativeProject;
+import org.netbeans.modules.cnd.api.utils.Path;
 import org.netbeans.modules.cnd.discovery.projectimport.ImportProject;
 import org.netbeans.modules.cnd.makeproject.MakeProjectType;
 import org.netbeans.modules.cnd.modelimpl.csm.core.ModelImpl;
 import org.netbeans.modules.cnd.modelimpl.repository.RepositoryUtils;
+import org.netbeans.modules.cnd.test.CndCoreTestUtils;
 import org.openide.WizardDescriptor;
 import org.openide.util.Cancellable;
 import org.openide.util.Exceptions;
 import org.openide.util.RequestProcessor.Task;
+import org.openide.util.Utilities;
 
 /**
  *
  * @author Alexander Simon
  */
-public abstract class MakeProjectBase  extends NbTestCase {
+public abstract class MakeProjectBase extends NbTestCase { //BaseTestCase {
     private static final boolean OPTIMIZE_NATIVE_EXECUTIONS =true;
+    private static final boolean TRACE = true;
 
     public MakeProjectBase(String name) {
         super(name);
+        if (TRACE) {
+            System.setProperty("cnd.discovery.trace.projectimport", "true"); // NOI18N
+        }
         System.setProperty("org.netbeans.modules.cnd.makeproject.api.runprofiles", "true"); // NOI18N
         System.setProperty("cnd.mode.unittest", "true");
+        System.setProperty("org.netbeans.modules.cnd.apt.level","OFF"); // NOI18N
         Logger.getLogger("org.netbeans.modules.editor.settings.storage.Utils").setLevel(Level.SEVERE);
+        MockServices.setServices(MakeProjectType.class);
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        MockServices.setServices(MakeProjectType.class);
+        //MockServices.setServices(MakeProjectType.class);
         startupModel();
     }
+
+//    @Override
+//    protected List<Class> getServises() {
+//        List<Class> list = new ArrayList<Class>();
+//        list.add(MakeProjectType.class);
+//        list.addAll(super.getServises());
+//        return list;
+//    }
 
     private void startupModel() {
         ModelImpl model = (ModelImpl) CsmModelAccessor.getModel();
@@ -116,10 +138,37 @@ public abstract class MakeProjectBase  extends NbTestCase {
         }
     }
 
+    private File detectConfigure(String path){
+        File configure = new File(path+File.separator+"configure");
+        if (configure.exists()) {
+            return configure;
+        }
+        configure = new File(path+File.separator+"CMakeLists.txt");
+        if (configure.exists()) {
+            return configure;
+        }
+        File base = new File(path);
+        File[] files = base.listFiles();
+        if (files != null){
+            for(File file : files) {
+                if (file.getAbsolutePath().endsWith(".pro")){
+                    return file;
+                }
+            }
+        }
+        return new File(path+File.separator+"configure");
+    }
+
     public void performTestProject(String URL, List<String> additionalScripts){
+        Map<String, String> tools = findTools();
+        if (tools == null) {
+            System.err.println("Test did not run because required tools do not found");
+            return;
+        }
         try {
-            final String path = download(URL, additionalScripts);
-            final File configure = new File(path+File.separator+"configure");
+            final String path = download(URL, additionalScripts, tools);
+
+            final File configure = detectConfigure(path);
             final File makeFile = new File(path+File.separator+"Makefile");
             if (!configure.exists()) {
                 if (!makeFile.exists()){
@@ -134,15 +183,25 @@ public abstract class MakeProjectBase  extends NbTestCase {
                     } else if ("path".equals(name)) {
                         return path;
                     } else if ("configureName".equals(name)) {
-                        if (OPTIMIZE_NATIVE_EXECUTIONS && makeFile.exists()) {
+                        if (OPTIMIZE_NATIVE_EXECUTIONS && makeFile.exists() && !configure.getAbsolutePath().endsWith("CMakeLists.txt")) {
                             // optimization on developer computer:
                             // run configure only once
                             return null;
                         } else {
-                            return path+"/configure";
+                            return configure.getAbsolutePath();
                         }
                     } else if ("realFlags".equals(name)) {
-                        return "CFLAGS=\"-g3 -gdwarf-2\" CXXFLAGS=\"-g3 -gdwarf-2\"";
+                        if (path.indexOf("cmake-")>0) {
+                            return "CFLAGS=\"-g3 -gdwarf-2\" CXXFLAGS=\"-g3 -gdwarf-2\" CMAKE_BUILD_TYPE=Debug CMAKE_CXX_FLAGS_DEBUG=\"-g3 -gdwarf-2\" CMAKE_C_FLAGS_DEBUG=\"-g3 -gdwarf-2\"";
+                        } else {
+                            if (configure.getAbsolutePath().endsWith("configure")) {
+                                return "CFLAGS=\"-g3 -gdwarf-2\" CXXFLAGS=\"-g3 -gdwarf-2\"";
+                            } else if (configure.getAbsolutePath().endsWith("CMakeLists.txt")) {
+                                return "-G \"Unix Makefiles\" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG=\"-g3 -gdwarf-2\" -DCMAKE_C_FLAGS_DEBUG=\"-g3 -gdwarf-2\"";
+                            } else if (configure.getAbsolutePath().endsWith(".pro")) {
+                                return "QMAKE_CFLAGS=\"-g3 -gdwarf-2\" QMAKE_CXXFLAGS=\"-g3 -gdwarf-2\"";
+                            }
+                        }
                     } else if ("buildProject".equals(name)) {
                         if (OPTIMIZE_NATIVE_EXECUTIONS && makeFile.exists() && findObjectFiles(path)) {
                             // optimization on developer computer:
@@ -155,16 +214,22 @@ public abstract class MakeProjectBase  extends NbTestCase {
                     return null;
                 }
             };
+
             ImportProject importer = new ImportProject(wizard);
             importer.setUILessMode();
             importer.create();
             OpenProjects.getDefault().open(new Project[]{importer.getProject()}, false);
+            int i = 0;
             while(!importer.isFinished()) {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException ex) {
                     Exceptions.printStackTrace(ex);
                 }
+                if (i > 10 && !OpenProjects.getDefault().isProjectOpen(importer.getProject())){
+                    break;
+                }
+                i++;
             }
             CsmModel model = CsmModelAccessor.getModel();
             Project makeProject = importer.getProject();
@@ -186,7 +251,7 @@ public abstract class MakeProjectBase  extends NbTestCase {
     private boolean findObjectFiles(String path){
         return findObjectFiles(new File(path));
     }
-    
+
     private boolean findObjectFiles(File file){
         if (file.isDirectory()) {
             for(File f : file.listFiles()){
@@ -203,33 +268,84 @@ public abstract class MakeProjectBase  extends NbTestCase {
         return false;
     }
 
-    abstract void perform(CsmProject csmProject);
+    protected List<String> requiredTools(){
+        List<String> list = new ArrayList<String>();
+        list.add("wget");
+        list.add("gzip");
+        list.add("tar");
+        list.add("rm");
+        return list;
+    }
 
-    private String download(String urlName, List<String> additionalScripts) throws IOException {
+    private Map<String, String> findTools(){
+        Map<String, String> map = new HashMap<String, String>();
+        for(String t: requiredTools()){
+            map.put(t, null);
+        }
+        if (findTools(map)){
+            return map;
+        }
+        return null;
+    }
+
+    private boolean findTools(Map<String, String> map){
+        if (map.isEmpty()) {
+            return true;
+        }
+        ArrayList<String> list = new ArrayList<String>(Path.getPath());
+        //String additionalPath = CndCoreTestUtils.getDownloadBase().getAbsolutePath()+File.separatorChar+"cmake-2.6.4/bin";
+        //list.add(additionalPath);
+        for (String path : list) {
+            for(Map.Entry<String, String> entry : map.entrySet()){
+                if (entry.getValue() == null) {
+                    String task = path+File.separatorChar+entry.getKey();
+                    File tool = new File(task);
+                    if (tool.exists() && tool.isFile()) {
+                        entry.setValue(task);
+                    } else if (Utilities.isWindows()) {
+                        task = task+".exe";
+                        tool = new File(task);
+                        if (tool.exists() && tool.isFile()) {
+                            entry.setValue(task);
+                        }   
+                    }
+                }
+            }
+        }
+        boolean res = true;
+        for(Map.Entry<String, String> entry : map.entrySet()){
+           if (entry.getValue() == null) {
+               System.err.println("Not found required tool: "+entry.getKey());
+               res =false;
+           } else {
+               System.err.println("Found required tool: "+entry.getKey()+"="+entry.getValue());
+           }
+        }
+        return res;
+    }
+
+    protected void perform(CsmProject csmProject) {
+        if (TRACE) {
+            System.err.println("Model content:");
+        }
+        csmProject.waitParse();
+        for (CsmFile file : csmProject.getAllFiles()) {
+            if (TRACE) {
+                System.err.println("\t"+file.getAbsolutePath());
+            }
+            for(CsmInclude include : file.getIncludes()){
+                assertTrue("Not resolved include directive "+include.getIncludeName()+" in file "+file.getAbsolutePath(), include.getIncludeFile() != null);
+            }
+        }
+    }
+
+    private String download(String urlName, List<String> additionalScripts, Map<String, String> tools) throws IOException {
         String zipName = urlName.substring(urlName.lastIndexOf('/')+1);
         String tarName = zipName.substring(0, zipName.lastIndexOf('.'));
         String packageName = tarName.substring(0, tarName.lastIndexOf('.'));
+        File fileDataPath = CndCoreTestUtils.getDownloadBase();
+        String dataPath = fileDataPath.getAbsolutePath();
 
-        String dataPath;
-        if (false) {
-            // local downloads
-            dataPath = getDataDir().getAbsolutePath();
-            if (dataPath.endsWith("/data") || dataPath.endsWith("\\data")) {
-                dataPath = dataPath.substring(0, dataPath.length()-4)+"downloads";
-            }
-        } else {
-            // downloads in tmp dir
-            dataPath = System.getProperty("java.io.tmpdir");
-            if (dataPath.endsWith(File.separator)) {
-                dataPath += System.getProperty("user.name") +  "-cnd-test-downloads";
-            } else {
-                dataPath += File.separator + System.getProperty("user.name") +  "-cnd-test-downloads";
-            }
-        }
-        File fileDataPath = new File(dataPath);
-        if (!fileDataPath.exists()) {
-            fileDataPath.mkdirs();
-        }
         String createdFolder = dataPath+"/"+packageName;
         final AtomicBoolean finish = new AtomicBoolean(false);
         ExecutionListener listener = new ExecutionListener() {
@@ -244,26 +360,56 @@ public abstract class MakeProjectBase  extends NbTestCase {
         if (!fileCreatedFolder.exists()){
             fileCreatedFolder.mkdirs();
         }
+        String command;
         if (fileCreatedFolder.list().length == 0){
-            ne = new NativeExecutor(dataPath,"wget", urlName, new String[0], "wget", "run", false, false);
+            if (!new File(dataPath+"/"+tarName).exists()) {
+                command = tools.get("wget");
+                System.err.println(dataPath+"#"+command+" "+urlName);
+                ne = new NativeExecutor(dataPath, command, urlName, new String[0], "wget", "run", false, false);
+                waitExecution(ne, listener, finish);
+
+                command = tools.get("gzip");
+                System.err.println(dataPath+"#"+command+" -d "+zipName);
+                ne = new NativeExecutor(dataPath, command, "-d "+zipName, new String[0], "gzip", "run", false, false);
+                waitExecution(ne, listener, finish);
+            }
+
+            command = tools.get("tar");
+            System.err.println(dataPath+"#"+command+" xf "+tarName);
+            ne = new NativeExecutor(dataPath, command, "xf "+tarName, new String[0], "tar", "run", false, false);
             waitExecution(ne, listener, finish);
-            ne = new NativeExecutor(dataPath,"gzip", "-d "+zipName, new String[0], "gzip", "run", false, false);
-            waitExecution(ne, listener, finish);
-            ne = new NativeExecutor(dataPath,"tar", "xf "+tarName, new String[0], "tar", "run", false, false);
-            waitExecution(ne, listener, finish);
-            if (additionalScripts != null) {
-                for(String s: additionalScripts){
-                    int i = s.indexOf(' ');
-                    String command = s.substring(0,i);
-                    String arguments = s.substring(i+1);
-                    ne = new NativeExecutor(dataPath,command, arguments, new String[0], command, "run", false, false);
-                    waitExecution(ne, listener, finish);
+
+            execAdditionalScripts(finish, listener, createdFolder, additionalScripts, tools);
+        } else {
+            final File configure = new File(createdFolder+File.separator+"configure");
+            final File makeFile = detectConfigure(createdFolder);
+            if (!configure.exists()) {
+                if (!makeFile.exists()){
+                    execAdditionalScripts(finish, listener, createdFolder, additionalScripts, tools);
                 }
             }
         }
-        ne = new NativeExecutor(createdFolder, "rm", "-rf nbproject", new String[0], "rm", "run", false, false);
+
+        //System.getenv().put("CFLAGS", "-g3 -gdwarf-2");
+        //System.getenv().put("CXXFLAGS", "-g3 -gdwarf-2");
+
+        command = tools.get("rm");
+        System.err.println(createdFolder+"#"+command+" -rf nbproject");
+        ne = new NativeExecutor(createdFolder, tools.get("rm"), "-rf nbproject", new String[0], "rm", "run", false, false);
         waitExecution(ne, listener, finish);
         return createdFolder;
+    }
+    private void execAdditionalScripts(final AtomicBoolean finish, ExecutionListener listener, String createdFolder, List<String> additionalScripts, Map<String, String> tools) throws IOException {
+        if (additionalScripts != null) {
+            for(String s: additionalScripts){
+                int i = s.indexOf(' ');
+                String command = s.substring(0,i);
+                String arguments = s.substring(i+1);
+                System.err.println(createdFolder+"#"+tools.get(command)+" "+arguments);
+                NativeExecutor ne = new NativeExecutor(createdFolder, tools.get(command), arguments, new String[0], command, "run", false, false);
+                waitExecution(ne, listener, finish);
+            }
+        }
     }
 
     private void waitExecution(NativeExecutor ne, ExecutionListener listener, AtomicBoolean finish){
