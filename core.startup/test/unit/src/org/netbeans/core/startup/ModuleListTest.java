1191234518650e3b6b4a2a263d3a1317d0f57b67
Jaroslav Tulach <jtulach@netbeans.org>
2009-06-11 10:55:51 +0200

    Adjusting the structure of the test to have ready $userdir/config/Modules directory. This is require for the Stamps infra to find some file and uset its lastModified() time

--- core.startup/test/unit/src/org/netbeans/core/startup/ModuleListTest.java
+++ core.startup/test/unit/src/org/netbeans/core/startup/ModuleListTest.java
@@ -41,20 +41,34 @@
 
 package org.netbeans.core.startup;
 
-//import junit.framework.*;
+import org.netbeans.SetupHid;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import org.netbeans.LoggedPCListener;
+import org.netbeans.MockModuleInstaller;
+import org.netbeans.MockEvents;
 import org.netbeans.Module;
 import org.netbeans.ModuleManager;
-import org.netbeans.junit.*;
-import java.util.*;
-import org.openide.util.*;
-import org.openide.modules.*;
-import java.io.*;
-import java.net.URI;
 import java.util.logging.Level;
 import org.netbeans.Stamps;
-import org.openide.filesystems.*;
-import org.openide.util.lookup.Lookups;
-import org.openide.util.lookup.ProxyLookup;
+import org.netbeans.junit.Log;
+import org.openide.filesystems.FileAttributeEvent;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileRenameEvent;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.LocalFileSystem;
+import org.openide.modules.InstalledFileLocator;
+import org.openide.util.test.MockLookup;
 
 /** Test the functions of the module list, i.e. finding modules on
  * disk and installing them, and writing out state as needed.
@@ -62,26 +76,16 @@ import org.openide.util.lookup.ProxyLookup;
  */
 public class ModuleListTest extends SetupHid {
     
-    static {
-        System.setProperty("org.openide.util.Lookup", L.class.getName());
-    }
     private File ud;
-    public static final class L extends ProxyLookup {
-        public L() {
-            super(new Lookup[] {
-                Lookups.singleton(new IFL()),
-            });
-        }
-    }
     
-    private static final File JARS = new File(URI.create(ModuleListTest.class.getResource("jars").toExternalForm()));
     private static final String PREFIX = "wherever/";
+    private LocalFileSystem fs;
     
-    private static final class IFL extends InstalledFileLocator {
+    private final class IFL extends InstalledFileLocator {
         public IFL() {}
         public File locate(String relativePath, String codeNameBase, boolean localized) {
             if (relativePath.startsWith(PREFIX)) {
-                File f = new File(JARS, relativePath.substring(PREFIX.length()).replace('/', File.separatorChar));
+                File f = new File(jars, relativePath.substring(PREFIX.length()).replace('/', File.separatorChar));
                 if (f.exists()) {
                     return f;
                 }
@@ -100,19 +104,20 @@ public class ModuleListTest extends SetupHid {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        clearWorkDir();
-        
+
+        MockLookup.setInstances(new IFL());
+
         ud = new File(getWorkDir(), "ud");
         ud.mkdirs();
         System.setProperty("netbeans.user", ud.getPath());
         
-        FakeModuleInstaller installer = new FakeModuleInstaller();
-        FakeEvents ev = new FakeEvents();
+        MockModuleInstaller installer = new MockModuleInstaller();
+        MockEvents ev = new MockEvents();
         mgr = new ModuleManager(installer, ev);
-        File dir = getWorkDir();
+        File dir = new File(ud, "config");
         File modulesdir = new File(dir, "Modules");
-        if (! modulesdir.mkdir()) throw new IOException("Making " + modulesdir);
-        LocalFileSystem fs = new LocalFileSystem();
+        if (! modulesdir.mkdirs()) throw new IOException("Making " + modulesdir);
+        fs = new LocalFileSystem();
         fs.setRootDirectory(dir);
         modulesfolder = fs.findResource("Modules");
         assertNotNull(modulesfolder);
@@ -120,7 +125,7 @@ public class ModuleListTest extends SetupHid {
     }
     
     private Module makeModule(String jarName) throws Exception {
-        File f = new File(JARS, jarName);
+        File f = new File(jars, jarName);
         Module m = mgr.create(f, new ModuleHistory(PREFIX + jarName), false, false, false);
         return m;
     }
@@ -133,8 +138,8 @@ public class ModuleListTest extends SetupHid {
         mgr.mutexPrivileged().enterWriteAccess();
         try {
             // XXX try to read an actual initial list too...
-            assertEquals(Collections.EMPTY_SET, list.readInitial());
-            Set modules = new HashSet();
+            assertEquals(Collections.emptySet(), list.readInitial());
+            Set<Module> modules = new HashSet<Module>();
             modules.add(makeModule("simple-module.jar"));
             modules.add(makeModule("depends-on-simple-module.jar"));
             list.trigger(modules);
@@ -155,8 +160,8 @@ public class ModuleListTest extends SetupHid {
             foo = xml[1];
             bar = xml[0];
         }
-        assertFile(FileUtil.toFile(foo), new File(JARS, "org-foo.xml"));
-        assertFile(FileUtil.toFile(bar), new File(JARS, "org-bar.xml"));
+        assertFile(FileUtil.toFile(foo), new File(data, "org-foo.xml"));
+        assertFile(FileUtil.toFile(bar), new File(data, "org-bar.xml"));
         // Checking that changes in memory will rewrite XML:
         LoggedFileListener listener = new LoggedFileListener();
         modulesfolder.addFileChangeListener(listener);
@@ -166,22 +171,22 @@ public class ModuleListTest extends SetupHid {
             assertNotNull(m1);
             Module m2 = mgr.get("org.bar");
             assertNotNull(m2);
-            mgr.disable(new HashSet(Arrays.asList(new Module[] {m1, m2})));
+            mgr.disable(new HashSet<Module>(Arrays.asList(m1, m2)));
         } finally {
             mgr.mutexPrivileged().exitWriteAccess();
         }
         // We expect it to have marked both as disabled now:
         listener.waitForChange("Modules/org-foo.xml");
         listener.waitForChange("Modules/org-bar.xml");
-        assertFile(new File(JARS, "org-foo_disabled.xml"), FileUtil.toFile(foo));
-        assertFile(new File(JARS, "org-bar_disabled.xml"), FileUtil.toFile(bar));
+        assertFile(new File(data, "org-foo_disabled.xml"), FileUtil.toFile(foo));
+        assertFile(new File(data, "org-bar_disabled.xml"), FileUtil.toFile(bar));
         // Check that changes in disk are parsed and applied (#13921)
         LoggedPCListener listener2 = new LoggedPCListener();
         Module m1 = mgr.get("org.foo");
         m1.addPropertyChangeListener(listener2);
-        copy(new File(JARS, "org-foo.xml"), foo);
+        copy(new File(data, "org-foo.xml"), foo);
         /* Does not seem to refresh reliably enough:
-        copy(new File(JARS, "org-foo.xml"), FileUtil.toFile(foo));
+        copy(new File(data, "org-foo.xml"), FileUtil.toFile(foo));
         foo.refresh();
          */
         // The change ought to be noticed by filesystems, picked up by
@@ -199,15 +204,13 @@ public class ModuleListTest extends SetupHid {
         File f = new File(new File(new File(System.getProperty("netbeans.user"), "var"), "cache"), "all-modules.dat");
         assertTrue("Cache exists", f.exists());
 
-        LocalFileSystem lfs = new LocalFileSystem();
-        lfs.setRootDirectory(getWorkDir());
-        FileObject mf = lfs.findResource(modulesfolder.getPath());
+        FileObject mf = fs.findResource(modulesfolder.getPath());
         assertNotNull("config folder exits", mf);
         
-        CountingSecurityManager.initialize(new File(lfs.getRootDirectory(), "Modules").getPath());
+        CountingSecurityManager.initialize(new File(fs.getRootDirectory(), "Modules").getPath());
         
-        FakeModuleInstaller installer = new FakeModuleInstaller();
-        FakeEvents ev = new FakeEvents();
+        MockModuleInstaller installer = new MockModuleInstaller();
+        MockEvents ev = new MockEvents();
         ModuleManager mgr2 = new ModuleManager(installer, ev);
         assertNotNull(mf);
         ModuleList list2 = new ModuleList(mgr2, mf, ev);
@@ -246,10 +249,10 @@ public class ModuleListTest extends SetupHid {
     public void testAddNewModuleViaXML() throws Exception {
         mgr.mutexPrivileged().enterWriteAccess();
         try {
-            assertEquals(Collections.EMPTY_SET, list.readInitial());
-            assertEquals(Collections.EMPTY_SET, mgr.getModules());
-            list.trigger(Collections.EMPTY_SET);
-            assertEquals(Collections.EMPTY_SET, mgr.getModules());
+            assertEquals(Collections.emptySet(), list.readInitial());
+            assertEquals(Collections.emptySet(), mgr.getModules());
+            list.trigger(Collections.<Module>emptySet());
+            assertEquals(Collections.emptySet(), mgr.getModules());
         } finally {
             mgr.mutexPrivileged().exitWriteAccess();
         }
@@ -259,7 +262,7 @@ public class ModuleListTest extends SetupHid {
             public void run() throws IOException {
                 // XXX this will require that there be an appropriate InstalledFileLocator in Lookup
                 FileObject fooxml = modulesfolder.createData("org-foo", "xml");
-                copy(new File(JARS, "org-foo.xml"), fooxml);
+                copy(new File(data, "org-foo.xml"), fooxml);
             }
         });
         assertTrue("PROP_MODULES fired", listener.waitForChange(mgr, ModuleManager.PROP_MODULES));
@@ -282,4 +285,68 @@ public class ModuleListTest extends SetupHid {
     // and check that layer-driven events are enough to cause
     // complex installations & uninstallations
     
+    private static void copy(File a, FileObject b) throws IOException {
+        OutputStream os = b.getOutputStream();
+        try {
+            copyStreams(new FileInputStream(a), os);
+        } finally {
+            os.close();
+        }
+    }
+
+    private static class LoggedFileListener implements FileChangeListener {
+
+        /** names of files that have changed: */
+        private final Set<String> files = new HashSet<String>(100);
+
+        private synchronized void change(FileEvent ev) {
+            files.add(ev.getFile().getPath());
+            notify();
+        }
+
+        public synchronized void waitForChanges() throws InterruptedException {
+            wait(5000);
+        }
+
+        public synchronized boolean hasChange(String fname) {
+            return files.contains(fname);
+        }
+
+        public synchronized boolean waitForChange(String fname) throws InterruptedException {
+            while (!hasChange(fname)) {
+                long start = System.currentTimeMillis();
+                waitForChanges();
+                if (System.currentTimeMillis() - start > 4000) {
+                    //System.err.println("changes=" + changes);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void fileDeleted(FileEvent fe) {
+            change(fe);
+        }
+
+        public void fileFolderCreated(FileEvent fe) {
+            change(fe);
+        }
+
+        public void fileDataCreated(FileEvent fe) {
+            change(fe);
+        }
+
+        public void fileAttributeChanged(FileAttributeEvent fe) {
+            // ignore?
+        }
+
+        public void fileRenamed(FileRenameEvent fe) {
+            change(fe);
+        }
+
+        public void fileChanged(FileEvent fe) {
+            change(fe);
+        }
+    }
+
 }
