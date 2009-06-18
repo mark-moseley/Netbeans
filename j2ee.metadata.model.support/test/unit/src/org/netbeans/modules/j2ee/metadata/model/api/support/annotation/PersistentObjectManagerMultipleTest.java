db4f472d4cded4f0e3499c9f2d6466b4da4b4ce4
pslechta@netbeans.org <none@none>
2009-06-18 11:22:01 +0200

    test fix

--- j2ee.metadata.model.support/test/unit/src/org/netbeans/modules/j2ee/metadata/model/api/support/annotation/PersistentObjectManagerMultipleTest.java
+++ j2ee.metadata.model.support/test/unit/src/org/netbeans/modules/j2ee/metadata/model/api/support/annotation/PersistentObjectManagerMultipleTest.java
@@ -60,7 +60,7 @@ import org.netbeans.api.java.source.TypesEvent;
 import org.netbeans.modules.j2ee.metadata.model.api.support.annotation.parser.AnnotationParser;
 import org.netbeans.modules.j2ee.metadata.model.support.PersistenceTestCase;
 import org.netbeans.modules.j2ee.metadata.model.support.TestUtilities;
-import org.netbeans.modules.java.source.usages.RepositoryUpdater;
+import org.netbeans.modules.parsing.api.indexing.IndexingManager;
 
 /**
  * Tests if PersistentObjectManager caches multiple objects based on
@@ -72,7 +72,7 @@ public class PersistentObjectManagerMultipleTest extends PersistenceTestCase {
 
     // XXX should refactor the waiting for added/changed/removed types to an utility class
 
-    private static final int EVENT_TIMEOUT = 10; // seconds
+    private static final int EVENT_TIMEOUT = 20; // seconds
 
     private PersistentObjectManager<ResourceImpl> manager;
 
@@ -81,7 +81,7 @@ public class PersistentObjectManagerMultipleTest extends PersistenceTestCase {
     }
 
     public void testChangedFiles() throws Exception {
-        RepositoryUpdater.getDefault().scheduleCompilationAndWait(srcFO, srcFO).await();
+        IndexingManager.getDefault().refreshIndexAndWait(srcFO.getURL(), null);
         ClasspathInfo cpi = ClasspathInfo.create(srcFO);
         final AnnotationModelHelper helper = AnnotationModelHelper.create(cpi);
         helper.runJavaSourceTask(new Runnable() {
