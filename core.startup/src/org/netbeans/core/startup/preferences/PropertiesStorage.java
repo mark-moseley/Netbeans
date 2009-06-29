83cc18fd2abb2eba213589dcc5aab37bd054ed83
Jesse Glick <jglick@netbeans.org>
2009-06-29 10:08:37 -0400

    #167745: recover from broken config/Preferences/**/*.properties files.

--- core.startup/src/org/netbeans/core/startup/preferences/PropertiesStorage.java
+++ core.startup/src/org/netbeans/core/startup/preferences/PropertiesStorage.java
@@ -47,14 +47,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem.AtomicAction;
 import org.openide.filesystems.FileUtil;
-import org.openide.filesystems.Repository;
+import org.openide.util.Exceptions;
 
 /**
  * No synchronization - must be called just from NbPreferences which
@@ -64,8 +66,7 @@ import org.openide.filesystems.Repository;
 class PropertiesStorage implements NbPreferences.FileStorage {
     private static final String USERROOT_PREFIX = "/Preferences";//NOI18N
     private static final String SYSTEMROOT_PREFIX = "/SystemPreferences";//NOI18N
-    private final static FileObject SFS_ROOT =
-            Repository.getDefault().getDefaultFileSystem().getRoot();
+    private final static FileObject SFS_ROOT = FileUtil.getConfigRoot();
     
     private final String folderPath;
     private String filePath;
@@ -194,12 +195,18 @@ class PropertiesStorage implements NbPreferences.FileStorage {
         Statistics.StopWatch sw = Statistics.getStopWatch(Statistics.LOAD, true);
         try {
             Properties retval = new Properties();
-            InputStream is = inputStream();
-            if (is != null) {
+            FileObject file = toPropertiesFile(false);
+            if (file != null) {
                 try {
-                    retval.load(is);
-                } finally {
-                    if (is != null) is.close();
+                    InputStream is = file.getInputStream();
+                    try {
+                        retval.load(is);
+                    } finally {
+                        is.close();
+                    }
+                } catch (IllegalArgumentException x) { // #167745
+                    Logger.getLogger(PropertiesStorage.class.getName()).log(Level.INFO, "While loading " + file, x);
+                    file.delete();
                 }
             }
             return retval;
@@ -238,15 +245,18 @@ class PropertiesStorage implements NbPreferences.FileStorage {
         }
     }
     
-    private InputStream inputStream() throws IOException {
-        FileObject file = toPropertiesFile(false);
-        return (file == null) ? null : file.getInputStream();
-    }
-    
     private OutputStream outputStream() throws IOException {
         FileObject fo = toPropertiesFile(true);
         final FileLock lock = fo.lock();
-        final OutputStream os = fo.getOutputStream(lock);
+        OutputStream os = null;
+        try {
+            os = fo.getOutputStream(lock);
+        } finally {
+            if(os == null && lock != null) {
+                // release lock if getOutputStream failed
+                lock.releaseLock();
+            }
+        }
         return new FilterOutputStream(os) {
             public void close() throws IOException {
                 super.close();
@@ -300,5 +310,17 @@ class PropertiesStorage implements NbPreferences.FileStorage {
         }
         assert (retval == null && !create) || (retval != null && retval.isData());
         return retval;
+    }
+
+    public void runAtomic(final Runnable run) {
+        try {
+            SFS_ROOT.getFileSystem().runAtomicAction(new AtomicAction() {
+                public void run() throws IOException {
+                    run.run();
+                }
+            });
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
