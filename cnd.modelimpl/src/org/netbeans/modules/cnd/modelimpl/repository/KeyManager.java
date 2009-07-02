0318c37fa720a34bc7c30b1f19c35e9efae42297
Vladimir Voskresensky <vv159170@netbeans.org>
2009-07-02 13:36:58 +0400

    sliced managers

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/repository/KeyManager.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/repository/KeyManager.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,39 +31,45 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
-package org.netbeans.modules.cnd.modelimpl.uid;
 
-import org.netbeans.modules.cnd.api.model.CsmUID;
+package org.netbeans.modules.cnd.modelimpl.repository;
+
+import org.netbeans.modules.cnd.repository.spi.Key;
 import org.netbeans.modules.cnd.utils.cache.WeakSharedSet;
 
 /**
  *
  * @author Vladimir Voskresensky
+ * @author Alexander Simon
  */
-public class UIDManager {
+public class KeyManager {
 
-    private final UIDStorage storage;
-    private static final int UID_MANAGER_DEFAULT_CAPACITY;
-    private static final int UID_MANAGER_DEFAULT_SLICED_NUMBER;
+    private final KeyStorage storage;
+    private static final int KEY_MANAGER_DEFAULT_CAPACITY;
+    private static final int KEY_MANAGER_DEFAULT_SLICED_NUMBER;
     static {
         int nrProc = Runtime.getRuntime().availableProcessors();
         if (nrProc <= 4) {
-            UID_MANAGER_DEFAULT_SLICED_NUMBER = 32;
-            UID_MANAGER_DEFAULT_CAPACITY = 512;
+            KEY_MANAGER_DEFAULT_SLICED_NUMBER = 32;
+            KEY_MANAGER_DEFAULT_CAPACITY = 512;
         } else {
-            UID_MANAGER_DEFAULT_SLICED_NUMBER = 128;
-            UID_MANAGER_DEFAULT_CAPACITY = 128;
+            KEY_MANAGER_DEFAULT_SLICED_NUMBER = 128;
+            KEY_MANAGER_DEFAULT_CAPACITY = 128;
         }
     }
-    private static final UIDManager instance = new UIDManager();
+    private static final KeyManager instance = new KeyManager();
 
-    /** Creates a new instance of UIDManager */
-    private UIDManager() {
-        storage = new UIDStorage(UID_MANAGER_DEFAULT_SLICED_NUMBER, UID_MANAGER_DEFAULT_CAPACITY);
+    /** Creates a new instance of KeyManager */
+    private KeyManager() {
+        storage = new KeyStorage(KEY_MANAGER_DEFAULT_SLICED_NUMBER, KEY_MANAGER_DEFAULT_CAPACITY);
     }
 
-    public static UIDManager instance() {
+    public static KeyManager instance() {
         return instance;
     }
     private static final class Lock {}
@@ -83,30 +83,30 @@ public class UIDManager {
      * @exception NullPointerException If the <code>uid</code> parameter
      *                                 is <code>null</code>.
      */
-    public final <T> CsmUID<T> getSharedUID(CsmUID<T> uid) {
-        if (uid == null) {
+    public final Key getSharedUID(Key key) {
+        if (key == null) {
             throw new NullPointerException("null string is illegal to share"); // NOI18N
         }
-        CsmUID<T> outUID = null;
+        Key outKey = null;
         synchronized (lock) {
-            outUID = storage.getSharedUID(uid);
+            outKey = storage.getSharedUID(key);
         }
-        assert (outUID != null);
-        assert (outUID.equals(uid));
-        return outUID;
+        assert (outKey != null);
+        assert (outKey.equals(key));
+        return outKey;
     }
 
     public final void dispose() {
         storage.dispose();
     }
 
-    private static final class UIDStorage {
+    private static final class KeyStorage {
 
-        private final WeakSharedSet<CsmUID<?>>[] instances;
+        private final WeakSharedSet<Key>[] instances;
         private final int segmentMask; // mask
         private final int initialCapacity;
 
-        private UIDStorage(int sliceNumber, int initialCapacity) {
+        private KeyStorage(int sliceNumber, int initialCapacity) {
             // Find power-of-two sizes best matching arguments
             int ssize = 1;
             while (ssize < sliceNumber) {
@@ -115,21 +115,20 @@ public class UIDManager {
             segmentMask = ssize - 1;
             this.initialCapacity = initialCapacity;
             @SuppressWarnings("unchecked")
-            WeakSharedSet<CsmUID<?>>[] ar = new WeakSharedSet[ssize];
+            WeakSharedSet<Key>[] ar = new WeakSharedSet[ssize];
             for (int i = 0; i < ar.length; i++) {
-                ar[i] = new WeakSharedSet<CsmUID<?>>(initialCapacity);
+                ar[i] = new WeakSharedSet<Key>(initialCapacity);
             }
             instances = ar;
         }
 
-        private WeakSharedSet<CsmUID<?>> getDelegate(CsmUID<?> uid) {
-            int index = uid.hashCode() & segmentMask;
+        private WeakSharedSet<Key> getDelegate(Key key) {
+            int index = key.hashCode() & segmentMask;
             return instances[index];
         }
 
-        @SuppressWarnings("unchecked")
-        public final <T> CsmUID<T> getSharedUID(CsmUID<T> uid) {
-            return (CsmUID<T>) getDelegate(uid).addOrGet(uid);
+        public final Key getSharedUID(Key key) {
+            return getDelegate(key).addOrGet(key);
         }
 
         public final void dispose() {
