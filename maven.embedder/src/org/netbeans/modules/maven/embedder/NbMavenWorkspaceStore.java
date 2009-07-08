c0614669be790407898e10ddc8848f35db49f490
Milos Kleint <mkleint@netbeans.org>
2009-07-08 09:22:02 +0200

    #168251 do clear some items from the workspace cache immediately, namely items not coming from the repository. That way sources updates shall be corrrectly reflected instead of reusing cached values.

---
+++ maven.embedder/src/org/netbeans/modules/maven/embedder/NbMavenWorkspaceStore.java
@@ -0,0 +1,121 @@
+package org.netbeans.modules.maven.embedder;
+
+import java.io.File;
+import org.codehaus.plexus.logging.LogEnabled;
+import org.codehaus.plexus.logging.Logger;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.build.model.ModelAndFile;
+import org.apache.maven.project.workspace.ProjectWorkspace;
+import org.apache.maven.workspace.MavenWorkspaceStore;
+
+public class NbMavenWorkspaceStore
+    implements MavenWorkspaceStore, LogEnabled
+{
+
+    private Map<String, TimedWeakReference<Map>> caches = new HashMap<String, TimedWeakReference<Map>>();
+//    private Logger logger;
+
+    public synchronized void clear() {
+        @SuppressWarnings("unchecked")
+        Map<String, ModelAndFile> cache1 = getWorkspaceCache(ProjectWorkspace.MODEL_AND_FILE_BYFILE_KEY);
+        if (cache1 != null) {
+            Iterator<Map.Entry<String, ModelAndFile>> it1 = cache1.entrySet().iterator();
+            while (it1.hasNext()) {
+                Map.Entry<String, ModelAndFile> ent = it1.next();
+                // remove entries based on workspace projects.
+                if (ent.getValue().getFile().getName().equals("pom.xml")) { //NOI18N
+                    it1.remove();
+                }
+            }
+        }
+        @SuppressWarnings("unchecked")
+        Map<String, ModelAndFile> cache2 = getWorkspaceCache(ProjectWorkspace.MODEL_AND_FILE_BYGAV_KEY);
+        if (cache2 != null) {
+            Iterator<Map.Entry<String, ModelAndFile>> it2 = cache2.entrySet().iterator();
+            while (it2.hasNext()) {
+                Map.Entry<String, ModelAndFile> ent = it2.next();
+                // remove entries based on workspace projects.
+                if (ent.getValue().getFile().getName().equals("pom.xml")) { //NOI18N
+                    it2.remove();
+                }
+            }
+        }
+        @SuppressWarnings("unchecked")
+        Map<String, MavenProject> cache3 = getWorkspaceCache(ProjectWorkspace.PROJECT_INSTANCE_BYFILE_KEY);
+        if (cache3 != null) {
+            Iterator<Map.Entry<String, MavenProject>> it3 = cache3.entrySet().iterator();
+            while (it3.hasNext()) {
+                Map.Entry<String, MavenProject> ent = it3.next();
+                // remove entries based on workspace projects.
+                if (ent.getKey().endsWith("pom.xml")) { //NOI18N
+                    it3.remove();
+                }
+            }
+        }
+        @SuppressWarnings("unchecked")
+        Map<String, MavenProject> cache4 = getWorkspaceCache(ProjectWorkspace.PROJECT_INSTANCE_BYGAV_KEY);
+        if (cache4 != null) {
+            Iterator<Map.Entry<String, MavenProject>> it4 = cache4.entrySet().iterator();
+            while (it4.hasNext()) {
+                Map.Entry<String, MavenProject> ent = it4.next();
+                // remove entries based on workspace projects.
+                File f = ent.getValue().getFile();
+                if (f == null) {
+                    continue;
+                }
+                if (ent.getValue().getFile().getName().equals("pom.xml")) { //NOI18N
+                    it4.remove();
+                }
+            }
+        }
+    }
+
+    //only called from netbeans code on occasions like when a pom file was changed or build was performed
+    public synchronized void doManualClear() {
+        caches.clear();
+    }
+
+    public synchronized Map getWorkspaceCache( String cacheType )
+    {
+        TimedWeakReference<Map> val = caches.get( cacheType );
+        Map result = null;
+        if ( val != null ) {
+            result = val.get();
+        }
+        if (result == null) {
+            result = new HashMap();
+            initWorkspaceCache( cacheType, result );
+        }
+
+//        getLogger().debug( "Retrieving workspace cache for: " + cacheType + " (" + result.size() + " entries)" );
+        return result;
+    }
+
+    public synchronized void initWorkspaceCache( String cacheType,
+                           Map cache )
+    {
+//        getLogger().debug( "Initializing workspace cache for: " + cacheType + " (" + cache.size() + " entries)" );
+        TimedWeakReference<Map> ref = new TimedWeakReference<Map>(cache);
+        caches.put( cacheType, ref );
+    }
+
+//    protected Logger getLogger()
+//    {
+//        if ( logger == null )
+//        {
+//            logger = new ConsoleLogger ( Logger.LEVEL_INFO, "internal" );
+//        }
+//
+//        return logger;
+//    }
+
+    public void enableLogging( Logger logger )
+    {
+//        this.logger = logger;
+    }
+
+}
