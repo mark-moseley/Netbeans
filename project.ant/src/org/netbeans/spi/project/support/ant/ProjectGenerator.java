75bdf48df75b0c16685658869f1702180c859e2f
Jesse Glick <jglick@netbeans.org>
2009-07-13 10:18:30 -0400

    Explicit use of FileLock no longer necessary.

--- project.ant/src/org/netbeans/spi/project/support/ant/ProjectGenerator.java
+++ project.ant/src/org/netbeans/spi/project/support/ant/ProjectGenerator.java
@@ -44,12 +44,11 @@ package org.netbeans.spi.project.support.ant;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Iterator;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectManager;
 import org.netbeans.modules.project.ant.AntBasedProjectFactorySingleton;
 import org.netbeans.modules.project.ant.ProjectLibraryProvider;
-import org.openide.filesystems.FileLock;
+import org.netbeans.spi.project.ui.support.DefaultProjectOperations;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Lookup;
@@ -113,8 +112,15 @@ public class ProjectGenerator {
         try {
             return ProjectManager.mutex().writeAccess(new Mutex.ExceptionAction<AntProjectHelper>() {
                 public AntProjectHelper run() throws IOException {
-                    if (ProjectManager.getDefault().findProject(directory) != null) {
-                        throw new IllegalArgumentException("Already a project in " + directory); // NOI18N
+                    Project prj = ProjectManager.getDefault().findProject(directory);
+                    if (prj != null) {
+                        // #139769: try harder to get rid of it...
+                        DefaultProjectOperations.performDefaultDeleteOperation(prj);
+                        System.gc();
+                    }
+                    prj = ProjectManager.getDefault().findProject(directory);
+                    if (prj != null) {
+                        throw new IllegalArgumentException("Already a " + prj.getClass().getName() + " in " + directory); // NOI18N
                     }
                     FileObject projectXml = directory.getFileObject(AntProjectHelper.PROJECT_XML_PATH);
                     if (projectXml != null) {
@@ -144,16 +150,11 @@ public class ProjectGenerator {
                             FileUtil.createData(f);
                         }
                     }
-                    FileLock lock = projectXml.lock();
+                    OutputStream os = projectXml.getOutputStream();
                     try {
-                        OutputStream os = projectXml.getOutputStream(lock);
-                        try {
-                            XMLUtil.write(doc, os, "UTF-8"); // NOI18N
-                        } finally {
-                            os.close();
-                        }
+                        XMLUtil.write(doc, os, "UTF-8"); // NOI18N
                     } finally {
-                        lock.releaseLock();
+                        os.close();
                     }
                     // OK, disk file project.xml has been created.
                     // Load the project into memory and mark it as modified.
@@ -164,7 +165,8 @@ public class ProjectGenerator {
                         for (AntBasedProjectType abpt : Lookup.getDefault().lookupAll(AntBasedProjectType.class)) {
                             if (abpt.getType().equals(type)) {
                                 // Well, the factory was there.
-                                throw new IllegalArgumentException("For some reason the folder " + directory + " with a new project of type " + type + " is still not recognized"); // NOI18N
+                                throw new IllegalArgumentException("For some reason the folder " + directory +
+                                        " with a new project of type " + type + " is still not recognized"); // NOI18N
                             }
                         }
                         throw new IllegalArgumentException("No Ant-based project factory for type " + type); // NOI18N
