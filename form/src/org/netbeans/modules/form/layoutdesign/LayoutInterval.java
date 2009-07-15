5e766f3d2e155043b0cacb9870e6efe7f0057b89
Jan Stola <jstola@netbeans.org>
2009-07-15 11:56:44 +0200

    Diagnostics and workaround for issue 118562.

--- form/src/org/netbeans/modules/form/layoutdesign/LayoutInterval.java
+++ form/src/org/netbeans/modules/form/layoutdesign/LayoutInterval.java
@@ -65,6 +65,8 @@ public final class LayoutInterval implements LayoutConstants {
     static final int ATTR_ALIGN_PRE = 64;
     static final int ATTR_ALIGN_POST = 128;
 
+    static final int ATTR_FORCED_DEFAULT = 256;
+
     static final int DESIGN_ATTRS = ATTR_DESIGN_CONTAINER_GAP
                                     | ATTR_DESIGN_RESIZING
                                     | ATTR_DESIGN_SUPPRESSED_RESIZING
@@ -439,6 +441,21 @@ public final class LayoutInterval implements LayoutConstants {
     }
 
     int add(LayoutInterval interval, int index) {
+        if (getParent() == interval) {
+            throw new IllegalArgumentException("Cannot add parent as a sub-interval!"); // NOI18N
+        }
+        if (interval.isComponent()) { // Issue 118562
+            LayoutComponent comp = interval.getComponent();
+            for (LayoutInterval subInterval : subIntervals) {
+                if (subInterval.isComponent() && comp.equals(subInterval.getComponent())) {
+                    if (System.getProperty("netbeans.ignore.issue118562") == null) { // NOI18N
+                        throw new IllegalArgumentException("Cannot add a component into a group twice!"); // NOI18N
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+        }
         if (index < 0) {
             index = subIntervals.size();
         }
@@ -471,7 +488,7 @@ public final class LayoutInterval implements LayoutConstants {
         return subIntervals != null ? subIntervals.indexOf(interval) : -1;
     }
 
-    boolean isParentOf(LayoutInterval interval) {
+    public boolean isParentOf(LayoutInterval interval) {
         if (isGroup()) {
             do {
                 interval = interval.getParent();
@@ -481,6 +498,10 @@ public final class LayoutInterval implements LayoutConstants {
             while (interval != null);
         }
         return false;
+    }
+
+    public LayoutInterval getRoot() {
+        return LayoutInterval.getRoot(this);
     }
 
     // -----
@@ -1038,10 +1059,18 @@ public final class LayoutInterval implements LayoutConstants {
             return edge;
         if (!beforeFixed && afterFixed)
             return edge^1;
-        if (beforeFixed && afterFixed)
-            return wantResize ? edge : parent.getAlignment();
+        if (beforeFixed && afterFixed) {
+            if (wantResize) {
+                return edge;
+            } else {
+                int parentAlignment = parent.getAlignment();
+                if (parentAlignment == LEADING || parentAlignment == TRAILING) {
+                    return parentAlignment;
+                }
+            }
+        }
 
-        return DEFAULT; // !leadingFixed && !trailingFixed
+        return DEFAULT;
     }
 
     /**
