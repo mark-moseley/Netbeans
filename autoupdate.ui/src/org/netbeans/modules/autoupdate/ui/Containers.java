9eff72c67b1dbc5875bdecc425a36cdbaddc890c
Dmitry Lipin <dlipin@netbeans.org>
2009-07-30 01:18:20 +0400

    Issue #169251 NullPointerException at org.netbeans.modules.autoupdate.ui.Unit$Update.isMarked

--- autoupdate.ui/src/org/netbeans/modules/autoupdate/ui/Containers.java
+++ autoupdate.ui/src/org/netbeans/modules/autoupdate/ui/Containers.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -64,86 +64,135 @@ public class Containers {
     
     private Containers(){}
     public static void initNotify() {
-        forAvailableNbms().removeAll();
-        forUpdateNbms().removeAll();
-        forAvailable().removeAll();
-        forUninstall().removeAll();
-        forUpdate().removeAll();
-        forEnable().removeAll();
-        forDisable().removeAll();
+        try {
+            forAvailableNbms().removeAll();
+            forUpdateNbms().removeAll();
+            forAvailable().removeAll();
+            forUninstall().removeAll();
+            forUpdate().removeAll();
+            forEnable().removeAll();
+            forDisable().removeAll();
+        } catch (NullPointerException npe) {
+            // doesn't matter, can ignore that
+        }
     }
 
     public static OperationContainer<InstallSupport> forAvailableNbms() {
         synchronized(Containers.class) {
-            if (INSTALL_FOR_NBMS == null || INSTALL_FOR_NBMS.get() == null) {
-                INSTALL_FOR_NBMS = new WeakReference<OperationContainer<InstallSupport>>(OperationContainer.createForInstall());
+            OperationContainer<InstallSupport> container = null;
+            if (INSTALL_FOR_NBMS != null) {
+                container = INSTALL_FOR_NBMS.get();
             }
-            return INSTALL_FOR_NBMS.get();
+            if (container==null) {
+                container = OperationContainer.createForInstall();
+                INSTALL_FOR_NBMS = new WeakReference<OperationContainer<InstallSupport>>(container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<InstallSupport> forUpdateNbms() {
         synchronized(Containers.class) {
-            if (UPDATE_FOR_NBMS == null || UPDATE_FOR_NBMS.get() == null) {
-                UPDATE_FOR_NBMS = new WeakReference<OperationContainer<InstallSupport>>(OperationContainer.createForUpdate());
+            OperationContainer<InstallSupport> container = null;
+            if (UPDATE_FOR_NBMS != null) {
+                container = UPDATE_FOR_NBMS.get();
             }
-            return UPDATE_FOR_NBMS.get();
+            if (container==null) {
+                container = OperationContainer.createForUpdate();
+                UPDATE_FOR_NBMS = new WeakReference<OperationContainer<InstallSupport>>(container);
+            }
+            return container;
         }        
     }
     
     public static OperationContainer<InstallSupport> forAvailable() {
         synchronized(Containers.class) {
-            if (INSTALL == null || INSTALL.get() == null) {
-                INSTALL = new WeakReference<OperationContainer<InstallSupport>>(OperationContainer.createForInstall());
+            OperationContainer<InstallSupport> container = null;
+            if (INSTALL != null) {
+                container = INSTALL.get();
             }
-            return INSTALL.get();
+            if (container == null) {
+                container  = OperationContainer.createForInstall();
+                INSTALL = new WeakReference<OperationContainer<InstallSupport>>(container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<InstallSupport> forUpdate() {
         synchronized(Containers.class) {
-            if (UPDATE == null || UPDATE.get() == null) {
-                UPDATE = new WeakReference<OperationContainer<InstallSupport>>(OperationContainer.createForUpdate());
+            OperationContainer<InstallSupport> container = null;
+            if (UPDATE != null) {
+                container = UPDATE.get();
             }
-            return UPDATE.get();
+            if (container == null) {
+                container = OperationContainer.createForUpdate();
+                UPDATE = new WeakReference<OperationContainer<InstallSupport>>(container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<OperationSupport> forUninstall() {
         synchronized(Containers.class) {
-            if (UNINSTALL == null || UNINSTALL.get() == null) {
-                UNINSTALL = new WeakReference<OperationContainer<OperationSupport>>(OperationContainer.createForUninstall());
+            OperationContainer<OperationSupport> container = null;
+            if (UNINSTALL != null) {
+                container = UNINSTALL.get();
             }
-            return UNINSTALL.get();
+            if (container == null) {
+                container = OperationContainer.createForUninstall();
+                UNINSTALL = new WeakReference<OperationContainer<OperationSupport>>(container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<OperationSupport> forEnable() {
         synchronized(Containers.class) {
-            if (ENABLE == null || ENABLE.get() == null) {
-                ENABLE = new WeakReference<OperationContainer<OperationSupport>>(OperationContainer.createForEnable());
+            OperationContainer<OperationSupport> container = null;
+            if (ENABLE != null) {
+                container = ENABLE.get();
             }
-            return ENABLE.get();
+            if(container == null) {
+                container = OperationContainer.createForEnable();
+                ENABLE = new WeakReference<OperationContainer<OperationSupport>>(container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<OperationSupport> forDisable() {
         synchronized(Containers.class) {
-            if (DISABLE == null || DISABLE.get() == null) {
-                DISABLE = new WeakReference<OperationContainer<OperationSupport>> (OperationContainer.createForDisable ());
+            OperationContainer<OperationSupport> container = null;
+            if (DISABLE != null) {
+                container = DISABLE.get();
             }
-            return DISABLE.get();
+            if(container == null) {
+                container = OperationContainer.createForDisable();
+                DISABLE = new WeakReference<OperationContainer<OperationSupport>>(container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<OperationSupport> forCustomInstall () {
         synchronized (Containers.class) {
-            if (CUSTOM_INSTALL == null || CUSTOM_INSTALL.get () == null) {
-                CUSTOM_INSTALL = new WeakReference<OperationContainer<OperationSupport>> (OperationContainer.createForCustomInstallComponent ());
+            OperationContainer<OperationSupport> container = null;
+            if (CUSTOM_INSTALL != null) {
+                container = CUSTOM_INSTALL.get ();
             }
-            return CUSTOM_INSTALL.get();
+            if(container == null) {
+                container = OperationContainer.createForCustomInstallComponent ();
+                CUSTOM_INSTALL = new WeakReference<OperationContainer<OperationSupport>> (container);
+            }
+            return container;
         }        
     }
     public static OperationContainer<OperationSupport> forCustomUninstall () {
         synchronized (Containers.class) {
-            if (CUSTOM_UNINSTALL == null || CUSTOM_UNINSTALL.get () == null) {
-                CUSTOM_UNINSTALL = new WeakReference<OperationContainer<OperationSupport>> (OperationContainer.createForCustomUninstallComponent ());
+            OperationContainer<OperationSupport> container = null;
+            if (CUSTOM_UNINSTALL != null) {
+                container = CUSTOM_UNINSTALL.get ();
             }
-            return CUSTOM_UNINSTALL.get();
+            if(container == null) {
+                container = OperationContainer.createForCustomUninstallComponent ();
+                CUSTOM_UNINSTALL = new WeakReference<OperationContainer<OperationSupport>> (container);
+            }
+            return container;
         }        
     }
 }
